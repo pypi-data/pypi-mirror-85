d2e80130c10df5f78ca9b97d58ed36342446a26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:16 +0000

    Add package ziim/0.0.8/ziim-0.0.8.tar.gz

---
+++ packages/ziim/ziim-0.0.8.tar.gz/ziim-0.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ziim",
+    version="0.0.8",
+    scripts=['./scripts/ziim.sh'],
+    author="Sanix-darker",
+    author_email="s4nixd@gmail.com",
+    description="Let your CLI find available solutions for errors/exceptions in your code for you, "
+                "no need open a Browser. and do something yourself, you just have to select the forum",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sanix-darker/ziim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
