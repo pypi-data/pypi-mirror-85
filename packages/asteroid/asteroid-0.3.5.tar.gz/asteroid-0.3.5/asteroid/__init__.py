e777eab60c46cfd8c9d262429a9ac5fdcae3af12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package asteroid/0.3.5/asteroid-0.3.5.tar.gz

---
+++ packages/asteroid/asteroid-0.3.5.tar.gz/asteroid-0.3.5/asteroid/__init__.py
@@ -0,0 +1,25 @@
+import pathlib
+
+from .models import ConvTasNet, DCCRNet, DCUNet, DPRNNTasNet, DPTNet, LSTMTasNet, DeMask
+from .utils import deprecation_utils, torch_utils  # noqa
+
+project_root = str(pathlib.Path(__file__).expanduser().absolute().parent.parent)
+__version__ = "0.3.5"
+
+
+def show_available_models():
+    from .utils.hub_utils import MODELS_URLS_HASHTABLE
+
+    print(" \n".join(list(MODELS_URLS_HASHTABLE.keys())))
+
+
+__all__ = [
+    "ConvTasNet",
+    "DPRNNTasNet",
+    "DPTNet",
+    "LSTMTasNet",
+    "DeMask",
+    "DCUNet",
+    "DCCRNet",
+    "show_available_models",
+]
