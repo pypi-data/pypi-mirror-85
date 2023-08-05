6cf9cd740995431b2308ccd6a4ee6cb7f4b157c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:24 +0000

    Add package itolparser/0.1.6/itolparser-0.1.6.tar.gz

---
+++ packages/itolparser/itolparser-0.1.6.tar.gz/itolparser-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='itolparser',
+     version='0.1.6',
+     scripts=['itolparser'] ,
+     author="Boas van der Putten",
+     author_email="boas.vanderputten@amsterdamumc.nl",
+     description="Small script to produce iTOL colorstrip metadata files from a table",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/boasvdp/itolparser",
+     packages=setuptools.find_packages(),
+     install_requires=['colorbrewer', 'pandas', 'numpy'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
