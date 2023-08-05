28fa8b09cdf031ae665fb7267120895db2a2ec42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package tx-tool/0.0.0/tx-tool-0.0.0.tar.gz

---
+++ packages/tx-tool/tx-tool-0.0.0.tar.gz/tx-tool-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tx-tool',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+)
