c0c61d0f29bbf47811f26e7cd479495033242ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package wog-tomer9000/0.0.19/wog_tomer9000-0.0.19-py3-none-any.whl

---
+++ packages/wog-tomer9000/wog_tomer9000-0.0.19-py3-none-any.whl/wog/__init__.py
@@ -0,0 +1,7 @@
+from .CurrencyRouletteGame import *
+from .Game import *
+from .GuessGame import *
+from .Helping_funcs import *
+from .Live import *
+from .MainGame import *
+from .MemoryGame import *
