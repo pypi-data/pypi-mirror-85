aab701b156512c3d3f759cbd99ebb4f8d6a3822a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:43 +0000

    Add package kg-lokistack/0.7.11/kg_lokistack-0.7.11.tar.gz

---
+++ packages/kg-lokistack/kg_lokistack-0.7.11.tar.gz/kg_lokistack-0.7.11/kg_lokistack/__init__.py
@@ -0,0 +1,12 @@
+from .builder import (
+    LokiStackBuilder
+)
+from .option import (
+    LokiStackOptions
+)
+__version__ = "0.7.11"
+
+__all__ = [
+    'LokiStackOptions',
+    'LokiStackBuilder',
+]
---
+++ packages/kg-lokistack/kg_lokistack-0.7.11.tar.gz/kg_lokistack-0.7.11/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+import re
+
+import setuptools
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    open('kg_lokistack/__init__.py', encoding='utf_8_sig').read()
+    ).group(1)
+
+HERE = pathlib.Path(__file__).parent
+INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kg_lokistack",
+    version=__version__,
+    author="Rangel Reale",
+    author_email="rangelreale@gmail.com",
+    description="KubraGen Builder: Loki Stack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RangelReale/kg_lokistack",
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    test_suite="kg_lokistack.tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
