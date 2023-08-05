a4d07bb235cf68327982bf26bc902573a4718aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package openke/0.84/openke-0.84.tar.gz

---
+++ packages/openke/openke-0.84.tar.gz/openke-0.84/setup.py
@@ -0,0 +1,43 @@
+import os
+from distutils.command.clean import clean
+
+import setuptools
+from setuptools.command.install import install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        os.system('g++ openke/base/Base.cpp -fPIC -shared -o openke/release/Base.so -pthread -O3 -march=native -std=c++11')
+        os.system('ls -alh openke')
+
+
+class CleanCommand(clean):
+    def run(self):
+        print('ok')
+
+
+setuptools.setup(
+    name='openke',
+    version='0.84',
+    scripts=['openke_'],
+    authors=["thunlp", "zeionara"],
+    author_email="zeionara@gmail.com",
+    description="A library for operating knowledge graph models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zeionara/OpenKE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['numpy'],
+    cmdclass={
+        'install': PostInstallCommand
+    }
+)
