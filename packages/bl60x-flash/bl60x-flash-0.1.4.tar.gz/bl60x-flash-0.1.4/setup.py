e53d117370a3ea66e461599a504fe95ca282da9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:12 +0000

    Add package bl60x-flash/0.1.4/bl60x-flash-0.1.4.tar.gz

---
+++ packages/bl60x-flash/bl60x-flash-0.1.4.tar.gz/bl60x-flash-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bl60x_flash']
+
+package_data = \
+{'': ['*'], 'bl60x_flash': ['contrib/*']}
+
+install_requires = \
+['pyserial>=3.4,<4.0', 'tqdm>=4.51.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['bl60x-flash = bl60x_flash.main:main']}
+
+setup_kwargs = {
+    'name': 'bl60x-flash',
+    'version': '0.1.4',
+    'description': 'Flash tool for Bouffalo Labs BL60x UART bootloader',
+    'long_description': None,
+    'author': 'Stefan Schake',
+    'author_email': 'stschake@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
