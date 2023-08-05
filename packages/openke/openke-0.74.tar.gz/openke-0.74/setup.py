d60d440b4d3b52f0a4a320dec5e569411c149409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package openke/0.74/openke-0.74.tar.gz

---
+++ packages/openke/openke-0.74.tar.gz/openke-0.74/setup.py
@@ -0,0 +1,35 @@
+import os
+
+import setuptools
+from setuptools.command.install import install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class PostInstallCommand(install):
+    def run(self):
+        os.system('ls -alh')
+        os.system('g++ base/Base.cpp -fPIC -shared -o release/Base.so -pthread -O3 -march=native -std=c++11')
+
+
+setuptools.setup(
+    name='openke',
+    version='0.74',
+    scripts=['openke/make.sh'],
+    authors=["thunlp", "zeionara"],
+    author_email="zeionara@gmail.com",
+    description="A library for operating knowledge graph models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zeionara/OpenKE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        'install': PostInstallCommand
+    }
+)
