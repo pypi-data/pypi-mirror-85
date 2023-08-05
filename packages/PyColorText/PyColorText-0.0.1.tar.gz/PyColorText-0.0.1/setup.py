fe4becff4b7ff02d83d9849003420ae386c9e111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package PyColorText/0.0.1/PyColorText-0.0.1.tar.gz

---
+++ packages/PyColorText/PyColorText-0.0.1.tar.gz/PyColorText-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='PyColorText',
+    version='0.0.1',
+    packages=['PyColorText'],
+    url='https://github.com/roshanlam/PyCT',
+    download_url="https://github.com/roshanlam/PyCT/blob/main/dist/PyColorText-0.0.1.tar.gz",
+    license='',
+    author='roshanlam',
+    description='A library for colored text',
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
