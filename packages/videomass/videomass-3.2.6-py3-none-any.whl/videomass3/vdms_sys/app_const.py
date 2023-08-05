73c4a5902e94984ed6fa573937d6412576128c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package videomass/3.2.6/videomass-3.2.6-py3-none-any.whl

---
+++ packages/videomass/videomass-3.2.6-py3-none-any.whl/videomass3/vdms_sys/app_const.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+# Author:  Werner F. Bruhin
+# Purpose: Application constants
+# Created: 06/04/2012
+
+import wx
+
+# language domain
+langDomain = "videomass"
+# languages you want to support
+supLang = {"en": wx.LANGUAGE_ENGLISH,
+           "it": wx.LANGUAGE_ITALIAN,
+           "fr": wx.LANGUAGE_FRENCH,
+           "de": wx.LANGUAGE_GERMAN,
+           "pl": wx.LANGUAGE_POLISH,
+           "es": wx.LANGUAGE_SPANISH,
+           }
