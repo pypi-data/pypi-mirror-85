c5eddb66cc4c33e5dd790a8585f59239c783cb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package openke/0.71/openke-0.71.tar.gz

---
+++ packages/openke/openke-0.71.tar.gz/openke-0.71/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='openke',
+    version='0.71',
+    scripts=['openke/make.sh'],
+    authors=["thunlp", "zeionara"],
+    author_email="zeionara@gmail.com",
+    description="A library for operating knowledge graph models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zeionara/OpenKE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
