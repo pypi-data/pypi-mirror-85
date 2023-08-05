f846d03f663833091f247a1f89f5930992ac1a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package tql/2020.11.11.14.12.33/tql-2020.11.11.14.12.33-py3-none-any.whl

---
+++ packages/tql/tql-2020.11.11.14.12.33-py3-none-any.whl/examples/colors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : colors
+# @Time         : 2019-07-04 14:03
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+class Colors:
+    ok = '\033[92m'
+    fail = '\033[91m'
+    close = '\033[0m'
+
+
+
+import csv
+
+print(csv.QUOTE_NONE)
+
+
+from sklearn import semi_supervised
+
+semi_supervised.LabelPropagation
+semi_supervised.LabelSpreading
---
+++ packages/tql/tql-2020.11.11.14.12.33-py3-none-any.whl/examples/json/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : __init__.py
+# @Time         : 2020-01-08 16:16
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
