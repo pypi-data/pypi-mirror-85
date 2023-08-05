afb8f9305c543729ff5a353bfb0982e623c7c948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:39 +0000

    Add package requestMr/1.1.0/requestMr-1.1.0-py3.7.egg

---
+++ packages/requestMr/requestMr-1.1.0-py3.7.egg/requestMr/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-10-30 15:11:09
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-10-30 15:11:09
