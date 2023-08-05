dffc3c538bd1daaee642065a0f0717b0bfb65e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package dsc.py/0.2.0/dsc.py-0.2.0-py3-none-any.whl

---
+++ packages/dsc.py/dsc.py-0.2.0-py3-none-any.whl/dsc/exceptions.py
@@ -0,0 +1,18 @@
+class NoToken(AttributeError):
+    pass
+
+
+class BadToken(AttributeError):
+    pass
+
+
+class Forbidden(AttributeError):
+    pass
+
+
+class BearerNoToken(AttributeError):
+    pass
+
+
+class BadLinkType(AttributeError):
+    pass
---
+++ packages/dsc.py/dsc.py-0.2.0-py3-none-any.whl/dsc/models/announcement.py
@@ -0,0 +1,16 @@
+from typing import Union
+
+
+class Announcement:
+    def __init__(self, data: dict):
+        self.author: Union[str, None] = data.get("author", None)
+        self.recipients: Union[str, None] = data.get("for", None)
+        self.message: Union[str, None] = data.get("message", None)
+        self.character: Union[str, None] = data.get("type", None)
+        self.raw: dict = data
+
+    def __str__(self) -> str:
+        return self.message
+
+
+
