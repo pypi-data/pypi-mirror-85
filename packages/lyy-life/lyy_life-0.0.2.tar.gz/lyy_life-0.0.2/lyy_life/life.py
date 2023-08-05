3996309a94f5c590959c312c70fba75839d9d186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package lyy-life/0.0.2/lyy_life-0.0.2.tar.gz

---
+++ packages/lyy-life/lyy_life-0.0.2.tar.gz/lyy_life-0.0.2/lyy_life/life.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def life(args):
+    '''
+    this is my First Python Package.
+    '''
+    res = False
+    if type(args) == int:
+        sum = 0 
+        for i in range(args + 1):
+            sum += i
+        res = True
+    else:
+        sum = '{0} 不是整数，无法计算'.format(args)
+    return res, sum
