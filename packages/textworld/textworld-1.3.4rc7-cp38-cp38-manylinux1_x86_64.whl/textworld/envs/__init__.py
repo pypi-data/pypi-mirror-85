706801e8277ab0252bb6356bb4f639a920a74d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:06 +0000

    Add package textworld/1.3.4rc7/textworld-1.3.4rc7-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/textworld/textworld-1.3.4rc7-cp38-cp38-manylinux1_x86_64.whl/textworld/envs/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+from textworld.envs.glulx.git_glulx import GitGlulxEnv
+from textworld.envs.zmachine.jericho import JerichoEnv
+from textworld.envs.wrappers.tw_inform7 import TWInform7
