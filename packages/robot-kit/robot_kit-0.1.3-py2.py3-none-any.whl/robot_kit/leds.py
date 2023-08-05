3fbffd26c019e3cae63aeaebac02b5a75fb6e6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package robot-kit/0.1.3/robot_kit-0.1.3-py2.py3-none-any.whl

---
+++ packages/robot-kit/robot_kit-0.1.3-py2.py3-none-any.whl/robot_kit/leds.py
@@ -0,0 +1,90 @@
+"""A lightweight wrapper around the NeoPixel LED Strip"""
+import board
+import neopixel
+import time
+
+
+class NeoPixelStrip():
+    """A lightweight wrapper around the NeoPixel LED Strip
+       Usage:
+            led_strip = NeoPixelStrip()
+            led_strip.on(255, 0, 0)  # R, G, B
+            time.sleep(1)
+            led_strip.off()
+    """
+    def __init__(self, led_pin=board.D18, num_leds=8, brightness=1.0):
+        """NeoPixelStrip Initialization"""
+        self.led_pin = led_pin
+        self.num_leds = num_leds
+        self.brightness = brightness
+        self.strip = neopixel.NeoPixel(self.led_pin, self.num_leds, brightness=self.brightness)
+
+    def on(self, red, green, blue):
+        """Turn all the LEDs to one color
+        Args:
+               red: red value (0-255)
+               green: green value (0-255)
+               blue: blue value (0-255)
+        """
+        self.strip.fill((red, green, blue))
+
+    def off(self):
+        """Turn all the LEDs off"""
+        self.strip.fill((0, 0, 0))
+
+    def cleanup(self):
+        """Method that's called when class is destroyed"""
+        print('Cleanup...')
+        self.off()
+        self.strip.deinit()
+
+
+class CommandBlink:
+
+    def __init__(self, function):
+        self.led_strip = NeoPixelStrip()
+        self.function = function
+
+    def __call__(self, *args, **kwargs):
+        # Turn LEDs blue
+        self.led_strip.on(0, 255, 0)
+
+        # Call the function
+        self.function(*args, **kwargs)
+
+        # Turn LEDs Off
+        time.sleep(0.05)  # Quick sleep in case function didn't take long
+        self.led_strip.off()
+
+
+def test():
+    """Test for the NeoPixelStrip class"""
+    led_strip = NeoPixelStrip()
+    for r, g, b in zip(range(128, 0, -1), range(0, 128), range(0, 128)):
+        led_strip.on(r, g, b)
+        time.sleep(0.01)
+
+    # Yellow
+    led_strip.on(128, 128, 0)
+    time.sleep(1.0)
+
+    # Orange
+    led_strip.on(255, 128, 0)
+    time.sleep(1.0)
+
+    # Red
+    led_strip.on(255, 0, 0)
+    time.sleep(1.0)
+
+    # Green
+    led_strip.on(0, 255, 0)
+    time.sleep(1.0)
+
+    led_strip.off()
+    led_strip.cleanup()
+
+
+if __name__ == '__main__':
+
+    # Run the test
+    test()
