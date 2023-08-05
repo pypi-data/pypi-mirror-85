90c4b26e56350228e3cacca7f3c8c86ee09ba024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:14 +0000

    Add package oc-probability/0.1/oc_probability-0.1.tar.gz

---
+++ packages/oc-probability/oc_probability-0.1.tar.gz/oc_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='oc_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['oc_probability'],
+      author='Otoniel Campos',
+      author_email='oc_campos16@hotmail.com',
+      zip_safe=False)
