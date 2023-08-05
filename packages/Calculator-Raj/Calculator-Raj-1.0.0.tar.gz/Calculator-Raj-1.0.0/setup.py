34b458f46b4a6b38c556b82f889b511c8b2b273b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:01 +0000

    Add package Calculator-Raj/1.0.0/Calculator-Raj-1.0.0.tar.gz

---
+++ packages/Calculator-Raj/Calculator-Raj-1.0.0.tar.gz/Calculator-Raj-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="Calculator-Raj",
+version="1.0.0",
+description="This is a simple calculator program",
+long_description="This is a very very long discription",
+author="Rajeev Singla",
+packages=['calci_raj'],
+install_requires=[]
+)
+
