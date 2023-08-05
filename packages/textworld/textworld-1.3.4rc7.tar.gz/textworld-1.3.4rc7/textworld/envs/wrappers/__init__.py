33fd42a7be71931331a7fce974e1a46c45177620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:07 +0000

    Add package textworld/1.3.4rc7/textworld-1.3.4rc7.tar.gz

---
+++ packages/textworld/textworld-1.3.4rc7.tar.gz/textworld-1.3.4rc7/textworld/envs/wrappers/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+
+from textworld.envs.wrappers.viewer import HtmlViewer
+from textworld.envs.wrappers.recorder import Recorder
+from textworld.envs.wrappers.filter import Filter
+from textworld.envs.wrappers.limit import Limit
+from textworld.envs.wrappers.tw_inform7 import TWInform7
+from textworld.envs.wrappers.generic import GenericEnvironment
