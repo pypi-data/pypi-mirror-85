ef5815405fc09cb49e873c60368e50517e63ca58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:08 +0000

    Add package criteo-marketing/1.0.171/criteo_marketing-1.0.171.tar.gz

---
+++ packages/criteo-marketing/criteo_marketing-1.0.171.tar.gz/criteo_marketing-1.0.171/criteo_marketing/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from criteo_marketing.api.advertisers_api import AdvertisersApi
+from criteo_marketing.api.audiences_api import AudiencesApi
+from criteo_marketing.api.authentication_api import AuthenticationApi
+from criteo_marketing.api.budgets_api import BudgetsApi
+from criteo_marketing.api.campaigns_api import CampaignsApi
+from criteo_marketing.api.categories_api import CategoriesApi
+from criteo_marketing.api.portfolio_api import PortfolioApi
+from criteo_marketing.api.publishers_api import PublishersApi
+from criteo_marketing.api.sellers_api import SellersApi
+from criteo_marketing.api.sellers_v2_api import SellersV2Api
+from criteo_marketing.api.sellers_v2_stats_api import SellersV2StatsApi
+from criteo_marketing.api.statistics_api import StatisticsApi
---
+++ packages/criteo-marketing/criteo_marketing-1.0.171.tar.gz/criteo_marketing-1.0.171/examples/authentication.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+import logging
+import sys
+from pprint import pprint
+
+import criteo_marketing
+from criteo_marketing import Configuration
+
+# There is only one accepted GRANT_TYPE
+GRANT_TYPE = 'client_credentials'
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise ValueError("You need to specify the CLIENT_ID and the CLIENT_SECRET")
+
+    configuration = Configuration(username=sys.argv[1], password=sys.argv[2])
+
+    # Enable/Disable debug httplib and criteo_marketing packages
+    # logging.basicConfig(level=logging.DEBUG)
+    # configuration.debug = True
+
+    client = criteo_marketing.ApiClient(configuration)
+
+    # Get a valid token. The configuration is accessible through the client
+    auth_api = criteo_marketing.AuthenticationApi(client)
+    auth_response = auth_api.o_auth2_token_post(client_id=client.configuration.username,
+                                                client_secret=client.configuration.password,
+                                                grant_type=GRANT_TYPE)
+    # Token type is always "BEARER"
+    token = auth_response.token_type + " " + auth_response.access_token
+
+    # Reuse the same client to benefit from the configuration in order to automatically refresh expired token
+    portfolio_api = criteo_marketing.PortfolioApi(client)
+
+    portfolio_response = portfolio_api.get_portfolio(token)
+    pprint(portfolio_response)
