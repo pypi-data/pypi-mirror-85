6aea5458ada4ceb5f37b747002f94527f181a35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:07 +0000

    Add package tsai/0.2.4/tsai-0.2.4-py3-none-any.whl

---
+++ packages/tsai/tsai-0.2.4-py3-none-any.whl/tsai/all.py
@@ -0,0 +1,10 @@
+import tsai
+from .imports import *
+from .utils import *
+from .data.all import *
+from .metrics import *
+from .learner import *
+from .optimizer import *
+from .callback import *
+from .models.all import *
+from .trading.all import *
---
+++ packages/tsai/tsai-0.2.4-py3-none-any.whl/tsai/data/all.py
@@ -0,0 +1,8 @@
+from .validation import *
+from .external import *
+from .core import *
+from .transforms import *
+from .image import *
+from .tabular import *
+from .mixed import *
+
