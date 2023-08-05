38ec0efb0706abf59d0e9d8a75939ebb2ecc98c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package django-moj-irat/0.5/django-moj-irat-0.5.tar.gz

---
+++ packages/django-moj-irat/django-moj-irat-0.5.tar.gz/django-moj-irat-0.5/README.rst
@@ -0,0 +1,43 @@
+IRaT support for Django
+=======================
+
+A set of tools to make it easier to add a Django-based service to Ministry of Justice's Incidence Response and Tuning:
+
+* ping.json view
+* healthcheck.json view with extensible healthchecks
+
+Usage
+-----
+
+Install using ``pip install django-moj-irat``.
+
+Django settings:
+
+.. code-block:: python
+
+    HEALTHCHECKS = [
+        'moj_irat.healthchecks.database_healthcheck',
+        # override default list of healthcheck callables
+    ]
+    AUTODISCOVER_HEALTHCHECKS = True  # whether to autodiscover and load healthcheck.py from all installed apps
+
+Development
+-----------
+
+.. image:: https://travis-ci.org/ministryofjustice/django-moj-irat.svg?branch=master
+    :target: https://travis-ci.org/ministryofjustice/django-moj-irat
+
+Please report bugs and open pull requests on `GitHub`_.
+
+Use ``python setup.py test`` to run all tests.
+
+Distribute a new version by updating the ``version`` argument in ``setup.py:setup`` and
+creating a release in GitHub (this triggers a GitHub Actions workflow to automatically upload it).
+
+Copyright
+---------
+
+Copyright (C) 2020 HM Government (Ministry of Justice Digital Services).
+See LICENSE.txt for further details.
+
+.. _GitHub: https://github.com/ministryofjustice/django-moj-irat
---
+++ packages/django-moj-irat/django-moj-irat-0.5.tar.gz/django-moj-irat-0.5/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup
+
+root_path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root_path, 'README.rst')) as readme:
+    README = readme.read()
+
+install_requires = ['Django>=2.2,<4', 'requests']
+tests_require = ['flake8', 'responses']
+
+setup(
+    name='django-moj-irat',
+    version='0.5',
+    author='Ministry of Justice Digital & Technology',
+    author_email='dev@digital.justice.gov.uk',
+    url='https://github.com/ministryofjustice/django-moj-irat',
+    packages=['moj_irat'],
+    include_package_data=True,
+    license='MIT',
+    description="Tools to support adding a Django-based service to "
+                "Ministry of Justice's Incidence Response and Tuning",
+    long_description=README,
+    keywords='moj django irat monitoring',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='tests.run',
+)
