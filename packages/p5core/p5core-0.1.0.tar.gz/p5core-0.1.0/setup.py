649f68b4a41fcee316e3d1d7710dfdac488e1ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package p5core/0.1.0/p5core-0.1.0.tar.gz

---
+++ packages/p5core/p5core-0.1.0.tar.gz/p5core-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['p5core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['py7zr>=0.11.0,<0.12.0',
+ 'requests>=2.20.0,<3.0.0',
+ 'simplelogging>=0.10,<0.12']
+
+setup_kwargs = {
+    'name': 'p5core',
+    'version': '0.1.0',
+    'description': 'Programmer en Python comme un Pro Par la Pratique',
+    'long_description': '# p5core\n\n[![PyPI](https://img.shields.io/pypi/v/p5core.svg)](https://pypi.python.org/pypi/p5core)\n[![Code style: Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)\n[![Downloads](https://pepy.tech/badge/p5core)](https://pepy.tech/project/p5core)\n\np⁵\xa0: Programmer en Python comme un Pro Par la Pratique\n\n## License\n\nCopyright Vincent Poulailleau 2020\n',
+    'author': 'Vincent Poulailleau',
+    'author_email': 'vpoulailleau@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/vpoulailleau/p5',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
