3234d684de8d1552141d2b76437ebce3a6669707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:24 +0000

    Add package graspnetAPI/1.1.2/graspnetAPI-1.1.2.tar.gz

---
+++ packages/graspnetAPI/graspnetAPI-1.1.2.tar.gz/graspnetAPI-1.1.2/meshpy/setup.py
@@ -0,0 +1,13 @@
+"""
+Setup of meshpy python codebase
+Author: Jeff Mahler
+"""
+from setuptools import setup, find_packages
+
+setup(name='meshpy',
+    version='0.1.0',
+    description='MeshPy project code',
+    author='Matt Matl',
+    author_email='mmatl@berkeley.edu',
+    packages=find_packages()
+)
