3471e73517c4cb9ac9de410999cbed9586bd2639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:42 +0000

    Add package mkdocs-git-committers-plugin/0.2.1/mkdocs-git-committers-plugin-0.2.1.tar.gz

---
+++ packages/mkdocs-git-committers-plugin/mkdocs-git-committers-plugin-0.2.1.tar.gz/mkdocs-git-committers-plugin-0.2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mkdocs-git-committers-plugin',
+    version='0.2.1',
+    description='An MkDocs plugin to create a list of contributors on the page',
+    long_description='The git-committers plugin will seed the template context with a list of github committers',
+    keywords='mkdocs pdf github',
+    url='https://github.com/byrnereese/mkdocs-git-committers-plugin/',
+    author='Byrne Reese',
+    author_email='byrne@majordojo.com',
+    license='MIT',
+    python_requires='>=3.5',
+    install_requires=[
+        'PyGithub>=1.43',
+        'mkdocs>=0.17'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'git-committers = mkdocs_git_committers_plugin.plugin:GitCommittersPlugin'
+        ]
+    }
+)
