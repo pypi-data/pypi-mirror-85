95d1cfa95f4a9696ada23293924e0fc13f90ccd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:27 +0000

    Add package asgi-tools/0.0.4/asgi_tools-0.0.4-py3-none-any.whl

---
+++ packages/asgi-tools/asgi_tools-0.0.4-py3-none-any.whl/asgi_tools/__init__.py
@@ -0,0 +1,28 @@
+""" ASGI-Tools -- Tools to make ASGI Applications """
+
+__version__ = "0.0.4"
+__license__ = "MIT"
+
+
+class ASGIError(Exception):
+    """Base class for ASGI-Tools Errors."""
+
+    pass
+
+
+class ASGIDecodeError(ASGIError):
+    """ASGI-Tools decoding error."""
+
+    pass
+
+
+SUPPORTED_SCOPES = {'http', 'websocket'}
+DEFAULT_CHARSET = 'utf-8'
+
+
+from .request import Request  # noqa
+from .response import Response, HTMLResponse, JSONResponse, PlainTextResponse  # noqa
+from .middleware import (  # noqa
+    RequestMiddleware, ResponseMiddleware, AppMiddleware, LifespanMiddleware, RouterMiddleware,
+    combine, parse_response
+)
