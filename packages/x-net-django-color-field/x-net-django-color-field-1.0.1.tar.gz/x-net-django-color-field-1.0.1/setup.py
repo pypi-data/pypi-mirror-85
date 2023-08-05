81531d79198b3663aed1dfc9db7f128ef262cc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package x-net-django-color-field/1.0.1/x-net-django-color-field-1.0.1.tar.gz

---
+++ packages/x-net-django-color-field/x-net-django-color-field-1.0.1.tar.gz/x-net-django-color-field-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+import x_net_django_color_field
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    version=x_net_django_color_field.__version__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/x-net-services/x-net-django-color-field",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "Django>=2.2.17",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+    ]
+)
