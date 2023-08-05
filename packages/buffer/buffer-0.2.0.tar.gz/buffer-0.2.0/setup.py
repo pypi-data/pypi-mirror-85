00b7b0a54dcd668c162400ae4932f00ba44af6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:44 +0000

    Add package buffer/0.2.0/buffer-0.2.0.tar.gz

---
+++ packages/buffer/buffer-0.2.0.tar.gz/buffer-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+
+requirements = Path('requirements.txt').read_text().split('\n')
+readme = Path('README.md').read_text()
+
+
+setup(name="buffer",
+      version="0.2.0",
+      description="A stream buffer backed by a spooled temporary file.",
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      url="https://alexdelorenzo.dev",
+      author="Alex DeLorenzo",
+      license="AGPL-3.0",
+      packages=['buffer'],
+      zip_safe=True,
+      install_requires=requirements,
+      keywords="stream-buffer stream buffer file buffer temporary file".split(' '),
+      python_requires='>=3.6',
+)
