bf7856e3d2af38b60ea265b79c280b061847d434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package daves-dev-tools/0.0.3/daves-dev-tools-0.0.3.tar.gz

---
+++ packages/daves-dev-tools/daves-dev-tools-0.0.3.tar.gz/daves-dev-tools-0.0.3/daves_dev_tools/utilities.py
@@ -0,0 +1,12 @@
+import sys
+from traceback import format_exception
+
+
+def get_exception_text() -> str:
+    """
+    When called within an exception, this function returns a text
+    representation of the error matching what is found in
+    `traceback.print_exception`, but is returned as a string value rather than
+    printing.
+    """
+    return ''.join(format_exception(*sys.exc_info()))
