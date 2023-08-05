565e203c1d0d9f2f4d0f848b2d6001c35fef0541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:54 +0000

    Add package nestpy/1.3.3/nestpy-1.3.3.tar.gz

---
+++ packages/nestpy/nestpy-1.3.3.tar.gz/nestpy-1.3.3/src/nestpy/VDetector.cpp
@@ -0,0 +1,20 @@
+//
+// VDetector.cpp
+//
+// Adapted from Quentin Riffard by Jacob Cutter, May 8, 2018
+
+// *********************************************************************
+// THIS DEFAULT VIRTUAL DETECTOR SHOULD ONLY BE MODIFIED BY DEVELOPERS.
+// PLEASE DEFINE YOUR OWN DETECTOR (see DetectorExample_XENON10.hh).
+// *********************************************************************
+
+#include <math.h>
+
+#include "VDetector.hh"
+
+VDetector::VDetector() { Initialization(); }
+
+VDetector::~VDetector() {}
+
+void VDetector::Initialization() {
+}
