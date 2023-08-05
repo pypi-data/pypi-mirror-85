b0a0d2a94216cf1a3349b4fac0a8ba3d2863ef26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package udacityDistributionEMC/0.1/udacityDistributionEMC-0.1.tar.gz

---
+++ packages/udacityDistributionEMC/udacityDistributionEMC-0.1.tar.gz/udacityDistributionEMC-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udacityDistributionEMC',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      author='Eoghan McCarey',
+      packages=['udacityDistributionEMC'],
+      zip_safe=False)
