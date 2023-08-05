aff3bc5be1a5eb9e9b770349a011af346644f2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package cs18-api-client/0.0.2.1640/cs18-api-client-0.0.2.1640.tar.gz

---
+++ packages/cs18-api-client/cs18-api-client-0.0.2.1640.tar.gz/cs18-api-client-0.0.2.1640/gateways/common/cs18_api_classes.py
@@ -0,0 +1,53 @@
+"""
+Container for object classes used in cs18-api-client
+"""
+
+import dateutil.parser
+
+
+class AccessLink:
+    def __init__(self, protocol: str, link: str):
+        self.link = link
+        self.protocol = protocol
+
+
+class Commit:
+    def __init__(self, data: dict):
+        self.sha = data["sha"]
+        self.author = data["commit"]["author"]["name"]
+        self.date = dateutil.parser.parse(data["commit"]["author"]["date"])
+        self.comment = data["commit"]["message"]
+
+    def __str__(self):
+        return "{0}: [{1}] {2}".format(self.date, self.sha[:7], self.comment)
+
+
+class ColonyAccount:
+    def __init__(
+            self, account: str, email: str, password: str, first_name: str, last_name: str
+    ):
+        self.account = account
+        self.default_space = "Trial"
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class BlueprintRepositoryDetails:
+    def __init__(self, repository_url: str, access_token: str, repository_type: str, branch: str = None):
+        self.repository_url = repository_url
+        self.repository_type = repository_type
+        self.access_token = access_token
+        self.branch = branch
+
+
+class AddAccountBlueprintRepositoryRequest:
+    def __init__(self, name: str, repository_url: str, access_token: str, repository_type: str, allow_sharing: bool,
+                 open_access: bool):
+        self.name = name
+        self.repository_url = repository_url
+        self.repository_type = repository_type
+        self.access_token = access_token
+        self.open_access = open_access
+        self.allow_sharing = allow_sharing
