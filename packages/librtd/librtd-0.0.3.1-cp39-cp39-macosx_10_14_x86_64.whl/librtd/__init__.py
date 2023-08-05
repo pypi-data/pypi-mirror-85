ff89d0d47a5436e343683dedc1396aed0315c0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:43 +0000

    Add package librtd/0.0.3.1/librtd-0.0.3.1-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/librtd/librtd-0.0.3.1-cp39-cp39-macosx_10_14_x86_64.whl/librtd/__init__.py
@@ -0,0 +1,7 @@
+import nimporter as _nimporter
+from librtdpy import (
+    same_kmer_return_times,
+    pairwise_kmer_return_times,
+    reverse_complement_return_times,
+    return_time_distribution,
+)
