dab32d637dd9425ae614f17523935da040eb27a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package u8darts/0.5.0/u8darts-0.5.0-py3-none-any.whl

---
+++ packages/u8darts/u8darts-0.5.0-py3-none-any.whl/darts/models/rnn_model.py
@@ -0,0 +1,172 @@
+"""
+Recurrent Neural Networks
+-------------------------
+"""
+
+import torch.nn as nn
+from numpy.random import RandomState
+from typing import List, Optional, Union
+
+from ..logging import raise_if_not, get_logger
+from ..utils.torch import random_method
+from .torch_forecasting_model import TorchForecastingModel
+
+logger = get_logger(__name__)
+
+
+# TODO add batch norm
+class _RNNModule(nn.Module):
+    def __init__(self,
+                 name: str,
+                 input_size: int,
+                 hidden_dim: int,
+                 num_layers: int,
+                 output_length: int = 1,
+                 output_size: int = 1,
+                 num_layers_out_fc: Optional[List] = None,
+                 dropout: float = 0.):
+
+        """ PyTorch module implementing a RNN to be used in `RNNModel`.
+
+        PyTorch module implementing a simple RNN with the specified `name` layer.
+        This module combines a PyTorch RNN module, together with a fully connected network, which maps the
+        last hidden layers to output of the desired size `output_length` and makes it compatible with
+        `RNNModel`s.
+
+        Parameters
+        ----------
+        name
+            The name of the specific PyTorch RNN module ("RNN", "GRU" or "LSTM").
+        input_size
+            The dimensionality of the input time series.
+        hidden_dim
+            The number of features in the hidden state `h` of the RNN module.
+        num_layers
+            The number of recurrent layers.
+        output_length
+            The number of steps to predict in the future.
+        output_size
+            The dimensionality of the output time series.
+        num_layers_out_fc
+            A list containing the dimensions of the hidden layers of the fully connected NN.
+            This network connects the last hidden layer of the PyTorch RNN module to the output.
+        dropout
+            The fraction of neurons that are dropped in all-but-last RNN layers.
+
+        Inputs
+        ------
+        x of shape `(batch_size, input_length, input_size)`
+            Tensor containing the features of the input sequence.
+
+        Outputs
+        -------
+        y of shape `(batch_size, out_len, output_size)`
+            Tensor containing the (point) prediction at the last time step of the sequence.
+        """
+
+        super(_RNNModule, self).__init__()
+
+        # Defining parameters
+        self.hidden_dim = hidden_dim
+        self.n_layers = num_layers
+        self.output_size = output_size
+        num_layers_out_fc = [] if num_layers_out_fc is None else num_layers_out_fc
+        self.out_len = output_length
+        self.name = name
+
+        # Defining the RNN module
+        self.rnn = getattr(nn, name)(input_size, hidden_dim, num_layers, batch_first=True, dropout=dropout)
+
+        # The RNN module is followed by a fully connected layer, which maps the last hidden layer
+        # to the output of desired length
+        last = hidden_dim
+        feats = []
+        for feature in num_layers_out_fc + [output_length * output_size]:
+            feats.append(nn.Linear(last, feature))
+            last = feature
+        self.fc = nn.Sequential(*feats)
+
+    def forward(self, x):
+        # data is of size (batch_size, input_length, input_size)
+        batch_size = x.size(0)
+
+        out, hidden = self.rnn(x)
+
+        """ Here, we apply the FC network only on the last output point (at the last time step)
+        """
+        if self.name == "LSTM":
+            hidden = hidden[0]
+        predictions = hidden[-1, :, :]
+        predictions = self.fc(predictions)
+        predictions = predictions.view(batch_size, self.out_len, self.output_size)
+
+        # predictions is of size (batch_size, output_length, 1)
+        return predictions
+
+
+class RNNModel(TorchForecastingModel):
+    @random_method
+    def __init__(self,
+                 model: Union[str, nn.Module] = 'RNN',
+                 input_size: int = 1,
+                 output_length: int = 1,
+                 output_size: int = 1,
+                 hidden_size: int = 25,
+                 n_rnn_layers: int = 1,
+                 hidden_fc_sizes: Optional[List] = None,
+                 dropout: float = 0.,
+                 random_state: Optional[Union[int, RandomState]] = None,
+                 **kwargs):
+
+        """ Recurrent Neural Network Model (RNNs).
+
+        This class provides three variants of RNNs:
+
+        * Vanilla RNN
+
+        * LSTM
+
+        * GRU
+
+        Parameters
+        ----------
+        model
+            Either a string specifying the RNN module type ("RNN", "LSTM" or "GRU"),
+            or a PyTorch module with the same specifications as
+            `darts.models.rnn_model.RNNModule`.
+        input_size
+            The dimensionality of the TimeSeries instances that will be fed to the fit function.
+        output_size
+            The dimensionality of the output time series.
+        output_length
+            Number of time steps to be output by the forecasting module.
+        hidden_size
+            Size for feature maps for each hidden RNN layer (:math:`h_n`).
+        n_rnn_layers
+            Number of layers in the RNN module.
+        hidden_fc_sizes
+            Sizes of hidden layers connecting the last hidden layer of the RNN module to the output, if any.
+        dropout
+            Fraction of neurons afected by Dropout.
+        random_state
+            Control the randomness of the weights initialization. Check this
+            `link <https://scikit-learn.org/stable/glossary.html#term-random-state>`_ for more details.
+        """
+
+        kwargs['output_length'] = output_length
+        kwargs['input_size'] = input_size
+        kwargs['output_size'] = output_size
+
+        # set self.model
+        if model in ['RNN', 'LSTM', 'GRU']:
+            hidden_fc_sizes = [] if hidden_fc_sizes is None else hidden_fc_sizes
+            self.model = _RNNModule(name=model, input_size=input_size, output_size=output_size, hidden_dim=hidden_size,
+                                    num_layers=n_rnn_layers, output_length=output_length,
+                                    num_layers_out_fc=hidden_fc_sizes, dropout=dropout)
+        else:
+            self.model = model
+        raise_if_not(isinstance(self.model, nn.Module), '{} is not a valid RNN model.\n Please specify "RNN", "LSTM", '
+                     '"GRU", or give your own PyTorch nn.Module'.format(model.__class__.__name__),
+                     logger)
+
+        super().__init__(**kwargs)
---
+++ packages/u8darts/u8darts-0.5.0-py3-none-any.whl/darts/utils/missing_values.py
@@ -0,0 +1,115 @@
+"""
+Utils for filling missing values
+--------------------------------
+"""
+
+from darts.timeseries import TimeSeries
+from darts.logging import get_logger, raise_if, raise_if_not
+
+from typing import Union
+
+logger = get_logger(__name__)
+
+
+def missing_values_ratio(series: TimeSeries) -> float:
+    """
+    Computes the ratio of missing values
+
+    Parameters
+    ----------
+    series
+        The time series to compute ratio on
+
+    Returns
+    -------
+    float
+        The ratio of missing values
+    """
+
+    return series.pd_dataframe().isnull().sum().mean() / len(series)
+
+
+def fill_missing_values(series: TimeSeries, fill: Union[str, float] = 'auto', **interpolate_kwargs) -> TimeSeries:
+    """
+    Fills missing values in the provided time series
+
+    Parameters
+    ----------
+    series
+        The time series for which to fill missing values
+    fill
+        The value used to replace the missing values.
+        If set to 'auto', will auto-fill missing values using the `pandas.Dataframe.interpolate()` method.
+    interpolate_kwargs
+        Keyword arguments for `pandas.Dataframe.interpolate()`, only used when fit is set to 'auto'.
+        See `the documentation
+        <https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.interpolate.html>`_
+        for the list of supported parameters.
+
+    Returns
+    -------
+    TimeSeries
+        A new TimeSeries with all missing values filled according to the rules above.
+    """
+    raise_if_not(isinstance(fill, str) or isinstance(fill, float),
+                 "`fill` should either be a string or a float",
+                 logger)
+    raise_if(isinstance(fill, str) and fill != 'auto',
+             "invalid string for `fill`: can only be set to 'auto'",
+             logger)
+
+    if fill == 'auto':
+        return _auto_fill(series, **interpolate_kwargs)
+    return _const_fill(series, fill)
+
+
+def _const_fill(series: TimeSeries, fill: float = 0) -> TimeSeries:
+    """
+    Fills the missing values of `series` with only the value provided (default zeroes).
+
+    Parameters
+    ----------
+    series
+        The TimeSeries to check for missing values.
+    fill
+        The value used to replace the missing values.
+
+    Returns
+    -------
+    TimeSeries
+        A TimeSeries, `series` with all missing values set to `fill`.
+    """
+
+    return TimeSeries.from_times_and_values(series.time_index(),
+                                            series.pd_dataframe().fillna(value=fill),
+                                            series.freq())
+
+
+def _auto_fill(series: TimeSeries, **interpolate_kwargs) -> TimeSeries:
+    """
+    This function fills the missing values in the TimeSeries `series`,
+    using the `pandas.Dataframe.interpolate()` method.
+
+    Parameters
+    ----------
+    series
+        The time series
+    interpolate_kwargs
+        Keyword arguments for `pandas.Dataframe.interpolate()`.
+        See `the documentation
+        <https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.interpolate.html>`_
+        for the list of supported parameters.
+    Returns
+    -------
+    TimeSeries
+        A new TimeSeries with all missing values filled according to the rules above.
+    """
+
+    series_temp = series.pd_dataframe()
+
+    # pandas interpolate wrapper, with chosen `method`
+    if 'limit_direction' not in interpolate_kwargs:
+        interpolate_kwargs['limit_direction'] = 'both'
+    interpolate_kwargs['inplace'] = True
+    series_temp.interpolate(**interpolate_kwargs)
+    return TimeSeries.from_times_and_values(series.time_index(), series_temp, series.freq())
