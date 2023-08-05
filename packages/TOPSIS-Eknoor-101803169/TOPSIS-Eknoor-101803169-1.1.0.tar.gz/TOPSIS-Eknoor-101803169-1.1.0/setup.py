c02bc0cb39d94c9188e4d7af2048d94ead383568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:59 +0000

    Add package TOPSIS-Eknoor-101803169/1.1.0/TOPSIS-Eknoor-101803169-1.1.0.tar.gz

---
+++ packages/TOPSIS-Eknoor-101803169/TOPSIS-Eknoor-101803169-1.1.0.tar.gz/TOPSIS-Eknoor-101803169-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="TOPSIS-Eknoor-101803169",
+    version="1.1.0",
+    description="A Python package for topsis analysis.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Eknoor Kaur",
+    author_email="eknoorbehla78677@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TOPSIS-Eknoor-101803169"],
+    include_package_data=True,
+    install_requires=["requests"],
+)
