b897ed33f7eac1c8cdcd363e2a91b2519c8bb41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:30 +0000

    Add package marshmallow-utils/0.3.2/marshmallow-utils-0.3.2.tar.gz

---
+++ packages/marshmallow-utils/marshmallow-utils-0.3.2.tar.gz/marshmallow-utils-0.3.2/docs/api.rst
@@ -0,0 +1,16 @@
+..
+    Copyright (C) 2020 CERN.
+
+    Marshmallow-Utils is free software; you can redistribute it and/or
+    modify it under the terms of the MIT License; see LICENSE file for more
+    details.
+
+
+API Docs
+========
+
+Fields
+------
+
+.. automodule:: marshmallow_utils.fields
+   :members:
---
+++ packages/marshmallow-utils/marshmallow-utils-0.3.2.tar.gz/marshmallow-utils-0.3.2/marshmallow_utils/fields/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2020 CERN.
+#
+# Marshmallow-Utils is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Marshmallow fields."""
+
+from .babel import FormatDate, FormatDatetime, FormatEDTF, FormatTime
+from .contrib import Function, Method
+from .edtfdatestring import EDTFDateString
+from .generated import GenFunction, GenMethod
+from .isodate import ISODateString
+from .isolanguage import ISOLangString
+from .links import Link, Links
+from .nestedattr import NestedAttribute
+from .sanitizedhtml import ALLOWED_HTML_ATTRS, ALLOWED_HTML_TAGS, SanitizedHTML
+from .sanitizedunicode import SanitizedUnicode
+from .trimmed import TrimmedString
+
+__all__ = (
+    'ALLOWED_HTML_ATTRS',
+    'ALLOWED_HTML_TAGS',
+    'EDTFDateString',
+    'FormatDate',
+    'FormatDatetime',
+    'FormatEDTF',
+    'FormatTime',
+    'Function',
+    'GenFunction',
+    'GenMethod',
+    'ISODateString',
+    'ISOLangString',
+    'Link',
+    'Links',
+    'Method',
+    'NestedAttribute',
+    'SanitizedHTML',
+    'SanitizedUnicode',
+    'TrimmedString',
+)
