5748864bd9b6d1e3a6be58080153abaf790cc1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:54 +0000

    Add package pythonbasics/1.0/pythonbasics-1.0.tar.gz

---
+++ packages/pythonbasics/pythonbasics-1.0.tar.gz/pythonbasics-1.0/python_basics/control_flows.py
@@ -0,0 +1,2 @@
+def control_flows():
+    print('It contains all the information about if-else, while-looop, for-loop etc')
---
+++ packages/pythonbasics/pythonbasics-1.0.tar.gz/pythonbasics-1.0/python_basics/exceptions.py
@@ -0,0 +1,2 @@
+def exception():
+    print('It is used for not to crash your computer when an error is occur')
