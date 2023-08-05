47eb77bc9e2ca9bf2a5b362d4a7025e24535e1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package pipelinewise-tap-mysql/1.4.0/pipelinewise-tap-mysql-1.4.0.tar.gz

---
+++ packages/pipelinewise-tap-mysql/pipelinewise-tap-mysql-1.4.0.tar.gz/pipelinewise-tap-mysql-1.4.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pipelinewise-tap-mysql',
+      version='1.4.0',
+      description='Singer.io tap for extracting data from MySQL - PipelineWise compatible',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='TransferWise',
+      url='https://github.com/transferwise/pipelinewise-tap-mysql',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=['tap_mysql'],
+      install_requires=[
+          'pendulum==1.2.0',
+          'pipelinewise-singer-python==1.*',
+          'PyMySQL==0.7.11',
+          'mysql-replication==0.21',
+          'pyyaml==5.3',
+          'plpygis==0.2.0',
+      ],
+      extras_require={
+          'test': [
+              'nose==1.3.*',
+              'pylint==2.4.*',
+              'nose-cov==1.6'
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-mysql=tap_mysql:main
+      ''',
+      packages=['tap_mysql', 'tap_mysql.sync_strategies'],
+      )
