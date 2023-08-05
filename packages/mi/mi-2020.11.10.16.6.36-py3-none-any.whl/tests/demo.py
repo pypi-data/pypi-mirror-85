75102f4021ef3d134da9ecae314f58a53a989c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package mi/2020.11.10.16.6.36/mi-2020.11.10.16.6.36-py3-none-any.whl

---
+++ packages/mi/mi-2020.11.10.16.6.36-py3-none-any.whl/tests/demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : mi.
+# @File         : demo
+# @Time         : 2020/10/26 2:58 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+
+语文 = float(input("语文："))
+数学 = float(input("数学："))
+英语 = float(input("英语："))
+
+print(0.5 * 语文 + 0.3 * 数学 + 0.2 * 英语)
