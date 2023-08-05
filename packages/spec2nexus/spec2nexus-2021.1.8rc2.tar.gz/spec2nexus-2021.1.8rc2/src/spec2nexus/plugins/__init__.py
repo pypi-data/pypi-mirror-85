54179107f3189ffc67001701b946ba5be7e2e074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:29 +0000

    Add package spec2nexus/2021.1.8rc2/spec2nexus-2021.1.8rc2.tar.gz

---
+++ packages/spec2nexus/spec2nexus-2021.1.8rc2.tar.gz/spec2nexus-2021.1.8rc2/src/spec2nexus/plugins/__init__.py
@@ -0,0 +1,17 @@
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2020, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from . import spec_common
+from . import fallback
+from . import apstools_specwriter
+from . import uim
+from . import unicat
+from . import uxml
+from . import XPCS
