791abf14e8e01c37aee66a3d1f52d30933c5cfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package spheres/0.3.0.5/spheres-0.3.0.5.tar.gz

---
+++ packages/spheres/spheres-0.3.0.5.tar.gz/spheres-0.3.0.5/spheres/__init__.py
@@ -0,0 +1,32 @@
+"""
+..  automodule:: spheres.stars
+	:members:
+
+..  automodule:: spheres.symmetrization
+	:members:
+
+.. automodule:: spheres.visualization
+	:members:
+
+.. automodule:: spheres.utils
+	:members:
+	
+"""
+
+import numpy as np
+import qutip as qt
+
+from spheres import utils
+from spheres.utils import *
+
+from spheres import stars
+from spheres.stars import *
+
+from spheres import symmetrization
+from spheres.symmetrization import *
+
+from spheres import visualization
+from spheres.visualization import *
+
+__version__ = "0.3.0.5"
+ 
