393bd99d2d2367b909d67652c5150e24f0bfe721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package pynubls/0.1.0/pynubls-0.1.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/pynubls/pynubls-0.1.0-cp38-cp38-manylinux1_x86_64.whl/pynubls/__init__.py
@@ -0,0 +1,12 @@
+from pynubls import *
+
+def hash_message(message: bytes, DST: bytes = None) -> bytes:
+    from hashlib import sha256
+    from py_ecc.bls.hash import i2osp
+    from py_ecc.bls.hash_to_curve import hash_to_G2
+    from py_ecc.bls.point_compression import compress_G2
+
+    DST = DST or b''    # TODO: Use a valid DST
+    mapped_msg = hash_to_G2(message, DST, sha256)
+    z1, z2 = compress_G2(mapped_msg)
+    return i2osp(z1, 48) + i2osp(z2, 48)
