6d1ea01b781ef0d64088d2867a383a8656e5bce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:48 +0000

    Add package hdrstats/0.1.9/hdrstats-0.1.9-py2.py3-none-any.whl

---
+++ packages/hdrstats/hdrstats-0.1.9-py2.py3-none-any.whl/hdrstats/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Stephen Wasilewski
+# =======================================================================
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# =======================================================================
+
+"""Top-level package for hdrstats."""
+
+__author__ = """Stephen Wasilewski"""
+__email__ = 'stephanwaz@gmail.com'
+__version__ = '0.1.9'
+__all__ = ['hdrstats', 'image']
+
+import hdrstats.hdrstats as hdrstats
+import hdrstats.image as image
