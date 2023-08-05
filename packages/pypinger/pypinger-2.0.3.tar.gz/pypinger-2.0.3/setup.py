69fe312a217270a213db24190473485227b2300a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:36 +0000

    Add package pypinger/2.0.3/pypinger-2.0.3.tar.gz

---
+++ packages/pypinger/pypinger-2.0.3.tar.gz/pypinger-2.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(name='pypinger',
+      version='2.0.3',
+      description='Another way to ping in Python',
+      url='https://github.com/DataKnox/pypinger',
+      author='Knox Hutchinson',
+      author_email='knox@knoxsdata.com',
+      license='MIT',
+      packages=['pypinger'],
+      keywords=['ping', 'icmp', 'network'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: System Administrators',
+          'Natural Language :: English'
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      entry_points={  # Optional
+          'console_scripts': [
+              'pyping=pypinger:pyping',
+          ],
+      },
+      zip_safe=False)
