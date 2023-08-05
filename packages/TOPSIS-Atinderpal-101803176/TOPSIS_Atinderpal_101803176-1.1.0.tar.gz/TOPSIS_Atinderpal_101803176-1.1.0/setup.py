8f4419f4c11622406cfb0958bd8fce4772f3e04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package TOPSIS-Atinderpal-101803176/1.1.0/TOPSIS_Atinderpal_101803176-1.1.0.tar.gz

---
+++ packages/TOPSIS-Atinderpal-101803176/TOPSIS_Atinderpal_101803176-1.1.0.tar.gz/TOPSIS_Atinderpal_101803176-1.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+  name = 'TOPSIS_Atinderpal_101803176',         # How you named your package folder (MyLib)
+  version="1.1.0",
+  packages = ['TOPSIS_Atinderpal_101803176'],   # Chose the same as "name"
+  description="A Python package for topsis analysis.",
+  long_description=readme(),
+  long_description_content_type="text/markdown",
+  url="",
+  author="Atinderpal Kaur",
+  author_email="kaur.atinder01@gmail.com",
+  license="MIT",
+  classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+  include_package_data=True,
+  install_requires=["requests"],
+  entry_points={
+        "console_scripts": [
+            "TOPSIS_Atinderpal_101803176=TOPSIS:main",
+        ]
+   },
+)
