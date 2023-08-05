28067b22e6025c058bc01a3e4f326f3ac0828a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:43 +0000

    Add package TOPSIS-Parth-101983047/0.0.7/TOPSIS-Parth-101983047-0.0.7.tar.gz

---
+++ packages/TOPSIS-Parth-101983047/TOPSIS-Parth-101983047-0.0.7.tar.gz/TOPSIS-Parth-101983047-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TOPSIS-Parth-101983047",
+    version="0.0.7",
+    author="Parth Verma",
+    author_email="vermaparth818@gmail.com",
+    description="A simplified package to perform TOPSIS Analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parth818/TOPSIS-Parth-101983047",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pandas'],
+    python_requires='>=3.6',
+)
