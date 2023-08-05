11543472380c5047d3bcba755c2280b4b3891c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package scoutr/1.2.2/scoutr-1.2.2.tar.gz

---
+++ packages/scoutr/scoutr-1.2.2.tar.gz/scoutr-1.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='scoutr',
+    description='Generic full access control API for talking to Dynamo via API Gateway',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='dynamo api rbac',
+    url='https://github.com/GESkunkworks/scoutr',
+    use_scm_version=True,
+    author='Michael Palmer',
+    packages=find_packages(),
+    setup_requires=[
+        'setuptools_scm>=3.3.3,<4.2.0'
+    ],
+    install_requires=[
+        'boto3>=1.12.39,<1.16.0'
+    ],
+    extras_require={
+        'flask': [
+            'Flask-API>=1.1,<3.0',
+            'Flask~=1.1.1',
+            'simplejson>=3.16,<3.18'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable'
+    ]
+)
