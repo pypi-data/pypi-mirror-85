42a5dfd796677b90e190391d6a87a8a0335d11a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:51 +0000

    Add package connio/0.2.0/connio-0.2.0.tar.gz

---
+++ packages/connio/connio-0.2.0.tar.gz/connio-0.2.0/connio/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.2.0"
+
+
+import urllib.parse
+
+
+SERIAL_SCHEMES = {'serial', 'serial-tcp', 'serial-tango', 'rfc2217'}
+SOCKET_SCHEMES = {'tcp'}
+
+
+def connection_for_url(url, *args, **kwargs):
+    url_result = urllib.parse.urlparse(url)
+    scheme = url_result.scheme
+    if scheme in SERIAL_SCHEMES:
+        from serialio import serial_for_url
+        return serial_for_url(url, *args, **kwargs)
+    elif scheme in SOCKET_SCHEMES:
+        from sockio import socket_for_url
+        return socket_for_url(url, *args, **kwargs)
+    else:
+        raise ValueError("unsupported scheme {!r} for {}".format(scheme, url))
