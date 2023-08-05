7779ad0a7ea01fdeb198bc26c7c6bc1ef1ba3702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:02 +0000

    Add package plone.staticresources/1.4.1/plone.staticresources-1.4.1-py2.py3-none-any.whl

---
+++ packages/plone.staticresources/plone.staticresources-1.4.1-py2.py3-none-any.whl/plone/staticresources/setuphandlers.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implementer
+
+
+try:
+    from Products.CMFPlone.interfaces import INonInstallable
+except ImportError:
+    from zope.interface import Interface
+
+    class INonInstallable(Interface):
+        pass
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+    def getNonInstallableProfiles(self):
+        """Hide all profiles from site-creation and quickinstaller."""
+        return [
+            "plone.staticresources:default",
+            "plone.staticresources:async",
+            "plone.staticresources:uninstall",
+            "plone.staticresources.upgrades:2",
+            "plone.staticresources.upgrades:3",
+            "plone.staticresources.upgrades:4",
+            "plone.staticresources.upgrades:5",
+            "plone.staticresources.upgrades:6",
+            "plone.staticresources.upgrades:7",
+            "plone.staticresources.upgrades:8",
+            "plone.staticresources.upgrades:9",
+            "plone.staticresources.upgrades:10",
+            "plone.staticresources.upgrades:11",
+        ]
