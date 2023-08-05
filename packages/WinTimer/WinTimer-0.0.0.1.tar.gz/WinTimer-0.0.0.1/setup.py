92debafc1b88d9c84bfe55fefa24670cb8738df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package WinTimer/0.0.0.1/WinTimer-0.0.0.1.tar.gz

---
+++ packages/WinTimer/WinTimer-0.0.0.1.tar.gz/WinTimer-0.0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools as s
+f=open("readme.txt","r")
+ld=f.read()
+f.close()
+s.setup(
+    name = "WinTimer",
+    version="0.0.0.1",
+    author = "Piyush",
+    author_email="somanip409@gmail.com",
+    description="Timer",
+    url="https://github.com/PS218909/",
+    install_requires=['winsounds'],
+    license="MIT",
+    long_description=ld,
+    long_description_content_type='text/markdown',
+    packages=s.find_packages(),
+    python_requires=">=3.6"
+    )
+
+                
