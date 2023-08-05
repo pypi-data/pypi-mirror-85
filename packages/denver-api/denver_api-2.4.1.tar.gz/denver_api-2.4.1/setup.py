d928b54c39a8fcbfe9ceba93f223ae726ceb334e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:23 +0000

    Add package denver-api/2.4.1/denver_api-2.4.1.tar.gz

---
+++ packages/denver-api/denver_api-2.4.1.tar.gz/denver_api-2.4.1/setup.py
@@ -0,0 +1,54 @@
+import os
+import setuptools
+
+with open("README.md") as file:
+    long_description = file.read()
+
+
+def exclude_files(filespath: list, exts: list):
+    rml = []
+    for x in range(len(filespath)):
+        if os.path.splitext(filespath[x])[1] in exts:
+            rml.append(filespath[x])
+    for x in rml:
+        filespath.remove(x)
+
+
+def find_package_data(package: str, package_name=None):
+    if package_name is None:
+        package_name = package
+    files = []
+    for r, d, f in os.walk(package):
+        files.extend([os.path.join(r, x)[len(package) + 1 :] for x in f])
+    exclude_files(files, [".py"])
+    root = {}
+    for x in files:
+        d = x.split(os.sep)
+        d.pop(-1)
+        d = ".".join(d)
+        root.setdefault(package_name + "." + d, [])
+        root[package_name + "." + d].append(os.path.basename(x))
+    return {k.strip("."): v for k, v in root.items()}
+
+
+setuptools.setup(
+    name="denver_api",
+    packages=setuptools.find_packages()
+    + setuptools.find_namespace_packages(include=["denverapi", "denverapi.*"]),
+    package_data=find_package_data("denverapi", "denverapi"),
+    version="2.4.1",
+    author="xcodz",
+    description="Denver API for python full-stack development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["setuptools", "requests", "playsound", "cryptography"],
+    extras_require={
+        "gui-tools": ["pygame"]
+    },
+    zip_safe=False,
+)
