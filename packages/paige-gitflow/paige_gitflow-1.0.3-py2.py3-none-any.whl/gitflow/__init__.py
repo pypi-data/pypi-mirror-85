89d07325f5bee044ae11635f1242c2738964ae40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package paige-gitflow/1.0.3/paige_gitflow-1.0.3-py2.py3-none-any.whl

---
+++ packages/paige-gitflow/paige_gitflow-1.0.3-py2.py3-none-any.whl/gitflow/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+"""
+git-flow -- A collection of Git extensions to provide high-level
+repository operations for Vincent Driessen's branching model.
+"""
+#
+# This file is part of `gitflow`.
+# Copyright (c) 2010-2011 Vincent Driessen
+# Copyright (c) 2012-2013 Hartmut Goebel
+# Copyright (c) 2015 Christian Assing
+# Copyright (c) 2020 yeonghak seo
+# Distributed under a BSD-like license. For full terms see the file LICENSE.txt
+#
+
+VERSION = (1, 0, 3,)
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "inspire12"
+__contact__ = "inspire12@ncosft.com"
+__homepage__ = "http://github.com/inspire12/gitflow/"
+__docformat__ = "restructuredtext"
+__copyright__ = "2010-2011 Vincent Driessen; 2012-2013 Hartmut Goebel; 2015 Christian Assing; 2020 yeonghak seo"
+__license__ = "BSD"
