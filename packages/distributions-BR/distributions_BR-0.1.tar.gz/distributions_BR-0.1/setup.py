832f24e2e4d8567734546edd87bc6fc30f7d2e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:17 +0000

    Add package distributions-BR/0.1/distributions_BR-0.1.tar.gz

---
+++ packages/distributions-BR/distributions_BR-0.1.tar.gz/distributions_BR-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_BR',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_BR'],
+      author= 'Benjamin Redmond',
+      author_email= 'benjamin.redmond1@gmail.com',
+      zip_safe=False)
