d980172d7a660422c9d35591c942dc4f3720c30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:42 +0000

    Add package xappt-qt/0.2.18/xappt_qt-0.2.18-py3-none-any.whl

---
+++ packages/xappt-qt/xappt_qt-0.2.18-py3-none-any.whl/xappt_qt/main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import xappt_qt
+import xappt_qt.browser
+import xappt_qt.launcher
+import xappt_qt.builder
+
+
+def main() -> int:
+    argc = len(sys.argv)
+    if argc == 1:
+        return xappt_qt.browser.entry_point()
+    else:
+        if sys.argv[1] == "builder" and argc > 2:
+            return xappt_qt.builder.main(sys.argv[2:])
+        return xappt_qt.launcher.entry_point()
+
+
+if __name__ == '__main__':
+    if getattr(xappt_qt, "__compiled__", None) is not None:
+        xappt_qt.executable = os.path.abspath(sys.argv[0])
+    sys.exit(main())
