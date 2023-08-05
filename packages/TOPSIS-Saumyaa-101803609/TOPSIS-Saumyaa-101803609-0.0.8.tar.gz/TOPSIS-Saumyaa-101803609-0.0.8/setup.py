9149f992def86d708ad95eae831b1cad55bb5759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:22 +0000

    Add package TOPSIS-Saumyaa-101803609/0.0.8/TOPSIS-Saumyaa-101803609-0.0.8.tar.gz

---
+++ packages/TOPSIS-Saumyaa-101803609/TOPSIS-Saumyaa-101803609-0.0.8.tar.gz/TOPSIS-Saumyaa-101803609-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+def readme():
+    with open('README.md', encoding='utf8') as f:
+        README =f.read()
+    return README
+setuptools.setup(
+    name="TOPSIS-Saumyaa-101803609",
+    version="0.0.8",
+    author="Saumyaa Mathur",
+    author_email="saumyaamathur27@gmail.com",
+    description="topsis package",
+    long_description= readme(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/pypa/sampleproject",
+    packages=['topsis_py'],
+    install_requires=['pandas','statistics'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_py.topsis:topsis",
+            "topsis_cmd=topsis_py.topsis_cmd:topsis",
+        ]
+    },
+)
