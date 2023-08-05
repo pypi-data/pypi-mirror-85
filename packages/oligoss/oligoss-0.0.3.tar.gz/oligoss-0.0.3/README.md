ef174b8964e5941227e25c98d75234c8d4626c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package oligoss/0.0.3/oligoss-0.0.3.tar.gz

---
+++ packages/oligoss/oligoss-0.0.3.tar.gz/oligoss-0.0.3/README.md
@@ -0,0 +1,26 @@
+# Oligomer Soup Sequencing (OLIGOSS)
+
+OLIGOSS is a package for _de novo_ sequencing of linear oligomers from
+tandem mass spectrometry data.
+
+Full documentation can be found here: https://croningroup.gitlab.io/origins/oligoss/index.html
+
+__Note__: proper use of this package requires a good working knowledge of both
+the chemistry and analytical methods used to obtain results. It is __not__ a
+magic bullet to solve all of your oligomer sequencing needs! Please ensure that
+all raw mass spectrometry data input to the package is of good quality. All
+results should be checked and validated wherever possible, especially if running
+data from a new instrument and / or oligomer class.
+
+## Installation
+
+OLIGOSS is available through Pip (Python Package Index):
+
+```
+pip install oligoss --user
+
+```
+
+## Source Code
+
+Source code can be viewedon github: https://github.com/croningp/oligoss.git
---
+++ packages/oligoss/oligoss-0.0.3.tar.gz/oligoss-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+HERE = os.path.dirname(__file__)
+with open(os.path.join(HERE, "README.md")) as o:
+    readme = o.read()
+
+setup(
+    name='oligoss',
+    version='0.0.3',
+    description='de novo sequencer for heterogeneous oligomer mixtures',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='ALife Team, Cronin Group',
+    author_email='daviddoran20161@gmail.com',
+    packages=find_packages(),
+    install_requires=["bson", "psutil", "mzmlripper", "pymongo"],
+    entry_points={
+        "console_scripts": [
+            "oligoss=oligoss.__main__:main"
+        ]
+    },
+    license="AGPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: Unix",
+        "Development Status :: 3 - Alpha"
+    ],
+    include_package_data=True
+)
