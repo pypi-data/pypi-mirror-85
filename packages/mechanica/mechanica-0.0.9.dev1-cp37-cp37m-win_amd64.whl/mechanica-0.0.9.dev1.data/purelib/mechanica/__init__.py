f6327a107fbb53cf8606c22e078899b2a177e62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:47 +0000

    Add package mechanica/0.0.9.dev1/mechanica-0.0.9.dev1-cp37-cp37m-win_amd64.whl

---
+++ packages/mechanica/mechanica-0.0.9.dev1-cp37-cp37m-win_amd64.whl/mechanica-0.0.9.dev1.data/purelib/mechanica/__init__.py
@@ -0,0 +1,41 @@
+from ._mechanica import *
+
+__all__ = ['forces', 'math']
+
+close = Simulator.close
+run = Simulator.run
+show = Simulator.show
+irun = Simulator.irun
+
+
+def _plot_potential(p, show=True, *args, **kwargs):
+    import matplotlib.pyplot as plt
+    import numpy as n
+
+    xx = None
+
+    min = kwargs["min"] if "min" in kwargs else p.min
+    max = kwargs["max"] if "max" in kwargs else p.max
+    step = kwargs["step"] if "step" in kwargs else (max-min)/1000.
+    range = kwargs["range"] if "range" in kwargs else (min, max, step)
+
+    xx = n.arange(*range)
+
+    if p.flags & POTENTIAL_SCALED:
+        ri = kwargs["ri"]
+        rj = kwargs["rj"]
+
+        y = [p(x, ri, rj) for x in xx]
+
+    else:
+        y = [p(x) for x in xx]
+
+    p = plt.plot(xx, y)
+
+    if show:
+        plt.show()
+
+    return p
+
+
+_mechanica.Potential._set_dict_value("plot", _plot_potential)
