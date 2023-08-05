d071ce6bc3298b7e8200c5b7591ee0a878d96f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:26 +0000

    Add package cloudsigma/1.0/cloudsigma-1.0-py3-none-any.whl

---
+++ packages/cloudsigma/cloudsigma-1.0-py3-none-any.whl/cloudsigma/errors.py
@@ -0,0 +1,28 @@
+class ApiClientError(Exception):
+    def __init__(self, message, status_code=None, request_id=''):
+        super(ApiClientError, self).__init__(message, status_code, request_id)
+
+        self.message = message
+        self.status_code = status_code
+        self.request_id = request_id
+
+    def __repr__(self):
+        return "<{s.__class__.__name__}(" \
+               "'{s.message}', status_code={s.status_code}, " \
+               "request_id='{s.request_id}')>".format(s=self)
+
+
+class PermissionError(ApiClientError):
+    pass
+
+
+class ClientError(ApiClientError):
+    pass
+
+
+class ServerError(ApiClientError):
+    pass
+
+
+class AuthError(ApiClientError):
+    pass
