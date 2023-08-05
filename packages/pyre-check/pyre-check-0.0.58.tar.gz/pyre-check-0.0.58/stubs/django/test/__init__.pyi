96241ac4ebcbaa37743487a49213eb5626c1e004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:11 +0000

    Add package pyre-check/0.0.58/pyre-check-0.0.58.tar.gz

---
+++ packages/pyre-check/pyre-check-0.0.58.tar.gz/pyre-check-0.0.58/stubs/django/test/__init__.pyi
@@ -0,0 +1,16 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-unsafe
+
+from unittest import TestCase as TestCase
+
+from django.test.client import Client as Client
+from django.test.utils import override_settings as override_settings
+
+class SimpleTestCase(TestCase):
+    client: testing.client.base.TestClient = ...
+    @contextlib.contextmanager
+    def settings(self, **kwargs) -> None: ...
