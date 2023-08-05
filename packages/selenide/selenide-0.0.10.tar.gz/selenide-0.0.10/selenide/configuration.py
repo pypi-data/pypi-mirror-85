7eb697edb09284e40c106b04a0763e5b4321bfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package selenide/0.0.10/selenide-0.0.10.tar.gz

---
+++ packages/selenide/selenide-0.0.10.tar.gz/selenide-0.0.10/selenide/configuration.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+
+class Config:
+    # global setting
+    DRIVER: webdriver
+    # enum: headless or usual
+    MODE: str
+    # large time judgment conditions
+    TIMEOUT: int = 6
+    # base url
+    URL: str
+    # desktop size
+    WIDTH: int
+    HEIGHT: int
