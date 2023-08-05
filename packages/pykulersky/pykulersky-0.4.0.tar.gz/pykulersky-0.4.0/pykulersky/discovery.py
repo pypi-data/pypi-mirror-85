2f32ead795bfccf6600d9c07a2cfe509fa51b11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package pykulersky/0.4.0/pykulersky-0.4.0.tar.gz

---
+++ packages/pykulersky/pykulersky-0.4.0.tar.gz/pykulersky-0.4.0/pykulersky/discovery.py
@@ -0,0 +1,38 @@
+"""Device discovery code"""
+import logging
+
+from .exceptions import PykulerskyException
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def discover_bluetooth_devices(timeout=10):
+    """Returns nearby discovered bluetooth devices."""
+    _LOGGER.info("Starting scan for local bluetooth devices")
+
+    import pygatt
+    adapter = pygatt.GATTToolBackend()
+
+    devices = []
+    try:
+        adapter.start(reset_on_start=False)
+        for device in adapter.scan(timeout=timeout):
+            # There doesn't seem to be a way to distinguish these, so the user
+            # will have to select the correct bluetooth device
+            _LOGGER.info(
+                "Discovered %s: %s", device['address'], device['name'])
+
+            # Skip devices without a name. These devices include a name, and
+            # removing other devices reduces the noise
+            if device['name'] is not None:
+                devices.append(device)
+    except pygatt.BLEError as ex:
+        raise PykulerskyException() from ex
+    finally:
+        try:
+            adapter.stop()
+        except pygatt.BLEError as ex:
+            raise PykulerskyException() from ex
+
+    _LOGGER.info("Scan complete")
+    return devices
---
+++ packages/pykulersky/pykulersky-0.4.0.tar.gz/pykulersky-0.4.0/tests/test_discovery.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import pygatt
+import pytest
+
+from pykulersky import discover_bluetooth_devices, PykulerskyException
+
+
+def test_discover_devices(adapter):
+    """Test the CLI."""
+    def scan(*args, **kwargs):
+        """Simulate a scanning response"""
+        return [
+            {
+                'address': 'AA:BB:CC:11:22:33',
+                'name': 'Living Room',
+            },
+            {
+                'address': 'AA:BB:CC:44:55:66',
+                'name': 'Bedroom',
+            },
+            {
+                'address': 'DD:EE:FF:11:22:33',
+                'name': 'Other',
+            },
+            {
+                'address': 'DD:EE:FF:44:55:66',
+                'name': None,
+            },
+        ]
+
+    adapter.scan.side_effect = scan
+
+    devices = discover_bluetooth_devices(15)
+
+    assert len(devices) == 3
+    assert devices[0]['address'] == 'AA:BB:CC:11:22:33'
+    assert devices[0]['name'] == 'Living Room'
+    assert devices[1]['address'] == 'AA:BB:CC:44:55:66'
+    assert devices[1]['name'] == 'Bedroom'
+    assert devices[2]['address'] == 'DD:EE:FF:11:22:33'
+    assert devices[2]['name'] == 'Other'
+
+    adapter.start.assert_called_with(reset_on_start=False)
+    adapter.scan.assert_called_with(timeout=15)
+    adapter.stop.assert_called_once()
+
+
+def test_exception_wrapping(adapter):
+    """Test the CLI."""
+    def raise_exception(*args, **kwargs):
+        raise pygatt.BLEError("TEST")
+
+    adapter.scan.side_effect = raise_exception
+
+    with pytest.raises(PykulerskyException):
+        discover_bluetooth_devices()
+
+    adapter.scan.side_effect = None
+    adapter.stop.side_effect = raise_exception
+
+    with pytest.raises(PykulerskyException):
+        discover_bluetooth_devices()
