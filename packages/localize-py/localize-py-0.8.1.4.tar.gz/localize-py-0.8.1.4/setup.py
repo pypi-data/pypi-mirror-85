6ed6826ef3710bbe7d0be5eeeb39b0104512922f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:41 +0000

    Add package localize-py/0.8.1.4/localize-py-0.8.1.4.tar.gz

---
+++ packages/localize-py/localize-py-0.8.1.4.tar.gz/localize-py-0.8.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='localize-py',
+      version='0.8.1.4',
+      description='Localization simplified.',
+      packages=['localize_py'],
+      author_email='alexlaki1@yandex.ru',
+      python_requires='>=3.6',
+      zip_safe=False,
+      url='https://github.com/Alex-Blade/localize-py',
+      long_description=long_description,
+      long_description_content_type='text/markdown')
