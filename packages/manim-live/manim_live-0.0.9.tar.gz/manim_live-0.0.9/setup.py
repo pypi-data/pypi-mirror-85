41365994971bd979cc0e36df69801e57e8fe0946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package manim-live/0.0.9/manim_live-0.0.9.tar.gz

---
+++ packages/manim-live/manim_live-0.0.9.tar.gz/manim_live-0.0.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Education',
+    'Operating System :: MacOS :: MacOS X',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='manim_live',
+    version='0.0.9',
+    description='Program that uses manim and can display animations and be controlled live',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Cory Alexander Balaton',
+    author_email='cory.balaton@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='animations',
+    packages=find_packages(),
+    install_requires=['pyglet==1.5.8']
+)
