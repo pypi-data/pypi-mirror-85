ef4a2d3a8620c4f3ea91099cd8bdb01890b176d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:48 +0000

    Add package polog/0.0.7/polog-0.0.7-py3-none-any.whl

---
+++ packages/polog/polog-0.0.7-py3-none-any.whl/polog/base_settings.py
@@ -0,0 +1,17 @@
+class BaseSettings(object):
+    pool_size = 2
+    original_exceptions = False
+    level = 1
+    service_name = 'base'
+    errors_level = 2
+    delay_before_exit = 1.0
+    handlers = []
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self.__class__, key, value)
+
+    def __new__(cls, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(BaseSettings, cls).__new__(cls)
+        return cls.instance
