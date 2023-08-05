750d98e0c40c92fa47e8486c4be804ad4cbae242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package vaspvis/0.2.4/vaspvis-0.2.4.tar.gz

---
+++ packages/vaspvis/vaspvis-0.2.4.tar.gz/vaspvis-0.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='vaspvis',
+    version='0.2.4',
+    description='A highly flexible and customizable library for visualizing electronic structure data from VASP calculations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #  packages=['vaspvis'],
+    packages=find_packages(),
+    install_requires = ['pymatgen', 'matplotlib', 'numpy', 'pandas', 'scipy', 'ase', 'pychemia'],
+    url='https://github.com/DerekDardzinski/vaspvis',
+    authour='Derek Dardzinski',
+    authour_email='dardzinski.derek@gmail.com',
+    license='MIT',
+)
