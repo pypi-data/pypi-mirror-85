131c4f5d90fbc0b1698ed341b2001bff9db87b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:05 +0000

    Add package ixbrlparse/0.1.4/ixbrlparse-0.1.4.tar.gz

---
+++ packages/ixbrlparse/ixbrlparse-0.1.4.tar.gz/ixbrlparse-0.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ixbrlparse",
+    packages=find_packages(),
+    version="0.1.4",
+    author="David Kane",
+    author_email="david@dkane.net",
+    description="A python module for getting useful data out of ixbrl files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drkane/ixbrl-parse",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        'beautifulsoup4',
+        'lxml',
+        'word2number',
+    ],
+)
