310338da9b3a256a966d9d2cae7fb1d811f773fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:15 +0000

    Add package satmad/0.0.6/satmad-0.0.6-py2.py3-none-any.whl

---
+++ packages/satmad/satmad-0.0.6-py2.py3-none-any.whl/satmad/coordinates/ground_location.py
@@ -0,0 +1,14 @@
+# SatMAD: Satellite Mission Analysis and Design for Python
+#
+# Copyright (C) 2020 Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+Ground and Earth Location classes. These are very similar to
+:class:`astropy.coordinates.EarthLocation` class.
+
+"""
+
+# class GroundLocation():
+#
+# class EarthLocation(GroundLocation):
