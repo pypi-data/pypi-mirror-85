fcd040760a50b149210a708c32886ccba9373718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:32 +0000

    Add package roboticstoolbox-python/0.6.1/roboticstoolbox_python-0.6.1-cp37-cp37m-win_amd64.whl

---
+++ packages/roboticstoolbox-python/roboticstoolbox_python-0.6.1-cp37-cp37m-win_amd64.whl/roboticstoolbox/models/ETS/__init__.py
@@ -0,0 +1,10 @@
+from roboticstoolbox.models.ETS.Panda import Panda
+from roboticstoolbox.models.ETS.Frankie import Frankie
+from roboticstoolbox.models.ETS.Puma560 import Puma560
+
+
+__all__ = [
+    'Panda',
+    'Frankie',
+    'Puma560',
+]
