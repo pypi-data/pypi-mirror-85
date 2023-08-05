b01bb1cd382fb216903a4e6dc60b600cb124ac33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package talos/1.0/talos-1.0.tar.gz

---
+++ packages/talos/talos-1.0.tar.gz/talos-1.0/setup.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2020 Mikko Kotila
+
+DESCRIPTION = "Talos Hyperparameter Tuning for Keras"
+LONG_DESCRIPTION = """\
+Talos radically changes the ordinary deep learning workflow by
+fully automating hyperparameter tuning and model evaluation.
+Talos supports Tensorflow and Pytorch.
+
+Talos exposes backend functionality entirely and there is 
+no new syntax or templates to learn.
+"""
+
+DISTNAME = 'talos'
+MAINTAINER = 'Mikko Kotila'
+MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
+URL = 'http://autonom.io'
+LICENSE = 'MIT'
+DOWNLOAD_URL = 'https://github.com/autonomio/talos/'
+VERSION = '1.0'
+
+
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+install_requires = ['tensorflow>=2.0.0',
+                    'statsmodels>=0.11.0',
+                    'wrangle',
+                    'numpy',
+                    'pandas',
+                    'astetik',
+                    'sklearn',
+                    'tqdm',
+                    'chances',
+                    'kerasplotlib',
+                    'requests']
+
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=install_requires,
+          packages=['talos',
+                    'talos.scan',
+                    'talos.templates',
+                    'talos.utils',
+                    'talos.model',
+                    'talos.parameters',
+                    'talos.reducers',
+                    'talos.metrics',
+                    'talos.commands',
+                    'talos.logging',
+                    'talos.autom8'],
+
+          classifiers=['Intended Audience :: Science/Research',
+                       'Programming Language :: Python :: 2.7',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
+                       'License :: OSI Approved :: MIT License',
+                       'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+                       'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                       'Topic :: Scientific/Engineering :: Mathematics',
+                       'Operating System :: POSIX',
+                       'Operating System :: Unix',
+                       'Operating System :: MacOS',
+                       'Operating System :: Microsoft :: Windows :: Windows 10'])
---
+++ packages/talos/talos-1.0.tar.gz/talos-1.0/talos/utils/gpu_utils.py
@@ -0,0 +1,54 @@
+def parallel_gpu_jobs(allow_growth=True, fraction=.5):
+
+    '''Sets the max used memory as a fraction for tensorflow
+    backend
+
+    allow_growth :: True of False
+
+    fraction :: a float value (e.g. 0.5 means 4gb out of 8gb)
+
+    '''
+
+    import keras.backend as K
+    import tensorflow as tf
+
+    gpu_options = tf.compat.v1.GPUOptions(allow_growth=allow_growth,
+                                  per_process_gpu_memory_fraction=fraction)
+    config = tf.compat.v1.ConfigProto(gpu_options=gpu_options)
+    session = tf.compat.v1.Session(config=config)
+    tf.compat.v1.keras.backend.set_session(session)
+
+
+def multi_gpu(model, gpus=None, cpu_merge=True, cpu_relocation=False):
+
+    '''Takes as input the model, and returns a model
+    based on the number of GPUs available on the machine
+    or alternatively the 'gpus' user input.
+
+    NOTE: this needs to be used before model.compile() in the
+    model inputted to Scan in the form:
+
+    from talos.utils.gpu_utils import multi_gpu
+    model = multi_gpu(model)
+
+    '''
+
+    from tensorflow.keras.utils import multi_gpu_model
+
+    return multi_gpu_model(model,
+                           gpus=gpus,
+                           cpu_merge=cpu_merge,
+                           cpu_relocation=cpu_relocation)
+
+
+def force_cpu():
+
+    '''Force CPU on a GPU system
+    '''
+
+    import tensorflow.keras.backend as K
+    import tensorflow as tf
+
+    config = tf.compat.v1.ConfigProto(device_count={'GPU': 0})
+    session = tf.compat.v1.Session(config=config)
+    tf.compat.v1.keras.backend.set_session(session)
