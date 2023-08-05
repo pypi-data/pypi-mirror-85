a178dd327ac580e95260ab23bc2bf9f1b7acd80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package vrt-ud/3.7.163543/vrt_ud-3.7.163543.tar.gz

---
+++ packages/vrt-ud/vrt_ud-3.7.163543.tar.gz/vrt_ud-3.7.163543/vrt_ud/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vrt_ud.api.configurator_api import ConfiguratorApi
+from vrt_ud.api.data_api import DataApi
+from vrt_ud.api.monitor_api import MonitorApi
+from vrt_ud.api.predictor_api import PredictorApi
+from vrt_ud.api.reports_api import ReportsApi
