64a35ab6a885549537870174a11eccc4b880c3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package gormanian/0.0.2/gormanian-0.0.2.tar.gz

---
+++ packages/gormanian/gormanian-0.0.2.tar.gz/gormanian-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='gormanian',
+    packages=['gormanian'],
+    version='0.0.2',
+    license='MIT',
+    description='A simple library to convert a datetime date to use the superior Gormanian Calendar',
+    author='Ian Havelock',
+    author_email='ian@morrolan.com',
+    url='https://github.com/Morrolan/gormanian',
+    download_url='https://github.com/Morrolan/gormanian/archive/v0.0.1.tar.gz',
+    keywords=['Gormanian', 'Gormanuary', 'Dave Gorman'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
