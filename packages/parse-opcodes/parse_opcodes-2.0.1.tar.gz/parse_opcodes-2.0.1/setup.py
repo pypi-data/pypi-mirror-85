2f782c0f0673c576846e2f413a10607b03f4bd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:25 +0000

    Add package parse-opcodes/2.0.1/parse_opcodes-2.0.1.tar.gz

---
+++ packages/parse-opcodes/parse_opcodes-2.0.1.tar.gz/parse_opcodes-2.0.1/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="parse_opcodes",
+    version="2.0.1",
+    description="takes a list of instruction names, opcodes, and arguments and turns it into a 32-column spreadsheet, with each column representing a bit",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coreydockser/parse-opcodes.git",
+    author="Corey Dockser",
+    author_email="coreydockser@gmail.com",
+    license="BSD-3-Clause",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["parse_opcodes"],
+    include_package_data=True,
+    install_requires=["xlsxwriter", "openpyxl"],
+    entry_points={
+        "console_scripts": [
+            "parse_opcodes=parse.__main__:main",
+        ]
+    },
+)
