ea9f35c0610d86efdae3551ff151be8362fea2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package udata-gouvfr/2.2.3/udata_gouvfr-2.2.3-py2.py3-none-any.whl

---
+++ packages/udata-gouvfr/udata_gouvfr-2.2.3-py2.py3-none-any.whl/udata_gouvfr/settings.py
@@ -0,0 +1,15 @@
+"""
+Default settings for udata-gouvfr
+"""
+
+# Frontend banner parameters
+BANNER_ACTIVATED = False
+BANNER_HTML_CONTENT = ''
+
+# Static pages from github repo
+PAGES_GH_REPO_NAME = 'etalab/datagouvfr-pages'
+PAGES_REPO_BRANCH = 'master'
+
+# api.gouv.fr
+APIGOUVFR_URL = 'https://api.gouv.fr/api/v1/apis'
+APIGOUVFR_ALLOW_OPENNESS = ['open', 'semi_open']
