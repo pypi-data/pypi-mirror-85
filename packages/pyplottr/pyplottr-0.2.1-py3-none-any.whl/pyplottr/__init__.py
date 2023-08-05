66b0c1e92f13cf12c4506f1e6ee3d04d781a1fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:01 +0000

    Add package pyplottr/0.2.1/pyplottr-0.2.1-py3-none-any.whl

---
+++ packages/pyplottr/pyplottr-0.2.1-py3-none-any.whl/pyplottr/__init__.py
@@ -0,0 +1,8 @@
+"""
+https://github.com/toaarnio/pyplottr
+"""
+
+from .pyplottr import Figure, DummyFigure, plot, plot3d
+
+__version__ = "0.2.1"
+__all__ = ["Figure", "DummyFigure", "plot", "plot3d"]
