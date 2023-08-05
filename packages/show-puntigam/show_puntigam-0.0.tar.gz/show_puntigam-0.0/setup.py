e95e2b677cdb3eb4e962b567c5be201ead5f6c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package show-puntigam/0.0/show_puntigam-0.0.tar.gz

---
+++ packages/show-puntigam/show_puntigam-0.0.tar.gz/show_puntigam-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='show_puntigam',
+      version='0.0',
+      description='Python package to evaluate urban lidar datasets. SHOW is co-funded by the EU under the H2020 Research and Innovation Programme (grant agreement No 875530). Package is provided by Virtual Vehicle Research GmbH. Virtual Vehicle research center is Michael Hartmann of this package. Virtual Vehicle Research Team: Joachim Hillebrand, Michael Hartmann, Markus Schratter',
+      packages=['show_puntigam'],
+      author_email='michael.hartmann@v2c2.at',
+      zip_safe=False)
