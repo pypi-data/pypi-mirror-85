709681a70d40117d7cd246222c2243c8b10b4a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:47 +0000

    Add package kubragen/0.7.25/kubragen-0.7.25.tar.gz

---
+++ packages/kubragen/kubragen-0.7.25.tar.gz/kubragen-0.7.25/kubragen/builder.py
@@ -0,0 +1,95 @@
+from typing import TypeVar, Sequence, Any, Mapping, Dict
+
+from .exception import KGException, InvalidNameError, NotFoundError, InvalidOperationError
+from .jsonpatch import FilterJSONPatches, FilterJSONPatches_Apply
+from .kubragen import KubraGen
+from .object import ObjectItem, Object
+from .types import TBuild, TBuildItem
+
+BuilderT = TypeVar('BuilderT', bound='Builder')
+
+
+class Builder:
+    """
+    Base class for configuration builders.
+    """
+    kubragen: KubraGen
+    _jsonpatches: FilterJSONPatches
+    _objectnames: Dict[str, str]
+
+    def __init__(self, kubragen: KubraGen):
+        self.kubragen = kubragen
+        self._jsonpatches = None
+        self._objectnames = {}
+
+    def option_get(self, name: str) -> Any:
+        """
+        Get an option value.
+
+        :param name: the option name in dot format (config.service_port)
+        :return: the option value
+        :raises: :class:`kubragen.exception.OptionError`
+        :raises: :class:`kubragen.exception.TypeError`
+        """
+        raise NotImplementedError()
+
+    def jsonpatches(self, patches: FilterJSONPatches = None) -> BuilderT:
+        self._jsonpatches = patches
+        return self
+
+    def object_name(self, obj) -> str:
+        if obj not in self._objectnames:
+            raise NotFoundError('Object name "{}" not found'.format(obj))
+        return self._objectnames[obj]
+
+    def object_names(self) -> Mapping[str, str]:
+        return self._objectnames
+
+    def object_names_init(self, names: Mapping[str, str]) -> BuilderT:
+        self._objectnames.update(names)
+        return self
+
+    def object_names_change(self, names: Mapping[str, str]) -> BuilderT:
+        for name, value in names.items():
+            if name not in self._objectnames:
+                raise InvalidNameError('Unknown object name "{}"'.format(name))
+            self._objectnames[name] = value
+        return self
+
+    def internal_build(self, buildname: TBuild) -> Sequence[ObjectItem]:
+        raise NotImplementedError()
+
+    def build_names(self) -> Sequence[TBuild]:
+        return []
+
+    def build_names_required(self) -> Sequence[TBuild]:
+        return []
+
+    def builditem_names(self) -> Sequence[TBuildItem]:
+        return []
+
+    def ensure_build_names(self, *buildnames: TBuild) -> None:
+        """
+        Ensures that all required build names will be build.
+
+        :param buildnames: list of build names
+        :raises: :class:`kubragen.exception.InvalidOperationError`
+        """
+        if not set(self.build_names_required()).issubset(set(buildnames)):
+            raise InvalidOperationError('Missing required build names')
+
+    def build(self, *buildnames: TBuild) -> Sequence[ObjectItem]:
+        ret = []
+        for b in buildnames:
+            if b not in self.build_names():
+                raise KGException('Unknown build name: "{}"'.format(b))
+            ret.extend(self.internal_build(b))
+        return FilterJSONPatches_Apply(items=ret, jsonpatches=self._jsonpatches)
+
+    def build_all(self) -> Sequence[ObjectItem]:
+        return self.build(*self.build_names())
+
+    def _check_object_must_have(self, items: Sequence[ObjectItem], names: Sequence[TBuildItem], source: str):
+        for must_have in names:
+            if next((i for i in items if isinstance(i, Object) and i.name == must_have), None) is None:
+                raise InvalidNameError('Missing item "{}" in "{}"'.format(must_have, source))
---
+++ packages/kubragen/kubragen-0.7.25.tar.gz/kubragen-0.7.25/kubragen/provider.py
@@ -0,0 +1,77 @@
+import base64
+from typing import Union, Sequence, Optional
+
+import semver
+
+from .consts import PROVIDER_GENERIC, PROVIDERSVC_GENERIC, DEFAULT_KUBERNETES_VERSION
+from .exception import InvalidParamError
+from .object import ObjectItem
+from .types import TProvider, TProviderSvc
+
+
+class Provider:
+    """
+    Represents a target Kubernetes service provider to allow builders to customize objects.
+
+    :param provider: The service provider (Google, Amazon, etc)
+    :param service: The Kubernetes service in the service provider (GKE, EKS, etc)
+    :param kubernetes_version: Target Kubernetes version, to be parsed using the :mod:`semver` module
+    """
+    provider: TProvider
+    service: TProviderSvc
+    kubernetes_version: semver.VersionInfo
+
+    def __init__(self, provider: TProvider, service: TProviderSvc,
+                 kubernetes_version: Optional[Union[str, semver.VersionInfo]] = None):
+        self.provider = provider
+        self.service = service
+        if kubernetes_version is None:
+            kubernetes_version = DEFAULT_KUBERNETES_VERSION
+        if isinstance(kubernetes_version, semver.VersionInfo):
+            self.kubernetes_version = kubernetes_version
+        else:
+            try:
+                self.kubernetes_version = semver.VersionInfo.parse(kubernetes_version)
+            except Exception as e:
+                raise InvalidParamError(str(e)) from e
+
+    def secret_data_encode(self, data: Union[bytes, str]) -> str:
+        """
+        Encode bytes or str secret using the current provider.
+        By default encoding is done using base64, using the utf-8 charset.
+
+        :param data: Data to encode
+        :return: encoded secret
+        :raises: KGException
+        """
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        return self.secret_data_encode_bytes(data).decode("utf-8")
+
+    def secret_data_encode_bytes(self, data: bytes) -> bytes:
+        """
+        Encode bytes secret using the current provider.
+        By default encoding is done using base64, and raw bytes are returned
+
+        :param data: Data to encode
+        :return: encoded secret
+        :raises: KGException
+        """
+        return base64.b64encode(data)
+
+    def objects_check(self, objects: Sequence[ObjectItem]) -> Sequence[ObjectItem]:
+        """
+        Checks and modifies the list of objects for provider-specific properties.
+
+        :param objects: list of :class:`kubragen.object.ObjectItem`
+        :return: the modified list of objects.
+        """
+        return objects
+
+
+class Provider_Generic(Provider):
+    """
+    A generic provider.
+    """
+    def __init__(self, kubernetes_version: Optional[Union[str, semver.VersionInfo]] = None):
+        super().__init__(provider=PROVIDER_GENERIC, service=PROVIDERSVC_GENERIC, kubernetes_version=kubernetes_version)
