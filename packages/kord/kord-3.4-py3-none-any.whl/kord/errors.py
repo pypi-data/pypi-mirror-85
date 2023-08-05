cf656c432f90cce1bd7b8a3de7eee508a057b31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:53 +0000

    Add package kord/3.4/kord-3.4-py3-none-any.whl

---
+++ packages/kord/kord-3.4-py3-none-any.whl/kord/errors.py
@@ -0,0 +1,17 @@
+class InvalidInstrument(Exception):
+    pass
+
+class InvalidScale(Exception):
+    pass
+
+class InvalidChord(Exception):
+    pass
+
+class InvalidNote(Exception):
+    pass
+
+class InvalidAlteration(Exception):
+    pass
+
+class InvalidOctave(Exception):
+    pass
