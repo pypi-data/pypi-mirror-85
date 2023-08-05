0b62a8473bda1c9184f5001787bff164b3db73d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package onyx-mclib/1.1.3/onyx-mclib-1.1.3.tar.gz

---
+++ packages/onyx-mclib/onyx-mclib-1.1.3.tar.gz/onyx-mclib-1.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="onyx-mclib",
+    version="1.1.3",
+    description="A python library to create minecraft datapacks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Double-Felix/Onyx/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
