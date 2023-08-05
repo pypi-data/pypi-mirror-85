c414454e28529ae259998b15f8f8523205056c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:32 +0000

    Add package 123TestUpload/1.0/123TestUpload-1.0.tar.gz

---
+++ packages/123TestUpload/123TestUpload-1.0.tar.gz/123TestUpload-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='123TestUpload',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['123TestUpload'],
+      zip_safe=False)
