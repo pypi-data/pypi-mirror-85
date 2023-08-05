e2c45614762f002895d4e70578f39380ce760c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:34 +0000

    Add package deepspeed/0.3.1.dev5/deepspeed-0.3.1.dev5-py3-none-any.whl

---
+++ packages/deepspeed/deepspeed-0.3.1.dev5-py3-none-any.whl/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.3.1.dev5'
+git_hash='24827fc'
+git_branch='jeffra/jit_extensions_v2'
+installed_ops={'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'utils': False}
+compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'utils': True}
+torch_info={'version': '1.2', 'cuda_version': '10.0'}
