d33942f815e136170a7c46f61e83dd886e0e089d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:20 +0000

    Add package Ziim.0.0.2/0.0.2/Ziim.0.0.2-0.0.2.tar.gz

---
+++ packages/Ziim.0.0.2/Ziim.0.0.2-0.0.2.tar.gz/Ziim.0.0.2-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Ziim.0.0.2",  # Replace with your own username
+    version="0.0.2",
+    author="Sanix-darker",
+    author_email="s4nixd@gmail.com",
+    description="Let your CLI find available solutions for errors / exceptions online on commands you hit, for you, "
+                "no need open a Browser. and find something yourself",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sanix-darker/ziim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
