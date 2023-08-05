d5e635061dfbb31971e1b4c497f9272cc2567c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:11 +0000

    Add package jardin/0.20.1/jardin-0.20.1.tar.gz

---
+++ packages/jardin/jardin-0.20.1.tar.gz/jardin-0.20.1/jardin/database/database_config.py
@@ -0,0 +1,32 @@
+from future.standard_library import install_aliases
+install_aliases()
+
+import sys
+if sys.version_info[0] >= 3:
+    unicode = str
+
+
+from urllib.parse import urlparse
+
+
+class UnknownConfigFormat(Exception): pass
+
+
+class DatabaseConfig(object):
+
+    lowercase_columns = False
+
+    def __init__(self, config):
+        if isinstance(config, str) or isinstance(config, unicode):
+            db = urlparse(config)
+            self.scheme = db.scheme
+            self.username = db.username
+            self.password = db.password
+            self.host = db.hostname
+            self.port = db.port
+            self.database = db.path[1:]
+        elif isinstance(config, dict):
+            for (k, v) in config.items():
+                setattr(self, k, v)
+        else:
+            raise UnknownConfigFormat(type(config), config)
