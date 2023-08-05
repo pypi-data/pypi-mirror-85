97871959826114c71c124fb745d81aa1b41a213d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:55 +0000

    Add package python-terminal-alpha/0.2.0/python_terminal_alpha-0.2.0.tar.gz

---
+++ packages/python-terminal-alpha/python_terminal_alpha-0.2.0.tar.gz/python_terminal_alpha-0.2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='python_terminal_alpha',
+      version='0.2.0',
+      description='Alpha version of terminal using python os',
+      packages=['python_terminal_alpha'],
+      author_email='kuz.damir.01@gmail.com',
+      zip_safe=False)
