a8c1152eab30b5266e034c6afa5324a597bffec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package cachetools-ext/0.0.4/cachetools_ext-0.0.4.tar.gz

---
+++ packages/cachetools-ext/cachetools_ext-0.0.4.tar.gz/cachetools_ext-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+TEST_REQUIRES = ["pytest", "pytest-cov"]
+DEV_REQUIRES = TEST_REQUIRES + ["pre-commit", "pylint", "black"]
+
+setup(
+    name="cachetools_ext",
+    version="0.0.4",
+    author="Oliver Rice",
+    url="https://github.com/olirice/cachetools_ext",
+    packages=find_packages(exclude=("test")),
+    install_requires=[],
+    extras_require={"dev": DEV_REQUIRES, "test": TEST_REQUIRES},
+)
