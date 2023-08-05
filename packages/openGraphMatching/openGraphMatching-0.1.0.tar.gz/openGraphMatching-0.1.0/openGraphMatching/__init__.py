69edd1ff3254731ca5f708a3503831adfc70a0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:37 +0000

    Add package openGraphMatching/0.1.0/openGraphMatching-0.1.0.tar.gz

---
+++ packages/openGraphMatching/openGraphMatching-0.1.0.tar.gz/openGraphMatching-0.1.0/openGraphMatching/__init__.py
@@ -0,0 +1,13 @@
+from openGraphMatching.SubGraphMatcher import SubGraphMatcher
+from openGraphMatching.NaiveMatcher import NaiveMatcher
+from openGraphMatching.GQLMatcher import GQLMatcher
+from openGraphMatching.CECIMatcher import CECIMatcher
+from openGraphMatching.filter_neural_matcher import FilterNeuralMatcher
+__all__ = [
+    'SubGraphMatcher',
+    'NaiveMatcher',
+    'GQLMatcher',
+    'CECIMatcher',
+    'FilterNeuralMatcher'
+]
+
