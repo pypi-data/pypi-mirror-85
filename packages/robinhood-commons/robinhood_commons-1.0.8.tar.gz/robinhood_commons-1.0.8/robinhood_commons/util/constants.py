f16d7b018bbc0d6274a3851229d9fd786f2c21fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:23 +0000

    Add package robinhood-commons/1.0.8/robinhood_commons-1.0.8.tar.gz

---
+++ packages/robinhood-commons/robinhood_commons-1.0.8.tar.gz/robinhood_commons-1.0.8/robinhood_commons/util/constants.py
@@ -0,0 +1,19 @@
+
+MAIN_EMAIL: str = 'mhowell234@gmail.com'
+BASE_PATH: str = './'
+BASE_PATH3: str = '/Users/mhowell234/code/robinhood/'
+BASE_PATH2: str = '/opt/code/robinhood/'
+LOG_DIR: str = f'{BASE_PATH}output/logs/'
+TMP_DIR = f'{BASE_PATH}tmp/'
+STOCKS_FILE_PATH: str = f'{TMP_DIR}stocks.json'
+
+TESTING: bool = True
+DEBUG: bool = False
+F_IT_MODE: bool = False
+PROFILE_INFO: bool = False
+RANDOM_INVESTORS: int = 7
+BUY_ONE_SELL_ONE_MODE: bool = True
+
+
+
+
