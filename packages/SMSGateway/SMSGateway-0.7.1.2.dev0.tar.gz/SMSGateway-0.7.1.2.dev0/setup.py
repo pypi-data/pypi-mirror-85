4d65b35951bd7674887b0222961e57420c142a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package SMSGateway/0.7.1.2.dev0/SMSGateway-0.7.1.2.dev0.tar.gz

---
+++ packages/SMSGateway/SMSGateway-0.7.1.2.dev0.tar.gz/SMSGateway-0.7.1.2.dev0/setup.py
@@ -0,0 +1,25 @@
+# from distutils.core import setup
+import setuptools
+
+setuptools.setup(
+    name='SMSGateway',
+    version='0.7.1.2dev0',
+    url="https://github.com/Craeckie/SMSGateway",
+    description="Forwards messages via SMS from and to instant messengers",
+    packages=setuptools.find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+
+    install_requires=[
+        "appdirs",
+        "telethon",
+        "fbchat",
+        "arrow",
+        'imapclient',
+        'cryptography',
+        'bs4',
+        'slackclient',
+        'asyncio',
+    ]
+)
