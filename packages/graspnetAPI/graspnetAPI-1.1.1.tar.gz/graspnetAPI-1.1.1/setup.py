f496cf5af9ccf27ed5586fbf9fa232f9df6871a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package graspnetAPI/1.1.1/graspnetAPI-1.1.1.tar.gz

---
+++ packages/graspnetAPI/graspnetAPI-1.1.1.tar.gz/graspnetAPI-1.1.1/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+from setuptools import find_packages
+from setuptools.command.install import install
+import os
+
+class PostInstallCmd(install):
+    def run(self):
+        os.system('python -m pip install ./meshpy')
+        os.system('python -m pip install ./dexnet')
+        os.system('cd graspnms\npython -m pip install .')
+        install.run(self)
+
+os.system('python -m pip install cython numpy')
+
+setup(
+    name='graspnetAPI',
+    version='1.1.1',
+    description='graspnet API',
+    author='Hao-Shu Fang, Chenxi Wang, Minghao Gou',
+    author_email='gouminghao@gmail.com',
+    url='https://graspnet.net',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'cython',
+        'scipy',
+        'transforms3d==0.3.1',
+        'open3d>=0.8.0.0',
+        'trimesh==3.8.4',
+        'tqdm',
+        'Pillow==7.2.0',
+        'opencv-python',
+        'pillow',
+        'matplotlib',
+        'pywavefront',
+        'trimesh',
+        'scikit-image',
+        'autolab_core',
+        'autolab-perception',
+        'cvxopt',
+        'dill',
+        'h5py',
+        'sklearn',
+    ],
+    cmdclass={
+        'install': PostInstallCmd
+    }
+)
