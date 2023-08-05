8bccac4155428f4fb7d0387756f140de4527c7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:55 +0000

    Add package k8kat/0.0.217/k8kat-0.0.217-py3-none-any.whl

---
+++ packages/k8kat/k8kat-0.0.217-py3-none-any.whl/k8kat/utils/main/types.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+from typing_extensions import TypedDict
+
+
+class UsageDict(TypedDict):
+  cpu: Optional[str]
+  memory: Optional[str]
+
+
+class ContainerMetricsDict(TypedDict):
+  usage: UsageDict
+
+
+class PodMetricsDict(TypedDict):
+  containers: List[ContainerMetricsDict]
+
+
+class NodeMetricsDict(TypedDict):
+  usage: UsageDict
+
