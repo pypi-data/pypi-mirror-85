ba82cc483444241051b56d1699de04089bd4fdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package Sergi0/0.1/Sergi0-0.1.tar.gz

---
+++ packages/Sergi0/Sergi0-0.1.tar.gz/Sergi0-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Sergi0',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Sergi0'],
+      author = 'Y.A alias sergio',
+      zip_safe=False)
