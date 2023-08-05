eedee9040994bb50b8b8ab4996702c4c293048cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:08 +0000

    Add package wwpolytechnique-distributions/0.5/wwpolytechnique_distributions-0.5.tar.gz

---
+++ packages/wwpolytechnique-distributions/wwpolytechnique_distributions-0.5.tar.gz/wwpolytechnique_distributions-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wwpolytechnique_distributions',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['wwpolytechnique_distributions'],
+      zip_safe=False)
