0ca269309c26e5bc5edc2e8866c390bb33ff18f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package cryocon/3.1.2/cryocon-3.1.2.tar.gz

---
+++ packages/cryocon/cryocon-3.1.2.tar.gz/cryocon-3.1.2/cryocon/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '3.1.2'
+
+__all__ = ['CryoCon']
+
+from .cryocon import CryoCon
---
+++ packages/cryocon/cryocon-3.1.2.tar.gz/cryocon-3.1.2/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+requirements = ['connio>=0.2']
+
+with open("README.md") as f:
+    description = f.read()
+
+
+setup(
+    name='cryocon',
+    author="Jairo Moldes",
+    author_email='jmoldes@cells.es',
+    version='3.1.2',
+    description="CryCon library",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    extras_require={
+        'tango': ['PyTango>=9'],
+        'simulator': ['sinstruments>=1', 'scpi-protocol>=0.2']
+    },
+    entry_points={
+        'console_scripts': [
+            'CryoCon = cryocon.tango.server:main [tango]',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    install_requires=requirements,
+    license="LGPLv3",
+    include_package_data=True,
+    keywords='cryocon, library, tango, simulator',
+    packages=find_packages(),
+    python_requires=">=3.5",
+    url='https://github.com/ALBA-Synchrotron/cryocon')
