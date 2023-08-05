b4b8ec3706707133859429914a5928aa5a269ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package netbox-animal-sounds-test/0.0.3/netbox-animal-sounds-test-0.0.3.tar.gz

---
+++ packages/netbox-animal-sounds-test/netbox-animal-sounds-test-0.0.3.tar.gz/netbox-animal-sounds-test-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='netbox-animal-sounds-test',
+    version='0.0.3',
+    description='An example NetBox plugin',
+    long_description=long_description,
+    url='https://github.com/netbox-community/netbox-animal-sounds',
+    author='Jeremy Stretch',
+    license='Apache 2.0',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+)
