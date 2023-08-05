ed68fb24afe0697a1cf2351294424bd2e574135b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:43 +0000

    Add package image-dehazer/0.0.3/image_dehazer-0.0.3.tar.gz

---
+++ packages/image-dehazer/image_dehazer-0.0.3.tar.gz/image_dehazer-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+     name='image_dehazer',
+     version='0.0.3',
+     author="utkarsh-deshmukh",
+     author_email="utkarsh.deshmukh@gmail.com",
+     description="remove haze from images",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Utkarsh-Deshmukh/Single-Image-Dehazing-Python",
+     download_url="https://github.com/Utkarsh-Deshmukh/Single-Image-Dehazing-Python/archive/master.zip",
+     install_requires=['numpy==1.19.0', 'opencv-python', 'scipy'],
+     license='MIT',
+     keywords=['Single Image Dehazing', 'Haze Removal'],
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
