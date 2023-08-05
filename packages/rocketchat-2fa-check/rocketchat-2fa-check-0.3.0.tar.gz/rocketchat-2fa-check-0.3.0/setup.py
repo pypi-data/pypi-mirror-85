ad5ef58bb7712dbe46559e5270a8efc4472c714f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package rocketchat-2fa-check/0.3.0/rocketchat-2fa-check-0.3.0.tar.gz

---
+++ packages/rocketchat-2fa-check/rocketchat-2fa-check-0.3.0.tar.gz/rocketchat-2fa-check-0.3.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rocketchat_2fa_check']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click-pathlib>=2020.3.13,<2021.0.0',
+ 'click>=7.0,<8.0',
+ 'pymongo>=3.8,<4.0',
+ 'requests>=2.22,<3.0']
+
+entry_points = \
+{'console_scripts': ['rc-check-2fa = rocketchat_2fa_check.__main__:_run_main']}
+
+setup_kwargs = {
+    'name': 'rocketchat-2fa-check',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ulrich Petri',
+    'author_email': 'github@ulo.pe',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
