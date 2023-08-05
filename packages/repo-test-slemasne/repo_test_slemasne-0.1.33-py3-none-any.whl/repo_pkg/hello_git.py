0228e8501931786f1f9340927a180069d147419c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package repo-test-slemasne/0.1.33/repo_test_slemasne-0.1.33-py3-none-any.whl

---
+++ packages/repo-test-slemasne/repo_test_slemasne-0.1.33-py3-none-any.whl/repo_pkg/hello_git.py
@@ -0,0 +1,14 @@
+import datetime
+import os
+
+now = datetime.datetime.now()
+test_string = f"Hello from slemasne at {now} in London"
+
+print(test_string)
+print("Now making another change - test")
+
+
+test = "test test test"
+
+
+test_dict = {"test1": 2, "test2": 3}
