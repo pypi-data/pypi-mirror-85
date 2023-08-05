c81a375f6e7c8711d4f4be6c50fe849b9586c70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:13 +0000

    Add package cozypy/1.2.3/cozypy-1.2.3-py3.9.egg

---
+++ packages/cozypy/cozypy-1.2.3-py3.9.egg/cozypy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+'''
+Provides authentification and row access to Cozytouch modules.
+'''
+name = 'cozypy'
+__version__ = '1.2.3'
+__all__ = ['cozypy']
+
+from .client import CozytouchClient
