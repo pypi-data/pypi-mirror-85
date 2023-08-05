87e0f1fe5210a5a6a2b6679d14a6c4e450a1525c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:10 +0000

    Add package catalog-client/1.0.1/catalog_client-1.0.1-py2.py3-none-any.whl

---
+++ packages/catalog-client/catalog_client-1.0.1-py2.py3-none-any.whl/catalog_client/__init__.py
@@ -0,0 +1,7 @@
+from .Selections import Selections
+from .Catalog import Catalog, FatalError
+
+import catalog_cli
+def main():
+    """Entry point for the application script"""
+    catalog_cli.main()
