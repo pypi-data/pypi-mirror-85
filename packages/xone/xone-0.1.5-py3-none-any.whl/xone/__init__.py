540f493184c6b664b814865352d01c8f375049e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package xone/0.1.5/xone-0.1.5-py3-none-any.whl

---
+++ packages/xone/xone-0.1.5-py3-none-any.whl/xone/__init__.py
@@ -0,0 +1,12 @@
+"""Frequently used functions for financial data analysis"""
+
+__version__ = '0.1.5'
+
+__submodules__ = [
+    'utils',
+    'calendar',
+    'files',
+    'procs',
+    'plots',
+    'xql',
+]
