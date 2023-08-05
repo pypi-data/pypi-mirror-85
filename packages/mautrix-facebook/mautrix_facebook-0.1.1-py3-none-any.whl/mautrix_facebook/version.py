db8a64800f159d8361a0fe30aba90d229481c66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:08 +0000

    Add package mautrix-facebook/0.1.1/mautrix_facebook-0.1.1-py3-none-any.whl

---
+++ packages/mautrix-facebook/mautrix_facebook-0.1.1-py3-none-any.whl/mautrix_facebook/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.1.1'
+git_revision = '594ed830'
+version = '0.1.1'
+linkified_version = '[0.1.1](https://github.com/tulir/mautrix-facebook/releases/tag/v0.1.1)'
