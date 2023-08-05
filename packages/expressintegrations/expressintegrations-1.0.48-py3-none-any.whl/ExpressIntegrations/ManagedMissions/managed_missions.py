8b1404b80df5f474756cdd0a64940ba75d7e8228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:55 +0000

    Add package expressintegrations/1.0.48/expressintegrations-1.0.48-py3-none-any.whl

---
+++ packages/expressintegrations/expressintegrations-1.0.48-py3-none-any.whl/ExpressIntegrations/ManagedMissions/managed_missions.py
@@ -0,0 +1,38 @@
+from ..HTTP.Requests import *
+
+
+class managed_missions:
+  def __init__(self, api_key):
+    self.headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    self.headers['Authorization'] = f"Bearer {access_token}"
+    self.base_url = 'https://app.managedmissions.com/API'
+
+  # Contains utilities for interacting with the Managed Missions api
+  def get_active_mission_trips(self, **kw):
+    post_url = f"{self.base_url}/MissionTripAPI/ActiveTrips"
+    result = get(post_url, self.headers, params=kw)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve active mission trips. Result: {result}")
+    return result
+
+  def get_mission_trips(self, **kw):
+    post_url = f"{self.base_url}/MissionTripAPI/List"
+    result = get(post_url, self.headers, params=kw)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve mission trips. Result: {result}")
+    return result
+
+  def get_trip_members(self, mission_trip_id, **kw):
+    post_url = f"{self.base_url}/MembersAPI/GetMembersOfTrip"
+    kw['MissionTripId'] = mission_trip_id
+    result = get(post_url, self.headers, params=kw)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve trip members. Result: {result}")
+    return result
+
+  def get_contributions(self, **kw):
+    post_url = f"{self.base_url}/ContributionAPI/List"
+    result = get(post_url, self.headers, params=kw)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve contributions. Result: {result}")
+    return result
