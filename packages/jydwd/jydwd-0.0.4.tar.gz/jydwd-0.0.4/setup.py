c609a2c5459480b9cdac7fc3ac8828dbbd48ddaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:48 +0000

    Add package jydwd/0.0.4/jydwd-0.0.4.tar.gz

---
+++ packages/jydwd/jydwd-0.0.4.tar.gz/jydwd-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jydwd", # Replace with your own username
+    version="0.0.4",
+    author="jiangyd",
+    author_email="962584902@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    install_requires=["requests>=2.23.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
