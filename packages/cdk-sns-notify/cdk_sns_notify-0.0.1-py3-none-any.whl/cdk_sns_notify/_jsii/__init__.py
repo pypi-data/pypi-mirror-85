fbb3c83a1c238fecd720ffe85720b4ef5d83ea9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:29 +0000

    Add package cdk-sns-notify/0.0.1/cdk_sns_notify-0.0.1-py3-none-any.whl

---
+++ packages/cdk-sns-notify/cdk_sns_notify-0.0.1-py3-none-any.whl/cdk_sns_notify/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_cloudwatch_actions._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-sns-notify", "0.0.1", __name__[0:-6], "cdk-sns-notify@0.0.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
