023eda25f07965fd48b152be8fafcde4545a51cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package mautrix-twitter/0.1.1/mautrix-twitter-0.1.1.tar.gz

---
+++ packages/mautrix-twitter/mautrix-twitter-0.1.1.tar.gz/mautrix-twitter-0.1.1/mautrix_twitter/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.1.1'
+git_revision = 'c60c0ce2'
+version = '0.1.1'
+linkified_version = '[0.1.1](https://github.com/tulir/mautrix-twitter/releases/tag/v0.1.1)'
