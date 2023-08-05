027de7b163633c8c7b2e8a2257c1094c4b702995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:25 +0000

    Add package numetrics/0.0.2/numetrics-0.0.2.tar.gz

---
+++ packages/numetrics/numetrics-0.0.2.tar.gz/numetrics-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+ 
+setup(
+  name='numetrics',
+  version='0.0.2',
+  description='Package For Numerai users to check their models performance',
+  url='',  
+  author='Tim Thabt',
+  author_email='haithamthabt@hotmail.com',
+  license='MIT', 
+  keywords='numetrics, numerai', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
+
