10ef08869bcfe855156fa92667eaf8fa602cc08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:29 +0000

    Add package topsis-kritika-101983031/0.7/TOPSIS_kritika_101983031-0.7.tar.gz

---
+++ packages/topsis-kritika-101983031/TOPSIS_kritika_101983031-0.7.tar.gz/TOPSIS_kritika_101983031-0.7/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'TOPSIS_kritika_101983031',         
+  packages = ['TOPSIS_kritika_101983031'],   
+  version = '0.7',
+  license='MIT',
+  description = 'This package allows you to implement topsis approach in python',
+  author = 'kritika aggarwal',                  
+  author_email = 'kritikaaggarwal1621@gmail.com',      
+  url = 'https://github.com/kritsid/TOPSIS-kritika-101983031', 
+  download_url = 'https://github.com/kritsid/TOPSIS-kritika-101983031/archive/v_01.tar.gz',  
+  keywords = ['TOPSIS'],   
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
