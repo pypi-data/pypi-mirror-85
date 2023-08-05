be2000bf2b960325193282d9c37f586a7068466c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:55 +0000

    Add package python-terminal-dk/0.1.0/python_terminal_dk-0.1.0.tar.gz

---
+++ packages/python-terminal-dk/python_terminal_dk-0.1.0.tar.gz/python_terminal_dk-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='python_terminal_dk',
+      version='0.1.0',
+      description='Alpha version of terminal using python os',
+      packages=['python_terminal_dk'],
+      author_email='kuz.damir.01@gmail.com',
+      zip_safe=False)
