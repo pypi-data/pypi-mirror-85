db6f6ca39e46e463f3d69d009366517e3e8f39b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:20 +0000

    Add package cgmquantify/0.4/cgmquantify-0.4.tar.gz

---
+++ packages/cgmquantify/cgmquantify-0.4.tar.gz/cgmquantify-0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    ld = f.read()
+    
+
+setup(name='cgmquantify',
+      version='0.4',
+      description='Quantifying glucose and glucose variability from CGM devices',
+      long_description= ld,
+      long_description_content_type= 'text/markdown',
+      url='https://github.com/brinnaebent/cgmquantify',
+      download_url='https://github.com/brinnaebent/cgmquantify/archive/0.4.tar.gz',
+      author='Brinnae Bent',
+      author_email='bmbent@ncsu.edu',
+      license='MIT',
+      packages=['cgmquantify'],
+      install_requires=['pandas','numpy','matplotlib','datetime','statsmodels',
+                        ],
+      zip_safe=False)
