4d23e1593db56b8b26f1550f598c3537ab2b5bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:54 +0000

    Add package zool/1.1.1/zool-1.1.1.tar.gz

---
+++ packages/zool/zool-1.1.1.tar.gz/zool-1.1.1/setup.py
@@ -0,0 +1,33 @@
+""" setup
+"""
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+DESCRIPTION = """
+A cli for reviewing PRs and zuul's progress
+""".strip()
+
+DEPENDENCIES = ["jinja2", "pyyaml", "requests", "pygments", "lxml"]
+TEST_DEPENDENCIES = []
+
+
+VERSION = "1.1.1"
+URL = "https://github.com/cidrblock/zool"
+
+setup(
+    name="zool",
+    version=VERSION,
+    author="cidrblock",
+    packages=["zool"],
+    entry_points={
+        "console_scripts": [
+            "zool = zool.zool:main",
+        ],
+    },
+    install_requires=DEPENDENCIES,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+)
