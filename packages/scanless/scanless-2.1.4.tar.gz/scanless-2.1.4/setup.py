912bb74d42d6f72f8be6b46bcd4e26fa7d4fa86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:21 +0000

    Add package scanless/2.1.4/scanless-2.1.4.tar.gz

---
+++ packages/scanless/scanless-2.1.4.tar.gz/scanless-2.1.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='scanless',
+    packages=[
+        'scanless',
+        'scanless.static'
+    ],
+    package_data = {
+        'scanless.static': ['*.txt']
+    },
+    version='2.1.4',
+    description='An online port scan scraper.',
+    license='Unlicense',
+    url='https://github.com/vesche/scanless',
+    author='Austin Jackson',
+    author_email='vesche@protonmail.com',
+    entry_points={
+        'console_scripts': [
+            'scanless = scanless.cli:main',
+        ]
+    },
+    install_requires=[
+        'beautifulsoup4',
+        'crayons',
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Information Technology',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Security'
+    ]
+)
