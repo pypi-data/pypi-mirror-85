b6682c79a0e1da0f97afbf92b7e47cdc74d29ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:32 +0000

    Add package testsupersecret/20.10.post1/testsupersecret-20.10.post1.tar.gz

---
+++ packages/testsupersecret/testsupersecret-20.10.post1.tar.gz/testsupersecret-20.10.post1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    #name="test_super_secret",
+    name="testsupersecret",
+    version="20.10.r1",
+    author="Ondrej",
+    description="testing pip version 3",
+    packages=find_packages(),
+)
