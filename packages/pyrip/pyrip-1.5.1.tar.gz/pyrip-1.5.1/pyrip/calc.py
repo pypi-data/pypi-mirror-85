d086ff46b0e85c27e271d4f7ec7fa0035f665355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:52 +0000

    Add package pyrip/1.5.1/pyrip-1.5.1.tar.gz

---
+++ packages/pyrip/pyrip-1.5.1.tar.gz/pyrip-1.5.1/pyrip/calc.py
@@ -0,0 +1,13 @@
+import string
+from .util import run_command
+
+
+def calculate(infiles, outfile, expr):
+    args = ['gdal_calc.py']
+    asciis = string.ascii_uppercase
+    for idx, infile in enumerate(infiles):
+        args.extend(['-{}'.format(asciis[idx]), infile])
+    args.append('--outfile={}'.format(outfile))
+    args.append('--calc="{}"'.format(expr))
+    run_command(args)
+    return outfile
