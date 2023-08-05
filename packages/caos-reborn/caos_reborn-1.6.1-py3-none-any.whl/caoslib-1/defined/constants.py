d595fd8526234f4c6547671a519ac0bef3a0ae8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package caos-reborn/1.6.1/caos_reborn-1.6.1-py3-none-any.whl

---
+++ packages/caos-reborn/caos_reborn-1.6.1-py3-none-any.whl/caoslib-1/defined/constants.py
@@ -0,0 +1,33 @@
+# tasks data path
+CAOS_DIR = '/Users/hi_melnikov/Desktop/hse/caos/tasks'
+
+# links
+SETTINGS = 'Settings'
+SUMMARY = 'Summary'
+SUBMISSIONS = 'Submissions'
+STANDINGS = 'User standings'
+CLAR = 'Submit clar'
+CLARS = 'Clars'
+
+# submission status
+OK = 'OK'
+REVIEW = 'Pending review'
+NOT_SUBMITTED = 'Not submitted'
+
+CLANG_FORMAT_STYLE_STRING = """\"{
+    Language: Cpp,
+    BasedOnStyle: Google,
+    IndentWidth: 4,
+    UseTab: Never,
+    NamespaceIndentation: All,
+    ColumnLimit: 80,
+    AccessModifierOffset: -4,
+    AlignAfterOpenBracket: AlwaysBreak,
+    AlignOperands: false,
+    AlwaysBreakTemplateDeclarations: Yes,
+    BinPackArguments: false,
+    BinPackParameters: false,
+    AllowShortFunctionsOnASingleLine: Empty,
+    BreakBeforeBraces: Custom,
+    BraceWrapping: { AfterEnum: true, AfterStruct: true }
+}\""""
