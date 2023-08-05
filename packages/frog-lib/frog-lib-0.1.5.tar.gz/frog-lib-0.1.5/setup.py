59234a4e41636266abdf8451b8d022eacee58172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package frog-lib/0.1.5/frog-lib-0.1.5.tar.gz

---
+++ packages/frog-lib/frog-lib-0.1.5.tar.gz/frog-lib-0.1.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['frog_lib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'frog-lib',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Frog Dev',
+    'author_email': 'dev@frog-mining.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
