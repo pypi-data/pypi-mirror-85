949c037437ffb56b331af8ea9d5a8ffba29d4273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package morph-api-tomcart/0.1.4/morph_api_tomcart-0.1.4.tar.gz

---
+++ packages/morph-api-tomcart/morph_api_tomcart-0.1.4.tar.gz/morph_api_tomcart-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='morph_api_tomcart',
+    version='0.1.4',
+    packages=['morph_api_tomcart'],
+    url='',
+    license='',
+    install_requires=[
+        'requests'
+    ],
+    author='Ricky Nelson',
+    author_email='',
+    description=''
+)
