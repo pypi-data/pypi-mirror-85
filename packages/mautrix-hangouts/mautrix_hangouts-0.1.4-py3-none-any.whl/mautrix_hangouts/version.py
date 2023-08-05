aa8c0e2d078c25ee214df527ac731a14d17841e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package mautrix-hangouts/0.1.4/mautrix_hangouts-0.1.4-py3-none-any.whl

---
+++ packages/mautrix-hangouts/mautrix_hangouts-0.1.4-py3-none-any.whl/mautrix_hangouts/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.1.4'
+git_revision = 'c74e2512'
+version = '0.1.4'
+linkified_version = '[0.1.4](https://github.com/tulir/mautrix-hangouts/releases/tag/v0.1.4)'
