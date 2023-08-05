7afdca48a205fdc509f4dcf61e06cc6a2cd3428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package jingdonglianmeng/0.0.3/jingdonglianmeng-0.0.3.tar.gz

---
+++ packages/jingdonglianmeng/jingdonglianmeng-0.0.3.tar.gz/jingdonglianmeng-0.0.3/jingdonglianmeng/spider/lianmeng/lianmeng_second.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""京东联盟商品第二步"""
+
+
+class LianMengTwo(object):
+
+    def __init__(self, *args, **kwargs):
+        self.params = {}
+        self.start_url = "https://item-soa.jd.com/getWareBusiness?skuId={}"
+        self.headers = {
+            'accept': 'application/json, text/plain, */*',
+            'content-type': 'application/json;charset=UTF-8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+        }
+        super(LianMengTwo, self).__init__()
+
+    def get_token(self):
+        """
+        获取渠道所需auth或cookie
+        :return:
+        """
+        return ""
+
+    def structure_params(self, params):
+        """
+        构造参数&请求url
+        :param params: 请求需要的参数
+        :return:
+        """
+        self.params = params
+        skuId = self.params.get("skuId")
+        # get
+        kwargs = {
+            "url": self.start_url.format(skuId),
+            "method": "get",
+            "headers": self.headers,
+            "proxies": "",
+            "session": False,
+        }
+
+        return kwargs
+
+    def check_response(self, response):
+        """
+        检查响应是否正确
+        :param response: 响应体
+        :return:
+        """
+        if response.status_code == 200:
+            print("请求成功")
+            return True
+
+        return False
+
+    def parse_response(self, response):
+        """
+        解析响应结果,获取所需字段
+        :return:
+        """
+        # 获取优惠券&市场价&售价
+        res_json = response.json()
+        price = res_json.get("price", {}).get("p")  # 售价
+        market_price = res_json.get("price", {}).get("op")  # 市场价
+        couponInfo = res_json.get("couponInfo", [])  # 优惠券
+        coupon_list = []
+        for info in couponInfo:
+            couponValue = info.get("couponValue", "")  # 折扣
+            if not couponValue:
+                quota = info.get("quota")  # 满
+                discount = info.get("discount")  # 减
+                couponValue = "满{}减{}".format(quota, discount)
+
+            coupon_list.append(couponValue)
+
+        self.params.update({
+            "price": price,
+            "market_price": market_price,
+            "coupon_list": coupon_list,
+        })
+
+        yield self.params
