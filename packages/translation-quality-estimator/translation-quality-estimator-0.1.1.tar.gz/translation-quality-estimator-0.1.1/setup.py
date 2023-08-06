8732ff0e467cacc0be8f0b64f50e50a97489712a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:10 +0000

    Add package translation-quality-estimator/0.1.1/translation-quality-estimator-0.1.1.tar.gz

---
+++ packages/translation-quality-estimator/translation-quality-estimator-0.1.1.tar.gz/translation-quality-estimator-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8", errors="ignore") as f:
+    long_description = f.read()
+
+setup(
+    name="translation-quality-estimator",
+    version="0.1.1",
+    description="To estimate the quality of translation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theRay07/Translation-Quality-Estimator",
+    author="Rishav Ray",
+    author_email="rishavray29@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="nlp bert translation quality estimator",
+    include_package_data=True,
+    packages=["tqe", "tqe/embedding", "tqe/similarity"],
+    install_requires=["sentence-transformers>=0.3.8"],
+    python_requires=">=3.6",
+)
