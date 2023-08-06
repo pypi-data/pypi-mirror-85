d34f03c1abeab8d1ad46c051080c41d264a853b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:29 +0000

    Add package TOPSIS-Ankush-101803384/0.0.2/TOPSIS_Ankush_101803384-0.0.2.tar.gz

---
+++ packages/TOPSIS-Ankush-101803384/TOPSIS_Ankush_101803384-0.0.2.tar.gz/TOPSIS_Ankush_101803384-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="TOPSIS_Ankush_101803384",
+    version="0.0.2",
+    description = 'Implementation of TOPSIS',
+    author="Ankush Gupta",
+    author_email="ankushgupta10092000@gmail.com",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/TOPSIS_AyushJain",
+    packages=["TOPSIS_Ankush_101803384"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data = True,
+    python_requires='>=3.0',
+    keywords='topsis pandas rank decision_matrix ',
+    install_requires=['pandas'],
+)
