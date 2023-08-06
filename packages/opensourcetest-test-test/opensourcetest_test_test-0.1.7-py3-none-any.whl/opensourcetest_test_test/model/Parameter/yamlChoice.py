6b7f3d34b4584bce84cc7c2b6a06757c8117b190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:34 +0000

    Add package opensourcetest-test-test/0.1.7/opensourcetest_test_test-0.1.7-py3-none-any.whl

---
+++ packages/opensourcetest-test-test/opensourcetest_test_test-0.1.7-py3-none-any.whl/opensourcetest_test_test/model/Parameter/yamlChoice.py
@@ -0,0 +1,23 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : interface_auto_frame
+@Time    : 2020/10/12 16:30
+@Auth    : chineseluo
+@Email   : 848257135@qq.com
+@File    : yamlChoice.py
+@IDE     : PyCharm
+------------------------------------
+"""
+from opensourcetest_test_test.builtin.autoParamInjection import AutoInjection
+
+
+class Login(AutoInjection):
+    def __init__(self):
+        super(Login, self).__init__(self.__class__.__name__)
+
+
+if __name__ == '__main__':
+    ...
