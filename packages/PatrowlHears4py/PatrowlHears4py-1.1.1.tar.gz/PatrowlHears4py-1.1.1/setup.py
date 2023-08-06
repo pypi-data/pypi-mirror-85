b50d03138156eb3ca945c712407b40529a0c405f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:34 +0000

    Add package PatrowlHears4py/1.1.1/PatrowlHears4py-1.1.1.tar.gz

---
+++ packages/PatrowlHears4py/PatrowlHears4py-1.1.1.tar.gz/PatrowlHears4py-1.1.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup file."""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PatrowlHears4py',
+    version='1.1.1',
+    description='Python API client for PatrowlHears',
+    url='https://github.com/Patrowl/PatrowlHears4py',
+    author='Nicolas Mattiocco',
+    author_email='nicolas@patrowl.io',
+    license='AGPLv3',
+    packages=find_packages(),
+)
