85dacadff9329d888f6aaefb49a85ed716027cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:08 +0000

    Add package finbull/1.0.8/finbull-1.0.8-py3-none-any.whl

---
+++ packages/finbull/finbull-1.0.8-py3-none-any.whl/finbull/finlib/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 hcyjs.com, Inc. All Rights Reserved
+
+"""
+File: __init__.py.py
+Author: wangjiangfeng(wangjiangfeng@hcyjs.com)
+Date: 2020/11/16 5:29 下午
+"""
