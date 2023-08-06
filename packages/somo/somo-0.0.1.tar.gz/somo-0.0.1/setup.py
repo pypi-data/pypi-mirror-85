8e785ed58594aae3c3b0dd6797e222754620bf68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:27 +0000

    Add package somo/0.0.1/somo-0.0.1.tar.gz

---
+++ packages/somo/somo-0.0.1.tar.gz/somo-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# a dummy setup.py used to reserve the name somo on pypi following
+# https://stackoverflow.com/questions/47676721/register-an-internal-package-on-pypi
+
+setup(
+    name='somo',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/GrauleM/somo',
+    author='Moritz A. Graule',
+    author_email='moritz@graule.ch',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
