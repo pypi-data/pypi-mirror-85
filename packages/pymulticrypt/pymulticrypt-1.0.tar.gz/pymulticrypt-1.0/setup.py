56ba9301f5324c7eb798bbcfabac42958de37db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:30 +0000

    Add package pymulticrypt/1.0/pymulticrypt-1.0.tar.gz

---
+++ packages/pymulticrypt/pymulticrypt-1.0.tar.gz/pymulticrypt-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+VERSION = "v1.0"
+
+readme = open("readme.md", "r").read();
+
+setup(
+    name = "pymulticrypt",
+    packages = ["pymulticrypt"],
+    version = VERSION,
+    license = "MIT",
+    description = "Python Module for secure End-2-End encryption using the Multicrypt algorithm made by me",
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    author = "Abhay Tripathi",
+    author_email = "abhay.triipathi@gmail.com",
+    url = "https://github.com/AbhayTr/PyMulticrypt",
+    download_url = "https://github.com/AbhayTr/PyMulticrypt/archive/" + VERSION + ".tar.gz",
+    keywords = ["PyMulticrypt", "Encryption", "Asymmetric Encryption", "Symmetric Encryption", "End2End Encryption", "End2End", "Signal Protocol"],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
