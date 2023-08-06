c1dba879ceed3a709305555026a86b38aee24d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package previs/1.01/previs-1.01.tar.gz

---
+++ packages/previs/previs-1.01.tar.gz/previs-1.01/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='previs',
+    packages=['previs'],
+    version='1.01',
+    description='PREVIS stands for PREprocessing and VIsualisation. '
+                'This package consists of essential code snippets required for preprocessing and '
+                'visualisation of data science tasks.',
+    author='Anand Zutshi (zutshianand)',
+    author_email='123anandji@gmail.com',
+    url='https://github.com/zutshianand/Previs.git',
+    download_url='https://github.com/zutshianand/Previs/archive/1.01.tar.gz',
+    keywords=['dataloaders', 'preprocessors', 'featuregeneration', 'convenience', 'visualisation', 'pytorch', 'models',
+              'deep-learning', 'dataset', 'snippets'],
+    classifiers=[],
+)
