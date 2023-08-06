84109bdd5987d93d86d8e5bb8e80e04536d4babb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:04 +0000

    Add package opensourcetest-test-test/0.2.0/opensourcetest_test_test-0.2.0-py3-none-any.whl

---
+++ packages/opensourcetest-test-test/opensourcetest_test_test-0.2.0-py3-none-any.whl/opensourcetest_test_test/model/run.py
@@ -0,0 +1,24 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : interface_auto_frame
+@Time    : 2020/8/25 16:10
+@Auth    : chineseluo
+@Email   : 848257135@qq.com
+@File    : run.py
+@IDE     : PyCharm
+------------------------------------
+"""
+import os
+import pytest
+from loguru import logger
+
+if __name__ == '__main__':
+    pytest.main()
+    # Generate assure Report
+    cmd = 'allure generate Report/allure-results -o Report/allure-report -c'
+    os.system(cmd)
+    logger.info("Local Test Report Address:http://127.0.0.1:63342/" + os.getcwd().split("\\")[-1]+"/Report/allure-report/index.html")
+
