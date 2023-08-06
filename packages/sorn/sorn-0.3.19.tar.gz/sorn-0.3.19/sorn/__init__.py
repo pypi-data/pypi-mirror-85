9653d6cf228114c5a946da9c780707d3fed81ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:33 +0000

    Add package sorn/0.3.19/sorn-0.3.19.tar.gz

---
+++ packages/sorn/sorn-0.3.19.tar.gz/sorn-0.3.19/sorn/__init__.py
@@ -0,0 +1,8 @@
+from .sorn import Simulator, Trainer
+import logging
+from .utils import *
+
+__author__ = "Saranraj Nambusubramaniyan"
+__version__ = "0.3.19"
+
+logging.basicConfig(level=logging.INFO)
