4c82a058f395cfec393010841a5ca816fee05d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:20 +0000

    Add package binomial-gaus-distrib/1.1/binomial_gaus_distrib-1.1.tar.gz

---
+++ packages/binomial-gaus-distrib/binomial_gaus_distrib-1.1.tar.gz/binomial_gaus_distrib-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binomial_gaus_distrib',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['binomial_gaus_distrib'],
+      author='Rahul Gupta',
+      author_email='385.rahul@gmail.com',
+      zip_safe=False)
