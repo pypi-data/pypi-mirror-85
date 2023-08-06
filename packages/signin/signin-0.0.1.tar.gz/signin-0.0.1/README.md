4ecb640093f0c4d435b68f15b79555dda0abf893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:44 +0000

    Add package signin/0.0.1/signin-0.0.1.tar.gz

---
+++ packages/signin/signin-0.0.1.tar.gz/signin-0.0.1/README.md
@@ -0,0 +1,5 @@
+# autocheckin
+
+LoveNishimiyaShouko/autocheckin is licensed under the
+
+GNU Affero General Public License v3.0
---
+++ packages/signin/signin-0.0.1.tar.gz/signin-0.0.1/signin.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("signin.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="signin",
+    version="0.0.1",
+    author="LoveNishimiyaShouko",
+    author_email="LoveNishimiyaShouko@LoveNishimiyaShouko.LoveNishimiyaShouko",
+    description="Auto sign in.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LoveNishimiyaShouko/signin",
+    license="AGPLv3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords="auto,sign in,auto sign in",
+    project_urls={
+        "Documentation": "https://github.com/LoveNishimiyaShouko/signin/tree/main/docs",
+        "Source": "https://github.com/LoveNishimiyaShouko/signin/tree/main/code"
+    },
+    packages=setuptools.find_packages(include=['signin']),
+    install_requires=["requests"],
+    python_requires='>=3',
+    package_data={
+        'signin_log': ['signin_log.txt'],
+        'signin_error': ['signin_error.txt']
+    },
+    entry_points={
+        'console_scripts': [
+            'signin=signin:main',
+        ],
+    },
+)
