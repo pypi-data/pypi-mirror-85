69e1957736145d4a702a9058d761578c6676cb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:38 +0000

    Add package AWSOM/0.11/AWSOM-0.11.tar.gz

---
+++ packages/AWSOM/AWSOM-0.11.tar.gz/AWSOM-0.11/setup.py
@@ -0,0 +1,45 @@
+import pathlib
+from setuptools import setup
+
+NAME = "AWSOM"
+URL = f'https://github.com/pfython/{NAME}'
+HERE = pathlib.Path(__file__).parent
+VERSION = "0.11"
+
+setup(
+  name = NAME,
+  packages = [NAME],
+  version = VERSION,
+  license='MIT',
+  description = 'Desktop automation tools for managing common tasks such as renaming media files and making backups, as well as interacting with Adobe Premiere Pro using the `Pymiere` library (Windows only).',
+  long_description=(HERE / "README.md").read_text(),
+  long_description_content_type="text/markdown",
+  author = 'Peter Fison',
+  author_email = 'peter@southwestlondon.tv',
+  url = URL,
+  download_url = f'{URL}/archive/{VERSION}.tar.gz',
+  keywords = [NAME, "search", "video", "audio", "media", "AWSOM", "TV", "content", "manage", "organise", "organize", "Premiere", "Adobe", "Pymiere", "YouTube", "metadata", "workflow", "automation", "Creative Cloud", "edit", "editor", "editing"],
+  install_requires=['cleverdict'],
+  # https://pypi.org/classifiers/
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Multimedia',
+    'Topic :: Multimedia :: Video',
+    'Topic :: Multimedia :: Video :: Non-Linear Editor',],
+  )
+
+# Run the following from the command prompt:
+
+# python -m pip install setuptools wheel twine
+# python setup.py sdist
+# python -m twine upload --repository testpypi dist/*
+# python -m twine upload --repository pypi dist/*
