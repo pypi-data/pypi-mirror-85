6f8ff44d3cf5d95dc94e1f39b7da25a101d0f764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:07 +0000

    Add package lab2FmtLipsky/0.3.1/lab2FmtLipsky-0.3.1.tar.gz

---
+++ packages/lab2FmtLipsky/lab2FmtLipsky-0.3.1.tar.gz/lab2FmtLipsky-0.3.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='lab2FmtLipsky',
+      version='0.3.1',
+      description='Lab2 Ruby formatter - naming and comments',
+      packages=['lab2FmtLipsky'],
+      author_email='lipsky787@gmail.com',
+      zip_safe=False)
