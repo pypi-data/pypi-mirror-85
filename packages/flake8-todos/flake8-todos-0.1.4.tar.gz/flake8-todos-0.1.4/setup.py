c48a97bad3495d982c0957246b3c8923a3f3dddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:00 +0000

    Add package flake8-todos/0.1.4/flake8-todos-0.1.4.tar.gz

---
+++ packages/flake8-todos/flake8-todos-0.1.4.tar.gz/flake8-todos-0.1.4/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os.path
+
+readme = ''
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), 'rb') as stream:
+    readme = stream.read().decode('utf8')
+
+setup(
+    long_description=readme,
+    name='flake8-todos',
+    version='0.1.4',
+    description='Check TODOs in the project',
+    project_urls={'repository': 'https://github.com/orsinium-labs/flake8-todos'},
+    author='Gram <gram@orsinium.dev>',
+    license='BSD 3-Clause',
+    keywords='flake8 flake8-plugin linter styleguide code quality',
+    classifiers=[
+        'Development Status :: 4 - Beta', 'Environment :: Console',
+        'Framework :: Flake8', 'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'License :: OSI Approved :: BSD License'
+    ],
+    entry_points={
+        'flake8.extension': ['T00 = flake8_todos:Checker'],
+    },
+    packages=['flake8_todos'],
+    package_data={},
+    install_requires=[
+        'flake8',
+        'pycodestyle',
+    ],
+    extras_require={
+        'dev': [
+            'isort[pyproject]',
+            'pytest',
+        ],
+    },
+)
