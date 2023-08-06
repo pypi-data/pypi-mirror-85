8dde71ff214b2ebb2a57fc22af777740b3bc5acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:20 +0000

    Add package mlstreamline/0.0.3/mlstreamline-0.0.3.tar.gz

---
+++ packages/mlstreamline/mlstreamline-0.0.3.tar.gz/mlstreamline-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlstreamline",
+    version="0.0.3",
+    author="Charles Crawford",
+    author_email="crawford.charles.g@gmail.com",
+    description="streamlined wrapper classes for ml",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/charles-crawford/ml-streamline",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
