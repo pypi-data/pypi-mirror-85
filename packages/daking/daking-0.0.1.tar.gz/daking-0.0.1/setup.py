9aec820281fa297c9283847c77275c15940b2370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:41 +0000

    Add package daking/0.0.1/daking-0.0.1.tar.gz

---
+++ packages/daking/daking-0.0.1.tar.gz/daking-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import pathlib
+from setuptools import setup
+
+# This call to setup() does all the work
+setup(
+    name="daking",
+    url="https://github.com/daking2014/daking",
+    author="daniel",
+    author_email="daking@hey.com",
+    version="0.0.1",
+)
