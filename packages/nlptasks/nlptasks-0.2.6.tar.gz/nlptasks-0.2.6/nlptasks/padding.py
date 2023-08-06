f068f13455b670e8cf8cafd24a068561704c9111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:23 +0000

    Add package nlptasks/0.2.6/nlptasks-0.2.6.tar.gz

---
+++ packages/nlptasks/nlptasks-0.2.6.tar.gz/nlptasks-0.2.6/nlptasks/padding.py
@@ -0,0 +1,67 @@
+import tensorflow.keras as keras  # pad_sequences
+from pad_sequences import pad_sequences_adjacency
+from pad_sequences import pad_sequences_sparse
+
+
+def pad_idseqs(func):
+    def wrapper(*args, **kwargs):
+        # read and remove padding settings
+        maxlen = kwargs.pop('maxlen', None)
+        padding = kwargs.pop('padding', 'pre')
+        truncating = kwargs.pop('truncating', 'pre')
+
+        # run the NLP task
+        idseqs, VOCAB = func(*args, **kwargs)
+
+        # padding and update vocabulary
+        if maxlen is not None:
+            idseqs = keras.preprocessing.sequence.pad_sequences(
+                idseqs, maxlen=maxlen, value=len(VOCAB),
+                padding=padding, truncating=truncating).tolist()
+            VOCAB.append("[PAD]")
+
+        return idseqs, VOCAB
+    return wrapper
+
+
+def pad_adjseqs(func):
+    def wrapper(*args, **kwargs):
+        # read and remove padding settings
+        maxlen = kwargs.pop('maxlen', None)
+        padding = kwargs.pop('padding', 'pre')
+        truncating = kwargs.pop('truncating', 'pre')
+
+        # run the NLP task
+        adjac_child, adjac_parent, seqs_lens = func(*args, **kwargs)
+
+        # pad adjacency matrix of children and parent relationships
+        if maxlen is not None:
+            adjac_child = pad_sequences_adjacency(
+                sequences=adjac_child, seqlen=seqs_lens,
+                maxlen=maxlen, padding=padding, truncating=truncating)
+            adjac_parent = pad_sequences_adjacency(
+                sequences=adjac_parent, seqlen=seqs_lens,
+                maxlen=maxlen, padding=padding, truncating=truncating)
+
+        return adjac_child, adjac_parent, seqs_lens
+    return wrapper
+
+
+def pad_maskseqs(func):
+    def wrapper(*args, **kwargs):
+        # read and remove padding settings
+        maxlen = kwargs.pop('maxlen', None)
+        padding = kwargs.pop('padding', 'pre')
+        truncating = kwargs.pop('truncating', 'pre')
+
+        # run the NLP task
+        maskseqs, seqs_lens, VOCAB = func(*args, **kwargs)
+
+        # pad sparse mask sequence
+        if maxlen is not None:
+            maskseqs = pad_sequences_sparse(
+                sequences=maskseqs, seqlen=seqs_lens,
+                maxlen=maxlen, padding=padding, truncating=truncating)
+
+        return maskseqs, seqs_lens, VOCAB
+    return wrapper
---
+++ packages/nlptasks/nlptasks-0.2.6.tar.gz/nlptasks-0.2.6/nlptasks/vocab.py
@@ -0,0 +1,69 @@
+from typing import List, Optional
+from collections import Counter
+
+
+def identify_vocab_mincount(data: List[str], 
+                            min_occurrences: Optional[int] = 20
+                           ) -> List[str]:
+    """Extract a vocabulary list where each token/word/lemma has minimum
+        number of occurrences.
+    
+    Parameters:
+    -----------
+    data : List[str]
+        All token/words/lemma of a corpus.
+
+    min_occurrences : int
+        The required number of occurences in a corpus.
+
+    Returns:
+    --------
+    VOCAB : List[str]
+        Vocabulary list, alphabetically sorted
+
+    Example:
+    --------
+        VOCAB = identify_vocab_mincount(
+            data=all_words_in_a_corpus, min_occurrences=30)
+    """
+    cnt = Counter(data)
+    VOCAB = sorted([k for k, v in cnt.items() if v >= min_occurrences])
+    return VOCAB
+
+
+def texttoken_to_index(sequence: List[str], VOCAB: List[str]) -> List[int]:
+    """Convert a sequence of strings to a sequence of IDs (int) based
+        on a given vocabulary
+    
+    Parameters:
+    -----------
+    sequence : List[str]
+        List of ID sequences
+
+    VOCAB : List[str]
+        Vocabulary list, alphabetically sorted
+
+    Returns:
+    --------
+    List[int]
+        List of ID sequences
+
+    Example:
+    --------
+        seqs_of_ids = [texttoken_to_index(seq, VOCAB) for seq in sequences]
+    """
+    # find ID for [UNK], i.e. unknown
+    try:
+        UNKIDX = VOCAB.index("[UNK]")
+    except:
+        UNKIDX = len(VOCAB)
+    # loop over each token
+    indicies = []
+    for token in sequence:
+        try:
+            indicies.append(VOCAB.index(token))
+        except:
+            indicies.append(UNKIDX)
+    # done
+    return indicies
+
