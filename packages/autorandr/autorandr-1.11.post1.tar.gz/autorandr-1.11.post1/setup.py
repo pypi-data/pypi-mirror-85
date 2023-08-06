6a951bc93fb3992dc459e41d268e7bde8ab39bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:23 +0000

    Add package autorandr/1.11.post1/autorandr-1.11.post1.tar.gz

---
+++ packages/autorandr/autorandr-1.11.post1.tar.gz/autorandr-1.11.post1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = 'Automatically select a display configuration based on connected devices'
+
+setup(
+    name='autorandr',
+
+    version='1.11.post1',
+
+    description='Automatically select a display configuration based on connected devices',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/phillipberndt/autorandr',
+
+    author='Phillip Berndt',
+    author_email='phillip.berndt@googlemail.com',
+
+    license='GPLv3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Environment :: Console',
+
+        'Intended Audience :: End Users/Desktop',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='xrandr',
+
+    py_modules=['autorandr'],
+
+    entry_points={
+        'console_scripts': [
+            'autorandr = autorandr:exception_handled_main',
+        ],
+    },
+)
