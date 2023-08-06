357534d389369509fcdde673b7056dc5a8310ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:35 +0000

    Add package Doraemon/3.1/Doraemon-3.1.tar.gz

---
+++ packages/Doraemon/Doraemon-3.1.tar.gz/Doraemon-3.1/Doraemon/__init__.py
@@ -0,0 +1,10 @@
+# Requests
+from Doraemon.Requests import chrome_dora
+from Doraemon.Requests import requests_dora
+from Doraemon.Requests import proxies_dora
+
+# OnlineSeach
+from Doraemon.Crawlers import google_KG
+from Doraemon.Crawlers import google_translator
+from Doraemon.Crawlers import dianping
+from Doraemon.Crawlers import whois
