63d4bcdf1f84f7809a8183cf95610fd1596c6347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:38 +0000

    Add package cwapi3d/1.1/cwapi3d-1.1.tar.gz

---
+++ packages/cwapi3d/cwapi3d-1.1.tar.gz/cwapi3d-1.1/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name='cwapi3d',
+  version='1.1',
+  author='Cadwork Montreal',
+  author_email='it@cadwork.ca',
+  description='Python bindings for CwAPI3D',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/CadworkMontreal/cwapi3d',
+  packages=[
+    'attribute_controller',
+    'bim_controller',
+    'cadwork',
+    'connector_axis_controller',
+    'element_controller',
+    'file_controller',
+    'geometry_controller',
+    'list_controller',
+    'machine_controller',
+    'material_controller',
+    'menu_controller',
+    'roof_controller',
+    'scene_controller',
+    'shop_drawing_controller',
+    'utility_controller',
+    'visualization_controller'
+  ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries',
+  ],
+  python_requires='>=3.7',
+  package_data={
+    '': ['*.pyi'],
+  },
+  include_package_data=True,
+)
