627c6aee8f575296b3880ea86980cdeacfbb2cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package zesty.zbs-api/1.0.2020.11.17.2/zesty.zbs-api-1.0.2020.11.17.2.tar.gz

---
+++ packages/zesty.zbs-api/zesty.zbs-api-1.0.2020.11.17.2.tar.gz/zesty.zbs-api-1.0.2020.11.17.2/zesty/models/agent_report.py
@@ -0,0 +1,19 @@
+from zesty.models.cpu_mon import CpuMonitor
+from zesty.models.disk_mon import DiskMonitor
+from zesty.models.mem_mon import MemoryMonitor
+from zesty.models.overview import MachineData
+
+
+class AgentReport:
+    agent_version = None
+    MachineData = None
+    Storage = None
+    Memory = None
+    Cpu = None
+
+    def __init__(self, agent_report):
+        self.agent_version = agent_report.get('agent', {}).get('version')
+        self.overview = MachineData(agent_report.get('overview', {}))
+        self.Storage = DiskMonitor(agent_report.get('plugins', {}).get('disk_mon', {}))
+        self.Memory = MemoryMonitor(agent_report.get('plugins', {}).get('mem_mon', {}))
+        self.Cpu = CpuMonitor(agent_report.get('plugins', {}).get('cpu_mon', {}))
