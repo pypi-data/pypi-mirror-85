9b63010ab25cc7dea4d9745fad632114b80607e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:37 +0000

    Add package pretf.aws/0.7.3/pretf.aws-0.7.3.tar.gz

---
+++ packages/pretf.aws/pretf.aws-0.7.3.tar.gz/pretf.aws-0.7.3/setup.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, "..", "pretf", "pretf", "version.py")) as open_file:
+        contents = open_file.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", contents, re.MULTILINE)
+    return match.group(1)
+
+setup(
+    name="pretf.aws",
+    version=get_version(),
+    author="Raymond Butcher",
+    author_email="randomy@gmail.com",
+    license="MIT License",
+    packages=["pretf"],
+    install_requires=["boto3", "pretf"],
+    zip_safe=False,
+)
