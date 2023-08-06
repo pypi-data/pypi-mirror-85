7040897582f1463c7e0d31ed08f1b84c7ecac04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:50 +0000

    Add package zmsai/0.0.1/zmsai-0.0.1-py3-none-any.whl

---
+++ packages/zmsai/zmsai-0.0.1-py3-none-any.whl/zmsai/__init__.py
@@ -0,0 +1,11 @@
+from . import utils
+from . import readData
+from . import zmsParser
+from . import base
+
+name = "zms-ai"
+__version__ = "0.0.1"
+
+
+if __name__ == "__main__":
+    zmsParser.main()
