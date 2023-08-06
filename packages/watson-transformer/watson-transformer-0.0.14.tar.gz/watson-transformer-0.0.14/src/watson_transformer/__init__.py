465ab753babbfb5ec5f18d9e931a4de686f68842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package watson-transformer/0.0.14/watson-transformer-0.0.14.tar.gz

---
+++ packages/watson-transformer/watson-transformer-0.0.14.tar.gz/watson-transformer-0.0.14/src/watson_transformer/__init__.py
@@ -0,0 +1,3 @@
+from watson_transformer.watson_service_transformer import WatsonServiceTransformer
+from watson_transformer.flat_column_transformer import FlatColumnTransformer
+from watson_transformer.json_transformer import JSONTransformer
