71f577971fb0fb30ae23827d0fa0d45f54d9c173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:40 +0000

    Add package TOPSIS-Vipul-101803491/1.2/TOPSIS_Vipul_101803491-1.2.tar.gz

---
+++ packages/TOPSIS-Vipul-101803491/TOPSIS_Vipul_101803491-1.2.tar.gz/TOPSIS_Vipul_101803491-1.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+URL = 'https://github.com/Vipul767/TOPSIS_Vipul_101803491'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='TOPSIS_Vipul_101803491',
+    version='1.2',
+    license='MIT',
+    author='Vipul Goel',
+    packages=['TOPSIS_Vipul_101803491'],
+    author_email='vgoel_be18@thapar.edu',
+    description='TOPSIS implementation',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    url = URL,
+    download_url = "https://github.com/Vipul767/TOPSIS_Vipul_101803491/archive/v_12.tar.gz",  
+    keywords=['TOSPSIS','Normalised_Matrix','Performance_score','Rank','Weighted_Normalised_matrix'],
+    install_requires=[
+                      'pandas',
+     ],
+	classifiers=[
+	'Development Status :: 3 - Alpha',     
+        'Intended Audience :: Developers',     
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    
+    
+)
