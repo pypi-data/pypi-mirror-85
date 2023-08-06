297423d1183c504061743643d88ba808acc5a4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:58 +0000

    Add package SyncGitlab2MSProject/0.0.2/SyncGitlab2MSProject-0.0.2-py2.py3-none-any.whl

---
+++ packages/SyncGitlab2MSProject/SyncGitlab2MSProject-0.0.2-py2.py3-none-any.whl/syncgitlab2msproject/exceptions.py
@@ -0,0 +1,22 @@
+class GitlabSyncError(ValueError):
+    pass
+
+
+class MovedIssueNotDefined(GitlabSyncError):
+    pass
+
+
+class MSProjectSyncError(ValueError):
+    pass
+
+
+class MSProjectValueSetError(MSProjectSyncError):
+    pass
+
+
+class LoadingError(MSProjectSyncError):
+    pass
+
+
+class ClassNotInitiated(MSProjectSyncError):
+    """Tried to load a function without properly initiate the class"""
