fdea763eb59e723ee3ff966dee21c7aae77f6ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package beerhawk-behavioural-clusters/0.0.13/beerhawk_behavioural_clusters-0.0.13.tar.gz

---
+++ packages/beerhawk-behavioural-clusters/beerhawk_behavioural_clusters-0.0.13.tar.gz/beerhawk_behavioural_clusters-0.0.13/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='beerhawk_behavioural_clusters',
+version='0.0.13',
+ description='Gaussian distributions',
+ packages=['beerhawk_behavioural_clusters'],
+ author='Shashi Bhushan Singh',
+ author_email='shashi.bhushansingh@ab-inbev.com',
+ long_description=long_description,
+    long_description_content_type='text/markdown'
+ ,zip_safe=False)
