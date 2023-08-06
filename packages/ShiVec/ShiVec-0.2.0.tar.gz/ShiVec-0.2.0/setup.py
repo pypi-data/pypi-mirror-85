597134f4ff4fe290b1bfa9db9507626da986b505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package ShiVec/0.2.0/ShiVec-0.2.0.tar.gz

---
+++ packages/ShiVec/ShiVec-0.2.0.tar.gz/ShiVec-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ShiVec',
+    version='v0.2.0',
+    packages=[''],
+    url='https://github.com/ShisatOwO/ShiVec',
+    license='MIT License',
+    author='Shisato',
+    author_email='',
+    description='ShiVec is a simple python package for working with vectors.'
+)
