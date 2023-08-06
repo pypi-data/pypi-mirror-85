d132fdbd8e6f24b296bde707d62a324a45ebac50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:05 +0000

    Add package mdscaling/0.0.2/mdscaling-0.0.2.tar.gz

---
+++ packages/mdscaling/mdscaling-0.0.2.tar.gz/mdscaling-0.0.2/setup.py
@@ -0,0 +1,26 @@
+"""`setup.py`"""
+from setuptools import setup, find_packages
+
+# # Package requirements
+# with open('requirements.txt') as f:
+#     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+INSTALL_REQUIRES=[
+'numpy',
+'scikit-learn',
+'scipy',
+'matplotlib',
+'prince',
+]
+
+setup(name='mdscaling',
+      version='0.0.2',
+      description='Multi-dimensional network scaling',
+      author='Pedro Ramaciotti Morales',
+      author_email='pedro.ramaciotti@gmail.com',
+      url = 'https://github.com/pedroramaciotti/MDScaling',
+      download_url = 'https://github.com/pedroramaciotti/MDScaling/archive/0.0.2.tar.gz',
+      keywords = ['multi-dimensional scaling','graph compression','dimentionality reduction'],
+      packages=find_packages(),
+      data_files=[('', ['LICENSE'])],
+      install_requires=INSTALL_REQUIRES)
