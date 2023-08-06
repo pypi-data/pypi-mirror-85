a67b61aa7eddbc15673c99a3ed3b994e7c78b6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:42 +0000

    Add package TOPSIS-SUKRITI-401803026/0.0.1/TOPSIS_SUKRITI_401803026-0.0.1.tar.gz

---
+++ packages/TOPSIS-SUKRITI-401803026/TOPSIS_SUKRITI_401803026-0.0.1.tar.gz/TOPSIS_SUKRITI_401803026-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='TOPSIS_SUKRITI_401803026',
+    version='0.0.1',
+    description='TOPSIS evaluator for given data. Enter all arguments as strings',
+    author='Sukriti Sharma',
+    author_email='ssharma_bemba18@thapar.edu',
+    license='MIT',
+    url='https://github.com/sukritishar/TOPSIS-Sukriti-401803026',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['pandas']
+)
