19c68aba8f79b3aaf3872fde436446ee41867159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:09 +0000

    Add package kg-rabbitmqonline/0.8.2/kg_rabbitmqonline-0.8.2-py3-none-any.whl

---
+++ packages/kg-rabbitmqonline/kg_rabbitmqonline-0.8.2-py3-none-any.whl/kg_rabbitmqonline/__init__.py
@@ -0,0 +1,19 @@
+from .builder import (
+    RabbitMQOnlineBuilder
+)
+from .option import (
+    RabbitMQOnlineOptions
+)
+from .configfile import (
+    RabbitMQOnlineConfigFileOptions,
+    RabbitMQOnlineConfigFile,
+)
+
+__version__ = "0.8.2"
+
+__all__ = [
+    'RabbitMQOnlineBuilder',
+    'RabbitMQOnlineOptions',
+    'RabbitMQOnlineConfigFileOptions',
+    'RabbitMQOnlineConfigFile',
+]
