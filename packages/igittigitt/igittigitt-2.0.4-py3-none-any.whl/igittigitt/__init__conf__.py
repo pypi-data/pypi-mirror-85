7f933a809260d476ad1c8032156f582715dafa20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:39 +0000

    Add package igittigitt/2.0.4/igittigitt-2.0.4-py3-none-any.whl

---
+++ packages/igittigitt/igittigitt-2.0.4-py3-none-any.whl/igittigitt/__init__conf__.py
@@ -0,0 +1,22 @@
+name = "igittigitt"
+title = "A spec-compliant gitignore parser for Python"
+version = "v2.0.4"
+url = "https://github.com/bitranox/igittigitt"
+author = "Robert Nowotny"
+author_email = "bitranox@gmail.com"
+shell_command = "igittigitt"
+
+
+def print_info() -> None:
+    print(
+        """\
+
+Info for igittigitt:
+
+    A spec-compliant gitignore parser for Python
+
+    Version : v2.0.4
+    Url     : https://github.com/bitranox/igittigitt
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com"""
+    )
