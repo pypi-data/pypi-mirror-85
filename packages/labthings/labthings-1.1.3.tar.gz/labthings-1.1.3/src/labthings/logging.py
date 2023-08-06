be092f58402654086178c2d858d37501f2a61a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:04 +0000

    Add package labthings/1.1.3/labthings-1.1.3.tar.gz

---
+++ packages/labthings/labthings-1.1.3.tar.gz/labthings-1.1.3/src/labthings/logging.py
@@ -0,0 +1,18 @@
+from logging import StreamHandler
+
+from .find import current_labthing
+
+
+class LabThingLogger(StreamHandler):
+    """ """
+
+    def __init__(self, labthing, *args, ignore_werkzeug=True, **kwargs):
+        StreamHandler.__init__(self, *args, **kwargs)
+        self.labthing = labthing
+        self.ignore_werkzeug = ignore_werkzeug
+
+    def emit(self, record):
+        if self.ignore_werkzeug and record.name == "werkzeug":
+            return
+        else:
+            self.labthing.emit("logging", record)
