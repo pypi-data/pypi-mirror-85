6c67f198235aec18341433c978bb5b31d1a37ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:12 +0000

    Add package TOPSIS-Utkarsh-101803613/0.2/TOPSIS_Utkarsh_101803613-0.2-py3-none-any.whl

---
+++ packages/TOPSIS-Utkarsh-101803613/TOPSIS_Utkarsh_101803613-0.2-py3-none-any.whl/TOPSIS-Utkarsh-101803613/__init__.py
@@ -0,0 +1,4 @@
+
+from TOPSIS-Utkarsh-101803613.topsis import topsis 
+name="TOPSIS-Utkarsh-101803613/TOPSIS-Utkarsh-101803613"
+__version__ = "1.0.0"
---
+++ packages/TOPSIS-Utkarsh-101803613/TOPSIS_Utkarsh_101803613-0.2-py3-none-any.whl/TOPSIS_Utkarsh_101803613/__init__.py
@@ -0,0 +1,4 @@
+
+from TOPSIS_Utkarsh_101803613.topsis import topsis 
+name="TOPSIS-Utkarsh-101803613/TOPSIS-Utkarsh-101803613"
+__version__ = "1.0.0"
