055926b63f123b6689fb255274f4e7c2f7f74c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:54 +0000

    Add package distributions-chain-udacity/0.2/distributions_chain_udacity-0.2.tar.gz

---
+++ packages/distributions-chain-udacity/distributions_chain_udacity-0.2.tar.gz/distributions_chain_udacity-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_chain_udacity',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_chain_udacity'],
+      author='Eduardo Chain',
+      author_email='eduardo.d.chain@gmail.com',   
+      zip_safe=False)
