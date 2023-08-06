1e6b9d2918b4c1c072981bc065165d767b6e41a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package pysimplendi/0.1.1/pysimplendi-0.1.1.tar.gz

---
+++ packages/pysimplendi/pysimplendi-0.1.1.tar.gz/pysimplendi-0.1.1/cmake-build-debug/CMakeFiles/simplendi.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nami/Documents/PROJECTS/pysimplendi/src/NDIReceiver.cpp" "/Users/nami/Documents/PROJECTS/pysimplendi/cmake-build-debug/CMakeFiles/simplendi.dir/src/NDIReceiver.cpp.o"
+  "/Users/nami/Documents/PROJECTS/pysimplendi/src/NDISender.cpp" "/Users/nami/Documents/PROJECTS/pysimplendi/cmake-build-debug/CMakeFiles/simplendi.dir/src/NDISender.cpp.o"
+  "/Users/nami/Documents/PROJECTS/pysimplendi/src/main.cpp" "/Users/nami/Documents/PROJECTS/pysimplendi/cmake-build-debug/CMakeFiles/simplendi.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "simplendi_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/include"
+  "/Users/nami/.pyenv/versions/3.8.1/include/python3.8"
+  "../pybind/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
