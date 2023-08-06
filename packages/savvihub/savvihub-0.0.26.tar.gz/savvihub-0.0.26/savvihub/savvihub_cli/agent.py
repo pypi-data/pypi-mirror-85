b73f1973f6814c061664a15b7c3c84dcc3f49fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:25 +0000

    Add package savvihub/0.0.26/savvihub-0.0.26.tar.gz

---
+++ packages/savvihub/savvihub-0.0.26.tar.gz/savvihub-0.0.26/savvihub/savvihub_cli/agent.py
@@ -0,0 +1,44 @@
+import subprocess
+import json
+import time
+
+import typer
+
+from savvihub import Context
+
+agent_app = typer.Typer()
+
+
+@agent_app.callback(hidden=True)
+def main():
+    return
+
+
+@agent_app.command()
+def collect_system_metrics(
+    metrics_collector_binary: str = typer.Option("system-metrics-collector", "--collector-binary"),
+    prometheus_url: str = typer.Option(..., "--prometheus-url"),
+    collect_period: int = typer.Option(5, "--collect-period", min=5),
+):
+    context = Context(experiment_required=True)
+    client = context.authorized_client
+
+    while True:
+        output = subprocess.check_output([
+            metrics_collector_binary,
+            "-prometheus-url",
+            prometheus_url,
+            "-experiment",
+            context.experiment.slug,
+        ]).decode('utf-8').strip()
+
+        metrics = {}
+        rows = json.loads(output)
+        for row in rows:
+            metrics[row['name']] = {
+                'timestamp': float(row['timestamp']),
+                'value': row['value'],
+            }
+
+        client.experiment_system_metrics_update(context.experiment.id, metrics)
+        time.sleep(collect_period)
