b51c7d66a69ef85a6265b580bbe93ba293863de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:07 +0000

    Add package icevision/0.4.0.post1/icevision-0.4.0.post1-py3-none-any.whl

---
+++ packages/icevision/icevision-0.4.0.post1-py3-none-any.whl/icevision/parsers/__init__.py
@@ -0,0 +1,6 @@
+from icevision.parsers.mixins import *
+from icevision.parsers.parser import *
+from icevision.parsers.defaults import *
+
+from icevision.parsers.coco_parser import *
+from icevision.parsers.voc_parser import *
