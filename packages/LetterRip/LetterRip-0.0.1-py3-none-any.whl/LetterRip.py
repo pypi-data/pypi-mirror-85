5b055e47cfcef3d0e8a0cd198343c1c51e4307c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package LetterRip/0.0.1/LetterRip-0.0.1-py3-none-any.whl

---
+++ packages/LetterRip/LetterRip-0.0.1-py3-none-any.whl/LetterRip.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def LetterRip():
+    letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    return np.random.choice(letters,1)
