866cd76441f9d81f0c992fb9d7c1c5fa545bc179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:31 +0000

    Add package final-proc/0.0.1/final_proc-0.0.1.tar.gz

---
+++ packages/final-proc/final_proc-0.0.1.tar.gz/final_proc-0.0.1/README.md
@@ -0,0 +1,1 @@
+Image Processing Library with my own functions made for academic projects.
---
+++ packages/final-proc/final_proc-0.0.1.tar.gz/final_proc-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="final_proc",
+    version="0.0.1",
+    author="Aman Kumar",
+    author_email="aman487jain@gmail.com",
+    description="A small Image Processing Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
