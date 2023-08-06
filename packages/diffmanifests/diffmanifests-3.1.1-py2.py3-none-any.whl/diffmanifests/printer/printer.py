3ae54bb59757158c2be92326437237107b741723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package diffmanifests/3.1.1/diffmanifests-3.1.1-py2.py3-none-any.whl

---
+++ packages/diffmanifests/diffmanifests-3.1.1-py2.py3-none-any.whl/diffmanifests/printer/printer.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import json
+import openpyxl
+import os
+import time
+
+from openpyxl.styles import Alignment, Font
+from ..proto.proto import Commit
+
+head = {
+    'A': Commit.DIFF,
+    'B': Commit.REPO,
+    'C': Commit.BRANCH,
+    'D': Commit.AUTHOR,
+    'E': Commit.DATE,
+    'F': Commit.COMMIT,
+    'G': Commit.MESSAGE,
+    'H': Commit.URL,
+    'I': Commit.CHANGE,
+    'J': Commit.COMMITTER,
+    'K': Commit.TOPIC
+}
+
+
+class PrinterException(Exception):
+    def __init__(self, info):
+        super().__init__(self)
+        self._info = info
+
+    def __str__(self):
+        return self._info
+
+
+class Printer(object):
+    _format = ['.json', '.txt', '.xlsx']
+
+    def __init__(self, config=None):
+        if config is None:
+            pass
+
+    @staticmethod
+    def format():
+        return Printer._format
+
+    def _json(self, data, name):
+        with open(name, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(data, ensure_ascii=False, indent=2))
+
+    def _txt(self, data, name):
+        def _txt_helper(data, out):
+            global head
+            out.write(u'%s%s: %s\n' % (' '*5, head['A'], data[head['A']]))
+            out.write(u'%s%s: %s\n' % (' '*5, head['B'], data[head['B']]))
+            out.write(u'%s%s: %s\n' % (' '*3, head['C'], data[head['C']]))
+            out.write(u'%s%s: %s\n' % (' '*3, head['D'], data[head['D']]))
+            out.write(u'%s%s: %s\n' % (' '*5, head['E'], data[head['E']]))
+            out.write(u'%s%s: %s\n' % (' '*3, head['F'], data[head['F']]))
+            out.write(u'%s%s: %s\n' % (' '*2, head['G'], data[head['G']]))
+            out.write(u'%s%s: %s\n' % (' '*6, head['H'], data[head['H']]))
+            out.write(u'%s%s: %s\n' % (' '*3, head['I'], data[head['I']]))
+            out.write(u'%s%s: %s\n' % (' '*0, head['J'], data[head['J']]))
+            out.write(u'%s%s: %s\n' % (' '*4, head['K'], data[head['K']]))
+            out.write('\n')
+
+        with open(name, 'w', encoding='utf8') as f:
+            f.write('')
+            for item in data:
+                _txt_helper(item, f)
+
+    def _xlsx(self, data, name):
+        def _styling_head(sheet):
+            for item in head.keys():
+                sheet[item+'1'].alignment = Alignment(horizontal='center', shrink_to_fit=True, vertical='center')
+                sheet[item+'1'].font = Font(bold=True, name='Calibri')
+            sheet.freeze_panes = sheet['B2']
+
+        def _styling_data(sheet, rows):
+            for key in head.keys():
+                for row in range(rows):
+                    sheet[key+str(row+2)].alignment = Alignment(vertical='center')
+                    sheet[key+str(row+2)].font = Font(bold=False, name='Calibri')
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        ws.append([head[key].upper() for key in sorted(head.keys())])
+        for item in data:
+            ws.append([item[head[key]] for key in sorted(head.keys())])
+        _styling_head(ws)
+        _styling_data(ws, len(data))
+        wb.save(filename=name)
+
+    def run(self, data, name):
+        func = Printer.__dict__.get(os.path.splitext(name)[1].replace('.', '_'), None)
+        if func is not None:
+            func(self, data, name)
---
+++ packages/diffmanifests/diffmanifests-3.1.1-py2.py3-none-any.whl/diffmanifests/querier/querier.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from ..gerrit.gerrit import Gerrit
+from ..gitiles.gitiles import Gitiles
+from ..logger.logger import Logger
+from ..proto.proto import Commit, Label, Repo
+
+
+class QuerierException(Exception):
+    def __init__(self, info):
+        super().__init__(self)
+        self._info = info
+
+    def __str__(self):
+        return self._info
+
+
+class Querier(object):
+    def __init__(self, config=None):
+        if config is None:
+            raise QuerierException('config invalid')
+        self.gerrit = Gerrit(config)
+        self.gitiles = Gitiles(config)
+
+    def _build(self, repo, branch, commit, label):
+        def _query(commit):
+            buf = self.gerrit.query('commit:' + commit, 0)
+            if buf is None or len(buf) != 1:
+                return '', ''
+            return self.gerrit.url().replace('/a', '/') + str(buf[0]['_number']), buf[0].get('topic', '')
+
+        change, topic = _query(commit['commit'])
+        return [{
+            Commit.AUTHOR: '%s <%s>' % (commit['author']['name'], commit['author']['email']),
+            Commit.BRANCH: branch,
+            Commit.CHANGE: change,
+            Commit.COMMIT: commit['commit'],
+            Commit.COMMITTER: '%s <%s>' % (commit['committer']['name'], commit['committer']['email']),
+            Commit.DATE: commit['author']['time'],
+            Commit.DIFF: label.upper(),
+            Commit.MESSAGE: commit['message'].split('\n')[0],
+            Commit.REPO: repo,
+            Commit.TOPIC: topic,
+            Commit.URL: self.gitiles.url() + '/' + repo + '/+/' + commit['commit']
+        }]
+
+    def _ahead(self, commit1, commit2):
+        buf1 = datetime.datetime.strptime(commit1['committer']['time'], '%a %b %d %H:%M:%S %Y %z')
+        buf2 = datetime.datetime.strptime(commit2['committer']['time'], '%a %b %d %H:%M:%S %Y %z')
+        return buf2 > buf1
+
+    def _commits(self, repo, commit1, commit2, backward):
+        def _helper(repo, commit1, commit2, backward):
+            buf = []
+            commit = self.gitiles.commit(repo, commit1[Repo.COMMIT])
+            if commit is None:
+                return [], None, False
+            commits = self.gitiles.commits(repo, commit2[Repo.BRANCH], commit2[Repo.COMMIT])
+            if commits is None:
+                return [], None, False
+            completed = False
+            next = commits.get('next', None) if backward else commits.get('previous', None)
+            for item in commits['log']:
+                if item['commit'] == commit1[Repo.COMMIT]:
+                    completed = True
+                    break
+                if (backward and self._ahead(item, commit)) \
+                        or (not backward and self._ahead(commit, item)):
+                    next = None
+                    break
+                buf.append(item)
+            return buf, next, completed
+
+        buf = []
+        while True:
+            commits, next, completed = _helper(repo, commit1, commit2, backward)
+            buf.extend(commits)
+            if completed is True:
+                status = True
+                break
+            else:
+                if next is None:
+                    status = False
+                    break
+            commit2 = {
+                Repo.BRANCH: commit2[Repo.BRANCH],
+                Repo.COMMIT: next
+            }
+        return buf, status
+
+    def _commit1(self, repo, commit1, commit2):
+        commits = self.gitiles.commits(repo, commit2[Repo.BRANCH], commit2[Repo.COMMIT])
+        if commits is None:
+            return None, ''
+        while True:
+            commit = None
+            for item in commits.get('log', []):
+                data = self.gitiles.commits(repo, commit1[Repo.BRANCH], item['commit'])
+                if data is not None and len(data['log']) != 0:
+                    commit = {
+                        Repo.BRANCH: commit1[Repo.BRANCH],
+                        Repo.COMMIT: item['commit']
+                    }
+                    break
+            if commit is not None:
+                break
+            data = commits.get('next', None)
+            if data is None:
+                break
+            commits = self.gitiles.commits(repo, commit2[Repo.BRANCH], data)
+        if commit is None:
+            return None, ''
+        data1 = self.gitiles.commit(repo, commit1[Repo.COMMIT])
+        data2 = self.gitiles.commit(repo, commit[Repo.COMMIT])
+        if data1 is None or data2 is None:
+            return None, ''
+        if self._ahead(data1, data2) is True:
+            label = Label.ADD_COMMIT
+        else:
+            label = Label.REMOVE_COMMIT
+        return commit, label
+
+    def _diff(self, repo, commit1, commit2):
+        buf = []
+        commit, label = self._commit1(repo, commit1, commit2)
+        if commit is None:
+            return []
+        commits, status = self._commits(repo, commit, commit2, True)
+        if status is False:
+            return []
+        for item in commits:
+            buf.extend(self._build(repo, commit2[Repo.BRANCH], item, Label.ADD_COMMIT))
+        if commit[Repo.COMMIT] != commit1[Repo.COMMIT]:
+            if label == Label.ADD_COMMIT:
+                commits, status = self._commits(repo, commit1, commit, True)
+            else:
+                commits, status = self._commits(repo, commit, commit1, True)
+            if status is False:
+                return []
+            for item in commits:
+                buf.extend(self._build(repo, commit1[Repo.BRANCH], item, label))
+        return buf
+
+    def _fetch(self, data, label):
+        def _helper(repo, commit, label):
+            buf1, buf2 = commit
+            Logger.info(label + ': ' + repo)
+            if label == Label.ADD_REPO:
+                data = self.gitiles.commit(repo, buf2[Repo.COMMIT])
+                if data is None:
+                    return []
+                return self._build(repo, buf2[Repo.BRANCH], data, label)
+            elif label == Label.REMOVE_REPO:
+                data = self.gitiles.commit(repo, buf1[Repo.COMMIT])
+                if data is None:
+                    return []
+                return self._build(repo, buf1[Repo.BRANCH], data, label)
+            elif label == Label.UPDATE_REPO:
+                return self._diff(repo, buf1, buf2)
+            else:
+                return []
+
+        buf = []
+        for key, val in data.get(label, {}).items():
+            buf.extend(_helper(key, val, label))
+        return buf
+
+    def run(self, data):
+        buf = []
+        buf.extend(self._fetch(data, Label.ADD_REPO))
+        buf.extend(self._fetch(data, Label.REMOVE_REPO))
+        buf.extend(self._fetch(data, Label.UPDATE_REPO))
+        return buf
