4c8c2cd88666cbf76f802b7c04467a12b2d51ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:10 +0000

    Add package spacytextblob/0.1.4/spacytextblob-0.1.4.tar.gz

---
+++ packages/spacytextblob/spacytextblob-0.1.4.tar.gz/spacytextblob-0.1.4/spacytextblob/spacytextblob.py
@@ -0,0 +1,47 @@
+from spacy.tokens import Doc, Span
+
+from textblob import TextBlob
+
+
+class SpacyTextBlob(object):
+    """A spacy pipeline object for sentiment analysis."""
+    name = "spaCyTextBlob"
+
+    def __init__(self):
+        """Set up the ojbect"""
+        Doc.set_extension("polarity", default=None, force=True)
+        Doc.set_extension("subjectivity", default=None, force=True)
+        Doc.set_extension("assessments", default=None, force=True)
+        Span.set_extension("polarity", default=None, force=True)
+        Span.set_extension("subjectivity", default=None, force=True)
+        Span.set_extension("assessments", default=None, force=True)
+
+    def __call__(self, doc):
+        """When the nlp is called
+
+        Parameters
+        ----------
+        doc : [type]
+            [description]
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        # sentiment at the doc level
+        blob = TextBlob(doc.text)
+        sentiment = blob.sentiment_assessments
+        doc._.set("polarity", sentiment.polarity)
+        doc._.set("subjectivity", sentiment.subjectivity)
+        doc._.set("assessments", sentiment.assessments)
+
+        # sentiment on the sentence level
+        for span in doc.sents:
+            blob = TextBlob(span.text)
+            sentiment = blob.sentiment_assessments
+            span._.set("polarity", sentiment.polarity)
+            span._.set("subjectivity", sentiment.subjectivity)
+            span._.set("assessments", sentiment.assessments)
+
+        return doc
