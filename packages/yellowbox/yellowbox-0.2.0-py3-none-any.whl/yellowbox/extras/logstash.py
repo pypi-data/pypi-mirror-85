041372f266642823301ca2e94dc829e2c44cb7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package yellowbox/0.2.0/yellowbox-0.2.0-py3-none-any.whl

---
+++ packages/yellowbox/yellowbox-0.2.0-py3-none-any.whl/yellowbox/extras/logstash.py
@@ -0,0 +1,19 @@
+from docker import DockerClient
+
+from yellowbox.containers import get_ports, create_and_pull
+from yellowbox.subclasses import SingleContainerService, RunMixin
+
+__all__ = ['LogstashService', 'LOGSTASH_DEFAULT_PORT']
+
+LOGSTASH_DEFAULT_PORT = 5959
+
+
+class LogstashService(SingleContainerService, RunMixin):
+    def __init__(self, docker_client: DockerClient, image='logstash:7.8.1', **kwargs):
+        super().__init__(create_and_pull(
+            docker_client, image, publish_all_ports=True, detach=True,
+            ports={LOGSTASH_DEFAULT_PORT: None}
+        ), **kwargs)
+
+    def client_port(self):
+        return get_ports(self.container)[LOGSTASH_DEFAULT_PORT]
