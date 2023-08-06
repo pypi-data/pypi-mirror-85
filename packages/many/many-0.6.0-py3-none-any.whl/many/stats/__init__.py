51ecd3c12245d8dc24fc5b9c2580db01daf4cec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:33 +0000

    Add package many/0.6.0/many-0.6.0-py3-none-any.whl

---
+++ packages/many/many-0.6.0-py3-none-any.whl/many/stats/__init__.py
@@ -0,0 +1,21 @@
+from .categorical_categorical import (
+    fisher_arr,
+    mat_fisher,
+    mat_fisher_naive,
+    mat_fisher_nan,
+)
+from .continuous_categorical import mat_mwu, mat_mwu_gpu, mat_mwu_naive
+from .continuous_continuous import (
+    mat_corr,
+    mat_corr_naive,
+    mat_corr_nan,
+    mat_corr_subtyped,
+)
+from .continuous_continuous_cmap import (
+    fast_cov,
+    nan_fast_cov,
+    fast_corr,
+    nan_fast_corr,
+    fast_spearman,
+    nan_fast_spearman,
+)
