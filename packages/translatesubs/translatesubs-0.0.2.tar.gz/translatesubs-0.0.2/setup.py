beb880e0b05329ea56fe1088c491c11807aa3c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:02 +0000

    Add package translatesubs/0.0.2/translatesubs-0.0.2.tar.gz

---
+++ packages/translatesubs/translatesubs-0.0.2.tar.gz/translatesubs-0.0.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+    
+with open('version.txt', 'r') as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name='translatesubs',
+    version=version,
+    license='Apache-2.0',
+    author='Montvydas Klumbys',
+    author_email='motnvydas.klumbys@gmail.com',
+    description='It is a tool to translate subtitles into any language, that is supported by google translator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Montvydas/translatesubs',
+    download_url=f'https://github.com/Montvydas/translatesubs/archive/v_{version}.tar.gz',
+    keywords=['SUBTITLES', 'TRANSLATE'],
+    packages=setuptools.find_packages(),
+    install_requires=[            # I get to this in a second
+        'pysubs2',
+        'googletrans',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License ',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Video',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'translatesubs=translatesubs.translatesubs:main'
+        ]
+    },
+)
