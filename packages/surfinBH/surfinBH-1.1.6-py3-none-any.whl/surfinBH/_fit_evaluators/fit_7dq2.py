438cf9d28ad3bef3561bd62b80e84e2d241f7d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:27 +0000

    Add package surfinBH/1.1.6/surfinBH-1.1.6-py3-none-any.whl

---
+++ packages/surfinBH/surfinBH-1.1.6-py3-none-any.whl/surfinBH/_fit_evaluators/fit_7dq2.py
@@ -0,0 +1,359 @@
+import numpy as np
+from surfinBH import surfinBH
+from surfinBH._lal_spin_evolution import evolve_pn_spins
+import surfinBH._utils as utils
+import warnings
+
+#=============================================================================
+class Fit7dq2(surfinBH.SurFinBH):
+    """ A class for the surfinBH7dq2 model presented in Varma et al.,
+    arxiv:1809.09125. This model predicts the final mass mf, final spin vector
+    chif and final kick velocity vector vf, for the remnants of precessing
+    binary black hole systems.  The fits are done using Gaussian Process
+    Regression (GPR) and also provide an error estimate along with the fit
+    value.
+
+    This model has been trained in the parameter space:
+        q <= 2, |chiA| <= 0.8, |chiB| <= 0.8
+
+    However, it extrapolates reasonably to:
+        q <= 4, |chiA| <= 1, |chiB| <= 1
+
+    =========================================================================
+    Usage:
+
+    import surfinBH
+
+    # Load the fit
+    fit = surfinBH.LoadFits('surfinBH7dq2')
+
+    We provide the following call methods:
+        # remnant mass and 1-sigma error estimate
+        mf, mf_err = fit.mf(q, chiA, chiB, **kwargs)
+
+        # remnant spin and 1-sigma error estimate
+        chif, chif_err = fit.chif(q, chiA, chiB, **kwargs)
+
+        # remnant recoil kick and 1-sigma error estimate
+        vf, vf_err = fit.vf(q, chiA, chiB, **kwargs)
+
+        # All of these together
+        mf, chif, vf, mf_err, chif_err, vf_err
+            = fit.all(q, chiA, chiB, **kwargs)
+
+    The arguments for each of these call methods are as follows:
+    Arguments:
+        q:      Mass ratio (q>=1)
+
+        chiA:   Dimensionless spin of the larger BH (array of size 3)
+
+        chiB:   Dimensionless spin of the smaller BH (array of size 3)
+
+            By default, the spins are assumed to be the component spins at
+            t=-100 M from the peak of the waveform, and in the coorbital frame,
+            defined as:
+            The z-axis is along the orbital angular momentum at t=-100M.
+            The x-axis is along the line of separation from the smaller BH to
+                the larger BH at this time.
+            The y-axis completes the triad.
+            We obtain this frame from the waveform as defined in
+            arxiv:1705.07089.
+            The returned spin and kick vectors are also in the same frame.
+
+            If 'omega0' is given, instead the spins are assumed to be the
+            component spins when the PN orbital frequency = omega0. The
+            spins are assumed to be in the inertial frame, defined as:
+            The z-axis is along the Newtonian orbital angular momentum when the
+                PN orbital frequency = omega0.
+            The x-axis is along the line of separation from the smaller BH to
+                the larger BH at this frequency.
+            The y-axis completes the triad.
+            We obtain this frame from PN.
+            Given the spins at omega0, we evolve the spins using PN until
+            the orbital frequency = omega_switch, then we further evolve the
+            spins using the NRSur7dq2 model (arxiv:1705.07089) until t=-100M
+            from the peak of the waveform. Then we evaluate the fits using the
+            spins at t=-100 M. Finally, we transform the remnant spin and kick
+            vectors back to the inertial frame defined above.
+
+    Optional arguments:
+        allow_extrap:
+            If False, raises a warning when q > 2.1 or |chiA|,|chiB| > 0.81,
+                and raises an error when q > 4.1 or |chiA|,|chiB| > 1.
+            If True, allows extrapolation to any q and |chiA|,|chiB| <= 1.
+                Use at your own risk.
+            Default: False.
+
+        omega0:
+            Initial dimensionless orbital frequency in units of 1/M, where M is
+            the total mass. If this is given, the spins in x are assumed to be
+            the component spins at this orbital frequency, and in the inertial
+            frame as defined above. The returned remnant spin and kick vectors
+            are also in the same frame.
+            Default: None.
+
+        PN_approximant:
+            Approximant used to do the PN spin evolution. Choose from
+            'SpinTaylorT4', 'SpinTaylorT1' or 'SpinTaylorT5'.
+            Default: 'SpinTaylorT4'.
+
+        PN_dt:
+            Dimensionless time step size in units of M (total mass), used for
+            the PN evolution. You may need to increase this if omega0 is very
+            low.
+            Default: 0.1
+
+        PN_spin_order:
+            Twice the PN order of spin effects. E.g., use 7 for 3.5PN.
+            Default: 7
+
+        PN_phase_order:
+            Twice the PN order in phase. E.g., use 7 for 3.5PN.
+            Default: 7
+
+        omega_switch:
+            Dimensionless orbital frequency at which to switch from PN to
+            NRSur7dq2 model. You may need to increase this if the NRSur7dq2
+            model raises an exception like:
+            "Got omega_ref = 0.0180 < 0.0184 = omega_0, too small!"
+            Default: 0.018
+    """
+
+    #-------------------------------------------------------------------------
+    def __init__(self, name, load_nrsur=False):
+
+        # Param limits beyond which to raise a warning
+        soft_param_lims = {
+            'q': 2.1,
+            'chiAmag': 0.81,
+            'chiBmag': 0.81,
+                }
+
+        # Param limits beyond which to raise an error
+        hard_param_lims = {
+            'q': 4.1,
+            'chiAmag': 1,
+            'chiBmag': 1,
+                }
+
+        super(Fit7dq2, self).__init__(name, soft_param_lims, hard_param_lims)
+        self.nrsur = None
+
+    #-------------------------------------------------------------------------
+    def _load_NRSur7dq2(self):
+        import NRSur7dq2
+        self.nrsur = NRSur7dq2.NRSurrogate7dq2()
+        print('Loaded NRSur7dq2 waveform model')
+
+    #-------------------------------------------------------------------------
+    def _load_fits(self, h5file):
+        """ Loads fits from h5file and returns a dictionary of fits. """
+        fits = {}
+        for key in ['mf']:
+            fits[key] = self._load_scalar_fit(fit_key=key, h5file=h5file)
+        for key in ['chif', 'vf']:
+            fits[key] = self._load_vector_fit(key, h5file)
+        return fits
+
+    #-------------------------------------------------------------------------
+    def _extra_regression_kwargs(self):
+        """ List of additional kwargs to use in regression tests.
+        """
+
+        # larger than default sometimes needed when extrapolating
+        omega_switch_test = 0.019
+
+        extra_args = []
+        extra_args.append({
+            'omega0': 5e-3,
+            'PN_approximant': 'SpinTaylorT4',
+            'PN_dt': 0.1,
+            'PN_spin_order': 7,
+            'PN_phase_order': 7,
+            'omega_switch': omega_switch_test,
+            })
+
+
+        extra_args.append({
+            'omega0': 6e-3,
+            'PN_approximant': 'SpinTaylorT1',
+            'PN_dt': 0.5,
+            'PN_spin_order': 5,
+            'PN_phase_order': 7,
+            'omega_switch': omega_switch_test,
+            })
+
+        extra_args.append({
+            'omega0': 7e-3,
+            'PN_approximant': 'SpinTaylorT5',
+            'PN_dt': 1,
+            'PN_spin_order': 7,
+            'PN_phase_order': 5,
+            'omega_switch': omega_switch_test,
+            })
+
+        # These should be pure NRSur7dq2
+        extra_args.append({'omega0': 3e-2})
+        extra_args.append({'omega0': 5e-2})
+
+        return extra_args
+
+    #-------------------------------------------------------------------------
+    def _get_fit_params(self, x, fit_key):
+        """ Transforms the input parameter to fit parameters for the 7dq2 model.
+    That is, maps from
+    x = [q, chiAx, chiAy, chiAz, chiBx, chiBy, chiBz]
+    fit_params = [np.log(q), chiAx, chiAy, chiHat, chiBx, chiBy, chi_a]
+
+    chiHat is defined in Eq.(3) of 1508.07253, but with chiAz and chiBz instead
+    of chiA and chiB.
+    chi_a = (chiAz - chiBz)/2.
+        """
+        q, chiAz, chiBz = x[0], x[3], x[6]
+        eta = q/(1.+q)**2
+        chi_wtAvg = (q*chiAz+chiBz)/(1.+q)
+        chiHat = (chi_wtAvg - 38.*eta/113.*(chiAz + chiBz))/(1. - 76.*eta/113.)
+        chi_a = (chiAz - chiBz)/2.
+
+        fit_params = [np.log(q), x[1], x[2], chiHat, x[4], x[5], chi_a]
+        return fit_params
+
+    #-------------------------------------------------------------------------
+    def _evolve_spins(self, q, chiA0, chiB0, omega0, PN_approximant,
+            PN_dt, PN_spin0, PN_phase0, omega0_nrsur):
+        """ Evolves spins of the component BHs from an initial orbital
+        frequency = omega0 until t=-100 M from the peak of the waveform.
+        If omega0 < omega0_nrsur, use PN to evolve the spins until
+        orbital frequency = omega0. Then evolves further with the NRSur7dq2
+        waveform model until t=-100M from the peak.
+
+        Assumes chiA0 and chiB0 are defined in the inertial frame defined
+        at orbital frequency = omega0 as:
+            The z-axis is along the Newtonian orbital angular momentum when the
+                PN orbital frequency = omega0.
+            The x-axis is along the line of separation from the smaller BH to
+                the larger BH at this frequency.
+            The y-axis completes the triad.
+
+        Returns spins in the coorbital frame at t=-100M, as well as the
+        coprecessing frame quaternion and orbital phase in the coprecessing
+        frame at this time.
+        """
+
+        if omega0 < omega0_nrsur:
+            # If omega0 is below the NRSur7dq2 start frequency, we use PN
+            # to evolve the spins until orbital frequency = omega0_nrsur.
+
+            # Note that we update omega0_nrsur here with the PN
+            # frequency that was closest to the input omega0_nrsur.
+            chiA0_nrsur_copr, chiB0_nrsur_copr, quat0_nrsur_copr, \
+                phi0_nrsur, omega0_nrsur \
+                = evolve_pn_spins(q, chiA0, chiB0, omega0,
+                    omega0_nrsur, approximant=PN_approximant,
+                    dt=PN_dt, spinO=PN_spin0,
+                    phaseO=PN_phase0)
+        else:
+            # If omega0>= omega0_nrsur, we evolve spins directly with NRSur7dq2
+            # waveform model. We set the coprecessing frame quaternion to
+            # identity and orbital phase to 0 at omega=omega0, hence the
+            # coprecessing frame is the same as the inertial frame here.
+
+            # Note that we update omega0_nrsur here and set it to omega0
+            chiA0_nrsur_copr, chiB0_nrsur_copr, quat0_nrsur_copr, \
+                phi0_nrsur, omega0_nrsur \
+                = chiA0, chiB0, [1,0,0,0], 0, omega0
+
+        # Load NRSur7dq2 if needed
+        if self.nrsur is None:
+            self._load_NRSur7dq2()
+
+        # evaluate NRSur7dq2 dynamics
+        # We set allow_extrapolation=True always since we test param limits
+        # independently
+        quat, orbphase, chiA_copr, chiB_copr = self.nrsur.get_dynamics(q,
+            chiA0_nrsur_copr, chiB0_nrsur_copr, init_quat=quat0_nrsur_copr,
+            init_phase=phi0_nrsur, omega_ref=omega0_nrsur,
+            allow_extrapolation=True)
+
+        # get data at time node where remnant fits are done
+        fitnode_time = -100
+        nodeIdx = np.argmin(np.abs(self.nrsur.tds - fitnode_time))
+        quat_fitnode = quat.T[nodeIdx]
+        orbphase_fitnode = orbphase[nodeIdx]
+
+        # get coorbital frame spins at the time node
+        chiA_coorb_fitnode = utils.rotate_in_plane(chiA_copr[nodeIdx],
+                orbphase_fitnode)
+        chiB_coorb_fitnode = utils.rotate_in_plane(chiB_copr[nodeIdx],
+                orbphase_fitnode)
+
+        return chiA_coorb_fitnode, chiB_coorb_fitnode, quat_fitnode, \
+            orbphase_fitnode
+
+    #-------------------------------------------------------------------------
+    def _eval_wrapper(self, fit_key, q, chiA, chiB, **kwargs):
+        """Evaluates the surfinBH7dq2 model.
+        """
+        chiA = np.array(chiA)
+        chiB = np.array(chiB)
+
+        # Warn/Exit if extrapolating
+        allow_extrap = kwargs.pop('allow_extrap', False)
+        self._check_param_limits(q, chiA, chiB, allow_extrap)
+
+        omega0 = kwargs.pop('omega0', None)
+        PN_approximant = kwargs.pop('PN_approximant', 'SpinTaylorT4')
+        PN_dt = kwargs.pop('PN_dt', 0.1)
+        PN_spin_order = kwargs.pop('PN_spin_order', 7)
+        PN_phase_order = kwargs.pop('PN_phase_order', 7)
+        omega_switch = kwargs.pop('omega_switch', 0.018)
+
+        self._check_unused_kwargs(kwargs)
+
+        if omega0 is None:
+            # If omega0 is given, assume chiA, chiB are the coorbital frame
+            # spins at t=-100 M.
+            x = np.concatenate(([q], chiA, chiB))
+        else:
+            # If omega0 is given, evolve the spins from omega0
+            # to t = -100 M from the peak.
+            chiA_coorb_fitnode, chiB_coorb_fitnode, quat_fitnode, \
+                orbphase_fitnode \
+                = self._evolve_spins(q, chiA, chiB, omega0,
+                    PN_approximant, PN_dt, PN_spin_order,
+                    PN_phase_order, omega_switch)
+            # x should contain coorbital frame spins at t=-100M
+            x = np.concatenate(([q], chiA_coorb_fitnode, chiB_coorb_fitnode))
+
+
+        def eval_vector_fit(x, fit_key):
+            res = self._evaluate_fits(x, fit_key)
+            fit_val = res.T[0]
+            fit_err = res.T[1]
+            if omega0 is not None:
+                # If spins were given in inertial frame at omega0,
+                # transform vectors and errors back to the same frame.
+                fit_val = utils.transform_vector_coorb_to_inertial(fit_val,
+                    orbphase_fitnode, quat_fitnode)
+                fit_err = utils.transform_error_coorb_to_inertial(fit_val,
+                    fit_err, orbphase_fitnode, quat_fitnode)
+            return fit_val, fit_err
+
+
+        if fit_key == 'mf' or fit_key == 'all':
+            mf, mf_err = self._evaluate_fits(x, 'mf')
+            if fit_key == 'mf':
+                return mf, mf_err
+
+        if fit_key == 'chif' or fit_key == 'all':
+            chif, chif_err = eval_vector_fit(x, 'chif')
+            if fit_key == 'chif':
+                return chif, chif_err
+
+        if fit_key == 'vf' or fit_key == 'all':
+            vf, vf_err = eval_vector_fit(x, 'vf')
+            if fit_key == 'vf':
+                return vf, vf_err
+
+        if fit_key == 'all':
+            return mf, chif, vf, mf_err, chif_err, vf_err
---
+++ packages/surfinBH/surfinBH-1.1.6-py3-none-any.whl/surfinBH/surfinBH.py
@@ -0,0 +1,315 @@
+"""surfinBH
+========
+
+Surrogate final black hole properties for mergers of binary black holes.
+See https://pypi.org/project/surfinBH/ for more details.
+"""
+__copyright__ = "Copyright (C) 2018 Vijay Varma"
+__email__ = "vvarma@caltech.edu"
+__status__ = "testing"
+__author__ = "Vijay Varma"
+__version__ = "1.1.6"
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import numpy as np
+import os, sys
+import h5py
+import warnings
+
+from . import _eval_pysur
+from ._dataPath import DataPath
+
+
+#=============================================================================
+class SurFinBH(object):
+    """
+Class to load and evaluate surrogate fits for final BH properties.
+Each derived class should do the following:
+
+    1. define _load_fits(self, h5file)
+    2. define _get_fit_params(self, x, fit_key)
+    3. define _eval_wrapper(self, fit_key, x, **kwargs)
+    4. define soft_param_lims and hard_param_lims.
+    5. define _extra_regression_kwargs, to test any additional kwargs used in
+          the _eval_wrapper method.
+
+See _fit_evaluators.fit_7dq2.py for an example.
+    """
+
+    #-------------------------------------------------------------------------
+    def __init__(self, name, soft_param_lims, hard_param_lims,
+            aligned_spin_only=False):
+        """
+        name: Name of the fit excluding the surfinBH prefix. Ex: 7dq2.
+        soft_param_lims: param limits beyond which to raise a warning.
+        hard_param_lims: param limits beyond which to raise an error.
+        aligned_spin_only: raise an error if given precessing spins.
+        See _fit_evaluators.fit_7dq2.py for an example.
+        """
+        self.name = name
+        self.soft_param_lims = soft_param_lims
+        self.hard_param_lims = hard_param_lims
+        self.aligned_spin_only = aligned_spin_only
+
+        h5file = h5py.File('%s/fit_%s.h5'%(DataPath(), name), 'r')
+        self.fits = self._load_fits(h5file)
+        h5file.close()
+
+    #-------------------------------------------------------------------------
+    def _read_dict(self, f):
+        """ Converts h5 groups to dictionaries
+        """
+        d = {}
+        for k, item in f.items():
+            if type(item) == h5py._hl.dataset.Dataset:
+                v = item[()]
+                if type(v) == np.string_:
+                    v = str(v)
+                if type(v) == str and v == "NONE":
+                    d[k] = None
+                elif type(v) == str and v == "EMPTYARR":
+                    d[k] = np.array([])
+                elif isinstance(v, bytes):
+                    d[k] = v.decode('utf-8')
+                else:
+                    d[k] = v
+
+            elif k[:5] == "DICT_":
+                d[k[5:]] = self._read_dict(item)
+            elif k[:5] == "LIST_":
+                tmpD = self._read_dict(item)
+                d[k[5:]] = [tmpD[str(i)] for i in range(len(tmpD))]
+        return d
+
+    #-------------------------------------------------------------------------
+    def _load_scalar_fit(self, fit_key=None, h5file=None, fit_data=None):
+        """ Loads a single fit
+        """
+        if (fit_key is None) ^ (h5file is None):
+            raise ValueError("Either specify both fit_key and h5file, or"
+                " neither")
+
+        if not ((fit_key is None) ^ (fit_data is None)):
+            raise ValueError("Specify exactly one of fit_key and fit_data.")
+
+        if fit_data is None:
+            fit_data = self._read_dict(h5file[fit_key])
+
+        if 'fitType' in fit_data.keys() and fit_data['fitType'] == 'GPR':
+            fit = _eval_pysur.evaluate_fit.getGPRFitAndErrorEvaluator(fit_data)
+        else:
+            fit = _eval_pysur.evaluate_fit.getFitEvaluator(fit_data)
+
+        return fit
+
+    #-------------------------------------------------------------------------
+    def _load_vector_fit(self, fit_key, h5file):
+        """ Loads a vector of fits
+        """
+        vector_fit = []
+        for i in range(len(h5file[fit_key].keys())):
+            fit_data = self._read_dict(h5file[fit_key]['comp_%d'%i])
+            vector_fit.append(self._load_scalar_fit(fit_data=fit_data))
+        return vector_fit
+
+    #-------------------------------------------------------------------------
+    def _evaluate_fits(self, x, fit_key):
+        """ Evaluates a particular fit by passing fit_key to self.fits.
+            Assumes self._get_fit_params() has been overriden.
+        """
+        fit = self.fits[fit_key]
+        fit_params = self._get_fit_params(np.copy(x), fit_key)
+        if type(fit) == list:
+            res = []
+            for i in range(len(fit)):
+                res.append(fit[i](fit_params))
+            return np.array(res)
+        else:
+            return fit(fit_params)
+
+    #-------------------------------------------------------------------------
+    def _check_unused_kwargs(self, kwargs):
+        """ Call this at the end of call module to check if all the kwargs have
+        been used. Assumes kwargs were extracted using pop.
+        """
+        if len(kwargs.keys()) != 0:
+            unused = ""
+            for k in kwargs.keys():
+                unused += "'%s', "%k
+            if unused[-2:] == ", ":     # get rid of trailing comma
+                unused = unused[:-2]
+            raise Exception('Unused keys in kwargs: %s'%unused)
+
+    #-------------------------------------------------------------------------
+    def _check_param_limits(self, q, chiA, chiB, allow_extrap):
+        """ Checks that params are within allowed range of paramters.
+        Raises a warning if outside self.soft_param_lims limits and
+        raises an error if outside self.hard_param_lims.
+        If allow_extrap=True, skips these checks.
+        """
+
+        if q < 1:
+            raise ValueError('Mass ratio should be >= 1.')
+
+        chiAmag = np.sqrt(np.sum(chiA**2))
+        chiBmag = np.sqrt(np.sum(chiB**2))
+        if chiAmag > 1 + 1e-14:
+            raise ValueError('Spin magnitude of BhA > 1.')
+        if chiBmag > 1 + 1e-14:
+            raise ValueError('Spin magnitude of BhB > 1.')
+
+        chiA = np.atleast_1d(chiA)
+        chiB = np.atleast_1d(chiB)
+        if len(chiA) != 3 or len(chiB) != 3:
+            raise TypeError("Expected input spins to be 3-vectors.")
+
+        if self.aligned_spin_only:
+            if np.sqrt(np.sum(chiA[:2]**2)) > 1e-14:
+                raise ValueError('The x & y components of chiA should be zero.')
+            if np.sqrt(np.sum(chiB[:2]**2)) > 1e-14:
+                raise ValueError('The x & y components of chiB should be zero.')
+
+        # Do not check param limits if allow_extrap=True
+        if allow_extrap:
+            return
+
+        if q > self.hard_param_lims['q']+ 1e-14:
+            raise ValueError('Mass ratio outside allowed range.')
+        elif q > self.soft_param_lims['q']:
+            warnings.warn('Mass ratio outside training range.')
+
+        if chiAmag > self.hard_param_lims['chiAmag']+ 1e-14:
+            raise ValueError('Spin magnitude of BhA outside allowed range.')
+        elif chiAmag > self.soft_param_lims['chiAmag']:
+            warnings.warn('Spin magnitude of BhA outside training range.')
+
+        if chiBmag > self.hard_param_lims['chiBmag']+ 1e-14:
+            raise ValueError('Spin magnitude of BhB outside allowed range.')
+        elif chiBmag > self.soft_param_lims['chiBmag']:
+            warnings.warn('Spin magnitude of BhB outside training range.')
+
+    #-------------------------------------------------------------------------
+    def _generate_random_params_for_tests(self):
+        """ Generate random parameters to use in tests.
+        """
+        # Generate params randomly within allowed values
+        q = np.random.uniform(1, self.hard_param_lims['q'])
+        chiAmag= np.random.uniform(0, self.hard_param_lims['chiAmag'])
+        chiBmag= np.random.uniform(0, self.hard_param_lims['chiBmag'])
+        if self.aligned_spin_only:
+            chiAph, chiBph = 0, 0
+            chiAth, chiBth = np.random.choice([0, np.pi]), \
+                np.random.choice([0, np.pi])
+        else:
+            chiAth = np.arccos(np.random.uniform(-1., 1.))
+            chiBth = np.arccos(np.random.uniform(-1., 1.))
+            chiAph = np.random.uniform(0, 2*np.pi)
+            chiBph = np.random.uniform(0, 2*np.pi)
+
+        chiA = [chiAmag*np.sin(chiAth)*np.cos(chiAph),
+                chiAmag*np.sin(chiAth)*np.sin(chiAph),
+                chiAmag*np.cos(chiAth)]
+
+        chiB = [chiBmag*np.sin(chiBth)*np.cos(chiBph),
+                chiBmag*np.sin(chiBth)*np.sin(chiBph),
+                chiBmag*np.cos(chiBth)]
+
+        return q, chiA, chiB
+
+
+    #-------------------------------------------------------------------------
+    #----------------------  Override these  ---------------------------------
+    #-------------------------------------------------------------------------
+
+    #-------------------------------------------------------------------------
+    def _load_fits(self, h5file):
+        """ Loads fits from h5file and returns a dictionary of fits. """
+        raise NotImplementedError("Please override me.")
+        return fits
+
+
+    #-------------------------------------------------------------------------
+    def _extra_regression_kwargs(self):
+        """ Add additional kwargs for regression tests. If not overriden,
+            this will be empty. See _fit_evaluators.fit_7dq2.py for an example.
+        """
+        return []
+
+    #-------------------------------------------------------------------------
+    def _get_fit_params(self, x, fit_key):
+        """ Maps from input params x to the fit_params used to evaluate the
+            fit.
+        """
+        raise NotImplementedError("Please override me.")
+        return fit_params
+
+    #-------------------------------------------------------------------------
+    def _eval_wrapper(self, fit_key, q, chiA, chiB, **kwargs):
+        """ Evaluates a particular fit. This varies for each surrogate.
+            Allowed values for fit_key are 'mf', 'chif' and 'vf' and 'all'.
+            chiA and chiB should have size 3.
+
+            Each derived class should have its own _eval_wrapper function but
+            call self._check_param_limits() first to do some sanity checks.
+            See _fit_evaluators.fit_7dq2.py for an example.
+        """
+        raise NotImplementedError("Please override me.")
+
+
+    #-------------------------------------------------------------------------
+    #----------------------   Call methods   ---------------------------------
+    #-------------------------------------------------------------------------
+
+    def mf(self, *args, **kwargs):
+        """ Evaluates fit and 1-sigma error estimate for remnant mass.
+        Returns:
+            mf, mf_err_est
+        """
+        return self._eval_wrapper('mf', *args, **kwargs)
+
+    def chif(self, *args, **kwargs):
+        """ Evaluates fit and 1-sigma error estimate for remnant spin.
+        Returns:
+            chif, chif_err_est
+
+        chif and chif_err_est are arrays of size 3.
+        """
+        return self._eval_wrapper('chif', *args, **kwargs)
+
+    def vf(self, *args, **kwargs):
+        """ Evaluates fit and 1-sigma error estimate for remnant kick velocity.
+        Returns:
+            vf, vf_err_est
+
+        vf and vf_err_est are arrays of size 3.
+        """
+        return self._eval_wrapper('vf', *args, **kwargs)
+
+    def all(self, *args, **kwargs):
+        """ Evaluates fit and 1-sigma error estimate for remnant mass, spin
+        and kick velocity.
+        Returns:
+            mf, chif, vf, mf_err_est, chif_err_est, vf_err_est
+
+        chif, vf, chif_err_est and vf_err_est are arrays of size 3.
+        """
+        return self._eval_wrapper('all', *args, **kwargs)
+
