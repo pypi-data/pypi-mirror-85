0857a7016861216cf4e33752323fd613134af50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:11 +0000

    Add package flake8-colors/0.1.7/flake8-colors-0.1.7.tar.gz

---
+++ packages/flake8-colors/flake8-colors-0.1.7.tar.gz/flake8-colors-0.1.7/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+
+import setuptools
+
+description = "Error highlight plugin for Flake8."
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = description
+
+requires = [
+    "flake8 > 3.0.0",
+]
+
+setuptools.setup(
+    name="flake8-colors",
+    license="MIT",
+    version="0.1.7",
+    description=description,
+    long_description=long_description or description,
+    long_description_content_type='text/markdown',
+    author="Andrew Dunai",
+    author_email="andrew@dun.ai",
+    url="https://github.com/and3rson/flake8-colors",
+    packages=[
+        "flake8_colors",
+    ],
+    install_requires=requires,
+    entry_points={
+        'flake8.extension': [
+            'flake8-colors = flake8_colors:ColorFormatter',
+        ],
+    },
+    classifiers=[
+        "Framework :: Flake8",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
