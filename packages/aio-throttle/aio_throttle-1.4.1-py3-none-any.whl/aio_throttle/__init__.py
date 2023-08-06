211a708e0e9ec8c977f9531e55986ef613d3664d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package aio-throttle/1.4.1/aio_throttle-1.4.1-py3-none-any.whl

---
+++ packages/aio-throttle/aio_throttle-1.4.1-py3-none-any.whl/aio_throttle/__init__.py
@@ -0,0 +1,3 @@
+from .throttle import Throttler  # noqa
+from .quotas import ThrottleCapacityQuota, MaxFractionCapacityQuota, ThrottleQuota, RandomRejectThrottleQuota  # noqa
+from .abc import ThrottlerBase, ThrottlePriority, ThrottleStats, ThrottleResult  # noqa
