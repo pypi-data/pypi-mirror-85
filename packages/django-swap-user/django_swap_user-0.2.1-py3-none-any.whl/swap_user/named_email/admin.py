6ad9ab43579e349449c0c06cd8ed02a76912b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:29 +0000

    Add package django-swap-user/0.2.1/django_swap_user-0.2.1-py3-none-any.whl

---
+++ packages/django-swap-user/django_swap_user-0.2.1-py3-none-any.whl/swap_user/named_email/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from swap_user.named_email.models import NamedEmailUser
+
+admin.site.register(NamedEmailUser)
---
+++ packages/django-swap-user/django_swap_user-0.2.1-py3-none-any.whl/swap_user/named_email/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NamedEmailUserConfig(AppConfig):
+    name = "swap_user.named_email"
+    label = "swap_user_named_email"
+    verbose_name = "NamedEmailUser"
