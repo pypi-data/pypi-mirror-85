63cd15bb76a020f0dbdbdc9f1f1af02a4b7c7d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package TOPSIS-Aditi-101803029/1.0.1/TOPSIS-Aditi-101803029-1.0.1.tar.gz

---
+++ packages/TOPSIS-Aditi-101803029/TOPSIS-Aditi-101803029-1.0.1.tar.gz/TOPSIS-Aditi-101803029-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as file:
+        README = file.read()
+    return README
+
+setup(
+    name="TOPSIS-Aditi-101803029",
+    version="1.0.1",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Aditi Dona",
+    author_email="adona_be18@thapar.edu",
+    url = 'https://coe1316.ml/',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["src"],
+    include_package_data=True,
+    install_requires=['sys',
+                      'math',
+                      'numpy',
+                      'pandas',
+     ],
+     entry_points={
+        "console_scripts": [
+            "topsis=src.topsis:main",
+        ]
+     },
+)
