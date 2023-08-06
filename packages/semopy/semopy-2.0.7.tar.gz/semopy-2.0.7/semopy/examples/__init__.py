d755c7fdfb10f9648e09aa015d3a0c8ce08609f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:15 +0000

    Add package semopy/2.0.7/semopy-2.0.7.tar.gz

---
+++ packages/semopy/semopy-2.0.7.tar.gz/semopy-2.0.7/semopy/examples/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Examples for semopy."""
+from . import political_democracy
+from . import univariate_regression
+from . import multivariate_regression
+from . import holzinger39
+from . import example_rf
+from . import example_article
