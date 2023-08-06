3ad48e605639f52b3f4cd9314c402fcd4bad39ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:33 +0000

    Add package tradfricoap/0.0.12/tradfricoap-0.0.12.tar.gz

---
+++ packages/tradfricoap/tradfricoap-0.0.12.tar.gz/tradfricoap-0.0.12/tradfricoap/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.12"
+
+class Error(Exception):
+    pass
+
+class IllegalMethodError(Exception):
+    pass
+
+class ApiNotFoundError(Error):
+    def __init__(self, api, message):
+        self.message = message
+        self.api = api
