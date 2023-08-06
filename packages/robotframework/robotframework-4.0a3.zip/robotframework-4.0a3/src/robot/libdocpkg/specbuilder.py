9380864dfdce7a178ce9bcc89ff16f83634a6b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:46 +0000

    Add package robotframework/4.0a3/robotframework-4.0a3.zip

---
+++ packages/robotframework/robotframework-4.0a3.zip/robotframework-4.0a3/src/robot/libdocpkg/specbuilder.py
@@ -0,0 +1,93 @@
+#  Copyright 2008-2015 Nokia Networks
+#  Copyright 2016-     Robot Framework Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os.path
+
+from robot.errors import DataError
+from robot.utils import ET, ETSource
+from robot.running.arguments import ArgumentSpec, ArgInfo
+
+from .model import LibraryDoc, KeywordDoc
+
+
+class SpecDocBuilder(object):
+
+    def build(self, path):
+        spec = self._parse_spec(path)
+        libdoc = LibraryDoc(name=spec.get('name'),
+                            type=spec.get('type').upper(),
+                            version=spec.find('version').text or '',
+                            doc=spec.find('doc').text or '',
+                            scope=spec.get('scope'),
+                            doc_format=spec.get('format', 'ROBOT'),
+                            source=spec.get('source'),
+                            lineno=int(spec.get('lineno', -1)))
+        libdoc.inits = self._create_keywords(spec, 'inits/init')
+        libdoc.keywords = self._create_keywords(spec, 'keywords/kw')
+        return libdoc
+
+    def _parse_spec(self, path):
+        if not os.path.isfile(path):
+            raise DataError("Spec file '%s' does not exist." % path)
+        with ETSource(path) as source:
+            root = ET.parse(source).getroot()
+        if root.tag != 'keywordspec':
+            raise DataError("Invalid spec file '%s'." % path)
+        version = root.get('specversion')
+        if version != '3':
+            raise DataError("Invalid spec file version '%s'. "
+                            "Robot Framework 4.0 and newer requires spec version 3." % version)
+        return root
+
+    def _create_keywords(self, spec, path):
+        return [self._create_keyword(elem) for elem in spec.findall(path)]
+
+    def _create_keyword(self, elem):
+        # "deprecated" attribute isn't read because it is read from the doc
+        # automatically. That should probably be changed at some point.
+        return KeywordDoc(name=elem.get('name', ''),
+                          args=self._create_arguments(elem),
+                          doc=elem.find('doc').text or '',
+                          shortdoc=elem.find('shortdoc').text or '',
+                          tags=[t.text for t in elem.findall('tags/tag')],
+                          source=elem.get('source'),
+                          lineno=int(elem.get('lineno', -1)))
+
+    def _create_arguments(self, elem):
+        spec = ArgumentSpec()
+        setters = {
+            ArgInfo.POSITIONAL_ONLY: spec.positional_only.append,
+            ArgInfo.POSITIONAL_ONLY_MARKER: lambda value: None,
+            ArgInfo.POSITIONAL_OR_NAMED: spec.positional_or_named.append,
+            ArgInfo.VAR_POSITIONAL: lambda value: setattr(spec, 'var_positional', value),
+            ArgInfo.NAMED_ONLY_MARKER: lambda value: None,
+            ArgInfo.NAMED_ONLY: spec.named_only.append,
+            ArgInfo.VAR_NAMED: lambda value: setattr(spec, 'var_named', value),
+        }
+        for arg in elem.findall('arguments/arg'):
+            name_elem = arg.find('name')
+            if name_elem is None:
+                continue
+            name = name_elem.text
+            setters[arg.get('kind')](name)
+            default_elem = arg.find('default')
+            if default_elem is not None:
+                spec.defaults[name] = default_elem.text or ''
+            type_elem = arg.find('type')
+            if type_elem is not None:
+                if not spec.types:
+                    spec.types = {}
+                spec.types[name] = type_elem.text
+        return spec
---
+++ packages/robotframework/robotframework-4.0a3.zip/robotframework-4.0a3/src/robot/parsing/parser/blockparsers.py
@@ -0,0 +1,82 @@
+#  Copyright 2008-2015 Nokia Networks
+#  Copyright 2016-     Robot Framework Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from ..lexer import Token
+from ..model import TestCase, Keyword, ForLoop
+from ..model.blocks import IfBlock
+
+
+class Parser(object):
+    """Base class for parsers."""
+
+    def __init__(self, model):
+        self.model = model
+
+    def handles(self, statement):
+        raise NotImplementedError
+
+    def parse(self, statement):
+        raise NotImplementedError
+
+
+class StepsParser(Parser):
+
+    def __init__(self, model, unhandled_tokens):
+        Parser.__init__(self, model)
+        self._subsection_parser_classes = {Token.FOR: ForLoopParser, Token.IF: IfParser}
+        self._unhandled_tokens = unhandled_tokens
+
+    def handles(self, statement):
+        return statement.type not in self._unhandled_tokens
+
+    def parse(self, statement):
+        parser_class = self._subsection_parser_classes.get(statement.type)
+        if parser_class:
+            parser = parser_class(statement)
+            self.model.body.append(parser.model)
+            return parser
+        self.model.body.append(statement)
+
+
+def TestCaseParser(header):
+    return StepsParser(TestCase(header), Token.HEADER_TOKENS + (Token.TESTCASE_NAME,))
+
+
+def KeywordParser(header):
+    return StepsParser(Keyword(header), Token.HEADER_TOKENS + (Token.KEYWORD_NAME,))
+
+
+class StepsWithEndParser(StepsParser):
+
+    def __init__(self, model):
+        StepsParser.__init__(self, model, Token.HEADER_TOKENS + (Token.TESTCASE_NAME, Token.KEYWORD_NAME))
+
+    def handles(self, statement):
+        if self.model.end:
+            return False
+        return StepsParser.handles(self, statement)
+
+    def parse(self, statement):
+        if statement.type == Token.END:
+            self.model.end = statement
+            return
+        return StepsParser.parse(self, statement)
+
+
+def ForLoopParser(header):
+    return StepsWithEndParser(ForLoop(header))
+
+def IfParser(header):
+    return StepsWithEndParser(IfBlock(header))
