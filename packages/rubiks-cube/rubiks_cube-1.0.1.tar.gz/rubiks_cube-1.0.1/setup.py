10005645a99b0411e40245841dd669f3240e22c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:15 +0000

    Add package rubiks-cube/1.0.1/rubiks_cube-1.0.1.tar.gz

---
+++ packages/rubiks-cube/rubiks_cube-1.0.1.tar.gz/rubiks_cube-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rubiks_cube',
+    version='1.0.1',
+    description='A Rubiks rubiks_cube Package for Python',
+    url='https://github.com/IguanaAzul/RubiksCube.git',
+    author='Ramon Griffo',
+    author_email='ramon.griffo98@gmail.com',
+    license='MIT License',
+    packages=['rubiks_cube'],
+    install_requires=['numpy'],
+)
