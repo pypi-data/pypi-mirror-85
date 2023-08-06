9d4a1ea3ea3e131079e513779e11decb0fb34572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:51 +0000

    Add package UniversalGPIO/1.0.1/UniversalGPIO-1.0.1-py3-none-any.whl

---
+++ packages/UniversalGPIO/UniversalGPIO-1.0.1-py3-none-any.whl/UniversalGPIO/__init__.py
@@ -0,0 +1,19 @@
+'''
+Python Library for interfacing GPIO Pins on Linux based Routers, Network Devices, and embeded Devices like Rasapberry pi and OpenWRT based Routers 
+
+Maintainer: Harsh Singhvi (@harshsinghvi)
+includes Modules:
+ - GPIO
+ - Dignostics (To be updated Soon)
+ - API Module (To be updated Soon)
+ 
+
+'''
+__version__ = "1.0.1"
+
+def version():
+	return __version__
+
+if __name__ == '__main__':
+	print("Welcome to UniversalGPIO Package")
+
