cd2b41b161cd0b2d403b2ff383230ef4764fc5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:31 +0000

    Add package graph-polisher/1.0.0/graph_polisher-1.0.0-py3-none-any.whl

---
+++ packages/graph-polisher/graph_polisher-1.0.0-py3-none-any.whl/graph_polisher/tests/test_cleaner.py
@@ -0,0 +1,29 @@
+import plotly.graph_objects as go
+
+from graph_polisher import cleaner
+from graph_polisher.configs import GREY
+
+
+def test_remove_grids():
+    """Should set show grid to false for y and x axis"""
+    fig = go.Figure()
+    cleaned_fig = cleaner.remove_grids(figure=fig)
+    assert not cleaned_fig.layout.xaxis.showgrid
+    assert not cleaned_fig.layout.yaxis.showgrid
+
+
+def test_remove_background():
+    """Should set paper bg color and plot bg color to rgba(0,0,0,0)"""
+    fig = go.Figure()
+    cleaned_fig = cleaner.remove_background(figure=fig)
+    assert cleaned_fig.layout.plot_bgcolor == 'rgba(0,0,0,0)'
+
+
+def test_send_to_background():
+    """Should set everything grey"""
+    fig = go.Figure()
+    cleaned_fig = cleaner.send_to_background(figure=fig)
+
+    assert cleaned_fig.layout.title.font.color == GREY
+    assert cleaned_fig.layout.xaxis.color == GREY
+    assert cleaned_fig.layout.yaxis.color == GREY
