7079ad8b53bbe204e82cec90fc242dd35ce0c463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:11 +0000

    Add package pyecoregen/0.5.0/pyecoregen-0.5.0.tar.gz

---
+++ packages/pyecoregen/pyecoregen-0.5.0.tar.gz/pyecoregen-0.5.0/CHANGELOG.md
@@ -0,0 +1,59 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project
+adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## 0.5.0
+### Added
+
+- Added generation of "many" derived `EStructuralFeature` as `EDerivedCollection`.
+- Added support for non unique collections of `EAttribute`.
+
+### Fixed
+- Missing generation of "single" derived `EReference`.
+- Keyword handling for metaclasses.
+
+### Removed
+- Removed support for Python 3.4.
+
+
+## 0.4.3
+### Added
+
+- Added option to auto-generate the Python code for the metamodel's dependencies,
+  see the `--with-dependencies` option.
+
+### Fixed
+- Missing imports to `EDataType` references that are contained in a metamodel dependency.
+- Missing default value generation for `EAttribute`.
+- Missing `eSubpackages` and `eSuperPackages` at module level.
+
+### Removed
+- Removed support for Python 3.3.
+
+## 0.4.2
+### Fixed
+
+- Missing 'templates' in the sdist package. The sdist package (`.tar.gz`) is
+  built using information of the `MANIFEST.in` for non-source files. The
+  templates were simply missing in it.
+
+## 0.4.1
+### Fixed
+
+- README
+
+## 0.4.0
+### Added
+
+- Added option to auto-register generated package in global registry, see the
+  `--auto-register-package` option.
+- Added option to specify a user module, from where mixin classes. These mixins must then implement
+  all end-user specific code like operations and derived attributes. See the `--user-module`
+  command line option.
+
+### Fixed
+
+- EDatatype registration.
+- Various derived attribute fixes.
---
+++ packages/pyecoregen/pyecoregen-0.5.0.tar.gz/pyecoregen-0.5.0/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+if sys.version_info < (3, 5):
+    sys.exit('Sorry, Python < 3.5 is not supported')
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        args = self.pytest_args if isinstance(self.pytest_args, list) else [self.pytest_args]
+        errno = pytest.main(args)
+        sys.exit(errno)
+
+
+setup(
+    name="pyecoregen",
+    version='0.5.0',
+    description="Model to text framework for PyEcore, including the Ecore to Python generator",
+    long_description=open('README.rst').read(),
+    keywords="model metamodel EMF Ecore code generator",
+    url="https://github.com/pyecore/pyecoregen",
+    author="Mike Pagel",
+    author_email="mike@mpagel.de",
+
+    packages=find_packages(exclude=['tests']),
+    package_data={'': ['README.rst', 'LICENSE'],
+                  'pyecoregen': ['templates/*']},
+    include_package_data=True,
+    install_requires=['pyecore', 'pymultigen', 'jinja2', 'autopep8'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    entry_points={'console_scripts': ['pyecoregen = pyecoregen.cli:main']},
+
+    license='BSD 3-Clause',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: BSD License",
+    ]
+)
