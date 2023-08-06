683888b83fed03adbd6faa6c80d7de9f86f03283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:16 +0000

    Add package TOPSIS-Shrey-101803346/1.0.0/TOPSIS-Shrey-101803346-1.0.0.tar.gz

---
+++ packages/TOPSIS-Shrey-101803346/TOPSIS-Shrey-101803346-1.0.0.tar.gz/TOPSIS-Shrey-101803346-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='TOPSIS-Shrey-101803346',
+  version='1.0.0',
+  description='A very basic file of topsis',
+  url='',  
+  author='SHREY',
+  author_email='shreysawhney1999@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='topsis', 
+  packages=['TOPSIS-Shrey-101803346'] ,
+  install_requires=[''] 
+)
