42d348d75d48133ad9484492ba2b2de751a66982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:04 +0000

    Add package oodocument/0.0.4/oodocument-0.0.4-py3-none-any.whl

---
+++ packages/oodocument/oodocument-0.0.4-py3-none-any.whl/oodocument/test.py
@@ -0,0 +1,6 @@
+from oodocument import oodocument
+data = {}
+data['holamundo'] = 'XXX'
+oo = oodocument('./input.docx', host='0.0.0.0', port=8001)
+oo.replace_with(data, './output.doc', 'doc')
+oo.dispose()
