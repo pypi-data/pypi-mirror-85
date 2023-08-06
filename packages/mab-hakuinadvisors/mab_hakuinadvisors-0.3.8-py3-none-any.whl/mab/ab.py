26f0146cea43e052f89c5917027ed0a43f1238f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:47 +0000

    Add package mab-hakuinadvisors/0.3.8/mab_hakuinadvisors-0.3.8-py3-none-any.whl

---
+++ packages/mab-hakuinadvisors/mab_hakuinadvisors-0.3.8-py3-none-any.whl/mab/ab.py
@@ -0,0 +1,76 @@
+from .mab import MAB
+import random
+
+
+class AB(MAB):
+    """AB testing Class for Multiarm Bandit Application
+
+    ...
+
+    Attributes:
+    ----------
+
+    counts : list[int]
+        number of times event happend for each arm
+
+    values : list[float]
+        total rewards for each arm
+
+    n_arms : int
+        number of arms
+
+    Methods:
+    -----------
+    All the methods from MAB plus
+
+    select_arm()
+        select arm one by one sequentially
+
+    """
+
+    def __init__(self, counts=None, values=None, n_arms=None):
+        """
+        Args:
+            counts (list[int]): number of times event happend for each arm.
+                                Defaults to [0] * n_arms
+            values (list[float]): total rewards for each arm
+                                Defaults to [0.0] * n_arms
+            n_arms (int): Number of arms. Defaults to len(counts)
+        """
+        super().__init__(counts, values, n_arms)
+        self.current_arm = 0
+
+    @property
+    def name(self):
+        """name of the algorythm
+
+        Returns:
+            str: name of the algorythm
+        """
+        return "AB-test"
+
+    @property
+    def marketing_name(self):
+        """High level produnction name"""
+        return "One by one A/B"
+
+    def select_arm(self):
+        """Select next arm one by one
+
+        Returns:
+            int: index of the next arm to select following to the previous arm
+        """
+        current_arm = self.current_arm
+        # switch arm to the next position
+        if self.current_arm == self.n_arms - 1:
+            self.current_arm = 0
+        else:
+            self.current_arm += 1
+
+        # return the arm before
+        return current_arm
+
+    def reset(self):
+        """Reset the algorithm to the initial state"""
+        super(AB, self).reset()
+        self.current_arm = 0
---
+++ packages/mab-hakuinadvisors/mab_hakuinadvisors-0.3.8-py3-none-any.whl/mab/viz.py
@@ -0,0 +1,119 @@
+import plotly.graph_objects as go
+import numpy as np
+
+
+def plot(
+    mc,
+    metric="accuracy",
+    title="",
+    horizon=None,
+    add_diagonal=False,
+    is_marketing_name=False,
+    is_percents=False,
+):
+    """
+        Help function to make plotly charts of the monte carlo simulation
+
+    Args:
+        mc (MonteCarloSimulation): mc simulaions for different algorythms
+        metric (str): Metric to plot. Defaults to "accuracy".
+        title (str): Title of the plot. Defaults to "".
+        horizon (int): Time horizon. Nuber of times arm was selected
+
+    Returns:
+        [plotly.fig]: returns figure with data
+    """
+    if horizon is None:
+        horizon = mc.horizon
+
+    fig = go.Figure()
+    for m in mc:
+        algorythm_name = m.marketing_name if is_marketing_name else m.name
+
+        fig.add_trace(
+            go.Scatter(
+                x=np.array(range(horizon)),
+                y=np.array(m.metrics[metric]),
+                mode="lines",
+                name=algorythm_name,
+            )
+        )
+
+    if add_diagonal:
+        fig.add_trace(
+            go.Scatter(
+                x=np.array(range(horizon)),
+                y=np.array(range(horizon)),
+                mode="lines",
+                name="Best Possible",
+            )
+        )
+
+    fig.update_layout(
+        title=title, autosize=True,
+    )
+    if is_percents:
+        fig.update_layout(yaxis=dict(tickformat="0.0%"))
+
+    return fig
+
+
+def plot(
+    metrics,
+    names,
+    metric_name="accuracy",
+    title=None,
+    horizon=None,
+    add_diagonal=False,
+    is_marketing_name=False,
+    is_percents=False,
+):
+    """
+        Help function to make plotly charts of the monte carlo simulation
+
+    Args:
+        measurements (list): mc simulaions for different algorythms
+        metric (str): Metric to plot. Defaults to "accuracy".
+        title (str): Title of the plot. Defaults to "".
+        horizon (int): Time horizon. Nuber of times arm was selected
+
+    Returns:
+        [plotly.fig]: returns figure with data
+    """
+    if horizon is None:
+        horizon = len(metrics[0][metric_name])
+
+    if title is None:
+        title = metric_name.replace("_", " ").capitalize()
+
+    fig = go.Figure()
+    for i, m in enumerate(metrics):
+        algorithm_name = names[i]
+
+        fig.add_trace(
+            go.Scatter(
+                x=np.array(range(horizon)),
+                y=np.array(m[metric_name]),
+                mode="lines",
+                name=algorithm_name,
+            )
+        )
+
+    if add_diagonal:
+        fig.add_trace(
+            go.Scatter(
+                x=np.array(range(horizon)),
+                y=np.array(range(horizon)),
+                mode="lines",
+                name="Best Possible",
+            )
+        )
+
+    fig.update_layout(
+        title=title, autosize=True,
+    )
+    fig.update_yaxes(rangemode="tozero")
+    if is_percents:
+        fig.update_layout(yaxis=dict(tickformat="0.0%"))
+
+    return fig
