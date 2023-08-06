c8f73e26244526ff800647d9f1e849fee982c13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:42 +0000

    Add package pysimplendi/0.1.3/pysimplendi-0.1.3.tar.gz

---
+++ packages/pysimplendi/pysimplendi-0.1.3.tar.gz/pysimplendi-0.1.3/setup.py
@@ -0,0 +1,62 @@
+import os
+import platform
+import pathlib
+
+from glob import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+
+# version info
+__version__ = "0.1.3"
+
+
+# add libndi location to library_dirs
+library_dirs = []
+libraries = []
+extra_link_args = []
+system = platform.system()
+
+if system == "Windows":
+    print(os.path.expandvars('$NDI_SDK_DIR'))
+    if os.environ['NDI_SDK_DIR']:
+        ndi_dir = pathlib.Path(os.environ['NDI_SDK_DIR'] + "/Lib/x86")
+        print(ndi_dir.as_posix())
+        library_dirs.append(ndi_dir.as_posix())
+        libraries.append("Processing.NDI.Lib.x86")
+elif system == "Darwin":
+    library_dirs.append("/Library/NDI SDK for Apple/lib/x64")
+    extra_link_args.append("-Wl,-rpath,"+"/Library/NDI SDK for Apple/lib/x64")
+    libraries.append("ndi")
+elif system == "Linux":
+    library_dirs.append("/usr/lib")
+    libraries.append("ndi")
+
+
+# define ext_module
+ext_module = Pybind11Extension("pysimplendi",
+                               sorted(glob("src/*.cpp")),
+                               include_dirs=["libs/include"],
+                               library_dirs=library_dirs,
+                               libraries=libraries,
+                               define_macros=[('VERSION_INFO', __version__)],
+                               extra_link_args=extra_link_args
+                               )
+
+
+setup(
+    name='pysimplendi',
+    version=__version__,
+    license='MIT',
+    author='Kosaku Namikawa',
+    author_email='cigarbox.nami@gmail.com',
+
+    description='simple python NDI sender/receiver',
+    long_description='',
+    url='https://github.com/73-ch/pySimpleNDI',
+
+    install_requires=["setuptools", "wheel", "pybind11==2.6.0"],
+    ext_modules=[ext_module],
+    cmdclass=dict(build_ext=build_ext),
+    zip_safe=False,
+)
---
+++ packages/pysimplendi/pysimplendi-0.1.3.tar.gz/pysimplendi-0.1.3/src/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+
+#include "NDISender.h"
+#include "NDIReceiver.h"
+
+PYBIND11_MODULE(pysimplendi, m) {
+    m.doc() = "Python binding of NDI";
+
+    py::class_<NDISender>(m, "NDISender")
+        .def(py::init<const std::string&>())
+        .def("send", (void (NDISender::*)(py::array_t<unsigned char>)) &NDISender::send)
+        .def("setName", (void (NDISender::*)(std::string)) &NDISender::setName);
+
+    py::class_<NDIReceiver>(m, "NDIReceiver")
+        .def(py::init<>())
+        .def("getSourceList", (std::vector<std::string> (NDIReceiver::*)()) &NDIReceiver::getSourceList)
+        .def("setSource", (bool (NDIReceiver::*)(const std::string)) &NDIReceiver::setSource)
+        .def("getCurrentFrame", (py::array_t<unsigned char>(NDIReceiver::*)()) &NDIReceiver::getCurrentFrame)
+        .def("addHandler", (void (NDIReceiver::*)(const std::string& name, const std::function<void(py::array_t<unsigned char>)> &f)) &NDIReceiver::addHandler)
+        .def("removeHandler", (void (NDIReceiver::*)(const std::string& name)) &NDIReceiver::removeHandler);
+}
