8b3d71aecf5615feea8ee088297ccd5dfc98af8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:13 +0000

    Add package OhMyProfiles/0.12/OhMyProfiles-0.12.tar.gz

---
+++ packages/OhMyProfiles/OhMyProfiles-0.12.tar.gz/OhMyProfiles-0.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='OhMyProfiles',
+    version='v0.12',
+    description='A  module for manager profile',
+    py_modules=['OhMyProfiles'],
+    author='johnpoint',
+    author_email='me@lvcshu.com',
+    url='https://github.com/johnpoint/Oh-My-Profiles',
+    requires=['hashlib', 'os', 'sys', 'json', 'uuid', 'zipfile'],
+    license='GPL-3.0',
+    entry_points={
+        'console_scripts': [
+            'omp=OhMyProfiles.omp:entry',
+        ]
+    }
+)
