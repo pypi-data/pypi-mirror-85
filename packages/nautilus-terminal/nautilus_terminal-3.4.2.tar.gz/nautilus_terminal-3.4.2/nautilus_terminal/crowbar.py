fe1436f862b03c21e1385d43893bf2b1d06149f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:07 +0000

    Add package nautilus-terminal/3.4.2/nautilus_terminal-3.4.2.tar.gz

---
+++ packages/nautilus-terminal/nautilus_terminal-3.4.2.tar.gz/nautilus_terminal-3.4.2/nautilus_terminal/crowbar.py
@@ -0,0 +1,25 @@
+from gi.repository import Gtk
+
+from . import logger
+from . import helpers
+from .nautilus_terminal import create_or_update_natilus_terminal
+
+
+class Crowbar(Gtk.EventBox):
+
+    def __init__(self, uri, window):
+        super(Crowbar, self).__init__()
+
+        self.uri = uri
+        self.nautilus_window = window
+        self.nautilus_app = window.get_application()
+        self.path = helpers.gvfs_uri_to_path(uri)
+
+        self.connect_after("parent-set", self._on_parent_set)
+
+    def _on_parent_set(self, widget, old_parent):
+        if old_parent:
+            self.destroy()
+            return
+        logger.log("Crowbar inserted at %s" % self.path)
+        create_or_update_natilus_terminal(self)
---
+++ packages/nautilus-terminal/nautilus_terminal-3.4.2.tar.gz/nautilus_terminal-3.4.2/nautilus_terminal/nautilus_terminal_extension.py
@@ -0,0 +1,35 @@
+# This file is a part of the Nautilus Temrinal package
+
+# This file is the Nautilus extension itself, it must be placed
+# in one of the following folder to make allow the extension to work:
+#
+# * /usr/share/nautilus-python/extensions/
+# * ~/.local/share/nautilus-python/extensions/
+
+import os
+import sys
+
+from gi.repository import GObject, Nautilus
+
+
+DEBUG = "NAUTILUS_TERMINAL_DEBUG_PACKAGE_PATH" in os.environ
+
+
+if DEBUG:
+    sys.path.insert(0, os.environ["NAUTILUS_TERMINAL_DEBUG_PACKAGE_PATH"])
+
+
+from nautilus_terminal.crowbar import Crowbar  # noqa
+
+
+if DEBUG:
+    print("\x1B[1;34m#### Starting Nautilus Terminal [DEBUG] ####\x1B[0m")
+    print("PYTHON_PATH: %s\n" % ":".join(sys.path))
+else:
+    print("* Starting Nautilus Terminal")
+
+
+class NautilusTerminalLocationWidgetProvider(GObject.GObject, Nautilus.LocationWidgetProvider):
+
+    def get_widget(self, uri, window):
+        return Crowbar(uri, window)
