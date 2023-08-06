ec9902debe54aa168dbdf6cadf10a08f5f367a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:35 +0000

    Add package xone/0.1.6b5/xone-0.1.6b5.tar.gz

---
+++ packages/xone/xone-0.1.6b5.tar.gz/xone-0.1.6b5/xone/__init__.py
@@ -0,0 +1,12 @@
+"""Frequently used functions for financial data analysis"""
+
+__version__ = '0.1.6b5'
+
+__submodules__ = [
+    'utils',
+    'calendar',
+    'files',
+    'procs',
+    'plots',
+    'xql',
+]
