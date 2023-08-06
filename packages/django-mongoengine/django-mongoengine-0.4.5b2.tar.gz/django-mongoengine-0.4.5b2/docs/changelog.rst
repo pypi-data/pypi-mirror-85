5e41d40c5480fe4799c12b9c2454b9712162f724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package django-mongoengine/0.4.5b2/django-mongoengine-0.4.5b2.tar.gz

---
+++ packages/django-mongoengine/django-mongoengine-0.4.5b2.tar.gz/django-mongoengine-0.4.5b2/docs/changelog.rst
@@ -0,0 +1,16 @@
+=========
+Changelog
+=========
+
+
+Changes in -master-
+===================
+* working sessions
+* forms?
+* added support for embedded fields
+
+
+Changes in 0.1
+===============
+
+* Released to PyPi
---
+++ packages/django-mongoengine/django-mongoengine-0.4.5b2.tar.gz/django-mongoengine-0.4.5b2/setup.py
@@ -0,0 +1,61 @@
+"""
+Django-MongoEngine
+------------------
+
+Django support for MongoDB using MongoEngine.
+
+This is work-in-progress. Some things working, some don't. Fix what you need and make
+pull-request.
+
+Links
+`````
+
+* `development version
+  <https://github.com/MongoEngine/django-mongoengine>`_
+
+"""
+from setuptools import setup, find_packages
+import sys
+import os
+
+
+__version__ = '0.4.5-beta.2'
+__description__ = 'Django support for MongoDB via MongoEngine'
+__license__ = 'BSD'
+__author__ = 'Ross Lawley'
+__email__ = 'ross.lawley@gmail.com'
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+setup(
+    name='django-mongoengine',
+    version=__version__,
+    url='https://github.com/mongoengine/django-mongoengine',
+    download_url='https://github.com/mongoengine/django-mongoengine/tarball/master',
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    description=__description__,
+    long_description=__doc__,
+    test_suite='nose.collector',
+    zip_safe=False,
+    platforms='any',
+    install_requires=["django>2.2,<3.2", "six", "mongoengine>=0.14"],
+    packages=find_packages(exclude=('doc', 'docs',)),
+    include_package_data=True,
+    # use python setup.py nosetests to test
+    setup_requires=['nose', 'coverage'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django'
+    ]
+)
