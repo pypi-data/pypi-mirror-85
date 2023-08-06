8992991cc3aa71a45038fcdada7e0a5b2ec74c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package wmc-dl-functions/1.1/wmc_dl_functions-1.1.tar.gz

---
+++ packages/wmc-dl-functions/wmc_dl_functions-1.1.tar.gz/wmc_dl_functions-1.1/wmc_dl/__init__.py
@@ -0,0 +1,7 @@
+'''
+python setup.py sdist
+python -m twine upload -u DanielBR08 -p DWhouse130_ --repository-url https://upload.pypi.org/legacy/ dist/*
+
+'''
+def add_numbers(num1,num2):
+    return num1+num2
