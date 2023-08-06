68317cfcbdf086bfc1c10889851cd34e7e810b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:48 +0000

    Add package instainfo/0.0.5/instainfo-0.0.5.tar.gz

---
+++ packages/instainfo/instainfo-0.0.5.tar.gz/instainfo-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    # Here is the module name.
+    name="instainfo",
+
+    # version of the module
+    version="0.0.5",
+
+    # Name of Author
+    author="Hayden Cordeiro",
+
+    # your Email address
+    author_email="cordeirohayden@gmail.com",
+
+    # Small Description about module
+    description="Get Profile information of a instagram user",
+
+    long_description=long_description,
+
+    # Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+
+    # Any link to reach this module, if you have any webpage or github profile
+    url="http://github.com/haydencordeiro",
+    packages=['instainfo'],
+    install_requires=['requests'],
+    # classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False)
