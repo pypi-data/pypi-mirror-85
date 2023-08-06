375dc25a2c84e64ba40b696b9c14493deac32c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:41 +0000

    Add package sbackup/0.0.2/sbackup-0.0.2.tar.gz

---
+++ packages/sbackup/sbackup-0.0.2.tar.gz/sbackup-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='sbackup',
+  version='0.0.2',
+  description='Folder/File backup manager by seletive file extensions',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Swarnadeep Seth',
+  author_email='swdseth@yahoo.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='backup, backup manager', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
