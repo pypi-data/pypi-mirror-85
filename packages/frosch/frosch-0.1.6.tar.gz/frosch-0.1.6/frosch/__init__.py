e86107ed9b39dd7e4987641ecd3cea8ab88f1c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package frosch/0.1.6/frosch-0.1.6.tar.gz

---
+++ packages/frosch/frosch-0.1.6.tar.gz/frosch-0.1.6/frosch/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+    frosch - Better runtime errors
+
+    Patrick Haller
+    betterthannothing.blog
+    patrickhaller40@googlemail.com
+
+    License MIT
+
+"""
+
+from .frosch import hook, print_exception
+
+__all__ = ["hook", "print_exception"]
