8f09aafe3e58a61a461ac776a90bc605a1f933d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package z3c.sqlalchemy/1.5.0/z3c.sqlalchemy-1.5.0-py3-none-any.whl

---
+++ packages/z3c.sqlalchemy/z3c.sqlalchemy-1.5.0-py3-none-any.whl/z3c/sqlalchemy/__init__.py
@@ -0,0 +1,16 @@
+##########################################################################
+# z3c.sqlalchemy - A SQLAlchemy wrapper for Python/Zope
+#
+# (C) Zope Corporation and Contributor
+# Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+# and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+from .util import *
+from .model import Model
+
+try:
+    from sqlalchemy.orm import sessionmaker
+except ImportError:
+    raise ImportError('z3c.sqlalchemy requires SQLAlchemy 0.4 or higher')
