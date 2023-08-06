b7ca1305e61d7ef5e4956745bc1e6a3b6c1a999f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:27 +0000

    Add package pycook/0.9.15/pycook-0.9.15.tar.gz

---
+++ packages/pycook/pycook-0.9.15.tar.gz/pycook-0.9.15/pycook/recipes/dpkg.py
@@ -0,0 +1,14 @@
+import pycook.elisp as el
+
+def ls(recipe, package):
+    if isinstance(recipe, int):
+        return ["dpkg -L " + package]
+    elif recipe[0] == "complete":
+        part = recipe[1]
+        if len(part) > 0:
+            part += "*"
+        return el.shell_command_to_string("dpkg-query -W -f '${Package}\n' " + part)
+
+
+def file_to_package(recipe, fname):
+    return ["dpkg -S " + fname]
