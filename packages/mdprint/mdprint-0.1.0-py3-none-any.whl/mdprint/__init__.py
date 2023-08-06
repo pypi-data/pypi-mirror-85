3aed670c8fbffd66095dacb22d4cc11253220729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package mdprint/0.1.0/mdprint-0.1.0-py3-none-any.whl

---
+++ packages/mdprint/mdprint-0.1.0-py3-none-any.whl/mdprint/__init__.py
@@ -0,0 +1,9 @@
+from mdprint.mdprint_functions import mdprint, mdprint_dict, mdprint_list
+__version__ = '0.1.0'
+
+__all__ = [
+    __version__,
+    mdprint,
+    mdprint_list,
+    mdprint_dict
+]
