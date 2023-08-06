f1bc6f09c467cc676d2d57180d0eb3c9177cc5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package quoters/0.14/quoters-0.14.tar.gz

---
+++ packages/quoters/quoters-0.14.tar.gz/quoters-0.14/quoters/constants.py
@@ -0,0 +1,3 @@
+URL = "https://www.yourselfquotes.com/thought-of-the-day/"
+CONN_URL = "www.google.com"
+SERIES_QUOTES_URL = "https://www.tvtime.com/article/top-50-unforgettable-tv-quotes"
---
+++ packages/quoters/quoters-0.14.tar.gz/quoters-0.14/quoters/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from quoters.extract_data import check_connection_and_generate_quote
+from quoters.enum import QuoteType
+
+class Quote:
+    """
+        Class Quote to print random `quotes` and `TV shows quotes`
+    """
+    @staticmethod
+    def print():
+        """
+            Function to return random quotes
+        """
+        result = check_connection_and_generate_quote(QuoteType.QUOTE)
+        if result != False:
+            return result
+        
+    @staticmethod
+    def print_series_quote():
+        """
+            Function to return random TV shows quotes
+        """
+        result = check_connection_and_generate_quote(QuoteType.SERIES_QUOTE)
+        if result != False:
+            return result
