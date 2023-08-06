40259cee097462eb3fa5ca5d35df9f92241398c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:50 +0000

    Add package catalyst/20.11/catalyst-20.11.tar.gz

---
+++ packages/catalyst/catalyst-20.11.tar.gz/catalyst-20.11/catalyst/contrib/__main__.py
@@ -0,0 +1,66 @@
+from argparse import ArgumentParser, RawTextHelpFormatter
+from collections import OrderedDict
+import logging
+
+from catalyst.contrib.scripts import collect_env, find_thresholds
+from catalyst.settings import SETTINGS
+
+logger = logging.getLogger(__name__)
+
+COMMANDS = OrderedDict(
+    [("collect-env", collect_env), ("find-thresholds", find_thresholds)]
+)
+
+try:
+    import nmslib  # noqa: F401
+    from catalyst.contrib.scripts import check_index_model, create_index_model
+
+    COMMANDS["check-index-model"] = check_index_model
+    COMMANDS["create-index-model"] = create_index_model
+except ImportError as ex:
+    if SETTINGS.nmslib_required:
+        logger.warning(
+            "nmslib not available, to install nmslib,"
+            " run `pip install nmslib`."
+        )
+        raise ex
+
+
+COMMANDS = OrderedDict(sorted(COMMANDS.items()))
+
+
+def build_parser() -> ArgumentParser:
+    """Builds parser.
+
+    Returns:
+        parser
+    """
+    parser = ArgumentParser(
+        "catalyst-contrib", formatter_class=RawTextHelpFormatter
+    )
+    all_commands = ", \n".join(map(lambda x: f"    {x}", COMMANDS.keys()))
+
+    subparsers = parser.add_subparsers(
+        metavar="{command}",
+        dest="command",
+        help=f"available commands: \n{all_commands}",
+    )
+    subparsers.required = True
+
+    for key, value in COMMANDS.items():
+        value.build_args(subparsers.add_parser(key))
+
+    return parser
+
+
+def main():
+    """@TODO: Docs. Contribution is welcome."""
+    parser = build_parser()
+
+    args, uargs = parser.parse_known_args()
+
+    COMMANDS[args.command].main(args, uargs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/catalyst/catalyst-20.11.tar.gz/catalyst-20.11/catalyst/utils/__init__.py
@@ -0,0 +1,132 @@
+# flake8: noqa
+"""
+All utils are gathered in :py:mod:`catalyst.utils` for easier access.
+
+.. note::
+    Everything from :py:mod:`catalyst.contrib.utils` is included in :py:mod:`catalyst.utils`
+"""
+
+
+from catalyst.utils.checkpoint import (
+    load_checkpoint,
+    pack_checkpoint,
+    save_checkpoint,
+    unpack_checkpoint,
+)
+from catalyst.utils.components import process_components
+from catalyst.utils.config import load_config, save_config
+from catalyst.utils.dict import (
+    get_key_str,
+    get_key_none,
+    get_key_list,
+    get_key_dict,
+    get_key_all,
+    get_dictkey_auto_fn,
+    merge_dicts,
+    flatten_dict,
+    split_dict_to_subdicts,
+)
+from catalyst.utils.distributed import (
+    get_nn_from_ddp_module,
+    get_slurm_params,
+    get_distributed_params,
+    get_distributed_env,
+    get_rank,
+    get_distributed_mean,
+    check_ddp_wrapped,
+    check_torch_distributed_initialized,
+    check_slurm_available,
+    check_apex_available,
+    check_amp_available,
+    initialize_apex,
+    assert_fp16_available,
+)
+from catalyst.utils.hash import get_hash, get_short_hash
+from catalyst.utils.initialization import (
+    get_optimal_inner_init,
+    outer_init,
+    reset_weights_if_possible,
+)
+from catalyst.utils.loaders import (
+    get_loaders_from_params,
+    validate_loaders,
+    get_loader,
+    get_native_batch_from_loader,
+    get_native_batch_from_loaders,
+)
+from catalyst.utils.misc import (
+    copy_directory,
+    format_metric,
+    get_fn_default_params,
+    get_fn_argsnames,
+    get_utcnow_time,
+    is_exception,
+    maybe_recursive_call,
+    get_attr,
+    set_global_seed,
+)
+from catalyst.utils.numpy import get_one_hot
+from catalyst.utils.parser import parse_config_args, parse_args_uargs
+from catalyst.utils.scripts import (
+    import_module,
+    dump_code,
+    dump_python_files,
+    prepare_config_api_components,
+    dump_experiment_code,
+    distributed_cmd_run,
+)
+from catalyst.utils.swa import (
+    average_weights,
+    get_averaged_weights_by_path_mask,
+)
+from catalyst.utils.sys import (
+    get_environment_vars,
+    list_conda_packages,
+    list_pip_packages,
+    dump_environment,
+)
+from catalyst.utils.torch import (
+    any2device,
+    get_activation_fn,
+    get_available_gpus,
+    get_device,
+    get_optimizable_params,
+    get_optimizer_momentum,
+    prepare_cudnn,
+    process_model_params,
+    set_optimizer_momentum,
+    get_requires_grad,
+    set_requires_grad,
+    get_network_output,
+    detach,
+    trim_tensors,
+)
+from catalyst.utils.tracing import (
+    trace_model,
+    trace_model_from_checkpoint,
+    trace_model_from_runner,
+    get_trace_name,
+    save_traced_model,
+    load_traced_model,
+)
+
+from catalyst.settings import IS_PRUNING_AVAILABLE
+
+if IS_PRUNING_AVAILABLE:
+    from catalyst.utils.pruning import prune_model, remove_reparametrization
+
+from catalyst.settings import IS_QUANTIZATION_AVAILABLE
+
+if IS_QUANTIZATION_AVAILABLE:
+    from catalyst.utils.quantization import (
+        save_quantized_model,
+        quantize_model_from_checkpoint,
+    )
+
+from catalyst.settings import IS_GIT_AVAILABLE
+
+if IS_GIT_AVAILABLE:
+    from catalyst.utils.pipelines import clone_pipeline
+    from catalyst.utils.wizard import run_wizard, Wizard
+
+from catalyst.contrib.utils import *
