d39c5c887b7050691e6a9cec0f1c70a1756e4e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:09 +0000

    Add package zeanr-distributions/0.1/zeanr_distributions-0.1.tar.gz

---
+++ packages/zeanr-distributions/zeanr_distributions-0.1.tar.gz/zeanr_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zeanr_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['zeanr_distributions'],
+      zip_safe=False)
