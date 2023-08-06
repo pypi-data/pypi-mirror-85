15f18de67bb8c06dc26985a51db1d75d005f8cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package rmout/0.2.0/rmout-0.2.0.tar.gz

---
+++ packages/rmout/rmout-0.2.0.tar.gz/rmout-0.2.0/rmout/core.py
@@ -0,0 +1,34 @@
+import os
+import click
+
+from rmout.files import (
+    get_extensions_set,
+    extract_files_by_extlist,
+)
+from rmout.trash import (
+    extract_target_from_userinput,
+    get_extensiondir_for_sendtotrash,
+    send_to_trash,
+)
+
+
+@click.command(help='\
+rmout is a tool that throws files with the extension written \
+in the .rmoutrc file into the trash.')
+def run():
+    debug = False
+    rmout(debug)
+    click.echo('finished.')
+
+
+def rmout(debug=True):
+    current_dir = os.getcwd()
+    target_ext_set = get_extensions_set(current_dir)
+    if not target_ext_set:
+        return
+    trash_candidate = extract_files_by_extlist(target_ext_set, current_dir)
+    if not trash_candidate:
+        return
+    codelist = extract_target_from_userinput()
+    throwaway = get_extensiondir_for_sendtotrash(codelist, trash_candidate)
+    send_to_trash(throwaway, debug)
---
+++ packages/rmout/rmout-0.2.0.tar.gz/rmout-0.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='rmout',
+    version='0.2.0',
+
+    author='kino',
+    author_email='simulation.space.labs@gmail.com',
+    url='https://github.com/simulation-lab/rmout',
+
+    description='rmout is a tool for throwing unnecessary files in a directory into the trash.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    python_requires='~=3.8',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=[
+        'click~=7.1',
+        'send2trash~=1.5',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'rmout=rmout.core:run'
+        ]
+    },
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+
+)
