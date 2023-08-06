1ad66de50249ad687154faf5fb35a03add4f1020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:24 +0000

    Add package pyknp-eventgraph/6.1.3/pyknp_eventgraph-6.1.3-py3-none-any.whl

---
+++ packages/pyknp-eventgraph/pyknp_eventgraph-6.1.3-py3-none-any.whl/pyknp_eventgraph/utils.py
@@ -0,0 +1,25 @@
+from io import open
+from typing import List
+
+from pyknp import KNP, BList
+
+
+def read_knp_result_file(filename: str) -> List[BList]:
+    """Read a KNP result file.
+
+    Args:
+        filename: A filename.
+
+    Returns:
+        A list of :class:`pyknp.knp.blist.BList` objects.
+    """
+    knp = KNP()
+    blists = []
+    with open(filename, 'rt', encoding='utf-8', errors='replace') as f:
+        chunk = ''
+        for line in f:
+            chunk += line
+            if line.strip() == 'EOS':
+                blists.append(knp.result(chunk))
+                chunk = ''
+    return blists
