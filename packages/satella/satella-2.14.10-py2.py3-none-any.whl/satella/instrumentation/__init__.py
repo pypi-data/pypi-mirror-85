bbffa506c8ae141dfd62e970deab78959c601798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:12 +0000

    Add package satella/2.14.10/satella-2.14.10-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.14.10-py2.py3-none-any.whl/satella/instrumentation/__init__.py
@@ -0,0 +1,5 @@
+from .dump_frames_on import install_dump_frames_on
+from .trace_back import Traceback, GenerationPolicy, StackFrame, StoredVariableValue
+
+__all__ = ['install_dump_frames_on', 'Traceback', 'GenerationPolicy', 'StoredVariableValue',
+           'StackFrame']
