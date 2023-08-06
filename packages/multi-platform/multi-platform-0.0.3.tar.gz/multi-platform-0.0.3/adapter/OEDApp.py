dbd6aafd12fc06f3110761121e5ef68d46e5bf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:23 +0000

    Add package multi-platform/0.0.3/multi-platform-0.0.3.tar.gz

---
+++ packages/multi-platform/multi-platform-0.0.3.tar.gz/multi-platform-0.0.3/adapter/OEDApp.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+'''跨平台Ke App类
+'''
+
+# import time
+from oed_native_lib.OEDApp import OEDApp
+
+
+class OEDXApp(OEDApp):
+    '''跨平台App类
+    '''
+
+    def __init__(self, device, *args, **kwargs):
+        self._device = device
+        super(OEDXApp,self).__init__(device._device)
+
+
