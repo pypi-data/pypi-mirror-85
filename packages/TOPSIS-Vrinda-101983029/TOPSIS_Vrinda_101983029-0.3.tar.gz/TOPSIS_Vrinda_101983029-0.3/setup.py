215ed81e81dd377a5e79a2bb0280f674529f8d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:56 +0000

    Add package TOPSIS-Vrinda-101983029/0.3/TOPSIS_Vrinda_101983029-0.3.tar.gz

---
+++ packages/TOPSIS-Vrinda-101983029/TOPSIS_Vrinda_101983029-0.3.tar.gz/TOPSIS_Vrinda_101983029-0.3/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name = "TOPSIS_Vrinda_101983029",
+      version = "0.3",
+      description = "This is an easy to use package for implementing TOPSIS",
+      author = 'Vrinda Agarwal',
+      packages = ['TOPSIS_Vrinda_101983029'])
