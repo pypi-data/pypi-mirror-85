b031284f6f2228ab6cf23ba1963700a835df0af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:08 +0000

    Add package kodexa/2.3.84/kodexa-2.3.84-py3-none-any.whl

---
+++ packages/kodexa/kodexa-2.3.84-py3-none-any.whl/kodexa/workflow/workflow.py
@@ -0,0 +1,44 @@
+from typing import List, Dict, Any
+
+
+class WorkflowStore:
+
+    def __init__(self, name: str, ref: str):
+        self.name: str = name
+        self.ref: str = ref
+
+
+class WorkflowConnector:
+
+    def __init__(self, name: str, ref: str, options=None):
+        if options is None:
+            options = {}
+        self.name: str = name
+        self.ref: str = ref
+        self.options: Dict[str, Any] = options
+
+
+class WorkflowPipeline:
+
+    def __init__(self, name: str, ref: str, subscription: str, target: str = None, parameters=None):
+        if parameters is None:
+            parameters = {}
+        self.name: str = name
+        self.ref: str = ref
+
+        self.subscription: str = subscription
+        self.parameters: Dict[str, Any] = parameters
+
+
+class Workflow:
+
+    def __init__(self):
+        self.stores: List[WorkflowStore] = []
+        self.pipelines: List[WorkflowPipeline] = []
+        self.connectors: List[WorkflowConnector] = []
+
+    def add_store(self, name: str, ref: str):
+        self.stores.append(WorkflowStore(name, ref))
+
+    def add_pipeline(self, name: str, ref: str, subscription: str = "*", target: str = None, parameters=None):
+        self.pipelines.append(WorkflowPipeline(name, ref, subscription, target, parameters))
