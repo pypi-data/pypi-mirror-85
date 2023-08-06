fb7e809b9cb8ba27e8ca045307ae90df47d81ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:41 +0000

    Add package jingdonglianmeng/0.0.5/jingdonglianmeng-0.0.5-py3-none-any.whl

---
+++ packages/jingdonglianmeng/jingdonglianmeng-0.0.5-py3-none-any.whl/jingdonglianmeng/model/lianmeng_model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+PRODUCT_MODEL = {
+    "product_id": str,
+    "business_id": int,
+    "title": str,
+    "category": str,
+    "business_type": int,
+    "price": int,
+    "date": int,
+    "spider_time": int,
+    "product_creat_time": int,
+    "product_type": int,
+    "group_time": int,
+}
