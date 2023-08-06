c92fe9b297e244aa0ee9328a2d4fa3a1987121dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:19 +0000

    Add package brain-py/0.2.4/Brain.Py-0.2.4.tar.gz

---
+++ packages/brain-py/Brain.Py-0.2.4.tar.gz/Brain.Py-0.2.4/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+# obtain version string from __init__.py
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'brainpy', '__init__.py'), 'r') as f:
+    init_py = f.read()
+version = re.search('__version__ = "(.*)"', init_py).groups()[0]
+
+# obtain long description from README and CHANGES
+with io.open(os.path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
+    README = f.read()
+
+# setup
+setup(
+    name='Brain.Py',
+    version=version,
+    description='BrainPy: A Just-In-Time compilation approach for neuronal dynamics simulation.',
+    long_description=README,
+    author='Chaoming Wang',
+    author_email='adaduo@outlook.com',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy>=1.15',
+        'matplotlib>=3.0',
+        'sympy>=1.2',
+        'autopep8',
+    ],
+    url='https://github.com/PKU-NIP-Lab/BrainPy',
+    keywords='computational neuroscience',
+    classifiers=[
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ]
+)
