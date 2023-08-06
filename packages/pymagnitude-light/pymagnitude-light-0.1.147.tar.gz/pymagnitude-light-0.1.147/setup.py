0ef06fe3cb56aee09a4e2677dfa4f26e342f78af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:00 +0000

    Add package pymagnitude-light/0.1.147/pymagnitude-light-0.1.147.tar.gz

---
+++ packages/pymagnitude-light/pymagnitude-light-0.1.147.tar.gz/pymagnitude-light-0.1.147/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    DESCRIPTION = f.read()
+
+setup(name="pymagnitude-light",
+      version="0.1.147",
+      description="Magnitude fork that only supports Word2Vec, GloVe and fastText embeddings",
+      long_description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url="https://github.com/davebulaval/magnitude-light",
+      license="MIT",
+      packages=find_packages(exclude=["tests", "tests.*"]),
+      install_requires=[
+        "fasteners>=0.14.1",
+        "lz4>=1.0.0",
+        "numpy>=1.14.0",
+        "xxhash>=1.0.1"
+      ] 
+)
