af7e588528fa2f6b42c50194dd965d95b9dda33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:35 +0000

    Add package django-hydra/0.9.2/django_hydra-0.9.2-py3-none-any.whl

---
+++ packages/django-hydra/django_hydra-0.9.2-py3-none-any.whl/hydra/mixins/__init__.py
@@ -0,0 +1,5 @@
+from .formsets import FormsetMixin
+from .breadcrumbs import BreadcrumbMixin
+from .urls import UrlMixin
+from .templates import TemplateMixin
+from .permissions import PermissionRequiredMixin
