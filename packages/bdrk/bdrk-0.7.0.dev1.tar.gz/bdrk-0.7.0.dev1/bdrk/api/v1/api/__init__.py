4d47055f5e5d7f00f732ba9843b0d4709d056c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:33 +0000

    Add package bdrk/0.7.0.dev1/bdrk-0.7.0.dev1.tar.gz

---
+++ packages/bdrk/bdrk-0.7.0.dev1.tar.gz/bdrk-0.7.0.dev1/bdrk/api/v1/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from bdrk.api.v1.api.internal_api import InternalApi
+from bdrk.api.v1.api.model_api import ModelApi
+from bdrk.api.v1.api.pipeline_api import PipelineApi
+from bdrk.api.v1.api.project_api import ProjectApi
+from bdrk.api.v1.api.serve_api import ServeApi
