17ef11184968d38760095e5e947a67f786b816f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:34 +0000

    Add package xyconvert/0.1.1/xyconvert-0.1.1.tar.gz

---
+++ packages/xyconvert/xyconvert-0.1.1.tar.gz/xyconvert-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='xyconvert',
+    version='0.1.1',
+    package_dir={'': 'xyconvert'},  # Optional
+    py_modules=["xyconvert"],
+    license='MIT',
+    description='Convert xy coordinates',
+    long_description=open('README.md').read(),
+	long_description_content_type="text/markdown",
+    install_requires=['numpy'],
+    url='https://github.com/cyang-kth/xyconvert',
+    author='Can Yang',
+    author_email='ycycy1990@hotmail.com'
+)
