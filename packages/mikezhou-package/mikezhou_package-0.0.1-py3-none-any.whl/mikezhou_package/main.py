542b2adecef0640b13b7ad71a2c99666ae8cacb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:56 +0000

    Add package mikezhou-package/0.0.1/mikezhou_package-0.0.1-py3-none-any.whl

---
+++ packages/mikezhou-package/mikezhou_package-0.0.1-py3-none-any.whl/mikezhou_package/main.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Mike Zhou
+# @Email : 公众号：测试开发技术
+# @File : app.py
+# @Project: 第22课时
+import itertools
+
+case_list = ['用户名', '密码']
+value_list = ['正确', '不正确', '特殊符号', '超过最大长度']
+
+
+def gen_case(item=case_list, value=value_list):
+    '''输出笛卡尔用例集合'''
+    for i in itertools.product(item, value):
+        print('输入'.join(i))
+
+gen_case()
