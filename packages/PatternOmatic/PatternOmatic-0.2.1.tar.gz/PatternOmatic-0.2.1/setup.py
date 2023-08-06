b1f5b27280e4f8075e15ed7345f05db7c02732b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:01 +0000

    Add package PatternOmatic/0.2.1/PatternOmatic-0.2.1.tar.gz

---
+++ packages/PatternOmatic/PatternOmatic-0.2.1.tar.gz/PatternOmatic-0.2.1/setup.py
@@ -0,0 +1,49 @@
+""" Setup tools (build distribution) module
+
+This file is part of PatternOmatic.
+
+Copyright © 2020  Miguel Revuelta Espinosa
+
+PatternOmatic is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+PatternOmatic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with PatternOmatic. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PatternOmatic",
+    version="0.2.1",
+    author="Miguel Revuelta Espinosa",
+    author_email="revuel22@hotmail.com",
+    description="AI/NLP (Spacy) Rule Based Matcher pattern finder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/revuel/PatternOmatic",
+    packages=setuptools.find_packages(),
+    scripts=['scripts/patternomatic.py'],
+    install_requires=[
+        'spacy==2.3.0',
+        'en-core-web-sm==2.3.0'
+    ],
+    dependency_links=['https://github.com/explosion/spacy-models/'
+                      'releases/download/en_core_web_sm-2.3.0/en_core_web_sm-2.3.0.tar.gz#egg=en_core_web_sm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
