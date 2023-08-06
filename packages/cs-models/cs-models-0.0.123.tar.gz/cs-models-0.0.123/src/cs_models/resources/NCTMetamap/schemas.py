67d48b79e3a0683df7e34a8dc78888f3dca73ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:33 +0000

    Add package cs-models/0.0.123/cs-models-0.0.123.tar.gz

---
+++ packages/cs-models/cs-models-0.0.123.tar.gz/cs-models-0.0.123/src/cs_models/resources/NCTMetamap/schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class NCTMetamapResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    nct_id = fields.String(validate=not_blank, required=True)
+    cui = fields.String(validate=not_blank, required=True)
+    preferred_name = fields.String(validate=not_blank, required=True)
+    type = fields.String(required=True)
+    text = fields.String(allow_none=True)
+    updated_at = fields.DateTime()
