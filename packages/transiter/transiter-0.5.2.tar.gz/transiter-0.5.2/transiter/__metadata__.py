88446f87267799824b9e72497a3e45a7f5975eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package transiter/0.5.2/transiter-0.5.2.tar.gz

---
+++ packages/transiter/transiter-0.5.2.tar.gz/transiter-0.5.2/transiter/__metadata__.py
@@ -0,0 +1,15 @@
+# When updating the version, also update it in the docs.
+__version__ = "0.5.2"
+
+# The following are set by Travis CI at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by Travis CI
+__version__ = "0.5.2"
+__build_number__ = 1083
+__build_timestamp__ = 1605328729
+__build_href__ = "https://travis-ci.org/jamespfennell/transiter/jobs/743547868"
+__git_commit_hash__ = "fa7a4c0f0ad1e19a416bc91f711626ce2d1112a8"
