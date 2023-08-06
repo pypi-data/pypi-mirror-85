3375b9017a5e7809c79740ec9b5c7e6edf6874d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:07 +0000

    Add package scene-cutter/0.0.4/scene_cutter-0.0.4.tar.gz

---
+++ packages/scene-cutter/scene_cutter-0.0.4.tar.gz/scene_cutter-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="scene_cutter",
+    version="0.0.4",
+    author="Péntek Zsolt",
+    description="ffmpeg scene cutter",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zselter07/scene_cutter",
+    packages=setuptools.find_packages(),
+    install_requires=["kcu"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
