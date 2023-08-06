00359f62f01e98ced890d8f644b3219fe3113023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package autoimport/0.2.0/autoimport-0.2.0-py3-none-any.whl

---
+++ packages/autoimport/autoimport-0.2.0-py3-none-any.whl/autoimport/__init__.py
@@ -0,0 +1,9 @@
+"""Python program to automatically import missing python libraries.
+
+Functions:
+    fix_code: Fix python source code to correct missed or unused import statements.
+
+    fix_files: Fix the python source code of a list of files.
+"""
+
+from .services import fix_code, fix_files  # noqa W0611
