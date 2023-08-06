2a1b898fc1a5e921e72cde37fce0f1c5cda5cfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:16 +0000

    Add package TOPOSIS-DALEEP-101803482/0.0.1/TOPOSIS-DALEEP-101803482-0.0.1.tar.gz

---
+++ packages/TOPOSIS-DALEEP-101803482/TOPOSIS-DALEEP-101803482-0.0.1.tar.gz/TOPOSIS-DALEEP-101803482-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.1'
+PACKAGE_NAME = 'TOPOSIS-DALEEP-101803482'
+AUTHOR = 'Daleep Singh'
+AUTHOR_EMAIL = 'axelsra31@gmial.com'
+URL = 'https://github.com/you/your_package'
+
+LICENSE = 'Apache License 2.0'
+
+DESCRIPTION = 'TOPSIS is an algorithm to determine the best choice out of many using Positive Ideal Solution and Negative Ideal'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'tabulate',
+      'scipy'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
