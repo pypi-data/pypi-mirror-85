6903df7447ec31de266ba9e85dc9f8ee799885ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package topsis-101983034/1.0.0/topsis_101983034-1.0.0.tar.gz

---
+++ packages/topsis-101983034/topsis_101983034-1.0.0.tar.gz/topsis_101983034-1.0.0/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="topsis_101983034",
+    version="1.0.0",
+    description="topsis",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SolarBoar808746/topsis_101983034",
+    author="Parshant Asija",
+    author_email="puru.asija19@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.__main__:main",
+        ]
+    },
+)
