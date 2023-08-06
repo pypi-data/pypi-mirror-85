0976719226d7e1a25e4051156eac8230e6ccac09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:28 +0000

    Add package Topsis-Ayush-101803635/0.0.3/Topsis_Ayush_101803635-0.0.3.tar.gz

---
+++ packages/Topsis-Ayush-101803635/Topsis_Ayush_101803635-0.0.3.tar.gz/Topsis_Ayush_101803635-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Topsis_Ayush_101803635',
+  version='0.0.3',
+  description='myTopsisPackage',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Ayush anand Saxena',
+  author_email='asaxena_be18@thapar.edu',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='TOPSIS', 
+  packages=find_packages(),
+  install_requires=['pandas','math'] 
+)
+
