b6ecc0d22f33ce5a68f6cfed1f0dcc55b25ad363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package TOPSIS-Ritwik-101803624/0.1/TOPSIS-Ritwik-101803624-0.1.tar.gz

---
+++ packages/TOPSIS-Ritwik-101803624/TOPSIS-Ritwik-101803624-0.1.tar.gz/TOPSIS-Ritwik-101803624-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'TOPSIS-Ritwik-101803624',        
+  packages = ['TOPSIS-Ritwik-101803624'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Topsis Assignment',   
+  author = 'Ritwik Sarkar',                  
+  author_email = 'rsarkar_be18@thapar.edu',      
+  url = 'https://github.com/ritwiksarkar11',   
+  download_url = 'https://github.com/ritwiksarkar11/TOPSIS-Ritwik-101803624/archive/0.1.tar.gz',    
+  keywords = ['TOPSIS', 'PYTHON', 'ASSIGNMENT'],  
+  install_requires=[            
+          'numpy',
+          'scrapeasy',
+          'pandas',
+          
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
