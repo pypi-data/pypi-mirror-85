de2fcdaa1082e325580820a76ae234a2a3b3c7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:37 +0000

    Add package scgx/0.0.0/scgx-0.0.0.tar.gz

---
+++ packages/scgx/scgx-0.0.0.tar.gz/scgx-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from package import __version__
+
+
+long_description = ''
+with open('./README.md') as f:
+    long_description = f.read()
+
+install_requires = []
+with open('./requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='scgx',
+    version=__version__,
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    author='',
+    author_email='',
+    license='PUBLIC',
+    packages=find_packages(),
+    install_requires=install_requires,
+    zip_safe=False)
