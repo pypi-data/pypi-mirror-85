0a804dce93aa645f347153f369bd5b6ae9972965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:52 +0000

    Add package tauntaun-live-editor/0.1.2/tauntaun_live_editor-0.1.2-py3-none-any.whl

---
+++ packages/tauntaun-live-editor/tauntaun_live_editor-0.1.2-py3-none-any.whl/tauntaun_live_editor/data/client/precache-manifest.908b7124d77ac45f3b26155d516bd3c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062a387f63e60d2dcb5924a17e56da21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1b942907bc9fecd65d",
+    "url": "/static/css/2.565eb0e4.chunk.css"
+  },
+  {
+    "revision": "feed459fdb78de225190",
+    "url": "/static/css/main.e7a74ba5.chunk.css"
+  },
+  {
+    "revision": "8e1b942907bc9fecd65d",
+    "url": "/static/js/2.caa47b96.chunk.js"
+  },
+  {
+    "revision": "6016cd98c667641ed935c60031c7e90b",
+    "url": "/static/js/2.caa47b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feed459fdb78de225190",
+    "url": "/static/js/main.404b156f.chunk.js"
+  },
+  {
+    "revision": "f92f431fcef8efa4a629",
+    "url": "/static/js/runtime-main.bb16c28e.js"
+  }
+]);
