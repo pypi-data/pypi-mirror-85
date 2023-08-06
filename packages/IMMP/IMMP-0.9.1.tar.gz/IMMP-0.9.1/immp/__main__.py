8d1ed994ebdad46dd59fe78d77d0e5d3a7362ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:29 +0000

    Add package IMMP/0.9.1/IMMP-0.9.1.tar.gz

---
+++ packages/IMMP/IMMP-0.9.1.tar.gz/IMMP-0.9.1/immp/__main__.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+from immp import LocalFilter  # For backwards compatibility only.
+from immp.hook.runner import main
+
+try:
+    import uvloop
+except ImportError:
+    uvloop = None
+
+
+def entrypoint():
+    if uvloop:
+        uvloop.install()
+    parser = ArgumentParser(prog="immp", add_help=False)
+    parser.add_argument("-w", "--write", action="store_true")
+    parser.add_argument("file", metavar="FILE")
+    args = parser.parse_args()
+    main(args.file, args.write)
+
+
+if __name__ == "__main__":
+    entrypoint()
