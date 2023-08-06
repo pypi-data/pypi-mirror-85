150034c2aacd05648ddb589b299e8e2734deef76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package withings-api-cdt/2.2.0/withings_api_cdt-2.2.0-py3-none-any.whl

---
+++ packages/withings-api-cdt/withings_api_cdt-2.2.0-py3-none-any.whl/withings_api/models/notify.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from enum import IntEnum
+from typing import cast, NamedTuple, Optional, Tuple
+
+from arrow import Arrow
+from pydantic import BaseModel
+
+from withings_api import str_or_raise
+from withings_api.common import _flexible_tuple_of, arrow_or_none, enum_or_raise, str_or_none
+
+
+class NotifyAppli(IntEnum):
+    """Data to notify_subscribe to."""
+
+    WEIGHT = 1
+    CIRCULATORY = 4
+    ACTIVITY = 16
+    SLEEP = 44
+    USER = 46
+    BED_IN = 50
+    BED_OUT = 51
+
+
+class NotifyListProfile(NamedTuple):
+    """NotifyListProfile."""
+
+    appli: NotifyAppli
+    callbackurl: str
+    expires: Optional[Arrow]
+    comment: Optional[str]
+
+
+class NotifyListResponse(NamedTuple):
+    """NotifyListResponse."""
+
+    profiles: Tuple[NotifyListProfile, ...]
+
+
+class NotifyGetResponse(NamedTuple):
+    """NotifyGetResponse."""
+
+    appli: NotifyAppli
+    callbackurl: str
+    comment: Optional[str]
+
+
+def new_notify_appli(value: Optional[int]) -> NotifyAppli:
+    """Create enum based on primitive."""
+    return cast(NotifyAppli, enum_or_raise(value, NotifyAppli))
+
+
+def new_notify_list_profile(data: dict) -> NotifyListProfile:
+    """Create ListSubscriptionProfile from json."""
+    return NotifyListProfile(
+        appli=new_notify_appli(data.get("appli")),
+        callbackurl=str_or_raise(data.get("callbackurl")),
+        expires=arrow_or_none(data.get("expires")),
+        comment=str_or_none(data.get("comment")),
+    )
+
+
+def new_notify_list_response(data: dict) -> NotifyListResponse:
+    """Create NotifyListResponse from json."""
+    return NotifyListResponse(
+        profiles=_flexible_tuple_of(data.get("profiles", ()), new_notify_list_profile)
+    )
+
+
+
+def new_notify_get_response(data: dict) -> NotifyGetResponse:
+    """Create NotifyGetResponse from json."""
+    return NotifyGetResponse(
+        appli=new_notify_appli(data.get("appli")),
+        callbackurl=str_or_raise(data.get("callbackurl")),
+        comment=str_or_none(data.get("comment")),
+    )
+
+class UnifiedNotifyWebhook(BaseModel):
+    """ the form data notify webhook will be received """
+    userid: str
+    appli: NotifyAppli
+
+    # applies to user.metrics [1,2] users.activity[44]
+    startdate: Optional[datetime]
+    enddate: Optional[datetime]
+
+    # applies to users.activity 12
+    date: Optional[datetime]
+
+    #applies to user.info
+    action: Optional[str]
+
+    # applies to user.sleepevents
+    deviceid: Optional[str]
