4f952d370803ada89215bf9e57fbc5d5e0f16bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:50 +0000

    Add package djangorestframework-simplejwt/4.6.0/djangorestframework_simplejwt-4.6.0.tar.gz

---
+++ packages/djangorestframework-simplejwt/djangorestframework_simplejwt-4.6.0.tar.gz/djangorestframework_simplejwt-4.6.0/README.rst
@@ -0,0 +1,28 @@
+Simple JWT
+==========
+
+.. image:: https://circleci.com/gh/SimpleJWT/django-rest-framework-simplejwt.svg?style=shield
+  :target: https://circleci.com/gh/SimpleJWT/django-rest-framework-simplejwt
+.. image:: https://codecov.io/gh/SimpleJWT/django-rest-framework-simplejwt/branch/master/graph/badge.svg
+  :target: https://codecov.io/gh/SimpleJWT/django-rest-framework-simplejwt
+.. image:: https://img.shields.io/pypi/v/djangorestframework-simplejwt.svg
+  :target: https://pypi.python.org/pypi/djangorestframework-simplejwt
+.. image:: https://img.shields.io/pypi/pyversions/djangorestframework-simplejwt.svg
+  :target: https://pypi.python.org/pypi/djangorestframework-simplejwt
+.. image:: https://readthedocs.org/projects/django-rest-framework-simplejwt/badge/?version=latest
+  :target: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/
+
+Abstract
+--------
+
+Simple JWT is a JSON Web Token authentication plugin for the `Django REST
+Framework <http://www.django-rest-framework.org/>`__.
+
+For full documentation, visit `django-rest-framework-simplejwt.readthedocs.io
+<https://django-rest-framework-simplejwt.readthedocs.io/en/latest/>`__.
+
+Looking for Maintainers
+-----------------------
+
+For more information, see `here
+<https://github.com/SimpleJWT/django-rest-framework-simplejwt/issues/207>`__.
---
+++ packages/djangorestframework-simplejwt/djangorestframework_simplejwt-4.6.0.tar.gz/djangorestframework_simplejwt-4.6.0/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+extras_require = {
+    'test': [
+        'cryptography',
+        'pytest-cov',
+        'pytest-django',
+        'pytest-xdist',
+        'pytest',
+        'tox',
+    ],
+    'lint': [
+        'flake8',
+        'pep8',
+        'isort',
+    ],
+    'doc': [
+        'Sphinx>=1.6.5,<2',
+        'sphinx_rtd_theme>=0.1.9',
+    ],
+    'dev': [
+        'bumpversion>=0.5.3,<1',
+        'pytest-watch',
+        'wheel',
+        'twine',
+        'ipython',
+    ],
+    'python-jose': [
+        'python-jose==3.0.0',
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev'] +  # noqa: W504
+    extras_require['test'] +  # noqa: W504
+    extras_require['lint'] +  # noqa: W504
+    extras_require['doc'] +  # noqa: W504
+    extras_require['python-jose']
+)
+
+
+setup(
+    name='djangorestframework_simplejwt',
+    version='4.6.0',
+    url='https://github.com/SimpleJWT/django-rest-framework-simplejwt',
+    license='MIT',
+    description='A minimal JSON Web Token authentication plugin for Django REST Framework',
+    long_description=open('README.rst', 'r', encoding='utf-8').read(),
+    author='David Sanders',
+    author_email='davesque@gmail.com',
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'pyjwt',
+    ],
+    python_requires='>=3.7',
+    extras_require=extras_require,
+    packages=find_packages(exclude=['tests', 'tests.*', 'licenses', 'requirements']),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
