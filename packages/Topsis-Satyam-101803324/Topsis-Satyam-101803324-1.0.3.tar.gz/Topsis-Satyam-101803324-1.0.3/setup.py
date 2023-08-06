b90248324032b374ee724514cf4a192fa4694df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package Topsis-Satyam-101803324/1.0.3/Topsis-Satyam-101803324-1.0.3.tar.gz

---
+++ packages/Topsis-Satyam-101803324/Topsis-Satyam-101803324-1.0.3.tar.gz/Topsis-Satyam-101803324-1.0.3/setup.py
@@ -0,0 +1,37 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis-Satyam-101803324",
+    version="1.0.3",
+    license='MIT',
+    author="Satyam Verma",
+    author_email="satyamv069@gmail.com",
+    description="This package can be used to calculate the topsis score of multiple component data and rank them accordingly",
+    long_description=long_description,
+    url='https://pypi.org/project/Topsis-Satyam-101803324',
+    long_description_content_type="text/markdown",
+    keywords = ['TOPSIS'],  
+    install_requires=[           
+          'pandas',
+          'numpy'
+      ],
+    packages=['source'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',     
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=source.main",
+        ]
+     },
+) 
