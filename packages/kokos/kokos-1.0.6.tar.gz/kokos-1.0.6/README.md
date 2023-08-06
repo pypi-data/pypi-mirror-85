4e38e671d8ad1a58fdda8e4b70c62b5f0cb28228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:06 +0000

    Add package kokos/1.0.6/kokos-1.0.6.tar.gz

---
+++ packages/kokos/kokos-1.0.6.tar.gz/kokos-1.0.6/README.md
@@ -0,0 +1,114 @@
+# KoKoS
+
+Утилита для удобного решения задач курса АКОС.
+
+Если есть вопросы по использованию &mdash; скорее всего это значит, что ответ на эти вопросы должен быть в README. Обязательно напишите об этом мне: [@darkkeks](https://t.me/darkkeks). Также принимаю любые предложения и запросы фич.
+
+Inspired by [DoomzD/caos-reborn](https://github.com/DoomzD/caos-reborn) and [petuhovskiy/acos](https://github.com/petuhovskiy/acos).
+
+## Installation
+
+### Из PyPi
+
+```shell script
+pip3 install kokos 
+# or to update
+pip3 install --upgrade kokos 
+```
+
+Возможно вы увидите варнинг вида
+
+```
+WARNING: The script kks is installed in '/home/darkkeks/.local/bin' which is not on PATH.
+Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+```
+
+Это значит, что скрипт не добавлен в PATH. Чтобы пользоваться им без указания пути, стоит добавить его в PATH, например в `.bashrc` вот так:
+```
+PATH="/home/darkkeks/.local/bin":$PATH
+```
+
+### Из исходников
+
+```shell script
+git clone https://github.com/DarkKeks/kks.git
+cd kks
+pip3 install .
+```
+
+## Usage
+
+### Про пароль
+
+Для использования не обязательна авторизация в ejudge. Сборка, линтер, тестирование и генерация тестов будет работать без авторизации.
+
+Авторизация используется, чтобы выкачать список задач либо парсить статус из ejudge.
+
+Также, у `kks auth` есть флаг `--no-store-password`, который сохранит локально только логин и id контеста, но не пароль. Пароль будет запрашиваться каждый раз, когда сессия протухает.
+
+Без этого флага, пароль хранится в **plaintext** в файле `~/.kks/config.ini`.
+
+### Демо
+
+<p align="center">
+<a href="https://asciinema.org/a/E2i72oAIMIG3mcRxEdds4Teth" target="_blank"><img src="https://asciinema.org/a/E2i72oAIMIG3mcRxEdds4Teth.svg" /></a>
+</p>
+
+### TLDR
+
+Почти у всех команд есть адекватный `--help`, там бывают полезные аргументы, не описанные ниже.
+
+```shell script
+# Create .kks-workspace in current directory to mark kks workspace root
+kks init
+
+# Parse tasks from ejudge and create directories with template solutions
+kks auth
+kks sync
+
+# Build and run solution in current directory
+cd sm01/1/
+kks run
+kks run -- arg_1 arg_2
+kks run < input.txt > output.txt
+
+# Format solution using clang-format
+kks lint
+
+# Generate tests/001.in - tests/100.in using gen.py; generate 001.out - 100.out using solve.py
+kks gen -r 1 100
+# Generate test 123.{in,out}
+kks gen -t 123
+# Only generate *.out files (don't overwrite *.in)
+# Useful if you entered tests manually and want to generate correct output
+kks gen -r 1 10 -o
+# Generate tests [1; 50] using other_gen.py and other_solve.py, overwriting existing tests
+kks gen -g other_gen.py -s other_solve.py -r 1 50 -f
+
+# Run all tests
+kks test
+```
+
+## Todo
+- run
+    - [ ] run lint on every build
+    - [x] run with test as input
+    - [ ] valgrind
+- test
+    - [x] only sample test with arg (-s, for example) and print output
+    - [ ] test solve.py
+    - [ ] .ans/.dat ?
+- build
+    - [ ] multiple language generator and solution
+    - [ ] configure compiler
+    - [ ] support multiple files
+    - [ ] support asm
+- lint
+    - [x] show lint diff
+    - [ ] dry run ?
+- ejudge
+    - [ ] standings
+- sync
+    - [ ] templates
+    - [ ] reload samples for contest/task
+- activate / deactivate tasks
---
+++ packages/kokos/kokos-1.0.6.tar.gz/kokos-1.0.6/kks/cmd/gen.py
@@ -0,0 +1,141 @@
+import subprocess
+from pathlib import Path
+
+import click
+from tqdm import tqdm
+
+from kks.util import get_solution_directory, find_test_output, format_file, test_number_to_name, find_test_pairs, \
+    get_matching_suffix
+
+
+@click.command(short_help='Run solution')
+@click.option('-o', '--output-only', is_flag=True,
+              help='If specified, only solution will be run. Useful to generate output for manually created tests')
+@click.option('-g', '--generator', type=click.Path(exists=True), multiple=True,
+              help='Script, used to generate .in files')
+@click.option('-s', '--solution', type=click.Path(exists=True), multiple=True,
+              help='Script, used to generate .out files')
+@click.option('-t', '--test', 'tests', type=int, multiple=True,
+              help='Test number to generate')
+@click.option('-r', '--range', 'test_range', type=int, nargs=2,
+              help='Tests to generate')
+@click.option('-f', '--force', is_flag=True,
+              help='Overwrite .in files')
+@click.argument('gen_args', nargs=-1, type=click.UNPROCESSED)
+def gen(output_only, generator, solution, tests, test_range, force, gen_args):
+    """
+    Generate tests
+
+    \b
+    Example usage:
+      kks gen -t 1337
+      kks gen -r 1 100
+      kks gen -t 1 -o
+      kks gen -g gen.py -s solve.py -r 1 50 -f
+    """
+
+    directory = get_solution_directory()
+
+    generator = Path(generator or directory / 'gen.py')
+    solution = Path(solution or directory / 'solve.py')
+
+    if not generator.exists() and not output_only:
+        click.secho(f'Generator {generator} does not exist', fg='red', err=True)
+        return
+
+    if not solution.exists():
+        click.secho(f'Solution {solution} does not exist', fg='red', err=True)
+        return
+
+    test_pairs = find_tests_to_gen(directory, tests, test_range, output_only)
+    test_pairs = sorted(test_pairs)
+
+    t = tqdm(test_pairs, leave=False)
+    for input_file, output_file in t:
+        if not output_only and input_file.exists() and not force:
+            click.secho(f'Input file {format_file(input_file)} '
+                        f'already exists, skipping. Specify -f to overwrite', fg='yellow', err=True)
+            continue
+
+        if output_file.exists() and not force:
+            click.secho(f'Output file {format_file(output_file)} '
+                        f'already exists, skipping. Specify -f to overwrite', fg='yellow', err=True)
+            continue
+
+        t.set_description(f'Generating test {format_file(input_file)}')
+
+        if not output_only:
+            with input_file.open('w') as f:
+                args = [input_file.stem] + list(gen_args)
+                if run_binary(generator, args, stdout=f) is None:
+                    return
+
+        with input_file.open('r') as f_in, output_file.open('w') as f_out:
+            args = [input_file.stem]
+            if run_binary(solution, args, stdin=f_in, stdout=f_out) is None:
+                return
+
+    click.secho('Generated tests!', fg='green', err=True)
+
+
+def find_tests_to_gen(directory, tests, test_range, output_only):
+    """Находит существующие тесты"""
+    tests_dir = directory / 'tests'
+    tests_dir.mkdir(exists_ok=True)
+
+    test_numbers = tests
+    if test_range:
+        l, r = sorted(test_range)
+        test_numbers += list(range(l, r + 1))
+
+    test_names = [test_number_to_name(number) for number in test_numbers]
+
+    pairs = find_test_pairs(tests_dir, test_names)
+
+    used_names = [input_file.stem for input_file, _ in pairs]
+    not_used_names = set(test_names) - set(used_names)
+
+    def output_file_for_input_file(input_file):
+        return input_file.with_suffix(get_matching_suffix(input_file))
+
+    result = []
+
+    # Добавляем выходной файл, если генерируем только out
+    if not output_only:
+        result += pairs
+    else:
+        result += [
+            (input_file, output_file or output_file_for_input_file(input_file))
+            for input_file, output_file in pairs
+        ]
+
+    # Если пары совсем не существует, добавляем
+    result += [
+        ((tests_dir / test_name).with_suffix('.in'),
+         (tests_dir / test_name).with_suffix('.out'))
+        for test_name in not_used_names
+    ]
+
+    return result
+
+
+def run_binary(binary, args, stdin=None, stdout=None):
+    ext = binary.stem
+
+    interpreter = 'python3' if ext in ['.py', '.py3'] \
+        else 'bash' if ext in ['.sh'] \
+        else None
+
+    if interpreter:
+        click.secho(f'Cant run Unrecognized file {format_file(binary)}', fg='red', err=True)
+        return None
+
+    process = subprocess.run(['python3', binary] + args, stdin=stdin, stdout=stdout)
+
+    if process.returncode != 0:
+        click.secho('Script exited with code ' +
+                    click.style(str(process.returncode), fg='red', bold=True) +
+                    ' (args: ' + ' '.join(args) + ')', fg='yellow')
+        return None
+
+    return process
