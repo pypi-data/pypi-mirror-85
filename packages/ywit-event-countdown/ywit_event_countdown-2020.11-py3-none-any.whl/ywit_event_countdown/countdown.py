70b095c84098e2c0473335db0c076d86a7ce02af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package ywit-event-countdown/2020.11/ywit_event_countdown-2020.11-py3-none-any.whl

---
+++ packages/ywit-event-countdown/ywit_event_countdown-2020.11-py3-none-any.whl/ywit_event_countdown/countdown.py
@@ -0,0 +1,23 @@
+"""
+This is the main functionality of our package. it implements the logic necessary
+to fetch the next event from the server and return it as a time delta for us to use.
+"""
+
+from datetime import datetime, timedelta
+
+import requests
+
+
+def get_time_difference(from_time: datetime = None) -> timedelta:
+    if not from_time:
+        from_time = datetime.now()
+    if not from_time.tzinfo:
+        from_time = from_time.replace(tzinfo=datetime.now().astimezone().tzinfo)
+
+    next_ywit_event = datetime.fromisoformat(_get_event_from_api())
+    return next_ywit_event - from_time
+
+
+def _get_event_from_api() -> str:
+    response = requests.get("https://netapp.ywit.io/api/get_next_event")
+    return response.json()["next_event"]
