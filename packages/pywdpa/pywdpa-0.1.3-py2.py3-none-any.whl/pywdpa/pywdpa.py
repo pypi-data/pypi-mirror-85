1404c087b8aaa0f2d577b4b82a2d5d25ca98436d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:32 +0000

    Add package pywdpa/0.1.3/pywdpa-0.1.3-py2.py3-none-any.whl

---
+++ packages/pywdpa/pywdpa-0.1.3-py2.py3-none-any.whl/pywdpa/pywdpa.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=2.7
+# license         :GPLv3
+# ==============================================================================
+
+"""
+pywdpa.pywdpa: provides entry point main().
+"""
+
+__version__ = "0.1.3"
+
+import sys
+from .get_wdpa import get_wdpa
+
+
+def main():
+    print("Executing pywdpa version %s." % __version__)
+    print("For country with isocode: %s" % sys.argv[1])
+    get_wdpa(sys.argv[1])
+    return None
+
+# End
