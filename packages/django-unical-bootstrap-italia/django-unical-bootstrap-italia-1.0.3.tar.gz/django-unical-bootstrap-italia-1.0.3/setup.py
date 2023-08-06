267f4acb94b6128cc4115ce054824cfd8e82957c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package django-unical-bootstrap-italia/1.0.3/django-unical-bootstrap-italia-1.0.3.tar.gz

---
+++ packages/django-unical-bootstrap-italia/django-unical-bootstrap-italia-1.0.3.tar.gz/django-unical-bootstrap-italia-1.0.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-unical-bootstrap-italia',
+    version='1.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description="Django theme for Università della Calabria (UNICAL) based on Bootstrap Italia theme",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/UniversitaDellaCalabria/django-unical-bootstrap-italia',
+    author='Giuseppe De Marco, Francesco Filicetti',
+    author_email='giuseppe.demarco@unical.it, francesco.filicetti@unical.it',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'design-django-theme>=1.3.9.post3',
+    ]
+)
