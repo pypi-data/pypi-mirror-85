6bfa09125c8fa5f1bdb391cd70640676a3edb1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:03 +0000

    Add package jade-helper/0.0.3/jade_helper-0.0.3.tar.gz

---
+++ packages/jade-helper/jade_helper-0.0.3.tar.gz/jade_helper-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+   
+setuptools.setup(
+    name="jade_helper",
+    version="0.0.3",
+    author="Jade Perdereau",
+    author_email="jade.perdereau@free.fr",
+    description="Package creation test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
