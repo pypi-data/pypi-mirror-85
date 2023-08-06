990e956f338e2396b80b47544f314da4aecf10fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:50 +0000

    Add package python-options/1.0.0/python-options-1.0.0.tar.gz

---
+++ packages/python-options/python-options-1.0.0.tar.gz/python-options-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-options',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/TinDang97/python-options',
+    license='MIT license',
+    author='tindang',
+    author_email='rainstone1029x@gmail.com',
+    description='https://github.com/TinDang97/python-options'
+)
