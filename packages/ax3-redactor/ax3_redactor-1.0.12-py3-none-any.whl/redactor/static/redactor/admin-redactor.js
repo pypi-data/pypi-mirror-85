18bb30f1bf117a9775b5d3958af84b641e415067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:23 +0000

    Add package ax3-redactor/1.0.12/ax3_redactor-1.0.12-py3-none-any.whl

---
+++ packages/ax3-redactor/ax3_redactor-1.0.12-py3-none-any.whl/redactor/static/redactor/admin-redactor.js
@@ -0,0 +1,9 @@
+django.jQuery(() => {
+    $R('textarea[enable-redactor]', {
+        linkTarget: '_blank',
+        plugins: ['imagemanager', 'video', 'widget'],
+        fileUpload: '/redactor/file/',
+        imageUpload: '/redactor/file/',
+        imageManagerJson: '/redactor/file/'
+    });
+});
