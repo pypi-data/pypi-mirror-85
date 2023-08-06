b951baaf4f4dad80db41fb33291352f81d8aed9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:41 +0000

    Add package TOPSIS-Paras-101983048/1.0.3/TOPSIS-Paras-101983048-1.0.3.tar.gz

---
+++ packages/TOPSIS-Paras-101983048/TOPSIS-Paras-101983048-1.0.3.tar.gz/TOPSIS-Paras-101983048-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('C:\\Users\\PARAS\\Desktop\\New Folder\\TOPSIS-Paras-101983048\\Readme.txt') as f:
+        README = f.read()
+    return README
+
+setup(
+    name= "TOPSIS-Paras-101983048", 
+    version="1.0.3", 
+    long_description=readme(),
+    packages=["topsis_create"],
+    include_package_data=True,
+    install_requires=["pandas" , "numpy"]
+)
