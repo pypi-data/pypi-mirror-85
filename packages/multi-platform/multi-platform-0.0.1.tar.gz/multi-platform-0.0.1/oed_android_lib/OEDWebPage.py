4adbc998920fc726ce0b2d8eb752ab92ca4e9fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package multi-platform/0.0.1/multi-platform-0.0.1.tar.gz

---
+++ packages/multi-platform/multi-platform-0.0.1.tar.gz/multi-platform-0.0.1/oed_android_lib/OEDWebPage.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from qt4w.webcontrols import *
+from qt4a.andrcontrols import *
+
+'''
+Ke WebPage类
+'''
+class OEDWebPage(WebPage):
+    def __init__(self, webview_or_webpage):
+        super(OEDWebPage, self).__init__(webview_or_webpage)
