8c733471a794e863e6e58b950fd5b5af77c7fda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:51 +0000

    Add package TOPSIS-Akshit-101803487/1.0.0/TOPSIS-Akshit-101803487-1.0.0.tar.gz

---
+++ packages/TOPSIS-Akshit-101803487/TOPSIS-Akshit-101803487-1.0.0.tar.gz/TOPSIS-Akshit-101803487-1.0.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as file:
+        README = file.read()
+    return README
+
+setup(
+    name="TOPSIS-Akshit-101803487",
+    version="1.0.0",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Akshit Kumar",
+    author_email="akshitmittal912@gmail.com",
+    url = 'https://coe1316.ml',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["src"],
+    include_package_data=True,
+    install_requires=['scipy',
+                      'tabulate',
+                      'numpy',
+                      'pandas',
+     ],
+     entry_points={
+        "console_scripts": [
+            "topsis=src.topsis:main",
+        ]
+     },
+)
