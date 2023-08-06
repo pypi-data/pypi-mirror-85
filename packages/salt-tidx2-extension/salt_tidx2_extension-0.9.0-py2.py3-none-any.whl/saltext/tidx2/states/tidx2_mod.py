3389d1a2874621a95e6a2d249d1d5d7d5a8a8dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package salt-tidx2-extension/0.9.0/salt_tidx2_extension-0.9.0-py2.py3-none-any.whl

---
+++ packages/salt-tidx2-extension/salt_tidx2_extension-0.9.0-py2.py3-none-any.whl/saltext/tidx2/states/tidx2_mod.py
@@ -0,0 +1,14 @@
+"""
+Salt state module
+"""
+import logging
+
+log = logging.getLogger(__name__)
+
+__virtualname__ = "tidx2"
+
+
+def __virtual__():
+    # To force a module not to load return something like:
+    #   return (False, "The tidx2 state module is not implemented yet")
+    return __virtualname__
