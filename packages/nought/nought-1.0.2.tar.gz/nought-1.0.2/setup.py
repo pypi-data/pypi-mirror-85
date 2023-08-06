d794469bf399a7c69e6837dd9bce16a7be7d8706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:02 +0000

    Add package nought/1.0.2/nought-1.0.2.tar.gz

---
+++ packages/nought/nought-1.0.2.tar.gz/nought-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nought", # Replace with your own username
+    version="1.0.2",
+		# scripts=['bin/nought'],
+		entry_points={
+        'console_scripts': ['nought=nought.__main__:main'],
+    },
+    author="Cole Wilson",
+    author_email="cole@colewilson.xyz",
+    description="A super customizable file cleaner/organizer/automator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cole-wilson/nought",
+    packages=setuptools.find_packages(),
+		install_requires=[
+   		'toml',
+		],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
