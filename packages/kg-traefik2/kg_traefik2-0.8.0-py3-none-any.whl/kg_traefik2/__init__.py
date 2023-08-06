bccf502ce817ee98cefc77d40b205d5efc59962e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package kg-traefik2/0.8.0/kg_traefik2-0.8.0-py3-none-any.whl

---
+++ packages/kg-traefik2/kg_traefik2-0.8.0-py3-none-any.whl/kg_traefik2/__init__.py
@@ -0,0 +1,21 @@
+from .builder import (
+    Traefik2Builder,
+)
+from .option import (
+    Traefik2OptionsPort,
+    Traefik2Options,
+)
+from .configfile import (
+    Traefik2ConfigFileOptions,
+    Traefik2ConfigFile,
+)
+
+__version__ = "0.8.0"
+
+__all__ = [
+    'Traefik2Builder',
+    'Traefik2Options',
+    'Traefik2OptionsPort',
+    'Traefik2ConfigFileOptions',
+    'Traefik2ConfigFile',
+]
