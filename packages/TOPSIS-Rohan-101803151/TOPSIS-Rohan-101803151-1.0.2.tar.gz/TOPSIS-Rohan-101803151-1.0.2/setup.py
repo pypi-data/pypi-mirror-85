75c2bad68e04bea7e6d45705a711543c11bb60db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:26 +0000

    Add package TOPSIS-Rohan-101803151/1.0.2/TOPSIS-Rohan-101803151-1.0.2.tar.gz

---
+++ packages/TOPSIS-Rohan-101803151/TOPSIS-Rohan-101803151-1.0.2.tar.gz/TOPSIS-Rohan-101803151-1.0.2/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TOPSIS-Rohan-101803151",
+    version="1.0.2",
+    author="Rohan Dutt",
+    author_email="duttrohan0302@gmail.com",
+    description="A package that makes it easy to create Pypi packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/duttrohan0302/TOPSIS-Rohan-101803151',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/duttrohan0302/TOPSIS-Rohan-101803151/archive/v_02.tar.gz',
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          'pandas',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+) 
