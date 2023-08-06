2e0edc1140afc40aebf4d747958f036cc68913f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:35 +0000

    Add package Poutyne/1.1/Poutyne-1.1-py3-none-any.whl

---
+++ packages/Poutyne/Poutyne-1.1-py3-none-any.whl/poutyne/framework/optimizers.py
@@ -0,0 +1,26 @@
+import torch.optim as optim
+
+all_optimizers_dict = dict(
+    adadelta=optim.Adadelta,
+    adagrad=optim.Adagrad,
+    adam=optim.Adam,
+    sparseadam=optim.SparseAdam,
+    adamax=optim.Adamax,
+    asgd=optim.ASGD,
+    lbfgs=optim.LBFGS,
+    rmsprop=optim.RMSprop,
+    rprop=optim.Rprop,
+    sgd=optim.SGD,
+)
+
+
+def get_optimizer(optimizer, module):
+    if isinstance(optimizer, str):
+        optimizer = optimizer.lower()
+        params = (p for p in module.parameters() if p.requires_grad)
+        if optimizer != 'sgd':
+            return all_optimizers_dict[optimizer](params)
+
+        return all_optimizers_dict[optimizer](params, lr=1e-2)
+
+    return optimizer
