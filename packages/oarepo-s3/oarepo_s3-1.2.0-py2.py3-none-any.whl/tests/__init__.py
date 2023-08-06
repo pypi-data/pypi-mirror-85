78a2b55dac0ddf82820a5eacf6c986bf26446b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:44 +0000

    Add package oarepo-s3/1.2.0/oarepo_s3-1.2.0-py2.py3-none-any.whl

---
+++ packages/oarepo-s3/oarepo_s3-1.2.0-py2.py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018, 2019, 2020 Esteban J. G. Gabancho.
+#
+# oarepo-s3 is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""Tests module."""
