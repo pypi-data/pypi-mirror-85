7f6d8c12f97bfc8d956b78f596a4380801ed2d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package Topsis-Yash-101803617/0.0.0/TOPSIS-Yash-101803617-0.0.0.tar.gz

---
+++ packages/Topsis-Yash-101803617/TOPSIS-Yash-101803617-0.0.0.tar.gz/TOPSIS-Yash-101803617-0.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='TOPSIS-Yash-101803617',
+    description='A tool to calculate TOPSIS Score and Rank for given Dataset.',
+    Long_description=open('readme.txt').read() + '\n\n' + open('changelog.txt').read(),
+    url='',
+    author='Yash Shukla',
+    author_email='yash981815@gmail.com',
+    License='MIT',
+    classifiers=classifiers,
+    keywords='TOPSIS Calculator',
+    packages=find_packages(),
+    install_requires=['pandas']
+)
