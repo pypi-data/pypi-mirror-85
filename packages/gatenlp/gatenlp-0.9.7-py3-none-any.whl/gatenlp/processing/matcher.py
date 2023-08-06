5fb689d5d75c294a449923c7e8453585594e51a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:57 +0000

    Add package gatenlp/0.9.7/gatenlp-0.9.7-py3-none-any.whl

---
+++ packages/gatenlp/gatenlp-0.9.7-py3-none-any.whl/gatenlp/processing/matcher.py
@@ -0,0 +1,15 @@
+"""
+Module that defines classes for matchers other than gazetteers which match e.g. regular expressions
+of strings or annotations.
+"""
+
+class StringRegexMatcher:
+    """
+    NOT YET IMPLEMENTED
+    """
+    pass
+
+
+#class AnnotationRegexMatcher:
+#    """ """
+#    pass
