96a04e409a869fa463e82e54b5a4e534c600de3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package TOPSIS-PRAKIRTI-101803167/0.0.1/TOPSIS_PRAKIRTI_101803167-0.0.1.tar.gz

---
+++ packages/TOPSIS-PRAKIRTI-101803167/TOPSIS_PRAKIRTI_101803167-0.0.1.tar.gz/TOPSIS_PRAKIRTI_101803167-0.0.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    #Here is the module name.
+    name="TOPSIS_PRAKIRTI_101803167",
+ 
+    #version of the module
+    version="0.0.1",
+ 
+    #Name of Author
+    author="PRAKIRTI GUMBER",
+ 
+    #your Email address
+    author_email="pgumber_be18@thapar.edu",
+ 
+    #Small Description about module
+    description="Technique for Order Preference by Similarity to Ideal Solution (TOPSIS) originated in the 1980s as a multi-criteria decision making method. TOPSIS chooses the alternative of shortest Euclidean distance from the ideal solution, and greatest distance from the negative-ideal solution.",
+ 
+    long_description=long_description,
+ 
+    #Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+ 
+    #Any link to reach this module, if you have any webpage or github profile
+    #url="",
+    packages=setuptools.find_packages(),
+ 
+    #classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
