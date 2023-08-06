3537b88982e00671a4ccdb171860d5d4ba8cd058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:45 +0000

    Add package django-changed-fields/0.2.1/django_changed_fields-0.2.1.tar.gz

---
+++ packages/django-changed-fields/django_changed_fields-0.2.1.tar.gz/django_changed_fields-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='django_changed_fields',
+    version='0.2.1',
+    description='Testing installation of Package',
+    url='https://github.com/diegojsanches/DjangoChangedFields',
+    author='Diego Sanches',
+    author_email='diegojsanches@hotmail.com',
+    license='MIT',
+    packages=['django_changed_fields'],
+    zip_safe=False
+)
