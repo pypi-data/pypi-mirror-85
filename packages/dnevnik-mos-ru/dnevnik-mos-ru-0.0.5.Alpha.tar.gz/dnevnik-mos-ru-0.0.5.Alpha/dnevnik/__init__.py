c1c9a16d1431fc49f269d50f2a36525df546c497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:36 +0000

    Add package dnevnik-mos-ru/0.0.5.Alpha/dnevnik-mos-ru-0.0.5.Alpha.tar.gz

---
+++ packages/dnevnik-mos-ru/dnevnik-mos-ru-0.0.5.Alpha.tar.gz/dnevnik-mos-ru-0.0.5.Alpha/dnevnik/__init__.py
@@ -0,0 +1,6 @@
+from .school import School
+from .class_unit import ClassUnit
+from .group import Group
+from .teacher import Teacher
+from .student_profile import StudentProfile
+from .client import Client
---
+++ packages/dnevnik-mos-ru/dnevnik-mos-ru-0.0.5.Alpha.tar.gz/dnevnik-mos-ru-0.0.5.Alpha/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dnevnik-mos-ru',
+    version="0.0.5 Alpha",
+    url='https://github.com/IvanProgramming/dnevnik_mos_ru',
+    author='Ivan Vlasov',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ]
+)
