a699ece8fe496a62f1b26e197073cf7b83f858ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:13 +0000

    Add package cs-models/0.0.120/cs_models-0.0.120-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.120-py3-none-any.whl/cs_models/resources/DrugIndicationUMLS/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class DrugIndicationUMLSResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    set_id = fields.String(validate=not_blank, required=True)
+    cui = fields.String(required=True)
+    preferred_name = fields.String(allow_none=True)
+    score = fields.Integer()
+    updated_at = fields.DateTime()
