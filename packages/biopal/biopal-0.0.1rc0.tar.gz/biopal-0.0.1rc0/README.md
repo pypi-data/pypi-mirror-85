292377e6e2c1ee4943e411279dff62cf6f3c1e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:12 +0000

    Add package biopal/0.0.1rc0/biopal-0.0.1rc0.tar.gz

---
+++ packages/biopal/biopal-0.0.1rc0.tar.gz/biopal-0.0.1rc0/README.md
@@ -0,0 +1,3 @@
+# BioPAL
+
+The **BIOMASS Product Algorithm Laboratory** hosts tools for processing and analysing ESA's BIOMASS mission data.
---
+++ packages/biopal/biopal-0.0.1rc0.tar.gz/biopal-0.0.1rc0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='biopal',
+    version='0.0.1rc',
+    description='BIOMASS Product Algorithm Laboratory',
+    long_description='',
+    url='',
+    maintainer='BioPAL developers',
+    maintainer_email='stefanie.lumnitz@gmail.com',
+    packages=[],
+    classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Topic :: Scientific/Engineering',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7'
+        ],
+)
