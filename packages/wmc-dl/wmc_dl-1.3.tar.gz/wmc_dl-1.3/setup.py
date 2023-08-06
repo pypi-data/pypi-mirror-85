a5b761953fcd0b259d6a231afa04de760f210967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:02 +0000

    Add package wmc-dl/1.3/wmc_dl-1.3.tar.gz

---
+++ packages/wmc-dl/wmc_dl-1.3.tar.gz/wmc_dl-1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='wmc_dl',
+    version='1.3',
+    description='Functions from WMC Datalake',
+    long_description=open('README.txt').read() +'\n\n'+ open('CHANGELOG.txt').read(),
+    url='',
+    author='Daniel Santhiago',
+    autor_email='daniel.santhiago@thrive-wmccann.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='WMCCann',
+    packages=find_packages(),
+    install_requires=['unidecode','criteo_marketing']
+
+
+)
