6e5d30fd3f702ba911f8a4abdc1f91bdf354b831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:25 +0000

    Add package EnergyFlow/1.3.0/EnergyFlow-1.3.0-py2.py3-none-any.whl

---
+++ packages/EnergyFlow/EnergyFlow-1.3.0-py2.py3-none-any.whl/energyflow/__init__.py
@@ -0,0 +1,33 @@
+"""The EnergyFlow package."""
+from __future__ import absolute_import
+
+# import top-level submodules
+from . import algorithms
+from . import base
+from . import datasets
+from . import efm
+from . import efp
+from . import emd
+from . import gen
+from . import measure
+from . import obs
+from . import utils
+
+# import top-level attributes
+from .datasets import *
+from .efm import *
+from .efp import *
+from .gen import *
+from .measure import *
+from .obs import *
+from .utils import *
+
+__all__ = (datasets.__all__ +
+           efm.__all__ +
+           efp.__all__ +
+           gen.__all__ +
+           measure.__all__ +
+           obs.__all__ +
+           utils.__all__)
+
+__version__ = '1.3.0'
---
+++ packages/EnergyFlow/EnergyFlow-1.3.0-py2.py3-none-any.whl/energyflow/archs/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+
+import warnings
+
+__all__ = []
+
+# requires keras/tensorflow
+try:
+    from . import archbase
+    from . import cnn
+    from . import dnn
+    from . import efn
+    from .cnn import *
+    from .dnn import *
+    from .efn import *
+
+    __all__ += cnn.__all__ + dnn.__all__ + efn.__all__
+
+except ImportError as e:
+    warnings.warn('could not import some architectures - ' + str(e))
+
+# requires sklearn
+try:
+    from . import linear
+    from .linear import *
+
+    __all__ += linear.__all__
+
+except ImportError as e:
+    warnings.warn('could not import some architectures - ' + str(e))
