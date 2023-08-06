dab3186c5b8c87c872a8655b9608cdaab27a2713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:24 +0000

    Add package yaweather/1.2.2/yaweather-1.2.2-py3-none-any.whl

---
+++ packages/yaweather/yaweather-1.2.2-py3-none-any.whl/yaweather/models/response.py
@@ -0,0 +1,27 @@
+import datetime
+from typing import List
+
+from .base import Base
+from .fact import Fact
+from .forecast import Forecast
+from .info import Info
+
+
+class Response(Base):
+    pass
+
+
+class ResponseForecast(Response):
+    now: int
+    now_dt: datetime.datetime
+    info: Info
+    fact: Fact
+    forecasts: List[Forecast]
+
+
+class ResponseInformers(Response):
+    now: int
+    now_dt: datetime.datetime
+    info: Info
+    fact: Fact
+    forecast: Forecast
