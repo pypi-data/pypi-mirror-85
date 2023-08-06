1b7bc936a0eac8dabd800dc028e7f8fb7ba3bafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:59 +0000

    Add package storagetest/1.1.2/storagetest-1.1.2.tar.gz

---
+++ packages/storagetest/storagetest-1.1.2.tar.gz/storagetest-1.1.2/storagetest/pkgs/raw/ut/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@file  : __init__.py.py
+@Time  : 2020/11/11 16:27
+@Author: Tao.Xu
+@Email : tao.xu2008@outlook.com
+"""
+
+from .raw_ut import *
+__all__ = ['RawUT', 'UnitTestCase']
+
+if __name__ == '__main__':
+    pass
