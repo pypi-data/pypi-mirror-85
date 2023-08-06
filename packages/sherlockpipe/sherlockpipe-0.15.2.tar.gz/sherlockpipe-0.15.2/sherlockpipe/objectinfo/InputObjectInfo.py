992f518010e803c8e5c5529be2b2b9b0642decfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:49 +0000

    Add package sherlockpipe/0.15.2/sherlockpipe-0.15.2.tar.gz

---
+++ packages/sherlockpipe/sherlockpipe-0.15.2.tar.gz/sherlockpipe-0.15.2/sherlockpipe/objectinfo/InputObjectInfo.py
@@ -0,0 +1,23 @@
+import os
+from sherlockpipe.objectinfo.ObjectInfo import ObjectInfo
+
+class InputObjectInfo(ObjectInfo):
+    """
+    Implementation of ObjectInfo to be used to characterize objects which are to be loaded from a csv file.
+    """
+    def __init__(self, input_file, initial_mask=None, initial_detrend_period=None, star_info=None):
+        """
+        @param input_file: the file to be used for loading the light curve
+        @param initial_mask: an array of time ranges provided to mask them into the initial object light curve.
+        @param initial_detrend_period: integer value specifying a fixed value for an initial period to be detrended
+        from the initial light curve before processing.
+        @param star_info: input star information
+        """
+        super().__init__(initial_mask, initial_detrend_period, star_info)
+        self.input_file = input_file
+
+    def sherlock_id(self):
+        return "INP_" + os.path.splitext(self.input_file)[0].replace("/", "_")
+
+    def mission_id(self):
+        return None
