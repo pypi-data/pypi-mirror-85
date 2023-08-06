44b963d9336ffc427995c6a5c30e9f6b8b787236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package auto-labeling-pipeline/0.1.1/auto_labeling_pipeline-0.1.1-py3-none-any.whl

---
+++ packages/auto-labeling-pipeline/auto_labeling_pipeline-0.1.1-py3-none-any.whl/auto_labeling_pipeline/postprocessing.py
@@ -0,0 +1,23 @@
+import abc
+from typing import Optional, Set
+
+from auto_labeling_pipeline.labels import Labels
+
+
+class BasePostProcessor(metaclass=abc.ABCMeta):
+
+    def __init__(self, stop_labels: Optional[Set[str]] = None, mapping: Optional[dict] = None):
+        self.stop_labels = stop_labels
+        self.mapping = mapping
+
+    @abc.abstractmethod
+    def transform(self, labels: Labels) -> Labels:
+        raise NotImplementedError
+
+
+class PostProcessor(BasePostProcessor):
+
+    def transform(self, labels: Labels) -> Labels:
+        labels = labels.filter_by_name(self.stop_labels)
+        labels = labels.replace_label(self.mapping)
+        return labels
