4d699fbe9974d491c2b18a445a67cd0d4ce1cc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:48 +0000

    Add package conectividadeapp/0.0.11/conectividadeapp-0.0.11-py3-none-any.whl

---
+++ packages/conectividadeapp/conectividadeapp-0.0.11-py3-none-any.whl/conectividadeapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from datetime import date
+
+
+class ActivityOptions(models.TextChoices):
+    INSTALACAO = 'IN', 'Instalação'
+    REMOCAO = 'RE', 'Remoção'
+
+
+class InstalacaoAtivo(models.Model):
+
+    comments = models.CharField(max_length=200)
+    atv_data = models.DateField(default=date.today)
+    reg_atv = models.DateTimeField(auto_now_add=True)
+    actor_id = models.ManyToManyField('Actor')
+    options = models.CharField(
+        max_length=2,
+        choices=ActivityOptions.choices,
+        default=ActivityOptions.INSTALACAO,
+    )
+
+    def __str__(self):
+        return self.comments
+
+
+class Actor(models.Model):
+
+    name = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20)
+    cellphone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
