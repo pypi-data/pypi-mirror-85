9369b28e24aec5367e475bce9a946db7cf18488b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:15 +0000

    Add package my_demo_617_20201114_05/2020.11.14.5/my_demo_617_20201114_05-2020.11.14.5.tar.gz

---
+++ packages/my_demo_617_20201114_05/my_demo_617_20201114_05-2020.11.14.5.tar.gz/my_demo_617_20201114_05-2020.11.14.5/package_test/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import os
+import setuptools
+
+setuptools.setup(
+    name='my_demo_617_20201114_05',
+    version='2020.11.14.5',
+    keywords='demo',
+    description='A demo for python packaging.',
+    long_description=open( os.path.join( os.path.dirname(__file__), 'Readme.rst' )).read(),
+    author='liuyaqi',  # 替换为你的Pypi官网账户名
+    author_email='yaqi_617@163.com',  # 替换为你Pypi账户名绑定的邮箱
+
+    url='https://github.com/***/***',  # 这个地方为github项目地址，貌似非必须
+    packages=setuptools.find_packages(),
+    license='JD'
+)
