d7e861a5d2560dfe154f9a4238317037f2ab12be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:51 +0000

    Add package dummy-wx/0.3.0/dummy_wx-0.3.0-py3-none-any.whl

---
+++ packages/dummy-wx/dummy_wx-0.3.0-py3-none-any.whl/wx/lib/floatcanvas/Utilities/__init__.py
@@ -0,0 +1,5 @@
+"""
+__init__ for the floatcanvas Utilities package
+
+"""
+pass
---
+++ packages/dummy-wx/dummy_wx-0.3.0-py3-none-any.whl/wx/xrc.py
@@ -0,0 +1,26 @@
+from wx import PyEventBinder
+
+# Based on wxPython
+# Copyright: (c) 2018 by Total Control Software
+# License:   wxWindows License
+
+
+def dummy_function(*args, **kwargs):
+	return 0
+
+
+class Animation: ...
+EmptyXmlResource = dummy_function
+ID_NONE = -3
+XRCCTRL = dummy_function
+XRCID = dummy_function
+XRC_NO_RELOADING = 4
+XRC_NO_SUBCLASSING = 2
+XRC_USE_ENVVARS = 8
+XRC_USE_LOCALE = 1
+class XmlResource: ...
+class XmlResourceFlags: ...
+class XmlResourceHandler: ...
+class XmlSubclassFactory: ...
+class XmlSubclassFactory_Python: ...
+_my_import = dummy_function
