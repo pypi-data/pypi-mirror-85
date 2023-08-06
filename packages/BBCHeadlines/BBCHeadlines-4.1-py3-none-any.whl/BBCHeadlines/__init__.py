1836db55b033b7aff2ee3effe1d11f9513e94c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:05 +0000

    Add package BBCHeadlines/4.1/BBCHeadlines-4.1-py3-none-any.whl

---
+++ packages/BBCHeadlines/BBCHeadlines-4.1-py3-none-any.whl/BBCHeadlines/__init__.py
@@ -0,0 +1,8 @@
+import feedparser
+
+def news(limit=False):
+    entries = feedparser.parse('http://feeds.bbci.co.uk/news/world/rss.xml')['entries']
+    if limit:
+        return(entries[:limit])
+    else:
+        return(entries)
