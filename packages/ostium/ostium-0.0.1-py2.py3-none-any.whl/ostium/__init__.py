beffc0a37927b02feea73ca50c5f7ecb1c0dd6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:18 +0000

    Add package ostium/0.0.1/ostium-0.0.1-py2.py3-none-any.whl

---
+++ packages/ostium/ostium-0.0.1-py2.py3-none-any.whl/ostium/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Gateway service in FastAPI/Starlette
+"""
+
+from .version import __version__
---
+++ packages/ostium/ostium-0.0.1-py2.py3-none-any.whl/ostium/version.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Module containing package version.
+"""
+
+__version__ = '0.0.1'
