5602ba0e437c19eec8d3c2a2ab08bd5c284da898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:19 +0000

    Add package cmeasures/0.3/cmeasures-0.3.tar.gz

---
+++ packages/cmeasures/cmeasures-0.3.tar.gz/cmeasures-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cmeasures',
+      version='0.3',
+      description='Centrality Measures',
+      packages=['cmeasures'],
+      author_email='buba222@list.ru',
+      zip_safe=False)
