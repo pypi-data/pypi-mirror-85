6c9e2a8f22dcbbf0b3cc1f24015c55834f84d802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:01 +0000

    Add package pynewyear/1.0.0/pynewyear-1.0.0.tar.gz

---
+++ packages/pynewyear/pynewyear-1.0.0.tar.gz/pynewyear-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pynewyear",
+    version="1.0.0",
+    author="Baboshin Sergey",
+    author_email="ijustbsd@gmail.com",
+    description="Happy New Year! 🎄",
+    url="https://github.com/ijustbsd/pynewyear",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
