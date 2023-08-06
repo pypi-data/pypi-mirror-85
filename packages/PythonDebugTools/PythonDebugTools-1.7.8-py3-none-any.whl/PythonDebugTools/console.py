30607bf8b060a21b0573e43e4fe86c2cce983a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package PythonDebugTools/1.7.8/PythonDebugTools-1.7.8-py3-none-any.whl

---
+++ packages/PythonDebugTools/PythonDebugTools-1.7.8-py3-none-any.whl/PythonDebugTools/console.py
@@ -0,0 +1,24 @@
+import pprint
+import traceback
+
+
+
+
+__all__ = ['PRINT', 'getPPrintStr', 'print_exception']
+debug = __debug__
+
+pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+def PRINT(title: str, *args, _pp: pprint.PrettyPrinter = None, **kwargs):
+    if not debug: return
+    print(f"\n ---------------- {title} ---------------- \n\r")
+    (_pp or pp).pprint(dict(args=args, kwargs=kwargs))
+
+
+
+def getPPrintStr(o: any, *, _pp: pprint.PrettyPrinter = None) -> str: return (_pp or pp).pformat(o)
+
+
+
+def print_exception(e: Exception):
+    if not debug: return
+    traceback.print_exception(type(e), e, e.__traceback__)
