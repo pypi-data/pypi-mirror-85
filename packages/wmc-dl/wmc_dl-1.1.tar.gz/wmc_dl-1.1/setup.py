fe43da91dbbcea94c7dc44bee59a46f1ab28e0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package wmc-dl/1.1/wmc_dl-1.1.tar.gz

---
+++ packages/wmc-dl/wmc_dl-1.1.tar.gz/wmc_dl-1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='wmc_dl',
+    version='1.1',
+    description='Functions from WMC Datalake',
+    long_description='',
+    url='',
+    author='Daniel Santhiago',
+    autor_email='daniel.santhiago@thrive-wmccann.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['']
+
+
+)
