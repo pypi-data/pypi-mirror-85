e53cb84bd4446b05f85691c1d256190cb771b2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:54 +0000

    Add package PyANSI/1.0.1b0/PyANSI-1.0.1b0.tar.gz

---
+++ packages/PyANSI/PyANSI-1.0.1b0.tar.gz/PyANSI-1.0.1b0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyANSI",
+    version="1.0.1-beta",
+    author="Oliver Jane",
+    description="Easy ANSI control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license="MPL",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
