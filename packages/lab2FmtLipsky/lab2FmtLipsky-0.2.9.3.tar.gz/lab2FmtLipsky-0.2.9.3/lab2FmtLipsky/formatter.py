d0c1c9d6c5c374c183c761e79f5d1c4e50a8570f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:55 +0000

    Add package lab2FmtLipsky/0.2.9.3/lab2FmtLipsky-0.2.9.3.tar.gz

---
+++ packages/lab2FmtLipsky/lab2FmtLipsky-0.2.9.3.tar.gz/lab2FmtLipsky-0.2.9.3/lab2FmtLipsky/formatter.py
@@ -0,0 +1,22 @@
+import logging
+
+from .fileFormatter import FileFormatter
+from .cmdParser import CmdParser
+import log
+
+
+logger = logging.getLogger('Formatter')
+
+logger.debug(f'program started')
+
+
+def run():
+    FileFormatter(CmdParser.parse_arguments()).run()
+
+
+logger.debug(f'program finished')
+
+#
+# import lab2FmtLipsky
+#
+# lab2FmtLipsky.FileFormatter(lab2FmtLipsky.CmdParser.parse_arguments()).run()
