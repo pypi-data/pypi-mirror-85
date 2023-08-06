a8cb415d53f42db804e7dab692d056a5fda8f643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:27 +0000

    Add package ktools/0.2.9/ktools-0.2.9.tar.gz

---
+++ packages/ktools/ktools-0.2.9.tar.gz/ktools-0.2.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="ktools",
+    version="0.2.9",
+    url="https://github.com/fx-kirin/ktools",
+
+    author="fx-kirin",
+    author_email="ono.kirin@gmail.com",
+
+    description="kirin's toolkit.",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
