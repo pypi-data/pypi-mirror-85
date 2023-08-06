ab694e5c17fa4d755bdc44c629061602f9806ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:38 +0000

    Add package vyper-config/1.0.0/vyper_config-1.0.0-py3-none-any.whl

---
+++ packages/vyper-config/vyper_config-1.0.0-py3-none-any.whl/vyper/constants.py
@@ -0,0 +1,5 @@
+# Universally supported extensions.
+SUPPORTED_EXTENSIONS = ["json", "toml", "yaml", "yml"]
+
+# Universally supported remote providers.
+SUPPORTED_REMOTE_PROVIDERS = ["etcd", "consul", "zookeeper"]
