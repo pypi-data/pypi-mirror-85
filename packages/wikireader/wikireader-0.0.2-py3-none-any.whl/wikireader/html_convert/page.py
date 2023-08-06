3b3bd4dee0da4fb5f0c94afb7743eb5708886b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:01 +0000

    Add package wikireader/0.0.2/wikireader-0.0.2-py3-none-any.whl

---
+++ packages/wikireader/wikireader-0.0.2-py3-none-any.whl/wikireader/html_convert/page.py
@@ -0,0 +1,15 @@
+from .page_template import page_layout
+from .css import get_css
+from .text2html import ArticleContent
+
+
+def generate_html(article):
+    """
+    generates html including css
+
+    :param article: Article object containing title and content as plain text
+    """
+    content = ArticleContent(article.content)
+    html =  page_layout(article.title, content, get_css())
+    return html
+
---
+++ packages/wikireader/wikireader-0.0.2-py3-none-any.whl/wikireader/main.py
@@ -0,0 +1,33 @@
+from .browser import run_browser
+from .wiki import search
+from .wiki import get_article
+from .html_convert import generate_html
+from .dmenu import run_dmenu
+
+
+def display(keyword):
+    """
+    Gets article and shows it in html viewer
+    """
+    content = get_article(keyword)
+    content_html = generate_html(content)
+    run_browser(content_html)
+
+
+def main():
+    """
+    Runs dmenu twice, first for a keyword, then it 
+    returns to offer a list from which the user can choose
+    Finally it loads the article
+    """
+    article = None
+    word = search(run_dmenu())
+    if word:
+        article = run_dmenu(word)
+        print(article)
+    if article:
+        display(article)
+
+
+if __name__ == '__main__':
+    main()
