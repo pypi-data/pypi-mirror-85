7ea74fd57369c763942f71108501037305ac94c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:02 +0000

    Add package mikezhou-package/1.0.0/mikezhou_package-1.0.0.tar.gz

---
+++ packages/mikezhou-package/mikezhou_package-1.0.0.tar.gz/mikezhou_package-1.0.0/mikezhou_package/main.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Mike Zhou
+# @Email : 公众号：测试开发技术
+# @File : app.py
+# @Project: 第22课时
+import itertools
+
+case_list = ['用户名', '密码']
+value_list = ['正确', '不正确', '特殊符号', '超过最大长度']
+
+
+def gen_case(item=case_list, value=value_list):
+    '''输出笛卡尔用例集合'''
+    for i in itertools.product(item, value):
+        print('输入'.join(i))
+
+def test_print():
+	print("hello package mikezhou !")
+#
+if __name__ == '__main__':
+    gen_case()
---
+++ packages/mikezhou-package/mikezhou_package-1.0.0.tar.gz/mikezhou_package-1.0.0/setup.py
@@ -0,0 +1,41 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Mike Zhou
+# @Email : 公众号：测试开发技术
+# @File : setup.py.py
+# @Project: 第22课时
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(name='mikezhou_package',  # 包名
+      version='1.0.0',  # 版本号
+      description='A small example package',
+      long_description=long_description,
+      author='mikezhou_talk',
+      author_email='762357658@qq.com',
+      url='https://github.com/zhoujinjian',
+      install_requires=[],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
