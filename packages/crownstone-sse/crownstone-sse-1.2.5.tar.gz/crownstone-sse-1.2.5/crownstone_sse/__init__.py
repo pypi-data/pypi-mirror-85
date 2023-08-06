92d6a97f3863227f16b95e1d4eb666ed91357900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:21 +0000

    Add package crownstone-sse/1.2.5/crownstone-sse-1.2.5.tar.gz

---
+++ packages/crownstone-sse/crownstone-sse-1.2.5.tar.gz/crownstone-sse-1.2.5/crownstone_sse/__init__.py
@@ -0,0 +1,3 @@
+"""Library that listens to Crownstone SSE events"""
+from crownstone_sse.client import CrownstoneSSE
+from crownstone_sse.util.eventbus import EventBus
---
+++ packages/crownstone-sse/crownstone-sse-1.2.5.tar.gz/crownstone-sse-1.2.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='crownstone-sse',
+    version='1.2.5',
+    url='https://github.com/crownstone/crownstone-lib-python-sse',
+    author='Crownstone B.V.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=['examples', 'tests']),
+    install_requires=list(package.strip() for package in open('requirements.txt')),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    python_requires='>=3.7',
+)
