02cf35054da9c649545d5866e985a44c7e11714e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:26 +0000

    Add package catsh/0.1.0/catsh-0.1.0-py3-none-any.whl

---
+++ packages/catsh/catsh-0.1.0-py3-none-any.whl/src/utils/Warning.py
@@ -0,0 +1,16 @@
+from colorama import init, Fore, Style
+
+
+class Warning:
+    def __init__(self, msg="Undefined warning message"):
+        self.msg = msg
+
+    def run(self):
+        print(
+            Fore.YELLOW
+            + Style.BRIGHT
+            + "Warning: "
+            + Fore.RESET
+            + Style.RESET_ALL
+            + self.msg
+        )
