628138e7634ef30f7bf8796b981eb22b647e359e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:17 +0000

    Add package BBIOPY/0.1.1/BBIOPY-0.1.1.tar.gz

---
+++ packages/BBIOPY/BBIOPY-0.1.1.tar.gz/BBIOPY-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="BBIOPY",
+	version="0.1.1",
+	author="Eric Morse, Joshua Key, and Mark Yoder",
+	description="Full python implementation of BBIO gpiod",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/EricMorse/ECE434-Project",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
