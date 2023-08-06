9e5ba0a9c7794f88cdc599a3a1eaef8bdf3d137c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:16 +0000

    Add package pyheos/0.7.2/pyheos-0.7.2.tar.gz

---
+++ packages/pyheos/pyheos-0.7.2.tar.gz/pyheos-0.7.2/pyheos/connection.py
@@ -0,0 +1,349 @@
+"""Define the connection module."""
+
+import asyncio
+from collections import defaultdict
+from datetime import datetime, timedelta
+import json
+import logging
+from typing import Any, DefaultDict, Dict, List, Optional
+
+from . import const
+from .command import HeosCommands
+from .error import CommandError, HeosError, format_error_message
+from .response import HeosResponse
+
+SEPARATOR = "\r\n"
+SEPARATOR_BYTES = SEPARATOR.encode()
+
+_LOGGER = logging.getLogger(__name__)
+
+
+_QUOTE_MAP = {"&": "%26", "=": "%3D", "%": "%25"}
+
+_MASKED_PARAMS = {"pw"}
+
+_MASK = "********"
+
+
+def _quote(string: str) -> str:
+    """Quote a string per the CLI specification."""
+    return "".join([_QUOTE_MAP.get(char, char) for char in str(string)])
+
+
+def _encode_query(items: dict, *, mask=False) -> str:
+    """Encode a dict to query string per CLI specifications."""
+    pairs = []
+    for key in sorted(items.keys()):
+        value = _MASK if mask and key in _MASKED_PARAMS else items[key]
+        item = "{}={}".format(key, _quote(value))
+        # Ensure 'url' goes last per CLI spec
+        if key == "url":
+            pairs.append(item)
+        else:
+            pairs.insert(0, item)
+    return "&".join(pairs)
+
+
+class HeosConnection:
+    """Define a class that encapsulates read/write."""
+
+    def __init__(
+        self,
+        heos,
+        host: str,
+        *,
+        timeout: float = const.DEFAULT_TIMEOUT,
+        heart_beat: Optional[float] = const.DEFAULT_HEART_BEAT,
+        all_progress_events=True
+    ):
+        """Init a new HeosConnection class."""
+        self._heos = heos
+        self._all_progress_events = all_progress_events
+        self.host = host  # type: str
+        self.commands = HeosCommands(self)
+        self.timeout = timeout  # type: int
+        self._reader = None  # type: asyncio.StreamReader
+        self._writer = None  # type: asyncio.StreamWriter
+        self._response_handler_task = None  # type: asyncio.Task
+        self._pending_commands = defaultdict(
+            list
+        )  # type: DefaultDict[str, List[ResponseEvent]]
+        self._sequence = 0  # type: int
+        self._state = const.STATE_DISCONNECTED  # type: str
+        self._auto_reconnect = False  # type: bool
+        self._reconnect_delay = const.DEFAULT_RECONNECT_DELAY  # type: float
+        self._reconnect_task = None  # type: asyncio.Task
+        self._last_activity = None  # type: datetime
+        self._heart_beat_interval = heart_beat  # type: Optional[float]
+        self._heart_beat_task = None  # type: asyncio.Task
+
+    async def connect(
+        self,
+        *,
+        auto_reconnect: bool = False,
+        reconnect_delay: float = const.DEFAULT_RECONNECT_DELAY
+    ):
+        """Invoke the connect operation."""
+        if self._state == const.STATE_CONNECTED:
+            return
+        # Ensure we don't try to reconnect during initial failures
+        self._auto_reconnect = False
+        await self._connect()
+        self._auto_reconnect = auto_reconnect
+        self._reconnect_delay = reconnect_delay
+
+    async def _connect(self):
+        """Perform core connection logic."""
+        try:
+            open_future = asyncio.open_connection(self.host, const.CLI_PORT)
+            self._reader, self._writer = await asyncio.wait_for(
+                open_future, self.timeout
+            )
+        except (OSError, ConnectionError, asyncio.TimeoutError) as err:
+            raise HeosError(format_error_message(err)) from err
+        # Start response handler
+        self._response_handler_task = asyncio.ensure_future(self._response_handler())
+        # Set state before calling command as it checks for handling
+        self._state = const.STATE_CONNECTED
+        await self.commands.register_for_change_events()
+        # Start heart beat if enabled.
+        if self._heart_beat_interval is not None and self._heart_beat_interval > 0:
+            self._heart_beat_task = asyncio.ensure_future(self._heart_beat())
+
+        _LOGGER.debug("Connected to %s", self.host)
+        self._heos.dispatcher.send(const.SIGNAL_HEOS_EVENT, const.EVENT_CONNECTED)
+
+    async def disconnect(self):
+        """Disconnect from the device."""
+        if self._state == const.STATE_DISCONNECTED:
+            return
+        # Cancel pending reconnect
+        if self._reconnect_task:
+            self._reconnect_task.cancel()
+            await self._reconnect_task
+            self._reconnect_task = None
+        # Core disconnect
+        await self._disconnect()
+        self._state = const.STATE_DISCONNECTED
+
+        _LOGGER.debug("Disconnected from %s", self.host)
+        self._heos.dispatcher.send(const.SIGNAL_HEOS_EVENT, const.EVENT_DISCONNECTED)
+
+    async def _disconnect(self):
+        # Cancel response handler
+        if self._heart_beat_task:
+            self._heart_beat_task.cancel()
+            try:
+                await self._heart_beat_task
+            except asyncio.CancelledError:
+                pass
+            self._heart_beat_task = None
+        if self._response_handler_task:
+            self._response_handler_task.cancel()
+            await self._response_handler_task
+            self._response_handler_task = None
+        # Close channel
+        if self._writer:
+            self._writer.close()
+            self._writer = None
+        self._reader = None
+        self._sequence = 0
+        self._pending_commands.clear()
+
+    async def _handle_connection_error(self, error: Exception):
+        """Handle connection failures and schedule reconnect."""
+        if self._reconnect_task:
+            return
+
+        await self._disconnect()
+
+        if self._auto_reconnect:
+            self._state = const.STATE_RECONNECTING
+            self._reconnect_task = asyncio.ensure_future(self._reconnect())
+        else:
+            self._state = const.STATE_DISCONNECTED
+
+        _LOGGER.debug("Disconnected from %s: %s", self.host, error)
+        self._heos.dispatcher.send(const.SIGNAL_HEOS_EVENT, const.EVENT_DISCONNECTED)
+
+    async def _reconnect(self):
+        """Perform core reconnection logic."""
+        while self._state != const.STATE_CONNECTED:
+            try:
+                await self._connect()
+                self._reconnect_task = None
+                return
+            except HeosError as err:
+                # Occurs when we could not reconnect
+                _LOGGER.debug("Failed to reconnect to %s: %s", self.host, err)
+                await self._disconnect()
+                await asyncio.sleep(self._reconnect_delay)
+            except asyncio.CancelledError:
+                # Occurs when reconnect is cancelled via disconnect
+                return
+
+    async def _response_handler(self):
+        while True:
+            # Wait for response
+            try:
+                result = await self._reader.readuntil(SEPARATOR_BYTES)
+                self._last_activity = datetime.utcnow()
+                data = json.loads(result.decode())
+                response = HeosResponse(data)
+
+                # Ignore processing
+                if response.is_under_process:
+                    _LOGGER.debug(
+                        "Command under process '%s': '%s'", response.command, data
+                    )
+                    continue
+                # Handle events
+                if response.is_event:
+                    asyncio.ensure_future(self._handle_event(response))
+                    continue
+                # Find pending command
+                commands = self._pending_commands[response.command]
+                if not commands:
+                    _LOGGER.debug(
+                        "Received response with no pending command: '%s'",
+                        response.command,
+                    )
+                    continue
+                sequence = response.get_message("sequence")
+                if sequence:
+                    sequence_id = int(sequence)
+                    event = next(
+                        (event for event in commands if event.sequence == sequence_id),
+                        None,
+                    )
+                    commands.remove(event)
+                else:
+                    event = commands.pop(0)
+                event.set(response)
+
+            except asyncio.CancelledError:
+                # Occurs when the task is being killed
+                return
+            except (
+                ConnectionError,
+                asyncio.IncompleteReadError,
+                RuntimeError,
+                OSError,
+            ) as error:
+                # Occurs when the connection breaks
+                asyncio.ensure_future(self._handle_connection_error(error))
+                return
+
+    async def _heart_beat(self):
+        while self._state == const.STATE_CONNECTED:
+            last_activity = datetime.utcnow() - self._last_activity
+            threshold = timedelta(seconds=self._heart_beat_interval)
+            if last_activity > threshold:
+                try:
+                    await self.commands.heart_beat()
+                except CommandError:
+                    pass
+            await asyncio.sleep(self._heart_beat_interval / 2)
+
+    async def command(
+        self, command: str, params: Dict[str, Any] = None
+    ) -> HeosResponse:
+        """Execute a command and get it's response."""
+        # Build command URI
+        sequence = self._sequence
+        self._sequence += 1
+        params = params or {}
+        params["sequence"] = sequence
+        uri = "{}{}?{}".format(const.BASE_URI, command, _encode_query(params))
+        masked_uri = "{}{}?{}".format(
+            const.BASE_URI, command, _encode_query(params, mask=True)
+        )
+
+        if self._state != const.STATE_CONNECTED:
+            _LOGGER.debug(
+                "Command failed '%s': %s", masked_uri, "Not connected to device"
+            )
+            raise CommandError(command, "Not connected to device")
+
+        # Add reservation
+        event = ResponseEvent(sequence)
+        self._pending_commands[command].append(event)
+        # Send command
+        try:
+            self._writer.write((uri + SEPARATOR).encode())
+            await self._writer.drain()
+            response = await asyncio.wait_for(event.wait(), self.timeout)
+        except (ConnectionError, asyncio.TimeoutError, OSError) as error:
+            # Occurs when the connection breaks
+            asyncio.ensure_future(self._handle_connection_error(error))
+            message = format_error_message(error)
+            _LOGGER.debug("Command failed '%s': %s", masked_uri, message)
+            raise CommandError(command, message) from error
+
+        if response.result:
+            _LOGGER.debug("Command executed '%s': '%s'", masked_uri, response)
+        else:
+            _LOGGER.debug("Command failed '%s': %s", masked_uri, response)
+        response.raise_for_result()
+        return response
+
+    async def _handle_event(self, response: HeosResponse):
+        """Handle a response event."""
+        if response.command in const.PLAYER_EVENTS:
+            player_id = response.get_player_id()
+            player = self._heos.players.get(player_id)
+            if player and (
+                await player.event_update(response, self._all_progress_events)
+            ):
+                self._heos.dispatcher.send(
+                    const.SIGNAL_PLAYER_EVENT, player_id, response.command
+                )
+                _LOGGER.debug("Event received for player %s: %s", player, response)
+        elif response.command in const.GROUP_EVENTS:
+            group_id = response.get_group_id()
+            group = self._heos.groups.get(group_id)
+            if group:
+                await group.event_update(response)
+                self._heos.dispatcher.send(
+                    const.SIGNAL_GROUP_EVENT, group_id, response.command
+                )
+                _LOGGER.debug("Event received for group %s: %s", group_id, response)
+        elif response.command in const.HEOS_EVENTS:
+            # pylint: disable=protected-access
+            result = await self._heos._handle_event(response)
+            self._heos.dispatcher.send(
+                const.SIGNAL_CONTROLLER_EVENT, response.command, result
+            )
+            _LOGGER.debug("Event received: %s", response)
+        else:
+            _LOGGER.debug("Unrecognized event: %s", response)
+
+    @property
+    def state(self) -> str:
+        """Get the current state of the connection."""
+        return self._state
+
+
+class ResponseEvent:
+    """Define an awaitable command event response."""
+
+    def __init__(self, sequence: int):
+        """Init a new instance of the CommandEvent."""
+        self._event = asyncio.Event()
+        self._sequence = sequence
+        self._response = None
+
+    @property
+    def sequence(self) -> int:
+        """Get the sequence that represents this event."""
+        return self._sequence
+
+    async def wait(self):
+        """Wait until the event is set."""
+        await self._event.wait()
+        return self._response
+
+    def set(self, response: HeosResponse):
+        """Set the response."""
+        self._response = response
+        self._event.set()
---
+++ packages/pyheos/pyheos-0.7.2.tar.gz/pyheos-0.7.2/pyheos/const.py
@@ -0,0 +1,289 @@
+"""Define consts for the pyheos package."""
+
+__title__ = "pyheos"
+__version__ = "0.7.2"
+
+CLI_PORT = 1255
+DEFAULT_TIMEOUT = 10.0
+DEFAULT_RECONNECT_DELAY = 10.0
+DEFAULT_HEART_BEAT = 10.0
+DEFAULT_STEP = 5
+
+STATE_CONNECTED = "connected"
+STATE_DISCONNECTED = "disconnected"
+STATE_RECONNECTING = "reconnecting"
+
+DATA_NEW = "new"
+DATA_MAPPED_IDS = "mapped_ids"
+
+PLAY_STATE_PLAY = "play"
+PLAY_STATE_PAUSE = "pause"
+PLAY_STATE_STOP = "stop"
+VALID_PLAY_STATES = (PLAY_STATE_PLAY, PLAY_STATE_PAUSE, PLAY_STATE_STOP)
+
+REPEAT_ON_ALL = "on_all"
+REPEAT_ON_ONE = "on_one"
+REPEAT_OFF = "off"
+VALID_REPEAT_MODES = (REPEAT_ON_ALL, REPEAT_ON_ONE, REPEAT_OFF)
+
+# Music Source Types
+TYPE_MUSIC_SERVICE = "music_service"
+TYPE_STATION = "station"
+TYPE_SONG = "song"
+TYPE_HEOS_SERVICE = "heos_service"
+TYPE_PLAYLIST = "playlist"
+
+# Music Sources
+MUSIC_SOURCE_PANDORA = 1
+MUSIC_SOURCE_RHAPSODY = 2
+MUSIC_SOURCE_TUNEIN = 3
+MUSIC_SOURCE_SPOTIFY = 4
+MUSIC_SOURCE_DEEZER = 5
+MUSIC_SOURCE_NAPSTER = 6
+MUSIC_SOURCE_IHEARTRADIO = 7
+MUSIC_SOURCE_SIRIUSXM = 8
+MUSIC_SOURCE_SOUNDCLOUD = 9
+MUSIC_SOURCE_TIDAL = 10
+MUSIC_SOURCE_RDIO = 12
+MUSIC_SOURCE_AMAZON = 13
+MUSIC_SOURCE_MOODMIX = 15
+MUSIC_SOURCE_JUKE = 16
+MUSIC_SOURCE_QQMUSIC = 18
+MUSIC_SOURCE_LOCAL_MUSIC = 1024
+MUSIC_SOURCE_PLAYLISTS = 1025
+MUSIC_SOURCE_HISTORY = 1026
+MUSIC_SOURCE_AUX_INPUT = 1027
+MUSIC_SOURCE_FAVORITES = 1028
+
+# Supported controls
+CONTROL_PLAY = "play"
+CONTROL_PAUSE = "pause"
+CONTROL_STOP = "stop"
+CONTROL_PLAY_NEXT = "play_next"
+CONTROL_PLAY_PREVIOUS = "play_previous"
+
+CONTROLS_ALL = [
+    CONTROL_PLAY,
+    CONTROL_PAUSE,
+    CONTROL_STOP,
+    CONTROL_PLAY_NEXT,
+    CONTROL_PLAY_PREVIOUS,
+]
+CONTROLS_FORWARD_ONLY = [CONTROL_PLAY, CONTROL_PAUSE, CONTROL_STOP, CONTROL_PLAY_NEXT]
+CONTROL_PLAY_STOP = [CONTROL_PLAY, CONTROL_STOP]
+
+SOURCE_CONTROLS = {
+    MUSIC_SOURCE_PANDORA: {TYPE_STATION: CONTROLS_FORWARD_ONLY},
+    MUSIC_SOURCE_RHAPSODY: {
+        TYPE_SONG: CONTROLS_ALL,
+        TYPE_STATION: CONTROLS_FORWARD_ONLY,
+    },
+    MUSIC_SOURCE_TUNEIN: {TYPE_SONG: CONTROLS_ALL, TYPE_STATION: CONTROL_PLAY_STOP},
+    MUSIC_SOURCE_SPOTIFY: {
+        TYPE_SONG: CONTROLS_ALL,
+        TYPE_STATION: CONTROLS_FORWARD_ONLY,
+    },
+    MUSIC_SOURCE_DEEZER: {TYPE_SONG: CONTROLS_ALL, TYPE_STATION: CONTROLS_FORWARD_ONLY},
+    MUSIC_SOURCE_NAPSTER: {
+        TYPE_SONG: CONTROLS_ALL,
+        TYPE_STATION: CONTROLS_FORWARD_ONLY,
+    },
+    MUSIC_SOURCE_IHEARTRADIO: {
+        TYPE_SONG: CONTROLS_ALL,
+        TYPE_STATION: CONTROL_PLAY_STOP,
+    },
+    MUSIC_SOURCE_SIRIUSXM: {TYPE_STATION: CONTROL_PLAY_STOP},
+    MUSIC_SOURCE_SOUNDCLOUD: {TYPE_SONG: CONTROLS_ALL},
+    MUSIC_SOURCE_TIDAL: {TYPE_SONG: CONTROLS_ALL},
+    MUSIC_SOURCE_AMAZON: {TYPE_SONG: CONTROLS_ALL, TYPE_STATION: CONTROLS_ALL},
+    MUSIC_SOURCE_AUX_INPUT: {TYPE_STATION: CONTROL_PLAY_STOP},
+}
+
+
+# Inputs
+INPUT_AUX_IN_1 = "inputs/aux_in_1"
+INPUT_AUX_IN_2 = "inputs/aux_in_2"
+INPUT_AUX_IN_3 = "inputs/aux_in_3"
+INPUT_AUX_IN_4 = "inputs/aux_in_4"
+INPUT_AUX_IN_SINGLE = "inputs/aux_single"
+INPUT_AUX1 = "inputs/aux1"
+INPUT_AUX2 = "inputs/aux2"
+INPUT_AUX3 = "inputs/aux3"
+INPUT_AUX4 = "inputs/aux4"
+INPUT_AUX5 = "inputs/aux5"
+INPUT_AUX6 = "inputs/aux6"
+INPUT_AUX7 = "inputs/aux7"
+INPUT_LINE_IN_1 = "inputs/line_in_1"
+INPUT_LINE_IN_2 = "inputs/line_in_2"
+INPUT_LINE_IN_3 = "inputs/line_in_3"
+INPUT_LINE_IN_4 = "inputs/line_in_4"
+INPUT_COAX_IN_1 = "inputs/coax_in_1"
+INPUT_COAX_IN_2 = "inputs/coax_in_2"
+INPUT_OPTICAL_IN_1 = "inputs/optical_in_1"
+INPUT_OPTICAL_IN_2 = "inputs/optical_in_2"
+INPUT_HDMI_IN_1 = "inputs/hdmi_in_1"
+INPUT_HDMI_IN_2 = "inputs/hdmi_in_2"
+INPUT_HDMI_IN_3 = "inputs/hdmi_in_3"
+INPUT_HDMI_IN_4 = "inputs/hdmi_in_4"
+INPUT_HDMI_ARC_1 = "inputs/hdmi_arc_1"
+INPUT_CABLE_SAT = "inputs/cable_sat"
+INPUT_DVD = "inputs/dvd"
+INPUT_BLURAY = "inputs/bluray"
+INPUT_GAME = "inputs/game"
+INPUT_MEDIA_PLAYER = "inputs/mediaplayer"
+INPUT_CD = "inputs/cd"
+INPUT_TUNER = "inputs/tuner"
+INPUT_HD_RADIO = "inputs/hdradio"
+INPUT_TV_AUDIO = "inputs/tvaudio"
+INPUT_PHONO = "inputs/phono"
+INPUT_USB_AC = "inputs/usbdac"
+INPUT_ANALOG_IN_1 = "inputs/analog_in_1"
+INPUT_ANALOG_IN_2 = "inputs/analog_in_2"
+INPUT_RECORDER_IN_1 = "inputs/recorder_in_1"
+
+VALID_INPUTS = (
+    INPUT_AUX_IN_1,
+    INPUT_AUX_IN_2,
+    INPUT_AUX_IN_3,
+    INPUT_AUX_IN_4,
+    INPUT_AUX_IN_SINGLE,
+    INPUT_AUX1,
+    INPUT_AUX2,
+    INPUT_AUX3,
+    INPUT_AUX4,
+    INPUT_AUX5,
+    INPUT_AUX6,
+    INPUT_AUX7,
+    INPUT_LINE_IN_1,
+    INPUT_LINE_IN_2,
+    INPUT_LINE_IN_3,
+    INPUT_LINE_IN_4,
+    INPUT_COAX_IN_1,
+    INPUT_COAX_IN_2,
+    INPUT_OPTICAL_IN_1,
+    INPUT_OPTICAL_IN_2,
+    INPUT_HDMI_IN_1,
+    INPUT_HDMI_IN_2,
+    INPUT_HDMI_IN_3,
+    INPUT_HDMI_IN_4,
+    INPUT_HDMI_ARC_1,
+    INPUT_CABLE_SAT,
+    INPUT_DVD,
+    INPUT_BLURAY,
+    INPUT_GAME,
+    INPUT_MEDIA_PLAYER,
+    INPUT_CD,
+    INPUT_TUNER,
+    INPUT_HD_RADIO,
+    INPUT_TV_AUDIO,
+    INPUT_PHONO,
+    INPUT_USB_AC,
+    INPUT_ANALOG_IN_1,
+    INPUT_ANALOG_IN_2,
+    INPUT_RECORDER_IN_1,
+)
+
+# Add to Queue Options
+ADD_QUEUE_PLAY_NOW = 1
+ADD_QUEUE_PLAY_NEXT = 2
+ADD_QUEUE_ADD_TO_END = 3
+ADD_QUEUE_REPLACE_AND_PLAY = 4
+
+VALID_ADD_QUEUE_OPTIONS = (
+    ADD_QUEUE_PLAY_NOW,
+    ADD_QUEUE_PLAY_NEXT,
+    ADD_QUEUE_ADD_TO_END,
+    ADD_QUEUE_REPLACE_AND_PLAY,
+)
+
+# Signals
+SIGNAL_PLAYER_EVENT = "player_event"
+SIGNAL_GROUP_EVENT = "group_event"
+SIGNAL_CONTROLLER_EVENT = "controller_event"
+SIGNAL_HEOS_EVENT = "heos_event"
+EVENT_CONNECTED = "connected"
+EVENT_DISCONNECTED = "disconnected"
+
+BASE_URI = "heos://"
+
+# Browse commands
+COMMAND_BROWSE_GET_SOURCES = "browse/get_music_sources"
+COMMAND_BROWSE_BROWSE = "browse/browse"
+COMMAND_BROWSE_PLAY_INPUT = "browse/play_input"
+COMMAND_BROWSE_PLAY_PRESET = "browse/play_preset"
+COMMAND_BROWSE_PLAY_STREAM = "browse/play_stream"
+COMMAND_BROWSE_ADD_TO_QUEUE = "browse/add_to_queue"
+
+# Player commands
+COMMAND_GET_PLAYERS = "player/get_players"
+COMMAND_GET_PLAY_STATE = "player/get_play_state"
+COMMAND_SET_PLAY_STATE = "player/set_play_state"
+COMMAND_GET_NOW_PLAYING_MEDIA = "player/get_now_playing_media"
+COMMAND_GET_VOLUME = "player/get_volume"
+COMMAND_SET_VOLUME = "player/set_volume"
+COMMAND_GET_MUTE = "player/get_mute"
+COMMAND_SET_MUTE = "player/set_mute"
+COMMAND_VOLUME_UP = "player/volume_up"
+COMMAND_VOLUME_DOWN = "player/volume_down"
+COMMAND_TOGGLE_MUTE = "player/toggle_mute"
+COMMAND_GET_PLAY_MODE = "player/get_play_mode"
+COMMAND_SET_PLAY_MODE = "player/set_play_mode"
+COMMAND_CLEAR_QUEUE = "player/clear_queue"
+COMMAND_PLAY_NEXT = "player/play_next"
+COMMAND_PLAY_PREVIOUS = "player/play_previous"
+COMMAND_PLAY_QUICK_SELECT = "player/play_quickselect"
+COMMAND_SET_QUICK_SELECT = "player/set_quickselect"
+COMMAND_GET_QUICK_SELECTS = "player/get_quickselects"
+
+# Group commands
+COMMAND_GET_GROUPS = "group/get_groups"
+COMMAND_SET_GROUP = "group/set_group"
+COMMAND_GET_GROUP_VOLUME = "group/get_volume"
+COMMAND_SET_GROUP_VOLUME = "group/set_volume"
+COMMAND_GET_GROUP_MUTE = "group/get_mute"
+COMMAND_SET_GROUP_MUTE = "group/set_mute"
+COMMAND_GROUP_TOGGLE_MUTE = "group/toggle_mute"
+COMMAND_GROUP_VOLUME_UP = "group/volume_up"
+COMMAND_GROUP_VOLUME_DOWN = "group/volume_down"
+
+# System commands
+COMMAND_REGISTER_FOR_CHANGE_EVENTS = "system/register_for_change_events"
+COMMAND_HEART_BEAT = "system/heart_beat"
+COMMAND_ACCOUNT_CHECK = "system/check_account"
+COMMAND_SIGN_IN = "system/sign_in"
+COMMAND_SIGN_OUT = "system/sign_out"
+
+# Events
+EVENT_PLAYER_STATE_CHANGED = "event/player_state_changed"
+EVENT_PLAYER_NOW_PLAYING_CHANGED = "event/player_now_playing_changed"
+EVENT_PLAYER_NOW_PLAYING_PROGRESS = "event/player_now_playing_progress"
+EVENT_PLAYER_VOLUME_CHANGED = "event/player_volume_changed"
+EVENT_PLAYER_PLAYBACK_ERROR = "event/player_playback_error"
+EVENT_PLAYER_QUEUE_CHANGED = "event/player_queue_changed"
+EVENT_GROUP_VOLUME_CHANGED = "event/group_volume_changed"
+EVENT_REPEAT_MODE_CHANGED = "event/repeat_mode_changed"
+EVENT_SHUFFLE_MODE_CHANGED = "event/shuffle_mode_changed"
+EVENT_SOURCES_CHANGED = "event/sources_changed"
+EVENT_PLAYERS_CHANGED = "event/players_changed"
+EVENT_GROUPS_CHANGED = "event/groups_changed"
+EVENT_USER_CHANGED = "event/user_changed"
+
+PLAYER_EVENTS = (
+    EVENT_PLAYER_STATE_CHANGED,
+    EVENT_PLAYER_NOW_PLAYING_CHANGED,
+    EVENT_PLAYER_NOW_PLAYING_PROGRESS,
+    EVENT_PLAYER_VOLUME_CHANGED,
+    EVENT_REPEAT_MODE_CHANGED,
+    EVENT_SHUFFLE_MODE_CHANGED,
+    EVENT_PLAYER_PLAYBACK_ERROR,
+    EVENT_PLAYER_QUEUE_CHANGED,
+)
+
+GROUP_EVENTS = EVENT_GROUP_VOLUME_CHANGED
+
+HEOS_EVENTS = (
+    EVENT_SOURCES_CHANGED,
+    EVENT_PLAYERS_CHANGED,
+    EVENT_GROUPS_CHANGED,
+    EVENT_USER_CHANGED,
+)
