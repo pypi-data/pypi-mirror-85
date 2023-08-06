81146685eeef2e30157a7563ca3cd70d752164de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:09 +0000

    Add package flake8-todos/0.1.5/flake8-todos-0.1.5.tar.gz

---
+++ packages/flake8-todos/flake8-todos-0.1.5.tar.gz/flake8-todos-0.1.5/flake8_todos/__init__.py
@@ -0,0 +1,18 @@
+
+# project
+from ._checker import Checker
+from ._error import Error
+from ._rules import register_rule, rules
+from ._token import Token
+
+
+__version__ = '0.1.5'
+
+# keep sorted
+__all__ = [
+    'Checker',
+    'Error',
+    'register_rule',
+    'rules',
+    'Token',
+]
