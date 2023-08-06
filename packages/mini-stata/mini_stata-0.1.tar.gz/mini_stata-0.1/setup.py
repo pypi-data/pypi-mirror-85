78c8ae704ed9c7e26efb3b8563e14c61da5c0393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:23 +0000

    Add package mini-stata/0.1/mini_stata-0.1.tar.gz

---
+++ packages/mini-stata/mini_stata-0.1.tar.gz/mini_stata-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mini_stata',
+      version='0.1',
+      description='The mini version of stata',
+      url='https://github.com/quyennguyen2201',
+      author='Quyen Nguyen',
+      author_email='quyen.nguyen@postgrad.otago.ac.nz',
+      license='MIT',
+      packages=['mini_stata'],
+      zip_safe=False)
