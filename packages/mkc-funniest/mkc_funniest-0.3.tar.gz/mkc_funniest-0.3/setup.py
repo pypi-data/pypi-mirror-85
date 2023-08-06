5a834ea052d4fee03a3781b458c8997f3f28d50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:25 +0000

    Add package mkc-funniest/0.3/mkc_funniest-0.3.tar.gz

---
+++ packages/mkc-funniest/mkc_funniest-0.3.tar.gz/mkc_funniest-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mkc_funniest',
+      version='0.3',
+      description='The funniest joke in the world',
+      long_description='The funniest joke in the world',
+      long_description_content_type="text/markdown",
+      url='http://github.com/storborg/funniest',
+      author='Michael Chen',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
