549682e9a027f6f185ca54888bcbe6f99cef1aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:08 +0000

    Add package nehushtan/0.1.3/nehushtan-0.1.3.tar.gz

---
+++ packages/nehushtan/nehushtan-0.1.3.tar.gz/nehushtan-0.1.3/README.md
@@ -0,0 +1,8 @@
+# Nehushtan
+
+A toolkit for projects in Python
+
+# History in Brief
+
+* 0.1.2: First version on PYPI with MySQL Toolkit.
+* 0.1.3: Add CLI Helper, Logger, SMTP.
---
+++ packages/nehushtan/nehushtan-0.1.3.tar.gz/nehushtan-0.1.3/nehushtan/mysql/MySQLTableExistence.py
@@ -0,0 +1,32 @@
+#  Copyright (c) 2020. Sinri Edogawa
+
+from abc import abstractmethod, ABC
+
+from nehushtan.mysql.MySQLKit import MySQLKit
+
+
+class MySQLTableExistence(ABC):
+    _mysql_kit: MySQLKit or None
+
+    def __init__(self):
+        pass
+
+    def get_mysql_kit(self) -> MySQLKit:
+        if self._mysql_kit is None:
+            raise Exception("MySQLKit is not available for querying...")
+        return self._mysql_kit
+
+    @abstractmethod
+    def mapping_table_name(self) -> str:
+        pass
+
+    # noinspection PyMethodMayBeStatic
+    def mapping_schema_name(self) -> str:
+        return ''
+
+    def get_table_expression(self) -> str:
+        e = ''
+        if self.mapping_schema_name() != '':
+            e += f'`{self.mapping_schema_name()}`.'
+        e += self.mapping_table_name()
+        return e
