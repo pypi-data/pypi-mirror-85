a3551037ef495b570f3c0fa34040c76bd0e8200e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:22 +0000

    Add package tnorm/0.1.20/tnorm-0.1.20-py2.py3-none-any.whl

---
+++ packages/tnorm/tnorm-0.1.20-py2.py3-none-any.whl/tnorm/__init__.py
@@ -0,0 +1,13 @@
+
+from tnorm.version import version, __version__
+
+from tnorm.TN_wrapper import TN_wrapper as load
+
+import tnorm.kernel
+import tnorm.extras
+import tnorm.utilities
+from tnorm.test import test
+#import tnorm.GUI
+
+
+#IN_SAGE=False
