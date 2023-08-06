7016b0cdc5b100fa63fbdce168e30f865def5945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:06 +0000

    Add package TOPSIS-PARNEET-101803075/1.1.0/TOPSIS_PARNEET_101803075-1.1.0.tar.gz

---
+++ packages/TOPSIS-PARNEET-101803075/TOPSIS_PARNEET_101803075-1.1.0.tar.gz/TOPSIS_PARNEET_101803075-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="TOPSIS_PARNEET_101803075",
+    version="1.1.0",
+    description="A Python package for topsis analysis.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Parneet Kaur",
+    author_email="parneetkaurr106@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TOPSIS_PARNEET_101803075"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "TOPSIS_PARNEET_101803075=TOPSIS_PARNEET_101803075.topsis:main",
+        ]
+    },
+)
