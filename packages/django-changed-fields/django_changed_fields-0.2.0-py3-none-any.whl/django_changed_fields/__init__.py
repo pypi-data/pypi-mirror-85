8a4005df8cb433f56da704f6f459cfb9d49c3aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:12 +0000

    Add package django-changed-fields/0.2.0/django_changed_fields-0.2.0-py3-none-any.whl

---
+++ packages/django-changed-fields/django_changed_fields-0.2.0-py3-none-any.whl/django_changed_fields/__init__.py
@@ -0,0 +1,28 @@
+"""
+Mixin to show changed fields per DRF resource.
+"""
+from django.forms import model_to_dict
+
+
+class ChangedFieldsMixin(object):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__last_queryset = None
+
+    def last_queryset(self):
+        if not self.__last_queryset:
+            self.__last_queryset = type(self).objects.get(pk=self.pk) if self.pk else None
+        return self.__last_queryset
+
+    def change_fields(self, *args, **kwargs):
+        new = set(model_to_dict(self, *args, **kwargs).items())
+        if not self.pk:
+            return list(dict(new).keys())
+
+        old = set(model_to_dict(self.last_queryset(), *args, **kwargs).items())
+        return list(dict(new - old).keys())
+
+    def save(self, *args, **kwargs):
+        self.last_queryset()
+        super().save(*args, **kwargs)
