1fec1cb2318ef35ea28968ae97f3e748dfc06eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:39 +0000

    Add package PyGithubFork/0.0.1/PyGithubFork-0.0.1.tar.gz

---
+++ packages/PyGithubFork/PyGithubFork-0.0.1.tar.gz/PyGithubFork-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyGithubFork',
+    version='0.0.1',
+    description='Code wraps PyGithub to simplify workflows dealing with forks',
+    author='hassenius',
+    url='https://github.com/multicloud-ops/PyGithubFork',
+    packages=['githubfork']
+    # install_requires=[
+    #     'pyyaml',
+    #     'pygithub'
+    # ]
+)
