56493b9c393ec7596ffc81f037febc8c44e0c12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:34 +0000

    Add package WarThunder/2.2.0/WarThunder-2.2.0.tar.gz

---
+++ packages/WarThunder/WarThunder-2.2.0.tar.gz/WarThunder-2.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name             = 'WarThunder',
+    packages         = ['WarThunder'],
+    version          = '2.2.0',
+    description      = 'Python package used to access air vehicle telemetry while in War Thunder air battles',
+    author           = 'Power_Broker',
+    author_email     = 'gitstuff2@gmail.com',
+    url              = 'https://github.com/PowerBroker2/WarThunder',
+    download_url     = 'https://github.com/PowerBroker2/WarThunder/archive/2.2.0.tar.gz',
+    keywords         = ['War Thunder'],
+    classifiers      = [],
+    install_requires = ['imagehash', 'requests', 'Pillow', 'WarThunder']
+)
