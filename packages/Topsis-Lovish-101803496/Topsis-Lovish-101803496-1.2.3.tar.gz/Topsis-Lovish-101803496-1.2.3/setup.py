fd9184a54df5d2999e7e47552ab2e16861dd1cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package Topsis-Lovish-101803496/1.2.3/Topsis-Lovish-101803496-1.2.3.tar.gz

---
+++ packages/Topsis-Lovish-101803496/Topsis-Lovish-101803496-1.2.3.tar.gz/Topsis-Lovish-101803496-1.2.3/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-Lovish-101803496",
+    version="1.2.3",
+    description="Implements Topsis",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Lovish Gupta",
+    author_email="lovish.gupta.121@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=['pandas'],
+)
