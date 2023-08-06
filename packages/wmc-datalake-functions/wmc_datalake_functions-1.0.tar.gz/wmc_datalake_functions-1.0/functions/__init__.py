e1f037044587d3f9e5e6c5c4a4a90c60d81eca3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:57 +0000

    Add package wmc-datalake-functions/1.0/wmc_datalake_functions-1.0.tar.gz

---
+++ packages/wmc-datalake-functions/wmc_datalake_functions-1.0.tar.gz/wmc_datalake_functions-1.0/functions/__init__.py
@@ -0,0 +1,10 @@
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python setup.py sdist
+#  python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python -m twine upload -u DanielBR08 -p DWhouse130_ --repository-url https://upload.pypi.org/legacy/ dist/*
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
