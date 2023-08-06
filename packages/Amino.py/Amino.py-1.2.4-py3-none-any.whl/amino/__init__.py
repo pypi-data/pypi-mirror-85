e6e0d5d7a1ff48c5e8a3a7584a0e6bc6f655558e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:46 +0000

    Add package Amino.py/1.2.4/Amino.py-1.2.4-py3-none-any.whl

---
+++ packages/Amino.py/Amino.py-1.2.4-py3-none-any.whl/amino/__init__.py
@@ -0,0 +1,16 @@
+__title__ = 'Amino.py'
+__author__ = 'Slimakoi'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-2020 Slimakoi'
+__version__ = '1.2.4'
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .lib.util import device, exceptions, headers, helpers, objects
+from .socket import Callbacks, SocketHandler
+from requests import get
+from ujson import loads
+
+if __version__ != loads(get("https://pypi.python.org/pypi/Amino.py/json").text)["info"]["version"]:
+    print(exceptions.LibraryUpdateAvailable)
