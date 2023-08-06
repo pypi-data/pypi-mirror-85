2b5d3f17db59290bed3c238f3926289184ca4bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package jhstest/0.1.0/jhstest-0.1.0-py3-none-any.whl

---
+++ packages/jhstest/jhstest-0.1.0-py3-none-any.whl/jhstest/__init__.py
@@ -0,0 +1,18 @@
+"""
+"""
+__docformat__ = "restructuredtext"
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("wordcloud", "matplotlib")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f"{dependency}: {e}")
+
+from .jhstest import (
+    get_cloud
+)
+
+#__all__ = ['get_cloud']
