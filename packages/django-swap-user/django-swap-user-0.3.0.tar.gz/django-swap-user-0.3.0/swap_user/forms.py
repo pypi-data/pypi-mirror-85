ddadadad296d7857d15b89cf552ea6c44603275e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:37 +0000

    Add package django-swap-user/0.3.0/django-swap-user-0.3.0.tar.gz

---
+++ packages/django-swap-user/django-swap-user-0.3.0.tar.gz/django-swap-user-0.3.0/swap_user/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+
+
+class BaseUserForm(forms.ModelForm):
+    password_1 = forms.CharField(
+        label="Enter a new password", widget=forms.PasswordInput, required=False,
+    )
+    password_2 = forms.CharField(
+        label="Repeat a new password", widget=forms.PasswordInput, required=False,
+    )
+
+    class Meta:
+        exclude = ["password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password_1 = cleaned_data["password_1"]
+        password_2 = cleaned_data["password_2"]
+
+        password_list = [password_1, password_2]
+        not_empty_password_list = [item for item in password_list if item]
+
+        # if one of passwords not filled - we are raising exception
+        if len(not_empty_password_list) == 1:
+            raise forms.ValidationError(
+                message="Provide both of passwords", code="provide_both_passwords",
+            )
+
+        # if passwords doesn't match - we are raising exception
+        if password_1 != password_2:
+            raise forms.ValidationError(
+                message="Passwords should be same", code="password_should_be_same",
+            )
+
+        return cleaned_data
+
+    def save(self, commit=False):
+        instance = super().save(commit)
+        password_1 = self.cleaned_data["password_1"]
+
+        if password_1:
+            instance.set_password(password_1)
+
+        instance.save()
+
+        return instance
