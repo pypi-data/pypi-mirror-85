d42162f9fc04c7bedd88f2eb9d9be6493ac090e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:08 +0000

    Add package sinto/0.7.2.2/sinto-0.7.2.2-py3-none-any.whl

---
+++ packages/sinto/sinto-0.7.2.2-py3-none-any.whl/sinto/addtags.py
@@ -0,0 +1,101 @@
+import pysam
+import gzip
+from multiprocessing import Pool
+import functools
+import random
+import string
+import os
+from sinto import utils
+from subprocess import call
+
+
+def _add_read_tags(intervals, bam, sam, output, cb, trim_suffix, mode):
+    inputBam = pysam.AlignmentFile(bam, "rb")
+    ident = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(6)
+    )
+    if sam:
+        outputBam = pysam.AlignmentFile(output + ident, "w", template=inputBam)
+    else:
+        outputBam = pysam.AlignmentFile(output + ident, "wb", template=inputBam)
+    for i in intervals:
+        for r in inputBam.fetch(i[0], i[1], i[2]):
+            if mode == "tag":
+                cell_barcode, _ = utils.scan_tags(r.tags)
+            elif mode == "readname":
+                cell_barcode = r.qname.split(":")[0]
+            else:
+                raise Exception("Unknown mode. Use either tag or readname")
+            if cell_barcode is not None:
+                if trim_suffix:
+                    cell_barcode = cell_barcode[:-2]
+                if cell_barcode in cb.keys():
+                    r.tags += cb[cell_barcode]
+            outputBam.write(r)
+    outputBam.close()
+    inputBam.close()
+    return output + ident
+
+
+def addtags(bam, tagfile, output, sam=False, trim_suffix=True, mode="tag", nproc=1):
+    """Add tags to reads from individual cells
+
+    Copies BAM entries to a new file, adding a read tag to cells matching an input table
+
+    Parameters
+    ----------
+    bam : str
+        Path to BAM file.
+    tagfile : str
+        Tab-delimited file containing cell barcode, read tag to be added, tag information
+    output : str
+        Name for output BAM file.
+    sam : bool, optional
+        Output SAM format. Default is BAM format.
+    trim_suffix: bool, optional
+        Remove trailing 2 characters from cell barcode in bam file (sometimes needed to match 10x barcodes).
+    nproc : int, optional
+        Number of processors to use. Default is 1.
+    mode : str
+        Either tag (default) or readname. Some BAM file store the cell barcode in the readname rather than under
+        a read tag.
+
+    Raises
+    ------
+    Exception
+        If samtools merge of temporary BAM files fails
+    """
+    nproc = int(nproc)
+    tags = utils.read_cell_barcode_tag_file(tagfile)
+    inputBam = pysam.AlignmentFile(bam, "rb")
+    intervals = utils.chunk_bam(inputBam, nproc)
+    # write sam header
+    headerfile = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    tmpmerge = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    header = pysam.AlignmentFile(headerfile, "w", header = inputBam.header)
+    header.close()
+    inputBam.close()
+    p = Pool(nproc)
+    tempfiles = p.map_async(
+        functools.partial(
+            _add_read_tags,
+            bam=bam,
+            sam=sam,
+            output=output,
+            cb=tags,
+            trim_suffix=trim_suffix,
+            mode=mode,
+        ),
+        intervals.values(),
+    ).get(9999999)
+    mergestring = (
+        "samtools merge -@ " + str(nproc) + " " + tmpmerge + " " + " ".join(tempfiles) + ";" +
+        "samtools reheader -P " + headerfile + " " + tmpmerge + " > " + output
+    )
+    call(mergestring, shell=True)
+    if os.path.exists(output):
+        [os.remove(i) for i in tempfiles]
+        os.remove(headerfile)
+        os.remove(tmpmerge)
+    else:
+        raise Exception("samtools merge failed, temp files not deleted")
---
+++ packages/sinto/sinto-0.7.2.2-py3-none-any.whl/sinto/filterbarcodes.py
@@ -0,0 +1,120 @@
+import pysam
+from multiprocessing import Pool
+import functools
+import random
+import string
+from sinto import utils
+from subprocess import call
+import re
+import os
+from itertools import chain
+
+
+def _iterate_reads(intervals, bam, cb, classes, trim_suffix, cellbarcode, readname_barcode):
+    inputBam = pysam.AlignmentFile(bam, "rb")
+    ident = "".join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(6)
+    )
+    filelist = [x + "_" + ident for x in classes]
+    bamlist = [pysam.AlignmentFile(x, "wb", template=inputBam) for x in filelist]
+    for i in intervals:
+        for r in inputBam.fetch(i[0], i[1], i[2]):
+            if readname_barcode is not None:
+                re_match = readname_barcode.match(r.qname)
+                cell_barcode = re_match.group()
+            else:
+                cell_barcode, _ = utils.scan_tags(r.tags, cb=cellbarcode)
+            if cell_barcode is not None:
+                if trim_suffix:
+                    cell_barcode = cell_barcode[:-2]
+                if cell_barcode in cb.keys():
+                    cell_classes = cb[cell_barcode]
+                    for i in cell_classes:
+                        fileindex = filelist.index(i + "_" + ident)
+                        bamlist[fileindex].write(r)
+    for i in bamlist:
+        i.close()
+    inputBam.close()
+    return ident
+
+
+def mergeAll(idents, classes, nproc,  header, remove=True):
+    """Merge all temp files for each class
+    If remove is True, remove temp files after
+    successful merging"""
+    for i in classes:
+        allfiles = [i + "_" + x for x in idents]
+        output = i + '.bam'
+        tmpmerge = i + ".tmp"
+        mergestring = (
+            "samtools merge -@ " + str(nproc) + " " + tmpmerge + " " + " ".join(allfiles) + ";" +
+            "samtools reheader -P " + header + " " + tmpmerge + " > " + output
+        )
+        call(mergestring, shell=True)
+        if os.path.exists(output):
+            [os.remove(i) for i in allfiles]
+            os.remove(tmpmerge)
+        else:
+            raise Exception("samtools merge failed, temp files not deleted")
+
+
+def filterbarcodes(
+    cells, bam, readname_barcode, cellbarcode, sam=False, trim_suffix=True, nproc=1
+):
+    """Filter reads based on input list of cell barcodes
+
+    Copy BAM entries matching a list of cell barcodes to a new BAM file.
+    Output BAM files will be named according to the group name in the 
+    file provided.
+
+    Parameters
+    ----------
+    cells : str
+        Path to file containing cell barcodes and the group associated with each barcode.
+        File can be gzip compressed. A separate BAM file will be created for each 
+        group of cells.
+    bam : str
+        Path to BAM file.
+    trim_suffix: bool, optional
+        Remove trailing 2 characters from cell barcode in bam file (sometimes needed to match 10x barcodes).
+    nproc : int, optional
+        Number of processors to use. Default is 1.
+    cellbarcode : str
+       Tag used for cell barcode. Default is CB (used by cellranger)
+    readname_barcode : regex
+        A regular expression for matching cell barcode in read name. If None (default),
+        use the read tags.
+
+    Raises
+    ------
+    Exception
+        If samtools merge of temporary BAM files fails
+    """
+    nproc = int(nproc)
+    cb = utils.read_cell_barcode_file(cells)
+    unique_classes = list(set(chain.from_iterable(cb.values())))
+    inputBam = pysam.AlignmentFile(bam, "rb")
+    intervals = utils.chunk_bam(inputBam, nproc)
+    # write sam header
+    ident = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    headerfile = "./" + ident + ".sam"
+    header = pysam.AlignmentFile(headerfile, "w", header = inputBam.header)
+    header.close()
+    inputBam.close()
+    if readname_barcode is not None:
+        readname_barcode = re.compile(readname_barcode)
+    p = Pool(nproc)
+    idents = p.map_async(
+        functools.partial(
+            _iterate_reads,
+            bam=bam,
+            cb=cb,
+            classes=unique_classes,
+            trim_suffix=trim_suffix,
+            cellbarcode=cellbarcode,
+            readname_barcode=readname_barcode
+        ),
+        intervals.values(),
+    ).get(9999999)
+    mergeAll(idents=idents, classes=unique_classes, nproc=nproc, header = headerfile, remove=True)
+    os.remove(headerfile)
