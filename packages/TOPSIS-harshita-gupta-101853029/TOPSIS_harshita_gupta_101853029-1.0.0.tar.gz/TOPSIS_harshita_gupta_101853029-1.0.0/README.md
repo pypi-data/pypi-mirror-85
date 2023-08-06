047ad6252ca98f8fabe58cb3b3d471906c1fa9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:17 +0000

    Add package TOPSIS-harshita-gupta-101853029/1.0.0/TOPSIS_harshita_gupta_101853029-1.0.0.tar.gz

---
+++ packages/TOPSIS-harshita-gupta-101853029/TOPSIS_harshita_gupta_101853029-1.0.0.tar.gz/TOPSIS_harshita_gupta_101853029-1.0.0/README.md
@@ -0,0 +1,3 @@
+
+ ## TOPSIS_harshita_gupta_101853029
+ This is the starter text for a python package.
---
+++ packages/TOPSIS-harshita-gupta-101853029/TOPSIS_harshita_gupta_101853029-1.0.0.tar.gz/TOPSIS_harshita_gupta_101853029-1.0.0/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TOPSIS_harshita_gupta_101853029",
+    version="1.0.0",
+    author="",
+    author_email="",
+    description="This is a test package for educational purposes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+) 
