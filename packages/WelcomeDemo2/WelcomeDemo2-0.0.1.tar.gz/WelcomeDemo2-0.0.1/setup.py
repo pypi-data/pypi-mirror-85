86c31b65c7ba2987085d995adee18e6c8e50fa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:00 +0000

    Add package WelcomeDemo2/0.0.1/WelcomeDemo2-0.0.1.tar.gz

---
+++ packages/WelcomeDemo2/WelcomeDemo2-0.0.1.tar.gz/WelcomeDemo2-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='WelcomeDemo2',
+    author="Dalitso Banda",
+    version='0.0.1',
+    description='Write/Read files',
+    py_modules=['Welcome'],
+    package_dir={'':'C:\\Users\\tenth\\cookiecutter2\\WelcomeDemo\\welcome'},
+)
