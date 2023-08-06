087615a996d7896092eb028dfb6a3eed585a1496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package pony-sayz/0.1.4/pony_sayz-0.1.4-py2.py3-none-any.whl

---
+++ packages/pony-sayz/pony_sayz-0.1.4-py2.py3-none-any.whl/pony_sayz/cli.py
@@ -0,0 +1,49 @@
+"""Console script for pony_sayz."""
+import argparse
+import sys
+import os
+
+def main():
+    """Console script for pony_sayz."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    #print("Arguments: " + str(args._))
+    #print("Replace this message by putting your code into "
+    #      "pony_sayz.cli.main")
+    msg = '(empty)'
+
+    if (len(args._) > 0):
+        msg = args._[0]
+    
+    nr_traces = len(msg) + 4
+    traces = '-' * nr_traces
+
+    line_nr = 1
+
+    exe_path = sys.executable
+    dir1 = os.path.dirname(exe_path) 
+    filepath = dir1+'/data/rainbow.pony'
+
+    with open(filepath, encoding='utf-8') as fp:
+        lines = fp.readlines()
+        for line in lines:
+            if line_nr > 21:
+                if ('balloon10' in line):
+                    print(traces)
+                    print(line.replace('balloon10',msg).replace('$','').replace('\n',''))
+                    print(traces)
+                else:
+                    print(line.replace('\n','').replace('$','')) 
+            line_nr += 1
+
+    return 0
+
+
+def load_file():
+    print('Load pony data file here.')
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
