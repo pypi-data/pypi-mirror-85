0eab0081ef65a6c73a85dfda411b87dc02f44ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:25 +0000

    Add package globals/0.0.43-25/globals-0.0.43-25.tar.gz

---
+++ packages/globals/globals-0.0.43-25.tar.gz/globals-0.0.43-25/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+import os
+
+version = '0.0.43-25'
+name = 'globals'
+url = f'https://github.com/SamuelJansen/{name}/'
+
+OS_SEPARATOR = os.path.sep
+
+setup(
+    name = name,
+    packages = [
+        name,
+        f'{name}{OS_SEPARATOR}api',
+        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src',
+        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service'
+    ],
+    version = version,
+    license = 'MIT',
+    description = 'import package handler',
+    author = 'Samuel Jansen',
+    author_email = 'samuel.jansenn@gmail.com',
+    url = url,
+    download_url = f'{url}archive/v{version}.tar.gz',
+    keywords = ['global', 'python global package', 'python package manager', 'global package manager'],
+    install_requires = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
