80381197bfc9a7e5612c1eaab34de0d574b54712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:53 +0000

    Add package visual-regression-tracker/4.5.0/visual_regression_tracker-4.5.0.tar.gz

---
+++ packages/visual-regression-tracker/visual_regression_tracker-4.5.0.tar.gz/visual_regression_tracker-4.5.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='visual_regression_tracker',
+    version='4.5.0',
+    description='Open source, self hosted solution for visual testing '
+                'and managing results of visual testing.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='',
+    license='APACHE',
+    url='https://github.com/Visual-Regression-Tracker/'
+        'Visual-Regression-Tracker',
+    packages=['visual_regression_tracker'],
+    python_requires='>=3.6',
+    install_requires=['requests', 'dacite', 'dataclasses;python_version<"3.7"'],
+)
