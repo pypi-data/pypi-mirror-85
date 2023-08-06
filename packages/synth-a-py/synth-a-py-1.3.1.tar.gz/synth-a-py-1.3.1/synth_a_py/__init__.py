26cf5276d4cb30b512457384118cec2da29a297b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:55 +0000

    Add package synth-a-py/1.3.1/synth-a-py-1.3.1.tar.gz

---
+++ packages/synth-a-py/synth-a-py-1.3.1.tar.gz/synth-a-py-1.3.1/synth_a_py/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "1.3.1"
+
+from .base import Dir, File, Project
+from .file import EmptyFile, SimpleFile
+from .license import License
+from .toml import TomlFile
+
+__all__ = [
+    "Dir",
+    "EmptyFile",
+    "File",
+    "License",
+    "Project",
+    "SimpleFile",
+    "TomlFile",
+]
