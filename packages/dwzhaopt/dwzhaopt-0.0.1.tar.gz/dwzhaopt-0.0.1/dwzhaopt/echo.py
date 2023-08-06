94793a5c03d804806f757c1061d4da3d8b3dc127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:32 +0000

    Add package dwzhaopt/0.0.1/dwzhaopt-0.0.1.tar.gz

---
+++ packages/dwzhaopt/dwzhaopt-0.0.1.tar.gz/dwzhaopt-0.0.1/dwzhaopt/echo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Usage: ./build.py group/project
+
+import sys
+
+
+def echo():
+    if len(sys.argv) < 2:
+        print 'Usage: {} msg'.format(__file__)
+        sys.exit(1)
+    msg = sys.argv[1]
+    print msg
---
+++ packages/dwzhaopt/dwzhaopt-0.0.1.tar.gz/dwzhaopt-0.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+def read(f_name):
+    return open(os.path.join(os.path.dirname(__file__), f_name)).read()
+
+
+setup(
+    name="dwzhaopt",
+    version="0.0.1",
+    author="dwzhao",
+    author_email="flybx@foxmail.com",
+    description="An demonstration of how to create, test, document and publish to pypi.org.",
+    license='MIT License',
+    keywords="pip test document tutorial",
+    url="https://github.com/betterzdw/dwzhaopt",
+    packages=find_packages(),
+    long_description=read('README.md'),
+
+    entry_points={
+        'console_scripts': [
+            'dwzhaopt = dwzhaopt.echo:echo',
+        ],
+    },
+)
