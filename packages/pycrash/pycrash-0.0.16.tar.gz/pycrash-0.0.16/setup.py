3da166b052ec9f899dc944626bb45766d786121f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package pycrash/0.0.16/pycrash-0.0.16.tar.gz

---
+++ packages/pycrash/pycrash-0.0.16.tar.gz/pycrash-0.0.16/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycrash",
+    version="0.0.16",
+    author="Joe Cormier",
+    author_email="joemcormier@outlook.com",
+    description="software tool for simulating vehicle motion and impacts based on \
+    fundamental physics and accident reconstruction techniques",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joe-cormier/pycrash",
+    packages=setuptools.find_packages(exclude=("__pycache__", )),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
