25a44064ecd25d7edffef95512c85f48c39a516c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package easykubeflow/0.0.10/easykubeflow-0.0.10.tar.gz

---
+++ packages/easykubeflow/easykubeflow-0.0.10.tar.gz/easykubeflow-0.0.10/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easykubeflow",
+    version="0.0.10",
+    author="Le Tuan Vu",
+    author_email="ltnv24@gmail.com",
+    description="High level of kubeflow SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['easykubeflow'],
+    install_requires=['kfp'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
