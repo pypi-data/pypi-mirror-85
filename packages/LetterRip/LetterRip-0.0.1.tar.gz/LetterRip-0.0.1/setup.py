2c62793973a2ce5ed463ded7a1f5fdde30e31d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package LetterRip/0.0.1/LetterRip-0.0.1.tar.gz

---
+++ packages/LetterRip/LetterRip-0.0.1.tar.gz/LetterRip-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='LetterRip',
+    version='0.0.1',
+    description='Let Numpy Decide!',
+    py_modules=["LetterRip"],
+    package_dir={'': 'src'},
+)
