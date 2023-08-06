23ec57bf4f521122ff7b26abb08cd5fb85086372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:18 +0000

    Add package TOPSIS-HIMANSHU-101983063/0.2/TOPSIS-HIMANSHU-101983063-0.2.tar.gz

---
+++ packages/TOPSIS-HIMANSHU-101983063/TOPSIS-HIMANSHU-101983063-0.2.tar.gz/TOPSIS-HIMANSHU-101983063-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name="TOPSIS-HIMANSHU-101983063",
+    version="0.2",
+    description="This is code to find topsis score.",
+    long_description="A python package to find topsis score."
+                     "\nTOPSIS-HIMANSHU-101983063 can be run as in command prompt as:"
+                     "\npython TOPSIS.py InputDataFile Weights Impacts ResultFileName",
+    long_description_content_type='text/markdown',
+    author="HIMANSHU - 101983063",
+    packages=['TOPSIS-HIMANSHU-101983063'],
+    install_requires=[])
