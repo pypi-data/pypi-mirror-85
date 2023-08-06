a041173633f58985327267beabf29b65083260f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:00 +0000

    Add package vtt-to-srt3/0.1.8.5/vtt_to_srt3-0.1.8.5.tar.gz

---
+++ packages/vtt-to-srt3/vtt_to_srt3-0.1.8.5.tar.gz/vtt_to_srt3-0.1.8.5/README.md
@@ -0,0 +1,37 @@
+# vtt_to_srt.py
+
+Convert vtt files to srt subtitle format
+
+## Note
+For Python 3.7 [you can get version for Python 2.7 here](https://github.com/jansenicus/vtt-to-srt.py)
+
+## Installation
+```shell
+pip install vtt_to_srt3
+```
+
+## Usage from terminal
+
+```shell
+python -m vtt_to_srt pathname [-r]
+
+pathname - a file or directory with files to be converted 
+
+-r       - walk path recursively                          
+```
+
+## Usage as a lib
+
+Convert vtt file
+```shell
+from vtt_to_srt import vtt_to_srt
+path = '/path/to/file.vtt'
+vtt_to_srt(path)
+```		
+		
+Recursively convert all vtt files in directory
+```shell
+from vtt_to_srt import vtts_to_srt
+path = '/path/to/directory'
+vtts_to_srt(path, rec = True)
+```
---
+++ packages/vtt-to-srt3/vtt_to_srt3-0.1.8.5.tar.gz/vtt_to_srt3-0.1.8.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+     name='vtt_to_srt3',
+     version='0.1.8.5',
+     author="Jeison Cardoso",
+     author_email="cardoso.jeison@gmail.com",
+     description="vtt to srt subtitles converter package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/amorvincitomnia/vtt-to-srt.py",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "Operating System :: OS Independent",
+     ],
+     entry_points={
+        "console_scripts": [
+            "vtt_to_srt=vtt_to_srt.vtt_to_srt:main",
+        ]
+    }
+
+ )
