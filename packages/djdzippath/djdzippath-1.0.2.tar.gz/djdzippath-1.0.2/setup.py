3f91ebd12ebb22ac378d3f3f0728380667cafed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package djdzippath/1.0.2/djdzippath-1.0.2.tar.gz

---
+++ packages/djdzippath/djdzippath-1.0.2.tar.gz/djdzippath-1.0.2/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='djdzippath',
+    version='1.0.2',
+    keywords="test",
+    description='zip path',
+    long_description='zip path',
+    author='heifade',
+    author_email='heifade@126.com',
+    url='https://heifade.com',
+    packages=['djdzippath'],
+    # data_files=file_data,
+    # include_package_data=True,
+    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3*",
+    # install_requires=["requests>=2.24.0"],
+    # zip_safe=False
+)
