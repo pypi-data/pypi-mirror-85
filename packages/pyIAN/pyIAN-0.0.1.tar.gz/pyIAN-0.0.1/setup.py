1d0ea8c59e3e1a567b59ca1646edae7fda07ec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package pyIAN/0.0.1/pyIAN-0.0.1.tar.gz

---
+++ packages/pyIAN/pyIAN-0.0.1.tar.gz/pyIAN-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import os
+import numpy
+
+
+
+setup(
+    name="pyIAN",
+    description='Python library for IAN multibehavior planner',
+    author='Daniel Dugas',
+    version='0.0.1',
+    packages=find_packages(),
+    ext_modules = cythonize("cIA/cia.pyx", annotate=True),
+    include_dirs=[numpy.get_include()],
+    #     ext_modules = cythonize("clib_clustering/lidar_clustering.pyx", annotate=True),
+)
