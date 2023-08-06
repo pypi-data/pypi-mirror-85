d6dcaaf140d76207f748512440460f05cdb94e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:44 +0000

    Add package TOPSIS-Sheramir-101803549/1.0.0/TOPSIS_Sheramir_101803549-1.0.0.tar.gz

---
+++ packages/TOPSIS-Sheramir-101803549/TOPSIS_Sheramir_101803549-1.0.0.tar.gz/TOPSIS_Sheramir_101803549-1.0.0/TOPSIS_Sheramir_101803549/__init__.py
@@ -0,0 +1,3 @@
+import topsis from .__main__
+name="TOPSIS_Sheramir_101803549/TOPSIS_Sheramir_101803549"
+__version__ = "1.0.0"
---
+++ packages/TOPSIS-Sheramir-101803549/TOPSIS_Sheramir_101803549-1.0.0.tar.gz/TOPSIS_Sheramir_101803549-1.0.0/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TOPSIS_Sheramir_101803549",
+    version="1.0.0",
+    author="Sheramir",
+    author_email="sheramir51@gmail.com",
+    description="This package is for TOPSIS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['pandas','numpy==1.19.3'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
