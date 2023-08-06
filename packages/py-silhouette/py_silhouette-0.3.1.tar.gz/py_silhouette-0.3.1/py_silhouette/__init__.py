a5e83ff4cf948580c8d727286d7758ccd377fb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package py-silhouette/0.3.1/py_silhouette-0.3.1.tar.gz

---
+++ packages/py-silhouette/py_silhouette-0.3.1.tar.gz/py_silhouette-0.3.1/py_silhouette/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+A Python driver for controlling cutters and plotters in the Silhouette Portrait
+family.
+"""
+
+from py_silhouette.version import __version__
+
+from py_silhouette.device import *
