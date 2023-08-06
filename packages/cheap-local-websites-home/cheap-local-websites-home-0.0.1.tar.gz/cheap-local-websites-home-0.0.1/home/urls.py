f163a007b49610efab344f20774e33eb034018cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:51 +0000

    Add package cheap-local-websites-home/0.0.1/cheap-local-websites-home-0.0.1.tar.gz

---
+++ packages/cheap-local-websites-home/cheap-local-websites-home-0.0.1.tar.gz/cheap-local-websites-home-0.0.1/home/urls.py
@@ -0,0 +1,9 @@
+from .views import IndexView, AboutView, ContactView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+]
