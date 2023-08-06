b421f8ed20e46139f37d3b6685866e83a17843bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:03 +0000

    Add package py-crtsh/0.0.1b0/py-crtsh-0.0.1b0.tar.gz

---
+++ packages/py-crtsh/py-crtsh-0.0.1b0.tar.gz/py-crtsh-0.0.1b0/README.md
@@ -0,0 +1,15 @@
+# Py-CRTSH
+
+Licensed under 3 Clause BSD<br />
+This module provides a method to do basic queries to the site, 
+[crt.sh](https://crt.sh).
+
+Usage is very basic:
+
+```python
+from crtsh import Crtsh
+query = Crtsh()
+results = query.identify('test.com')
+```
+
+More advanced usage may be added as needed/requested
---
+++ packages/py-crtsh/py-crtsh-0.0.1b0.tar.gz/py-crtsh-0.0.1b0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-crtsh",
+    version="0.0.1b",
+    author="Plant Daddy",
+    author_email="CqP5TZ77NYBf5uQt@protonmail.com",
+    description="A simple package to do basic queries of crt.sh",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PlantDaddy/py-crtsh",
+    install_requires=['scrapy', 'requests'],
+    packages=setuptools.find_packages(),
+    project_urls= {'Tracker': 'https://github.com/PlantDaddy/py-crtsh'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
