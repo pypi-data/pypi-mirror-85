15beaa8f54e03ee3f5d12ded6529df5e1c59fef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:05 +0000

    Add package soadrl-crowdnav/0.0.1/soadrl-crowdnav-0.0.1.tar.gz

---
+++ packages/soadrl-crowdnav/soadrl-crowdnav-0.0.1.tar.gz/soadrl-crowdnav-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    name='soadrl-crowdnav',
+    version='0.0.1',
+    packages=[
+        'crowd_nav',
+        'crowd_nav.policy',
+        'crowd_nav.utils',
+        'crowd_sim',
+        'crowd_sim.envs',
+        'crowd_sim.envs.policy',
+        'crowd_sim.envs.utils',
+    ],
+    install_requires=[
+        'gitpython',
+        'gym',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'torch',
+        'torchvision',
+    ],
+    extras_require={
+        'test': [
+            'pylint',
+            'pytest',
+        ],
+    },
+    data_files=[('config', ['configs/test_soadrl_static.config'])],
+)
