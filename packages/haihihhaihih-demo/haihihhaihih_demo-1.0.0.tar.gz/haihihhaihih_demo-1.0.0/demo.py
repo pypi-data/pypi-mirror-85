3b286d836d5de5071ec1c876ce6795aa3a4cecee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:05 +0000

    Add package haihihhaihih-demo/1.0.0/haihihhaihih_demo-1.0.0.tar.gz

---
+++ packages/haihihhaihih-demo/haihihhaihih_demo-1.0.0.tar.gz/haihihhaihih_demo-1.0.0/demo.py
@@ -0,0 +1,4 @@
+def test():
+    while(1):
+        print("师丽真靓啊")
+test()
---
+++ packages/haihihhaihih-demo/haihihhaihih_demo-1.0.0.tar.gz/haihihhaihih_demo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="haihihhaihih_demo",
+      version="1.0.0",
+      description="师丽",
+      long_description="爱健身的师丽",
+      author="小虎",
+      author_email="250128632@qq.com",
+      url= "http://www.galiee.com",
+      py_modules=["demo"])
