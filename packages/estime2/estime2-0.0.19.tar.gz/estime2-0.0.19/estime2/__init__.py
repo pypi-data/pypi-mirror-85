b2aa358b635ab20e5a6b6dfeacb566ad8d2ed865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package estime2/0.0.19/estime2-0.0.19.tar.gz

---
+++ packages/estime2/estime2-0.0.19.tar.gz/estime2-0.0.19/estime2/__init__.py
@@ -0,0 +1,11 @@
+
+__version__ = "0.0.19"
+from estime2.config import (
+    get_option,
+    option_context,
+    options,
+    reset_option,
+    set_option
+)
+from estime2.age import Age
+from estime2.provpoptable import ProvPopTable
