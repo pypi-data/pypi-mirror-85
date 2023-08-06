d5e177004122d0f6e53eda2c57f266f963c0ea6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:06 +0000

    Add package TOPSIS-PANKAJ-101803352/0.4/TOPSIS_PANKAJ_101803352-0.4.tar.gz

---
+++ packages/TOPSIS-PANKAJ-101803352/TOPSIS_PANKAJ_101803352-0.4.tar.gz/TOPSIS_PANKAJ_101803352-0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    #Here is the module name.
+    name="TOPSIS_PANKAJ_101803352",
+ 
+    #version of the module
+    version="0.4",
+ 
+    #Name of Author
+    author="Pankaj Gupta",
+ 
+    #your Email address
+    author_email="pankaj140699@gmail.com",
+ 
+    #Small Description about module
+    description="Multiple Criteria Decision Making using Topsis",
+ 
+    long_description=long_description,
+ 
+    #Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+
+    #Any link to reach this module, if you have any webpage or github profile
+    # url="https://github.com/Pushkar-Singh-14/",
+    packages=['TOPSIS_PANKAJ_101803352'],
+    install_requires=['pandas']
+    #classifiers like program is suitable for python3, just leave as it is.
+)
