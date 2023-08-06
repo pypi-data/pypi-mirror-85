d2cfc1510c960cc309c6f3cce5e2fb84f81bb2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package quarchpy/2.0.15.dev18/quarchpy-2.0.15.dev18-py2.py3-none-any.whl

---
+++ packages/quarchpy/quarchpy-2.0.15.dev18-py2.py3-none-any.whl/quarchpy/disk_test/dtsGlobals.py
@@ -0,0 +1,15 @@
+class dtsGlobals:
+    GUI_TCP_IP = "127.0.0.1"
+    choiceResponse = None
+    continueTest = True
+    validVersion = True
+    minQCSVersion = "1.04"
+    QCSVersionValid = False
+    runUntilFirstFailure = False
+    send_to_gui = True
+
+    guiAddress = "localhost"
+    guiPort = 9921
+
+
+
