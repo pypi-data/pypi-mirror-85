9c9c57ccf10b3d73797ea6fb94b74a0ca4e355b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:43 +0000

    Add package statick/0.4.5/statick-0.4.5.tar.gz

---
+++ packages/statick/statick-0.4.5.tar.gz/statick-0.4.5/statick_tool/plugins/discovery/catkin_discovery_plugin.py
@@ -0,0 +1,29 @@
+"""Discovery plugin to find catkin packages."""
+import os
+from typing import Optional
+
+from statick_tool.discovery_plugin import DiscoveryPlugin
+from statick_tool.exceptions import Exceptions
+from statick_tool.package import Package
+
+
+class CatkinDiscoveryPlugin(DiscoveryPlugin):
+    """Discovery plugin to find catkin packages."""
+
+    def get_name(self) -> str:
+        """Get name of discovery type."""
+        return "catkin"
+
+    def scan(
+        self, package: Package, level: str, exceptions: Optional[Exceptions] = None
+    ) -> None:
+        """Scan package looking for catkin files."""
+        cmake_file = os.path.join(package.path, "CMakeLists.txt")
+        package_file = os.path.join(package.path, "package.xml")
+
+        if os.path.isfile(cmake_file) and os.path.isfile(package_file):
+            print("  Package is catkin.")
+            package["catkin"] = True
+        else:
+            print("  Package is not catkin.")
+            package["catkin"] = False
