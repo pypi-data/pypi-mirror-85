44eb71af0ce2b87d41155066fdb1f5ad850c16e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package brain-py/0.2.3/Brain.Py-0.2.3.tar.gz

---
+++ packages/brain-py/Brain.Py-0.2.3.tar.gz/Brain.Py-0.2.3/README.rst
@@ -0,0 +1,190 @@
+
+
+.. image:: https://github.com/PKU-NIP-Lab/BrainPy/blob/master/docs/images/logo.png
+    :target: https://github.com/PKU-NIP-Lab/BrainPy
+    :align: center
+    :alt: Logo
+
+.. image:: https://anaconda.org/brainpy/brainpy/badges/license.svg
+    :target: https://github.com/PKU-NIP-Lab/BrainPy
+    :alt: LICENSE
+
+.. image:: https://readthedocs.org/projects/brainpy/badge/?version=latest
+    :target: https://brainpy.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://anaconda.org/brainpy/brainpy/badges/version.svg
+    :target: https://anaconda.org/brainpy/brainpy
+    :alt: Conda version
+
+.. image:: https://badge.fury.io/py/Brain.Py.svg
+    :target: https://badge.fury.io/py/Brain.Py
+    :alt: Pypi Version
+
+
+
+
+**Note**: *BrainPy is a project under development.*
+*More features are coming soon. Contributions are welcome.*
+
+
+Why to use BrainPy
+=====================
+
+``BrainPy`` is a lightweight framework based on latest Just-In-Time (JIT)
+compilers. The goal of ``BrainPy`` is to provide
+a highly flexible and efficient neural simulation framework for Python users.
+It endows the users with the fully data/logic flow control.
+The core of the framework is a micro-kernel, and it's easy to understand (see
+*the document coming soon*).
+Based on the kernel, the extension of the new models or the customization of the
+data/logic flows are very simple for users. Ample examples (such as LIF neuron,
+HH neuron, or AMPA synapse, GABA synapse and GapJunction) are also provided.
+Besides the consideration of **flexibility**, for accelerating the running
+**speed** of NumPy codes, `Numba` is used. For most of the times,
+models running on `Numba` backend is very fast.
+
+.. figure:: https://github.com/PKU-NIP-Lab/NumpyBrain/blob/master/docs/images/speed.png
+    :alt: Speed of BrainPy
+    :figclass: align-center
+    :width: 400px
+
+
+Installation
+============
+
+Install ``BrainPy`` using ``conda``::
+
+    > conda install -c brainpy brainpy
+
+
+Install ``BrainPy`` using ``pip``::
+
+    > pip install brain.py
+
+Install from source code::
+
+    > git clone https://github.com/PKU-NIP-Lab/BrainPy
+    > python setup.py install
+    >
+    > # or
+    >
+    > pip install git+https://github.com/PKU-NIP-Lab/BrainPy
+
+
+The following packages need to be installed to use ``BrainPy``:
+
+- Python >= 3.7
+- NumPy >= 1.13
+- Sympy >= 1.2
+- Matplotlib >= 2.0
+- autopep8
+
+Packages recommended to install:
+
+- Numba >= 0.40.0
+
+
+Define a Hodgkin–Huxley neuron model
+====================================
+
+.. code-block:: python
+
+    import brainpy.numpy as np
+    import brainpy as bp
+
+    def HH(noise=0., E_Na=50., g_Na=120., E_K=-77., g_K=36.,
+           E_Leak=-54.387, g_Leak=0.03, C=1.0, Vth=20.):
+
+        ST = bp.types.NeuState(
+            {'V': -65., 'm': 0., 'h': 0., 'n': 0., 'sp': 0., 'inp': 0.},
+            help='Hodgkin–Huxley neuron state.\n'
+                 '"V" denotes membrane potential.\n'
+                 '"n" denotes potassium channel activation probability.\n'
+                 '"m" denotes sodium channel activation probability.\n'
+                 '"h" denotes sodium channel inactivation probability.\n'
+                 '"sp" denotes spiking state.\n'
+                 '"inp" denotes synaptic input.\n'
+        )
+
+        @bp.integrate
+        def int_m(m, t, V):
+            alpha = 0.1 * (V + 40) / (1 - np.exp(-(V + 40) / 10))
+            beta = 4.0 * np.exp(-(V + 65) / 18)
+            return alpha * (1 - m) - beta * m
+
+        @bp.integrate
+        def int_h(h, t, V):
+            alpha = 0.07 * np.exp(-(V + 65) / 20.)
+            beta = 1 / (1 + np.exp(-(V + 35) / 10))
+            return alpha * (1 - h) - beta * h
+
+        @bp.integrate
+        def int_n(n, t, V):
+            alpha = 0.01 * (V + 55) / (1 - np.exp(-(V + 55) / 10))
+            beta = 0.125 * np.exp(-(V + 65) / 80)
+            return alpha * (1 - n) - beta * n
+
+        @bp.integrate
+        def int_V(V, t, m, h, n, Isyn):
+            INa = g_Na * m ** 3 * h * (V - E_Na)
+            IK = g_K * n ** 4 * (V - E_K)
+            IL = g_Leak * (V - E_Leak)
+            dvdt = (- INa - IK - IL + Isyn) / C
+            return (dvdt, noise / C)
+
+        def update(ST, _t_):
+            m = np.clip(int_m(ST['m'], _t_, ST['V']), 0., 1.)
+            h = np.clip(int_h(ST['h'], _t_, ST['V']), 0., 1.)
+            n = np.clip(int_n(ST['n'], _t_, ST['V']), 0., 1.)
+            V = int_V(ST['V'], _t_, m, h, n, ST['inp'])
+            sp = np.logical_and(ST['V'] < Vth, V >= Vth)
+            ST['sp'] = sp
+            ST['V'] = V
+            ST['m'] = m
+            ST['h'] = h
+            ST['n'] = n
+            ST['inp'] = 0.
+
+        return bp.NeuType(name='HH',
+                          requires=dict(ST=ST),
+                          steps=update)
+
+
+
+Define an AMPA synapse model
+============================
+
+.. code-block:: python
+
+    def AMPA(g_max=0.10, E=0., tau_decay=2.0):
+
+        requires = dict(
+            ST=bp.types.SynState(
+                ['s'], help='AMPA synapse state.'),
+            pre=bp.types.NeuState(
+                ['sp'], help='Pre-synaptic state must have "sp" item.'),
+            post=bp.types.NeuState(
+                ['V', 'inp'], help='Post-synaptic neuron must have "V" and "inp" items.')
+        )
+
+        @bp.integrate(method='euler')
+        def ints(s, t):
+            return - s / tau_decay
+
+        def update(ST, _t_, pre):
+            s = ints(ST['s'], _t_)
+            s += pre['sp']
+            ST['s'] = s
+
+        @bp.delayed
+        def output(ST, post):
+            post_val = - g_max * ST['s'] * (post['V'] - E)
+            post['inp'] += post_val
+
+        return bp.SynType(name='AMPA',
+                          requires=requires,
+                          steps=(update, output),
+                          vector_based=False)
+
+
---
+++ packages/brain-py/Brain.Py-0.2.3.tar.gz/Brain.Py-0.2.3/brainpy/core_system/neurons.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+
+from .base import BaseEnsemble
+from .base import BaseType
+from .constants import _NEU_GROUP
+from .. import numpy as np
+from ..errors import ModelUseError
+
+__all__ = [
+    'NeuType',
+    'NeuGroup',
+    'NeuSubGroup',
+]
+
+_NEU_GROUP_NO = 0
+
+
+class NeuType(BaseType):
+    """Abstract Neuron Type.
+
+    It can be defined based on a group of neurons or a single neuron.
+    """
+
+    def __init__(self, name, requires, steps, vector_based=True, heter_params_replace=None, extra_functions=()):
+        super(NeuType, self).__init__(requires=requires,
+                                      steps=steps,
+                                      name=name,
+                                      vector_based=vector_based,
+                                      heter_params_replace=heter_params_replace,
+                                      extra_functions=extra_functions)
+
+
+class NeuGroup(BaseEnsemble):
+    """Neuron Group.
+
+    Parameters
+    ----------
+    model : NeuType
+        The instantiated neuron type model.
+    geometry : int, tuple
+        The neuron group geometry.
+    pars_update : dict, None
+        Parameters to update.
+    monitors : list, tuple, None
+        Variables to monitor.
+    name : str, None
+        The name of the neuron group.
+    """
+
+    def __init__(self, model, geometry, pars_update=None, monitors=None, name=None):
+        # name
+        # -----
+        if name is None:
+            global _NEU_GROUP_NO
+            name = f'NG{_NEU_GROUP_NO}'
+            _NEU_GROUP_NO += 1
+        else:
+            name = name
+
+        # num and geometry
+        # -----------------
+        if isinstance(geometry, (int, float)):
+            geometry = num = int(geometry)
+            self.indices = np.asarray(np.arange(int(geometry)), dtype=np.int_)
+        elif isinstance(geometry, (tuple, list)):
+            if len(geometry) == 1:
+                height, width = 1, geometry[0]
+            elif len(geometry) == 2:
+                height, width = geometry[0], geometry[1]
+            else:
+                raise ModelUseError('Do not support 3+ dimensional networks.')
+            geometry = (height, width)
+            num = height * width
+            indices = np.arange(num).reshape((height, width))
+            self.indices = np.asarray(indices, dtype=np.int_)
+        else:
+            raise ValueError()
+        self.geometry = geometry
+        self.size = np.size(self.indices)
+
+        # model
+        # ------
+        try:
+            assert isinstance(model, NeuType)
+        except AssertionError:
+            raise ModelUseError(f'{NeuGroup.__name__} receives an instance of {NeuType.__name__}, '
+                                f'not {type(model).__name__}.')
+
+        # initialize
+        # ----------
+        super(NeuGroup, self).__init__(model=model,
+                                       pars_update=pars_update,
+                                       name=name,
+                                       num=num,
+                                       monitors=monitors,
+                                       cls_type=_NEU_GROUP)
+
+        # ST
+        # --
+        self.ST = self.requires['ST'].make_copy(num)
+
+    @property
+    def _keywords(self):
+        return super(NeuGroup, self)._keywords + ['geometry', ]
+
+    def __getitem__(self, item):
+        """Return a subset of neuron group.
+
+        Parameters
+        ----------
+        item : slice, int, tuple of slice
+
+        Returns
+        -------
+        sub_group : NeuSubGroup
+            The subset of the neuron group.
+        """
+
+        if isinstance(item, int):
+            try:
+                assert item < self.num
+            except AssertionError:
+                raise ModelUseError(f'Index error, because the maximum number of neurons'
+                                    f'is {self.num}, but got "item={item}".')
+            d1_start, d1_end, d1_step = item, item + 1, 1
+            indices = self.indices[d1_start:d1_end:d1_step]
+            check_slice(d1_start, d1_end, self.num)
+        elif isinstance(item, slice):
+            d1_start, d1_end, d1_step = item.indices(self.num)
+            indices = self.indices[d1_start:d1_end:d1_step]
+            check_slice(d1_start, d1_end, self.num)
+        elif isinstance(item, tuple):
+            if not isinstance(self.geometry, (tuple, list)):
+                raise ModelUseError(f'{self.name} has a 1D geometry, cannot use a tuple of slice.')
+            if len(item) != 2:
+                raise ModelUseError(f'Only support 2D network, cannot make {len(item)}D slice.')
+
+            if isinstance(item[0], slice):
+                d1_start, d1_end, d1_step = item[0].indices(self.geometry[0])
+            elif isinstance(item[0], int):
+                d1_start, d1_end, d1_step = item[0], item[0] + 1, 1
+            else:
+                raise ModelUseError("Only support slicing syntax or a single index.")
+            check_slice(d1_start, d1_end, self.geometry[0])
+
+            if isinstance(item[1], slice):
+                d2_start, d2_end, d2_step = item[1].indices(self.geometry[1])
+            elif isinstance(item[1], int):
+                d2_start, d2_end, d2_step = item[1], item[1] + 1, 1
+            else:
+                raise ModelUseError("Only support slicing syntax or a single index.")
+            check_slice(d1_start, d1_end, self.geometry[1])
+
+            indices = self.indices[d1_start:d1_end:d1_step, d2_start:d2_end:d2_step]
+        else:
+            raise ModelUseError('Subgroups can only be constructed using slicing syntax, '
+                                'a single index, or an array of contiguous indices.')
+
+        return NeuSubGroup(source=self, indices=indices)
+
+
+def check_slice(start, end, length):
+    if start >= end:
+        raise ModelUseError(f'Illegal start/end values for subgroup, {start}>={end}')
+    if start >= length:
+        raise ModelUseError(f'Illegal start value for subgroup, {start}>={length}')
+    if end > length:
+        raise ModelUseError(f'Illegal stop value for subgroup, {end}>{length}')
+    if start < 0:
+        raise ModelUseError('Indices have to be positive.')
+
+
+class NeuSubGroup(object):
+    """Subset of a `NeuGroup`.
+    """
+
+    def __init__(self, source, indices):
+        try:
+            assert isinstance(source, NeuGroup)
+        except AssertionError:
+            raise ModelUseError('NeuSubGroup only support an instance of NeuGroup.')
+
+        self.source = source
+        self.indices = indices
+        self.num = np.size(indices)
+
+    def __getattr__(self, item):
+        if item in ['source', 'indices', 'num']:
+            return getattr(self, item)
+        else:
+            return getattr(self.source, item)
