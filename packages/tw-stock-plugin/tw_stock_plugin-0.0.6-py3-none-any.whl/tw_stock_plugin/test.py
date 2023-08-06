124c42f384822f5ce7b9dd0b3b1ea1f222762422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:47 +0000

    Add package tw-stock-plugin/0.0.6/tw_stock_plugin-0.0.6-py3-none-any.whl

---
+++ packages/tw-stock-plugin/tw_stock_plugin-0.0.6-py3-none-any.whl/tw_stock_plugin/test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+from datetime import datetime
+from tw_stock_plugin.core.stock_shareholdings import StockShareholdings
+
+# UpdateStock.main()
+
+date_ = datetime(2020, 11, 6).date()
+
+stock_share_holdings = StockShareholdings()
+
+print(stock_share_holdings.get_newest().get('0050')[1].__dict__)
