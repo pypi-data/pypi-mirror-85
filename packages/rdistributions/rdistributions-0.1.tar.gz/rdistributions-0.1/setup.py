5e02758e26e2193791670e01e185d577b1326280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:55 +0000

    Add package rdistributions/0.1/rdistributions-0.1.tar.gz

---
+++ packages/rdistributions/rdistributions-0.1.tar.gz/rdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rdistributions'],
+      zip_safe=False)
