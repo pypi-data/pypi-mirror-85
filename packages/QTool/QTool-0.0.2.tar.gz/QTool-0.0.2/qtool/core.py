f85e51d6aceb438ed6bf06a3ea975a68e56e3e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:34 +0000

    Add package QTool/0.0.2/QTool-0.0.2.tar.gz

---
+++ packages/QTool/QTool-0.0.2.tar.gz/QTool-0.0.2/qtool/core.py
@@ -0,0 +1,30 @@
+# Insert your code here. 
+
+
+def drawHistory(history):
+    '''
+    :param history:  When using keras and model.fit, it will return training history, that is what this method receive
+                     Please note that the metric=['accuracy']
+    :return: None
+    '''
+    import matplotlib.pyplot as plt
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.show()
+
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
