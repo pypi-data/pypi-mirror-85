dfe1ce45ca665bc89e393cb7bb60f17fe53cf073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package py-algorand-sdk/1.4.1/py-algorand-sdk-1.4.1.tar.gz

---
+++ packages/py-algorand-sdk/py-algorand-sdk-1.4.1.tar.gz/py-algorand-sdk-1.4.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="py-algorand-sdk",
+    description="Algorand SDK in Python",
+    author="Algorand",
+    author_email="pypiservice@algorand.com",
+    version="1.4.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    project_urls={
+        "Source": "https://github.com/algorand/py-algorand-sdk",
+    },
+    install_requires=["pynacl", "pycryptodomex>=3.6.0", "msgpack"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    package_data={'': ['data/langspec.json']},
+    include_package_data=True,
+)
