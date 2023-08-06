63ae8c4b771e88e7f42f0aa34db365a8499c301a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package fill-voids/2.0.1/fill_voids-2.0.1.tar.gz

---
+++ packages/fill-voids/fill_voids-2.0.1.tar.gz/fill_voids-2.0.1/automated_test.py
@@ -0,0 +1,127 @@
+import pytest 
+
+import fill_voids
+import scipy.ndimage
+from scipy.ndimage.morphology import binary_fill_holes
+
+from tqdm import tqdm
+
+import numpy as np 
+
+img = np.load('test_data.npy')
+SEGIDS = np.unique(img)[1:]
+
+def test_scipy_comparison3d():
+  segids = np.copy(SEGIDS)
+  np.random.shuffle(segids)
+
+  for segid in tqdm(segids[:10]):
+    print(segid)
+    binimg = img == segid
+    slices = scipy.ndimage.find_objects(binimg)[0]
+    binimg = binimg[slices]
+
+    orig_binimg = np.copy(binimg, order='F')
+    fv = fill_voids.fill(binimg, in_place=False)
+    fvip = fill_voids.fill(binimg, in_place=True)
+
+    assert np.all(fv == fvip)
+
+    spy = binary_fill_holes(binimg)
+
+    assert np.all(fv == spy)
+
+def test_scipy_comparison2d():
+  segids = np.copy(SEGIDS)
+  np.random.shuffle(segids)
+
+  for segid in tqdm(segids[:10]):
+    print(segid)
+    for z in tqdm(range(img.shape[2])):
+      binimg = img[:,:,z] == segid
+
+      orig_binimg = np.copy(binimg, order='F')
+      fv = fill_voids.fill(binimg, in_place=False)
+      fvip = fill_voids.fill(binimg, in_place=True)
+
+      assert np.all(fv == fvip)
+
+      spy = binary_fill_holes(binimg)
+
+      assert np.all(fv == spy)
+
+def test_2d_3d_differ():
+  labels = np.zeros((10,10), dtype=np.bool)
+  labels[1:9,1:9] = True
+  labels[4:8,4:8] = False
+
+  expected_result2d = np.zeros((10,10), dtype=np.bool)
+  expected_result2d[1:9,1:9] = True
+
+  expected_result3d = np.copy(labels).reshape(10,10,1)
+
+  filled_labels, N = fill_voids.fill(labels, in_place=False, return_fill_count=True)
+  assert N == 16
+  assert np.all(filled_labels == expected_result2d)
+
+  labels = labels[..., np.newaxis]
+  
+  filled_labels, N = fill_voids.fill(labels, in_place=False, return_fill_count=True)
+  assert N == 0
+  assert np.all(filled_labels == expected_result3d)
+
+DTYPES = (
+  np.bool, np.int8, np.uint8, np.uint16, np.int16, 
+  np.int32, np.uint32, np.int64, np.uint64, 
+  np.float32, np.float64
+)
+
+@pytest.mark.parametrize("dtype", DTYPES)
+def test_dtypes(dtype):
+  binimg = img == SEGIDS[0]
+
+  comparison = fill_voids.fill(binimg, in_place=False)
+  res = fill_voids.fill(binimg.astype(dtype), in_place=False)
+  assert np.all(comparison == res)
+
+def test_zero_array():
+  labels = np.zeros((0,), dtype=np.uint8)
+  # just don't throw an exception
+  fill_voids.fill(labels, in_place=False)
+  fill_voids.fill(labels, in_place=True)
+
+  labels = np.zeros((128,128,128), dtype=np.uint8)
+  fill_voids.fill(labels, in_place=True)
+  assert not np.any(labels)
+
+def test_return_count():
+  labels = np.ones((10, 10, 10), dtype=bool)
+  labels[3:6,3:6,3:6] = False
+
+  filled = fill_voids.fill(labels)
+  assert np.all(filled == 1)
+
+  filled, ct = fill_voids.fill(labels, return_fill_count=True)
+  assert np.any(labels == False)
+  assert ct == 27
+
+@pytest.mark.parametrize("dimension", [1,2,3,4,5,6])
+def test_dimensions(dimension):
+  size = [5] * dimension
+  for i in range(3, dimension):
+    size[i] = 1
+
+  labels = np.ones(size, dtype=np.uint8)
+  labels = fill_voids.fill(labels)
+  assert labels.ndim == dimension
+
+  if dimension <= 3:
+    return
+    
+  size[dimension - 1] = 2
+  labels = np.ones(size, dtype=np.uint8)
+  try:
+    labels = fill_voids.fill(labels)
+    assert False 
+  except fill_voids.DimensionError:
+    pass
---
+++ packages/fill-voids/fill_voids-2.0.1.tar.gz/fill_voids-2.0.1/fill_voids.pyx
@@ -0,0 +1,191 @@
+"""
+Fill in holes in 3D binary images. 
+
+Similar to scipy.morphology.binary_fill_holes
+but designed to be more performant.
+
+The power of this library derives from the emptiness of space and the
+latent energies of the void.
+
+Author: William Silversmith
+Affiliation: Seung Lab, Princeton Neuroscience Institute
+Date: December 2019
+
+*****************************************************************
+This file is part of fill_voids.
+
+fill_voids is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+fill_voids is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with fill_voids.  If not, see <https://www.gnu.org/licenses/>.
+*****************************************************************
+"""
+cimport cython
+from libc.stdlib cimport calloc, free
+from libc.stdint cimport (
+  int8_t, int16_t, int32_t, int64_t,
+  uint8_t, uint16_t, uint32_t, uint64_t
+)
+from libcpp cimport bool
+from cpython cimport array 
+import array
+import sys
+
+from libcpp.vector cimport vector
+from libcpp.unordered_map cimport unordered_map
+from libcpp.utility cimport pair as cpp_pair
+
+cimport numpy as cnp
+import numpy as np
+
+import fastremap
+
+cdef extern from "math.h":
+  float INFINITY
+
+ctypedef fused NUMBER: 
+  int8_t
+  int16_t
+  int32_t
+  int64_t
+  uint8_t
+  uint16_t
+  uint32_t
+  uint64_t
+  unsigned char
+  float 
+  double
+
+cdef extern from "fill_voids.hpp" namespace "fill_voids":
+  cdef size_t binary_fill_holes2d[T](
+    T* labels, 
+    size_t sx, size_t sy
+  )
+  cdef size_t binary_fill_holes3d[T](
+    T* labels, 
+    size_t sx, size_t sy, size_t sz
+  )
+
+
+class DimensionError(Exception):
+  pass
+
+def fill(labels, in_place=False, return_fill_count=False):
+  """
+  fill(cnp.ndarray[NUMBER, cast=True, ndim=3] labels, in_place=False, return_fill_count=False)
+
+  labels: a binary valued numpy array of any common 
+    integer or floating dtype
+
+  in_place: bool, Allow modification of the input array (saves memory)
+  return_fill_count: Also return the number of voxels that were filled in.
+
+  Let IMG = a void filled binary image of the same dtype as labels
+
+  if return_fill_count:
+    Return: (IMG, number of filled in background voxels)
+  else:
+    Return: IMG
+  """
+  ndim = labels.ndim 
+  shape = labels.shape 
+
+  if labels.ndim < 2:
+    labels = labels[..., np.newaxis]
+  while labels.ndim > 3:
+    if labels.shape[-1] == 1:
+      labels = labels[..., 0]
+    else:
+      raise DimensionError("The input volume must be (effectively) a 1D, 2D or 3D image: " + str(shape))
+
+  dtype = labels.dtype
+  if labels.dtype == np.bool:
+    labels = labels.view(np.uint8)
+
+  if labels.size == 0:
+    num_filled = 0
+  elif labels.ndim == 2:
+    (labels, num_filled) = _fill2d(labels, in_place)
+  elif labels.ndim == 3:
+    (labels, num_filled) = _fill3d(labels, in_place)
+  else:
+    raise DimensionError("fill_voids only handles 1D, 2D, and 3D data. Got: " + str(shape))
+
+  while labels.ndim > ndim:
+    labels = labels[..., 0]
+  while labels.ndim < ndim:
+    labels = labels[..., np.newaxis]
+
+  labels = labels.view(dtype)
+
+  if return_fill_count:
+    return (labels, num_filled)
+  else:
+    return labels
+
+def _fill3d(cnp.ndarray[NUMBER, cast=True, ndim=3] labels, in_place=False):
+  if not in_place:
+    labels = np.copy(labels, order='F')
+  else:
+    labels = fastremap.asfortranarray(labels)
+
+  dtype = labels.dtype
+
+  cdef size_t num_filled = 0
+
+  if dtype in (np.uint8, np.int8, np.bool):
+    num_filled = binary_fill_holes3d[uint8_t](<uint8_t*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  elif dtype in (np.uint16, np.int16):
+    num_filled = binary_fill_holes3d[uint16_t](<uint16_t*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  elif dtype in (np.uint32, np.int32):
+    num_filled = binary_fill_holes3d[uint32_t](<uint32_t*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  elif dtype in (np.uint64, np.int64):
+    num_filled = binary_fill_holes3d[uint64_t](<uint64_t*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  elif dtype == np.float32:
+    num_filled = binary_fill_holes3d[float](<float*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  elif dtype == np.float64:
+    num_filled = binary_fill_holes3d[double](<double*>&labels[0,0,0], labels.shape[0], labels.shape[1], labels.shape[2])
+  else:
+    raise TypeError("Type {} not supported.".format(dtype))
+
+  return (labels, num_filled)
+
+def _fill2d(cnp.ndarray[NUMBER, cast=True, ndim=2] labels, in_place=False):
+  if not in_place:
+    labels = np.copy(labels, order='F')
+  else:
+    labels = fastremap.asfortranarray(labels)
+
+  dtype = labels.dtype
+
+  cdef size_t num_filled = 0
+
+  if dtype in (np.uint8, np.int8, np.bool):
+    num_filled = binary_fill_holes2d[uint8_t](<uint8_t*>&labels[0,0], labels.shape[0], labels.shape[1])
+  elif dtype in (np.uint16, np.int16):
+    num_filled = binary_fill_holes2d[uint16_t](<uint16_t*>&labels[0,0], labels.shape[0], labels.shape[1])
+  elif dtype in (np.uint32, np.int32):
+    num_filled = binary_fill_holes2d[uint32_t](<uint32_t*>&labels[0,0], labels.shape[0], labels.shape[1])
+  elif dtype in (np.uint64, np.int64):
+    num_filled = binary_fill_holes2d[uint64_t](<uint64_t*>&labels[0,0], labels.shape[0], labels.shape[1])
+  elif dtype == np.float32:
+    num_filled = binary_fill_holes2d[float](<float*>&labels[0,0], labels.shape[0], labels.shape[1])
+  elif dtype == np.float64:
+    num_filled = binary_fill_holes2d[double](<double*>&labels[0,0], labels.shape[0], labels.shape[1])
+  else:
+    raise TypeError("Type {} not supported.".format(dtype))
+
+  return (labels, num_filled)
+
+def void_shard():
+  """??? what's this ???"""
+  print("Play Starcraft 2!")
+
