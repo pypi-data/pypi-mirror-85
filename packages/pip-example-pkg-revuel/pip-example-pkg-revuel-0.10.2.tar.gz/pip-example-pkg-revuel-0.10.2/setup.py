78b775027a30fbfe1ea13867351f4a353b00311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:59 +0000

    Add package pip-example-pkg-revuel/0.10.2/pip-example-pkg-revuel-0.10.2.tar.gz

---
+++ packages/pip-example-pkg-revuel/pip-example-pkg-revuel-0.10.2.tar.gz/pip-example-pkg-revuel-0.10.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip-example-pkg-revuel",
+    version="0.10.2",
+    author="revuel",
+    author_email="revuel22@hotmail.com",
+    description="Tutorial to distribute a pip package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/revuel/pip-example-pkg-revule",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'spacy==2.3.0',
+        'en-core-web-sm==2.3.0'
+    ],
+    dependency_links=['https://github.com/explosion/spacy-models/'
+                      'releases/download/en_core_web_sm-2.3.0/en_core_web_sm-2.3.0.tar.gz#egg=en_core_web_sm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
