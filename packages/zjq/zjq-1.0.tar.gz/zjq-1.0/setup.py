b423cbb5081ec809fd50848a056c57494254d1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:32 +0000

    Add package zjq/1.0/zjq-1.0.tar.gz

---
+++ packages/zjq/zjq-1.0.tar.gz/zjq-1.0/setup.py
@@ -0,0 +1,12 @@
+
+
+from distutils.core import setup
+
+setup(
+    name='zjq',  #对外我们模块的名字
+    version='1.0', #版本号
+    description='这是第一个对外发布的模块', #描述
+    author='z',  #作者
+    author_email='zhu_jiangqiang@163.com',
+    py_modules=['zjq.demo01','zjq.demo02']
+)
