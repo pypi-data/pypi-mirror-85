7b06c3839297f88373ee7daa34461e70e9ad1979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package django-swap-user/0.2.6/django_swap_user-0.2.6-py3-none-any.whl

---
+++ packages/django-swap-user/django_swap_user-0.2.6-py3-none-any.whl/swap_user/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+
+
+class BaseUserForm(forms.ModelForm):
+    password_1 = forms.CharField(
+        label="Enter a new password.", widget=forms.PasswordInput, required=False,
+    )
+    password_2 = forms.CharField(
+        label="Repeat a new password.", widget=forms.PasswordInput, required=False,
+    )
+
+    class Meta:
+        exclude = ["password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password_1 = cleaned_data["password_1"]
+        password_2 = cleaned_data["password_2"]
+
+        if not password_1 or not password_2:
+            return cleaned_data
+
+        if password_1 != password_2:
+            raise forms.ValidationError(
+                message="Passwords should be same", code="password_should_be_same",
+            )
+
+        return cleaned_data
+
+    def save(self, commit=False):
+        instance = super().save(commit)
+        password_1 = self.cleaned_data["password_1"]
+
+        if password_1:
+            instance.set_password(password_1)
+
+        instance.save()
+
+        return instance
