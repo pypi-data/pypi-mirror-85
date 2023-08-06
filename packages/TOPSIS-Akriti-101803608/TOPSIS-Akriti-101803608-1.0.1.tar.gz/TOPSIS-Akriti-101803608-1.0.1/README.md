3e4e2215f75d95cc3a300fa4268f56318dfcca06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:29 +0000

    Add package TOPSIS-Akriti-101803608/1.0.1/TOPSIS-Akriti-101803608-1.0.1.tar.gz

---
+++ packages/TOPSIS-Akriti-101803608/TOPSIS-Akriti-101803608-1.0.1.tar.gz/TOPSIS-Akriti-101803608-1.0.1/README.md
@@ -0,0 +1,12 @@
+# TOPSIS-Akriti-101803608
+
+A python package which implements TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) which is a multi-criteria decision analysis method.
+
+# Usage
+
+Running the following query on the command-line interface will help to rank the models i.e. choose the best alternative available from a given set of models 
+based on multiple, usually conflicting criteria.
+  
+    TOPSIS-Akriti-101803608 <data_file_name> <weights> <impacts> <result_file_name>
+
+Note - Make sure that you run the command in the folder you have stored your data files , else provide the complete path of the file.
---
+++ packages/TOPSIS-Akriti-101803608/TOPSIS-Akriti-101803608-1.0.1.tar.gz/TOPSIS-Akriti-101803608-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="TOPSIS-Akriti-101803608",
+    packages=["Topsis_Ranking"],
+    version="1.0.1",
+    license="MIT",
+    description="A Python package to get best alternative available using TOPSIS method.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Akriti Singhal",
+    author_email="akritisinghal1663@gmail.com",
+    url="https://github.com/Akriti0100/TOPSIS-Akriti-101803608",
+    classifiers=[
+        "Development Status :: 3 - Alpha",      
+        "Intended Audience :: Developers",  
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    include_package_data=True,
+    install_requires=["pandas"],
+    entry_points={
+        "console_scripts": [
+            "TOPSIS-Akriti-101803608=Topsis_Ranking.topsis:main",
+        ]
+    },
+)
