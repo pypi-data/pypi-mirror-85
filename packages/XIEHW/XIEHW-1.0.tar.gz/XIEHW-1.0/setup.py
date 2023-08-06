86bc4fa8d441b8cd3044d86c2b98a6211bac4fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:06 +0000

    Add package XIEHW/1.0/XIEHW-1.0.tar.gz

---
+++ packages/XIEHW/XIEHW-1.0.tar.gz/XIEHW-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'XIEHW',
+    version = '1.0',
+    description = '第一次发布模块，进行测试',
+    author = 'XieHongWei',
+    author_email= '1323981989@qq.com',
+    py_modules = ['XIEHW.modulepath']
+)
