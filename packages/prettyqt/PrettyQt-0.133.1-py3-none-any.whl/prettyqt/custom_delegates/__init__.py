f72e2ec15874c7b3b8213da4a11d9ff36803fda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:11 +0000

    Add package prettyqt/0.133.1/PrettyQt-0.133.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.133.1-py3-none-any.whl/prettyqt/custom_delegates/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Module containing custom delegate classes."""
+
+from .buttondelegate import ButtonDelegate
+from .radiodelegate import RadioDelegate
+from .icondelegate import IconDelegate
+from .nofocusdelegate import NoFocusDelegate
+
+__all__ = [
+    "ButtonDelegate",
+    "RadioDelegate",
+    "IconDelegate",
+    "NoFocusDelegate",
+]
