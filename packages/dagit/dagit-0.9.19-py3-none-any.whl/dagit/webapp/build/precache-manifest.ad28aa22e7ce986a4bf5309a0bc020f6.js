35c5ab25d8128c3f4bc96f929203371a927f280b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package dagit/0.9.19/dagit-0.9.19-py3-none-any.whl

---
+++ packages/dagit/dagit-0.9.19-py3-none-any.whl/dagit/webapp/build/precache-manifest.ad28aa22e7ce986a4bf5309a0bc020f6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce6008eaf854c4725374003079dd6d9",
+    "url": "/c4aa85b9f05195d32f37.worker.js"
+  },
+  {
+    "revision": "b8714132b55319d9985b269752700df3",
+    "url": "/c4aa85b9f05195d32f37.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63e115393d96cd1570f74d9dcbc517f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ddd2fec60f794f534d",
+    "url": "/static/css/2.2098b62f.chunk.css"
+  },
+  {
+    "revision": "1983ed79cea80f0dc075",
+    "url": "/static/css/main.15dcaa88.chunk.css"
+  },
+  {
+    "revision": "60ddd2fec60f794f534d",
+    "url": "/static/js/2.658eb67b.chunk.js"
+  },
+  {
+    "revision": "c77e24cf387bbb30fda7387e57b810d7",
+    "url": "/static/js/2.658eb67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1983ed79cea80f0dc075",
+    "url": "/static/js/main.8e9d5403.chunk.js"
+  },
+  {
+    "revision": "613376841ee7f87ed6c0",
+    "url": "/static/js/runtime-main.2edff307.js"
+  },
+  {
+    "revision": "8abbb9d98c0c7304060190592408ab78",
+    "url": "/static/media/OpenSans-Regular.8abbb9d9.woff2"
+  },
+  {
+    "revision": "2595cae6483bb50cbeb0cb40e3292231",
+    "url": "/static/media/OpenSans-SemiBold.2595cae6.woff2"
+  },
+  {
+    "revision": "56379426c7040caed282b012caac0cc0",
+    "url": "/static/media/SourceCodePro-Bold.56379426.woff2"
+  },
+  {
+    "revision": "8f65fdc58449383aa56c9c86e1b3043a",
+    "url": "/static/media/SourceCodePro-Medium.8f65fdc5.woff2"
+  },
+  {
+    "revision": "74eb057a7a4eb8c7450565ebda3efc8f",
+    "url": "/static/media/SourceCodePro-Regular.74eb057a.woff2"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
