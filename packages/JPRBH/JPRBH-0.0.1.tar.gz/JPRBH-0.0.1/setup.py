d1d12ac27c479245fa235dc3da4216de240233aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:29 +0000

    Add package JPRBH/0.0.1/JPRBH-0.0.1.tar.gz

---
+++ packages/JPRBH/JPRBH-0.0.1.tar.gz/JPRBH-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JPRBH",
+    version="0.0.1",
+    author="John Patrick Roach",
+    author_email="contact@johnpatrickroach.com",
+    description="JPR Bayesian Health Code Test Submission",
+    url="https://github.com/johnpatrickroach/JPRBH",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
