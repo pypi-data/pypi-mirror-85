9add8d3c8405a8a3f9c3254bf23ec82859c1df75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package spell/0.39.41/spell-0.39.41-py2.py3-none-any.whl

---
+++ packages/spell/spell-0.39.41-py2.py3-none-any.whl/spell/serving/server.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+from prometheus_client import make_asgi_app as make_custom_metrics_app
+
+from starlette.applications import Starlette
+from starlette.exceptions import ExceptionMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.gzip import GZipMiddleware
+from starlette.routing import Mount
+import yaml
+
+from spell.serving import settings
+from spell.serving.exceptions import InvalidServerConfiguration
+from spell.serving.api import BatchedAPI, API
+
+READY_FILE = "/ready"
+
+
+def make_api() -> API:
+    if not settings.CONFIG_FILE.is_file():
+        raise InvalidServerConfiguration("Config file must be a file")
+    try:
+        with settings.CONFIG_FILE.open() as f:
+            config = yaml.safe_load(f)
+    except yaml.scanner.ScannerError as e:
+        raise InvalidServerConfiguration("Could not read config file") from e
+    api_class = BatchedAPI if settings.USE_BATCH_PREDICT else API
+    api = api_class.from_settings()
+    api.initialize_predictor(config)
+    return api
+
+
+def create_ready_file() -> None:
+    open(READY_FILE, "a").close()
+
+
+def make_app(api: Optional[API] = None, debug: bool = False) -> Starlette:
+    if not api:
+        api = make_api()
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_methods=["*"],
+            allow_headers=["*"],
+            allow_credentials=True,
+        ),
+        Middleware(ExceptionMiddleware),
+        Middleware(GZipMiddleware),
+    ]
+    routes = api.get_routes()
+    routes.append(Mount("/metrics", make_custom_metrics_app()))
+    return Starlette(
+        debug=debug,
+        routes=routes,
+        middleware=middleware,
+        on_startup=[create_ready_file],
+    )
