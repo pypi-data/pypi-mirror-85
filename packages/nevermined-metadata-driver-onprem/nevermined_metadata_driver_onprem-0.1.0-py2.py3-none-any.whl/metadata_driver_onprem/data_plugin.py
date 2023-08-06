9694f6db3bfcbca4d7962bcf542c723aec0435ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package nevermined-metadata-driver-onprem/0.1.0/nevermined_metadata_driver_onprem-0.1.0-py2.py3-none-any.whl

---
+++ packages/nevermined-metadata-driver-onprem/nevermined_metadata_driver_onprem-0.1.0-py2.py3-none-any.whl/metadata_driver_onprem/data_plugin.py
@@ -0,0 +1,34 @@
+from metadata_driver_interface.data_plugin import AbstractPlugin
+
+
+class Plugin(AbstractPlugin):
+    def __init__(self, config=None):
+        self.config = config
+
+    def type(self):
+        """str: the type of this plugin (``'On premise'``)"""
+        return 'On premise'
+
+    def upload(self, local_file, remote_file):
+        pass
+
+    def download(self, remote_file, local_file):
+        pass
+
+    def list(self, remote_folder):
+        pass
+
+    def generate_url(self, remote_file):
+        return remote_file
+
+    def delete(self, remote_file):
+        pass
+
+    def copy(self, source_path, dest_path):
+        pass
+
+    def create_directory(self, remote_folder):
+        pass
+
+    def retrieve_availability_proof(self):
+        pass
