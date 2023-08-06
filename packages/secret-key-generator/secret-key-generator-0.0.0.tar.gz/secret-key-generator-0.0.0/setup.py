ce8b30c74ff3dffeacb16bd83243dfa4cd6f5988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:02 +0000

    Add package secret-key-generator/0.0.0/secret-key-generator-0.0.0.tar.gz

---
+++ packages/secret-key-generator/secret-key-generator-0.0.0.tar.gz/secret-key-generator-0.0.0/setup.py
@@ -0,0 +1,24 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="secret-key-generator",
+    version=os.environ.get("RELEASE_NAME"),
+    author="Kirill Sayed",
+    author_email="sayed.kirill@gmail.com",
+    description="Package for secret key generation in Django framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PickBas/django_secret_key_generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
