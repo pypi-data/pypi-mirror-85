eb4de7e5bde5632048d63f500b07058d296bdb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:07 +0000

    Add package ccobra/1.2.1/ccobra-1.2.1.tar.gz

---
+++ packages/ccobra/ccobra-1.2.1.tar.gz/ccobra-1.2.1/ccobra/benchmark/comparators/equality.py
@@ -0,0 +1,44 @@
+""" Equality comparator.
+
+"""
+
+from ccobra import CCobraComparator, tuple_to_string
+
+
+class EqualityComparator(CCobraComparator):
+    """ Equality comparator. Checks if both responses are equal.
+
+    """
+
+    @staticmethod
+    def compare(obj_a, obj_b):
+        """ Compares two response objects based on equality.
+
+        Parameters
+        ----------
+        obj_a : tuple
+            Response tuple A for comparison.
+
+        obj_b : tuple
+            Response tuple B for comparison.
+
+        Returns
+        -------
+        bool
+            True if both objects are equal, false otherwise.
+
+        """
+
+        return int(tuple_to_string(obj_a) == tuple_to_string(obj_b))
+
+    def get_name(self):
+        """ Returns the name of the comparator.
+
+        Returns
+        -------
+        string
+            Comparator name.
+
+        """
+
+        return 'Accuracy'
---
+++ packages/ccobra/ccobra-1.2.1.tar.gz/ccobra-1.2.1/ccobra/benchmark/evaluation_handler.py
@@ -0,0 +1,85 @@
+import copy
+
+import pandas as pd
+import numpy as np
+
+from .. import tuple_to_string
+
+class EvaluationHandler():
+    def __init__(self, data_column, comparator, predict_fn_name, adapt_fn_name, task_encoders, resp_encoders):
+        self.data_column = data_column
+        self.comparator = comparator
+        self.predict_fn_name = predict_fn_name
+        self.adapt_fn_name = adapt_fn_name
+        self.task_encoders = task_encoders
+        self.resp_encoders = resp_encoders
+
+        # Prepare result dataframe
+        self.result = []
+
+    def predict(self, model, modelname, item, target, aux):
+        item = copy.deepcopy(item)
+        aux = copy.deepcopy(aux)
+
+        # Obtain the model prediction
+        pred_fn = getattr(model, self.predict_fn_name, None)
+        if pred_fn is None:
+            raise NotImplementedError("{} has to be implemented in {}".format(self.predict_fn_name, modelname))
+
+        prediction = pred_fn(item, **aux)
+        score = self.comparator.compare(prediction, target)
+
+        # Collect the evaluation result data
+        res_dict = {
+            'model': modelname,
+            'id': item.identifier,
+            'domain': item.domain,
+            'response_type': item.response_type,
+            'sequence': item.sequence_number,
+            'task': item.task_str,
+            'choices': item.choices_str,
+            'truth': tuple_to_string(target),
+            'prediction': tuple_to_string(prediction),
+            'score': score
+        }
+
+        if self.task_encoders:
+            domain = res_dict['domain']
+            res_dict['task_enc'] = self.task_encoders[domain].encode_task(item.task) if domain in self.task_encoders else np.nan
+
+        if self.resp_encoders:
+            domain = res_dict['domain']
+            res_dict['truth_enc_{}'.format(self.data_column)] = self.resp_encoders[domain].encode_response(target, item.task) if domain in self.resp_encoders else np.nan
+            res_dict['prediction_enc_{}'.format(self.data_column)] = self.resp_encoders[domain].encode_response(prediction, item.task) if domain in self.resp_encoders else np.nan
+
+        self.result.append(res_dict)
+
+    def adapt(self, model, item, full):
+        if self.adapt_fn_name is None:
+            return
+
+        item = copy.deepcopy(item)
+        full = copy.deepcopy(full)
+
+        target = full[self.data_column]
+        aux = {x: y for x, y in full.items() if x != self.data_column}
+        adapt_fn = getattr(model, self.adapt_fn_name, None)
+        if adapt_fn is None:
+            return
+
+        adapt_fn(item, target, **aux)
+
+    def get_result_df(self):
+        return pd.DataFrame(self.result)
+
+    def __repr__(self):
+        s = 'EvaluationHandler(data_column={}, comparator={}, predict_fn_name={}, adapt_fn_name={}, task_encoders={}, resp_encoders={})'.format(
+            self.data_column,
+            self.comparator,
+            self.predict_fn_name,
+            self.adapt_fn_name,
+            self.task_encoders,
+            self.resp_encoders
+        )
+        return s
+
