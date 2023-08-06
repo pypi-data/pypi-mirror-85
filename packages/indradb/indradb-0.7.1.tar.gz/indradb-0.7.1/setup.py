367d07bb62c88fb20368471651dc47d480e78907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:41 +0000

    Add package indradb/0.7.1/indradb-0.7.1.tar.gz

---
+++ packages/indradb/indradb-0.7.1.tar.gz/indradb-0.7.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = "indradb",
+    version = "0.7.1",
+    author = "Yusuf Simonson",
+    package_data={"": ["indradb.capnp"]},
+
+    packages = [
+        "indradb",
+    ],
+
+    install_requires = [
+        "pycapnp>=0.6.4"
+    ]
+)
