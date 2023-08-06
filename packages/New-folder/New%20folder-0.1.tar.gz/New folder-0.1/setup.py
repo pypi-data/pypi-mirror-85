1289ade2c4f06881f49495229dec7b4e0e0eeb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:38 +0000

    Add package New-folder/0.1/New%20folder-0.1.tar.gz

---
+++ packages/New-folder/New%20folder-0.1.tar.gz/New folder-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'New folder',         
+  packages = ['New folder'],   
+  version = '0.1',    
+  license='MIT',        
+  description = 'Topsis implementation',   
+  author = 'Sukhman',              
+  author_email = 'sukhmanpabla05@gmail.com',
+ 
+  
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
