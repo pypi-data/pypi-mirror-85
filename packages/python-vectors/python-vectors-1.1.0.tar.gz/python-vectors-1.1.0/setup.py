c11e65c8a57cddf54e39576e8095eb1c249dd1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:27 +0000

    Add package python-vectors/1.1.0/python-vectors-1.1.0.tar.gz

---
+++ packages/python-vectors/python-vectors-1.1.0.tar.gz/python-vectors-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "python-vectors",
+    version = "1.1.0",
+    author = "Foster Reichert",
+    author_email = "dev@fosterreichert.com",
+    description = "A vector library to contain and manipulate 2D and 3D points.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://pypi.org/project/python-vectors/",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.6",
+)
