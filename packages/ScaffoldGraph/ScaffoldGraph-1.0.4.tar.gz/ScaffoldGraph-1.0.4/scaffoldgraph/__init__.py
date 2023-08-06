0dd506504717b688f2fa75f7e5b24eef0ddebfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:11 +0000

    Add package ScaffoldGraph/1.0.4/ScaffoldGraph-1.0.4.tar.gz

---
+++ packages/ScaffoldGraph/ScaffoldGraph-1.0.4.tar.gz/ScaffoldGraph-1.0.4/scaffoldgraph/__init__.py
@@ -0,0 +1,33 @@
+"""
+scaffoldgraph
+"""
+
+from loguru import logger
+
+from . import prioritization
+from . import utils
+
+from .core import (
+    get_next_murcko_fragments,
+    get_all_murcko_fragments,
+    get_murcko_scaffold,
+)
+
+from .network import ScaffoldNetwork, HierS
+from .tree import ScaffoldTree, tree_frags_from_mol
+
+__version__ = '1.0.4'
+
+
+__all__ = [
+    '__version__',
+    'HierS',
+    'ScaffoldNetwork',
+    'ScaffoldTree',
+    'tree_frags_from_mol',
+    'get_next_murcko_fragments',
+    'get_all_murcko_fragments',
+    'get_murcko_scaffold',
+]
+
+logger.disable(__name__)
---
+++ packages/ScaffoldGraph/ScaffoldGraph-1.0.4.tar.gz/ScaffoldGraph-1.0.4/scaffoldgraph/analysis/__init__.py
@@ -0,0 +1,18 @@
+"""
+scaffoldgraph.analysis
+
+The analysis package contains functions for analyzing ScaffoldGraphs
+"""
+
+from .representation import calc_average_pairwise_similarity, get_over_represented_scaffold_classes
+from .enrichment import calc_scaffold_enrichment, compound_set_enrichment
+from .general import get_virtual_scaffolds, get_singleton_scaffolds
+
+__all__ = [
+    'calc_average_pairwise_similarity',
+    'get_over_represented_scaffold_classes',
+    'calc_scaffold_enrichment',
+    'compound_set_enrichment',
+    'get_virtual_scaffolds',
+    'get_singleton_scaffolds',
+]
