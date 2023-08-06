55199dd06443fa3595966f4a2ea96e4c124b923c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:08 +0000

    Add package davtelepot/2.6.19/davtelepot-2.6.19.tar.gz

---
+++ packages/davtelepot/davtelepot-2.6.19.tar.gz/davtelepot-2.6.19/davtelepot/__init__.py
@@ -0,0 +1,22 @@
+"""Information about this package.
+
+See `bot.py` for information about Bot class.
+    ```python3.5+
+    from davtelepot.bot import Bot
+    help(Bot)
+    ```
+"""
+
+__author__ = "Davide Testa"
+__email__ = "davide@davte.it"
+__credits__ = ["Marco Origlia", "Nick Lee @Nickoala"]
+__license__ = "GNU General Public License v3.0"
+__version__ = "2.6.19"
+__maintainer__ = "Davide Testa"
+__contact__ = "t.me/davte"
+
+from . import (administration_tools, authorization, bot, helper, languages,
+               suggestions, useful_tools, utilities)
+
+__all__ = [administration_tools, authorization, bot, helper, languages,
+           suggestions, useful_tools, utilities]
