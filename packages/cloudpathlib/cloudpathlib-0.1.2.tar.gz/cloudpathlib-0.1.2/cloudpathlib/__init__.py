626a7678e18beff887e75fab6437c764a9a95255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:12 +0000

    Add package cloudpathlib/0.1.2/cloudpathlib-0.1.2.tar.gz

---
+++ packages/cloudpathlib/cloudpathlib-0.1.2.tar.gz/cloudpathlib-0.1.2/cloudpathlib/__init__.py
@@ -0,0 +1,41 @@
+import sys
+
+from .azure.azblobclient import AzureBlobClient
+from .azure.azblobpath import AzureBlobPath
+from .cloudpath import CloudPath
+from .s3.s3client import S3Client
+from .s3.s3path import S3Path
+
+
+if sys.version_info[:2] >= (3, 8):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+__version__ = importlib_metadata.version(__name__.split(".", 1)[0])
+
+
+# exceptions
+from .cloudpath import (
+    ClientMismatch,
+    DirectoryNotEmpty,
+    InvalidPrefix,
+    MissingDependencies,
+    OverwriteDirtyFile,
+    OverwriteNewerLocal,
+)
+
+__all__ = [
+    "AzureBlobClient",
+    "AzureBlobPath",
+    "ClientMismatch",
+    "CloudPath",
+    "DirectoryNotEmpty",
+    "InvalidPrefix",
+    "MissingDependencies",
+    "OverwriteDirtyFile",
+    "OverwriteNewerLocal",
+    "S3Client",
+    "S3Path",
+]
