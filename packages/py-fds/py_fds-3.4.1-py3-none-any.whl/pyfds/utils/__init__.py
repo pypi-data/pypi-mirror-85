a75bb33c9868a6707a14eb9feeb7223f333f7b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package py-fds/3.4.1/py_fds-3.4.1-py3-none-any.whl

---
+++ packages/py-fds/py_fds-3.4.1-py3-none-any.whl/pyfds/utils/__init__.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+from .node import Node
+from .dnode import Node
+from .pair import Pair
+
+
+def change(node1, node2):
+    bid = node1.data
+    node1.data = node2.data
+    node2.data = bid
+    del bid
+=======
+from .node import Node
+from .dnode import Node
+from .tnode import Node
+from .pair import Pair
+
+
+def change(node1, node2):
+    bid = node1.data
+    node1.data = node2.data
+    node2.data = bid
+    del bid
+>>>>>>> pyfds
