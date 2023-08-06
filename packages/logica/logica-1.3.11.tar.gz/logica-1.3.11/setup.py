03422e74de630443c066e1f3d486f7267a58e7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:45 +0000

    Add package logica/1.3.11/logica-1.3.11.tar.gz

---
+++ packages/logica/logica-1.3.11.tar.gz/logica-1.3.11/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("logica/README.md", "r") as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name = "logica",
+  version = "1.3.11",
+  author = "Evgeny Skvortsov",
+  author_email = "logica@evgeny.ninja",
+  description = "Logica language.",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url="https://github.com/evgskv/logica",
+  packages=setuptools.find_namespace_packages(),
+  classifiers = [
+      "Topic :: Database",
+      "License :: OSI Approved :: Apache Software License"
+  ],
+  entry_points = {
+    'console_scripts': ['logica=logica.logica:run_main']
+  },
+  python_requires= ">=3.0"
+)
+
