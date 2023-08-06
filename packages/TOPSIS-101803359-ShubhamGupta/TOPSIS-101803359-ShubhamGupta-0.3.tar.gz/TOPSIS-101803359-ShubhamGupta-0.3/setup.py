b1e648db0e808bf255fcc36bff74a1cd9e53cd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:13 +0000

    Add package TOPSIS-101803359-ShubhamGupta/0.3/TOPSIS-101803359-ShubhamGupta-0.3.tar.gz

---
+++ packages/TOPSIS-101803359-ShubhamGupta/TOPSIS-101803359-ShubhamGupta-0.3.tar.gz/TOPSIS-101803359-ShubhamGupta-0.3/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+setup(name="TOPSIS-101803359-ShubhamGupta",
+    version="0.3",
+    description="This is code to find topsis score.",
+    long_description="A python package to find topsis score."
+                     "\nTOPSIS-101803359-Shubham can be run as in command prompt as:"
+                     "\npython topsis.py InputDataFile Weights Impacts ResultFileName",
+    long_description_content_type='text/markdown',
+    author="Shubham Gupta - 101803359",
+    packages=['TOPSIS-101803359-ShubhamGupta'],
+    install_requires=[])
