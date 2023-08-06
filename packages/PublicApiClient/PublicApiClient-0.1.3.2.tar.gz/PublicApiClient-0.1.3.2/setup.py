2e16deff579e6c79cb38fe279c05e1a15be92244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:01 +0000

    Add package PublicApiClient/0.1.3.2/PublicApiClient-0.1.3.2.tar.gz

---
+++ packages/PublicApiClient/PublicApiClient-0.1.3.2.tar.gz/PublicApiClient-0.1.3.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='PublicApiClient',
+      version='0.1.3.2',
+      license='MIT',
+      python_requires='>=3',
+      description='Tradernet API PublicApiClient lib for Python 3',
+      long_description='Tradernet API PublicApiClient lib for Python 3',
+      packages=['TradernetPublicApiClient'],
+      author='Mr. TradeBot & Robotrader Jr; Kostikov Alex',
+      url='https://tradernet.com/tradernet-api/public-api-client',
+      author_email='kostikov@tradernet.com',
+      zip_safe=True)
