05bbd04122b116dc0291dfd126d777c14b19c6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:16 +0000

    Add package cf-pretty/0.1.2/cf_pretty-0.1.2.tar.gz

---
+++ packages/cf-pretty/cf_pretty-0.1.2.tar.gz/cf_pretty-0.1.2/cf_pretty/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import fileinput
+from itertools import zip_longest
+
+from rapidtables import print_table
+
+
+def main():
+    lines = fileinput.input()
+
+    header_line = next(lines)
+    while len(header_line) < 10:
+        header_line = next(lines)
+
+    headers = {}
+    for header in [x.strip() for x in header_line.split('  ') if x]:
+        i = header_line.index(header)
+        headers[header] = i
+
+    result = []
+    for line in lines:
+        if len(line) < 1:
+            continue
+        if line.startswith('TIP:'):
+            continue
+
+        values = []
+        for h in headers:
+            x = line[headers[h]:].split('  ')[0].strip()
+            values.append(x)
+
+        new_entry = {header: value.split(', ') for header, value in zip(headers, values)}
+
+        for current in zip_longest(*new_entry.values(), fillvalue=''):
+            result.append({header: value for header, value in zip(headers, current)})
+
+    result.append({})
+
+    print_table(result, tablefmt='md')
+
+
+if __name__ == '__main__':
+    main()
