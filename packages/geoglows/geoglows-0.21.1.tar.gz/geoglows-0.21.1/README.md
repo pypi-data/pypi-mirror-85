d82378693637e47db1ce6e3920a3cb23660dcdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:19 +0000

    Add package geoglows/0.21.1/geoglows-0.21.1.tar.gz

---
+++ packages/geoglows/geoglows-0.21.1.tar.gz/geoglows-0.21.1/README.md
@@ -0,0 +1,21 @@
+# GEOGloWS
+[![Conda Version](https://img.shields.io/conda/vn/conda-forge/geoglows.svg)](https://img.shields.io/conda/vn/conda-forge/geoglows.svg)
+[![Conda Downloads](https://img.shields.io/conda/dn/conda-forge/geoglows.svg)](https://anaconda.org/conda-forge/geoglows)
+[![PyPI](https://img.shields.io/pypi/v/geoglows)](https://pypi.org/project/geoglows)
+
+[Visit the Conda Forge page](https://github.com/conda-forge/geoglows-feedstock)
+```bash
+conda install -c conda-forge geoglows
+```
+
+Also available on pypi
+
+[Visit the PyPi page](https://pypi.org/project/geoglows)
+
+```bash
+pip install geoglows
+```
+
+Tools to access and process data coming from the GEOGloWS initiative.
+
+https://geoglows.readthedocs.io
---
+++ packages/geoglows/geoglows-0.21.1.tar.gz/geoglows-0.21.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open('requirements.txt', 'r') as req:
+    install_requires = req.read().splitlines()
+
+setup(
+    name='geoglows',
+    packages=['geoglows'],
+    version='0.21.1',
+    description='Package for accessing data and APIs developed for the GEOGloWS initiative',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Riley Hales',
+    url='https://geoglows.org',
+    project_urls=dict(Documentation='https://geoglows.readthedocs.io',
+                      Source='https://github.com/BYU-Hydroinformatics/geoglows'),
+    license='BSD 3-Clause',
+    license_family='BSD',
+    package_data={'': ['*.ipynb', '*.html']},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Hydrology',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+    ],
+    install_requires=install_requires
+)
