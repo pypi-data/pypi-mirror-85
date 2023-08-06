fc7c9b8f3c8fa758b7c3af808a05bdbe16cb3030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:38 +0000

    Add package toposis-LuvGupta-101803453/0.1/toposis-LuvGupta--101803453-0.1.tar.gz

---
+++ packages/toposis-LuvGupta-101803453/toposis-LuvGupta--101803453-0.1.tar.gz/toposis-LuvGupta--101803453-0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 20:34:06 2020
+
+@author: HP
+"""
+
+from setuptools import setup
+
+VERSION = '0.1'
+PACKAGE_NAME = 'toposis-LuvGupta--101803453'
+AUTHOR = 'Luv Gupta'
+AUTHOR_EMAIL = 'lgupta1_be18@thapar.edu'
+
+LICENSE = 'Apache License 2.0'
+
+DESCRIPTION = 'TOPSIS is an algorithm to determine the best choice out of many using Positive Ideal Solution and Negative Ideal'
+
+INSTALL_REQUIRES = ['numpy', 'pandas']
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      install_requires=INSTALL_REQUIRES,
+      )
