a92b3eaaf3750058b4652fa77a1be8fac7bfc903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package graphite-paper/0.0.2/graphite-paper-0.0.2.tar.gz

---
+++ packages/graphite-paper/graphite-paper-0.0.2.tar.gz/graphite-paper-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("readme.md", "r") as fh:
+        long_description = fh.read()
+
+setup(
+    name='graphite-paper',
+    version='0.0.2',
+    description='Graphite Paper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://www.impactdistillery.com/graphite',
+    author='Larissa Wunderlich and Marcel Hebing',
+    author_email='info@impactdistillery.com',
+    license='None',
+    packages=['graphite_paper'],
+    python_requires='>=3.6',
+    install_requires=[
+        "Django",
+        "Jinja2",
+        "Markdown",
+        "MarkupSafe",
+        "numpy",
+        "pandas",
+        "python-dateutil",
+        "pytz",
+        "PyYAML",
+        "six",
+    ],
+)
