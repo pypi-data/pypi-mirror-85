57beaef9bc720b32f6aff13d78aa88ee99de42a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:17 +0000

    Add package dyspatch-python/6.0.0/dyspatch_python-6.0.0-py3-none-any.whl

---
+++ packages/dyspatch-python/dyspatch_python-6.0.0-py3-none-any.whl/dyspatch_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from dyspatch_client.api.drafts_api import DraftsApi
+from dyspatch_client.api.localizations_api import LocalizationsApi
+from dyspatch_client.api.templates_api import TemplatesApi
