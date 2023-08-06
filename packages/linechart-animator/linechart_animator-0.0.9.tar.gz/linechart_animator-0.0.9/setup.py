143c2e104498778cdf33e4469f09bca657410e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:57 +0000

    Add package linechart-animator/0.0.9/linechart_animator-0.0.9.tar.gz

---
+++ packages/linechart-animator/linechart_animator-0.0.9.tar.gz/linechart_animator-0.0.9/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="linechart_animator",
+    version="0.0.9",
+    author="Péntek Zsolt",
+    description="Create animated plots",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zselter07/linechart_animator",
+    packages=setuptools.find_packages(),
+    install_requires=["scipy", "numpy", "matplotlib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
