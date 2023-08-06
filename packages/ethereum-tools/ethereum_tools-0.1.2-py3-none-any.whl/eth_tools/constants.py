da388f3d304ac18425063d6af5a4e0d78bd5fa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:05 +0000

    Add package ethereum-tools/0.1.2/ethereum_tools-0.1.2-py3-none-any.whl

---
+++ packages/ethereum-tools/ethereum_tools-0.1.2-py3-none-any.whl/eth_tools/constants.py
@@ -0,0 +1,56 @@
+DEFAULT_BLOCK_FIELDS = [
+    "number",
+    "hash",
+    "gasUsed",
+    "gasLimit",
+    "miner",
+    "timestamp",
+    "sha3Uncles",
+    "difficulty",
+    "totalDifficulty",
+    "size",
+    "extraData",
+    "receiptsRoot",
+    "stateRoot",
+    "transactions_count",
+]
+
+ETHERSCAN_TRANSACTION_KEYS = [
+    "blockNumber",
+    "timeStamp",
+    "hash",
+    "nonce",
+    "blockHash",
+    "transactionIndex",
+    "from",
+    "to",
+    "value",
+    "gas",
+    "gasPrice",
+    "isError",
+    "txreceipt_status",
+    "input",
+    "contractAddress",
+    "cumulativeGasUsed",
+    "gasUsed",
+    "confirmations",
+]
+
+ETHERSCAN_INTERNAL_TRANSACTION_KEYS = [
+    "blockNumber",
+    "timeStamp",
+    "hash",
+    "from",
+    "to",
+    "value",
+    "contractAddress",
+    "input",
+    "type",
+    "gas",
+    "gasUsed",
+    "traceId",
+    "isError",
+    "errCode",
+]
+
+LOG_FORMAT = '%(asctime)-15s - %(levelname)s - %(message)s'
