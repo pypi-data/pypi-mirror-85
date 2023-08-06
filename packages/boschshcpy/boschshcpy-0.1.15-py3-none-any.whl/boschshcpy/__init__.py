a2710493fee6e941ca3a83e705d153a3a2efded4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:26 +0000

    Add package boschshcpy/0.1.15/boschshcpy-0.1.15-py3-none-any.whl

---
+++ packages/boschshcpy/boschshcpy-0.1.15-py3-none-any.whl/boschshcpy/__init__.py
@@ -0,0 +1,22 @@
+from .device import SHCDevice
+from .device_helper import (
+    SHCBatteryDevice,
+    SHCCameraEyes,
+    SHCClimateControl,
+    SHCDeviceHelper,
+    SHCIntrusionDetectionSystem,
+    SHCMotionDetector,
+    SHCShutterContact,
+    SHCShutterControl,
+    SHCSmartPlug,
+    SHCSmokeDetector,
+    SHCThermostat,
+    SHCTwinguard,
+    SHCUniversalSwitch,
+    SHCWallThermostat,
+)
+from .scenario import SHCScenario
+from .session import SHCSession
+from .information import SHCInformation
+
+from .exceptions import SHCAuthenticationError, SHCConnectionError, SHCmDNSError
