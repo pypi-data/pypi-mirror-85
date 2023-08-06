646fc7ecc29c58bd53d289877e98ea548e04eff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:07 +0000

    Add package kenoAPI/2.0.0/kenoAPI-2.0.0.tar.gz

---
+++ packages/kenoAPI/kenoAPI-2.0.0.tar.gz/kenoAPI-2.0.0/README.md
@@ -0,0 +1,27 @@
+# Keno API - Unofficial Keno API
+
+## What is it?
+kenoAPI is Python package that provides data from the offical [Keno](https://www.keno.com.au/) website.
+
+## Main Features
+* Monitor Draws in real time.
+* Real-time Jackpot's.
+* Monitor Current Patterns (Trends, Hot And Cold Numbers)
+
+## Where to get it
+```
+pip install kenoAPI
+```
+
+## Dependencies
+* [Requests](https://github.com/psf/requests)
+* [Pandas](https://github.com/pandas-dev/pandas)
+* Datetime
+
+## License
+[BSD 3](https://github.com/JGolafshan/KenoAPI/blob/main/LICENSE)
+
+This software has been developed for educational use only. Use at your own risk.
+
+## Documentation
+[Wiki / Documentation](https://github.com/JGolafshan/KenoAPI/wiki)
---
+++ packages/kenoAPI/kenoAPI-2.0.0.tar.gz/kenoAPI-2.0.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kenoAPI",
+    version="2.0.0",
+    author="Joshua David Golafshan",
+    description="Python-based API used to fetch real-time and historical data from the Australian Keno website.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/JGolafshan/KenoAPI",
+    keywords=["keno API", "keno", "algo", "algobetting"],
+    packages=["keno"],
+    install_requires=[
+        'datetime',
+        'pandas',
+        'requests',
+    ],
+    package_dir=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
+
+
+"""
+python setup.py sdist
+python setup.py bdist_wheel
+
+2: twine upload --skip-existing dist/*
+"""
