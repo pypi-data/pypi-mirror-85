720541809581b9c28a37621e02771a45797aa407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:12 +0000

    Add package JPRBH/0.0.6/JPRBH-0.0.6-py3-none-any.whl

---
+++ packages/JPRBH/JPRBH-0.0.6-py3-none-any.whl/JPRBH/__init__.py
@@ -0,0 +1,9 @@
+import asyncio
+from JPRBH.initialize import initialize
+from JPRBH.functions import oldest_album, average_price
+
+async def main():
+    await initialize()
+
+if __name__ == "__main__":
+	asyncio.run(main())
---
+++ packages/JPRBH/JPRBH-0.0.6-py3-none-any.whl/JPRBH/functions.py
@@ -0,0 +1,24 @@
+import sqlite3
+from contextlib import closing
+
+def oldest_album(artist_name):
+    """
+    oldest_album(artist_name): Return the name of the oldest album
+    that the given artist has produced, and the release date.
+    """
+    with closing(sqlite3.connect("itunes.db")) as connection:
+        with closing(connection.cursor()) as cursor:
+            rows = cursor.execute("""SELECT collectionName, releaseDate FROM albums WHERE artistName = ? ORDER BY releaseDate ASC LIMIT 1""", (artist_name,)).fetchall()
+            return rows
+
+
+def average_price(artist_name):
+    """
+    average_price(artist_name): Return the average price in USD for
+    the artist's albums
+    """
+    with closing(sqlite3.connect("itunes.db")) as connection:
+        with closing(connection.cursor()) as cursor:
+            prices = [row[0] for row in cursor.execute("SELECT collectionPrice FROM albums WHERE artistName = ?", (artist_name,)).fetchall()]
+            avg = sum(prices)/len(prices)
+            return avg
