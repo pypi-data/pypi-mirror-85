3dacd66f3ea33ac0b8071395e5cb8ced33cf7522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:17 +0000

    Add package pmxbot/1122.10.0/pmxbot-1122.10.0-py3-none-any.whl

---
+++ packages/pmxbot/pmxbot-1122.10.0-py3-none-any.whl/pmxbot/__main__.py
@@ -0,0 +1,4 @@
+import pmxbot.core
+
+if __name__ == '__main__':
+    pmxbot.core.run()
---
+++ packages/pmxbot/pmxbot-1122.10.0-py3-none-any.whl/tests/unit/test_util.py
@@ -0,0 +1,16 @@
+import pytest
+
+from pmxbot import util
+
+
+@pytest.mark.xfail(reason="Wordnik is unreliable")
+def test_lookup(needs_wordnik):
+    assert util.lookup('dachshund') is not None
+
+
+def test_emergency_compliment(needs_internet):
+    assert util.load_emergency_compliments()
+
+
+def test_acronym_lookup(needs_internet):
+    assert util.lookup_acronym('NSFW')
