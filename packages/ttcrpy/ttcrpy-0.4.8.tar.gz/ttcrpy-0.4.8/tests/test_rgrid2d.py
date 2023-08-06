7a0c83a9356e3ef4c9b7f841810d31c0f8e5a2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package ttcrpy/0.4.8/ttcrpy-0.4.8.tar.gz

---
+++ packages/ttcrpy/ttcrpy-0.4.8.tar.gz/ttcrpy-0.4.8/tests/test_rgrid2d.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""Tests for verifying python wrappers, module rgrid in 2D"""
+
+import unittest
+
+import numpy as np
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+
+import ttcrpy.rgrid as rg
+
+
+def get_tt(filename):
+    reader = vtk.vtkXMLRectilinearGridReader()
+    reader.SetFileName(filename)
+    reader.Update()
+    data = reader.GetOutput()
+    x = vtk_to_numpy(data.GetXCoordinates())
+    z = vtk_to_numpy(data.GetZCoordinates())
+    names = ('Travel Time', 'Travel time', 'travel time')
+    for name in names:
+        if data.GetPointData().HasArray(name):
+            break
+    tt = vtk_to_numpy(data.GetPointData().GetArray(name))
+    dim = (x.size, z.size)
+    return tt.reshape(dim, order='F').flatten()
+
+
+class TestGrid2dc(unittest.TestCase):
+
+    def setUp(self):
+        reader = vtk.vtkXMLRectilinearGridReader()
+        reader.SetFileName('layers_coarse2d.vtr')
+        reader.Update()
+
+        data = reader.GetOutput()
+        self.x = vtk_to_numpy(data.GetXCoordinates())
+        self.z = vtk_to_numpy(data.GetZCoordinates())
+
+        self.slowness = vtk_to_numpy(data.GetCellData().GetArray('Slowness'))
+
+        self.src = np.loadtxt('src2d.dat',skiprows=1)
+        self.src = self.src.reshape((1, 3))
+        self.rcv = np.loadtxt('rcv2d.dat',skiprows=1)
+
+    def test_Grid2Dfs(self):
+        g = rg.Grid2d(self.x, self.z, method='FSM')
+        tt = g.raytrace(self.src, self.rcv, slowness=self.slowness)
+        tt = g.get_grid_traveltimes()
+        tt = tt.flatten()
+        tt_ref = get_tt('fsm2d_d_p_lc_src2d_all_tt.vtr')
+        self.assertLess(np.sum(np.abs(tt-tt_ref))/tt.size, 0.01,
+                        'FSM accuracy failed (slowness in cells)')
+
+    def test_Grid2Dsp(self):
+        g = rg.Grid2d(self.x, self.z, method='SPM', nsnx=5, nsnz=5)
+        tt = g.raytrace(self.src, self.rcv, slowness=self.slowness)
+        tt = g.get_grid_traveltimes()
+        tt = tt.flatten()
+        tt_ref = get_tt('spm2d_d_p_lc_05_src2d_all_tt.vtr')
+        self.assertLess(np.sum(np.abs(tt-tt_ref))/tt.size, 0.01,
+                        'SPM accuracy failed (slowness in cells)')
+
+
+class TestGrid2dn(unittest.TestCase):
+
+    def setUp(self):
+        reader = vtk.vtkXMLRectilinearGridReader()
+        reader.SetFileName('gradient_coarse2d.vtr')
+        reader.Update()
+
+        data = reader.GetOutput()
+        self.x = vtk_to_numpy(data.GetXCoordinates())
+        self.z = vtk_to_numpy(data.GetZCoordinates())
+
+        self.slowness = vtk_to_numpy(data.GetPointData().GetArray('Slowness'))
+
+        self.src = np.loadtxt('src2d.dat',skiprows=1)
+        self.src = self.src.reshape((1, 3))
+        self.rcv = np.loadtxt('rcv2d.dat',skiprows=1)
+
+    def test_Grid2Dfs(self):
+        g = rg.Grid2d(self.x, self.z, method='FSM', cell_slowness=0)
+        tt = g.raytrace(self.src, self.rcv, slowness=self.slowness)
+        tt = g.get_grid_traveltimes()
+        tt = tt.flatten()
+        tt_ref = get_tt('fsm2d_d_p_gc_src2d_all_tt.vtr')
+        self.assertLess(np.sum(np.abs(tt-tt_ref))/tt.size, 0.01,
+                        'FSM accuracy failed (slowness at nodes)')
+
+    def test_Grid2Dsp(self):
+        g = rg.Grid2d(self.x, self.z, method='SPM', nsnx=5, nsnz=5, cell_slowness=0)
+        tt = g.raytrace(self.src, self.rcv, slowness=self.slowness)
+        tt = g.get_grid_traveltimes()
+        tt = tt.flatten()
+        tt_ref = get_tt('spm2d_d_p_gc_05_src2d_all_tt.vtr')
+        self.assertLess(np.sum(np.abs(tt-tt_ref))/tt.size, 0.01,
+                        'SPM accuracy failed (slowness at nodes)')
+
+
+class Data_kernel(unittest.TestCase):
+
+    def test_2d(self):
+
+        V = np.ones((11, 13))
+        V[:, 7:] = 2
+        slowness = 1. / V.flatten()
+
+        grx = np.arange(12.)
+        grz = np.arange(14.)
+
+        z = 0.5 + np.arange(13.)
+        Tx = np.vstack((0.5+np.zeros((13,)),
+                        z)).T
+        Rx = np.vstack((10.5+np.zeros((13,)),
+                        z)).T
+        nTx = Tx.shape[0]
+        nRx = Rx.shape[0]
+        Tx = np.kron(Tx, np.ones((nRx,1)))
+        Rx = np.kron(np.ones((nTx,1)), Rx)
+
+        L = rg.Grid2d.data_kernel_straight_rays(Tx, Rx, grx, grz)
+        tt = L.dot(slowness)
+
+        tt2 = np.zeros(tt.shape)
+        d = np.sqrt(np.sum((Tx-Rx)**2, axis=1))
+
+        ind = np.logical_and(Tx[:,1]>7, Rx[:,1]>7)
+        tt2[ind] = d[ind]/2
+
+        ind2 = np.logical_and(Tx[:,1]<7, Rx[:,1]<7)
+        tt2[ind2] = d[ind2]
+
+        ind3 = np.logical_and(np.logical_not(ind), np.logical_not(ind2))
+
+        f = (7-Tx[ind3,1]) / (Rx[ind3,1]-Tx[ind3,1])
+        ind = (Rx[ind3,1]-Tx[ind3,1]) < 0
+        f[ind] = 1-f[ind]
+        tt2[ind3] = d[ind3]*f + d[ind3]*(1-f)/2
+
+        self.assertAlmostEqual(np.sum(np.abs(tt-tt2)), 0.0 )
+
+if __name__ == '__main__':
+
+    unittest.main()
---
+++ packages/ttcrpy/ttcrpy-0.4.8.tar.gz/ttcrpy-0.4.8/ttcr/Grid2Drc.h
@@ -0,0 +1,314 @@
+//
+//  Grid2Drc.h
+//  ttcr
+//
+//  Created by Bernard Giroux on 15-12-23.
+//  Copyright © 2015 Bernard Giroux. All rights reserved.
+//
+
+#ifndef Grid2Drc_h
+#define Grid2Drc_h
+
+#include <algorithm>
+#include <cstring>
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#ifdef VTK
+#include "vtkDoubleArray.h"
+#include "vtkPointData.h"
+#include "vtkRectilinearGrid.h"
+#include "vtkSmartPointer.h"
+#include "vtkXMLRectilinearGridWriter.h"
+#endif
+
+#include "Grid2D.h"
+
+namespace ttcr {
+    
+    template<typename T1, typename T2, typename S, typename NODE, typename CELL>
+    class Grid2Drc : public Grid2D<T1,T2,S> {
+    public:
+        Grid2Drc(const T2 nx, const T2 nz, const T1 ddx, const T1 ddz,
+                 const T1 minx, const T1 minz, const size_t nt=1);
+        
+        virtual ~Grid2Drc() {
+        }
+        
+        void getSlowness(std::vector<T1>& slowness) const {
+            if (slowness.size() != ncx*ncz) {
+                slowness.resize(ncx*ncz);
+            }
+            for (size_t n=0; n<slowness.size(); ++n) {
+                slowness[n] = cells.getSlowness(n);
+            }
+        }
+        
+        void setSlowness(const std::vector<T1>& s) {
+            try {
+                cells.setSlowness( s );
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setXi(const std::vector<T1>& x) {
+            try {
+                cells.setXi( x );
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setTiltAngle(const std::vector<T1>& t) {
+            try {
+                cells.setTiltAngle( t );
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setVp0(const std::vector<T1>& s) {
+            try {
+                cells.setVp0(s);
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setVs0(const std::vector<T1>& s) {
+            try {
+                cells.setVs0(s);
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setDelta(const std::vector<T1>& s) {
+            try {
+                cells.setDelta(s);
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setEpsilon(const std::vector<T1>& s) {
+            try {
+                cells.setEpsilon(s);
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+        void setGamma(const std::vector<T1>& s) {
+            try {
+                cells.setGamma(s);
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+
+        size_t getNumberOfNodes(const bool primary=false) const {
+            if ( primary ) {
+                return (ncx+1) * (ncz+1);
+            } else {
+                return nodes.size();
+            }
+        }
+        size_t getNumberOfCells() const { return ncx*ncz; }
+        
+        void getTT(std::vector<T1>& tt, const size_t threadNo=0) const {
+            size_t nPrimary = (ncx+1) * (ncz+1);
+            tt.resize(nPrimary);
+            size_t n = 0;
+            for ( size_t nn=0; nn<nodes.size(); ++nn ) {
+                if ( nodes[nn].isPrimary() ) {
+                    tt[n++] = nodes[nn].getTT(threadNo);
+                }
+            }
+        }
+
+        T2 getCellNo(const S& pt) const {
+            T1 x = xmax-pt.x < small ? xmax-.5*dx : pt.x;
+            T1 z = zmax-pt.z < small ? zmax-.5*dz : pt.z;
+            T2 nx = static_cast<T2>( small + (x-xmin)/dx );
+            T2 nz = static_cast<T2>( small + (z-zmin)/dz );
+            return nx*ncz + nz;
+        }
+        
+        void saveSlownessXYZ(const char filename[]) const {
+            std::ofstream fout( filename );
+            
+            for ( T2 j=0, n=0; j<ncx; ++j ) {
+                T1 x = xmin + (0.5+j)*dx;
+                for ( T2 i=0; i<ncz; ++i, ++n ) {
+                    T1 z = zmin + (0.5+i)*dz;
+                    fout << x << "   " << z << "   " << cells.getSlowness(n) << '\n';
+                }
+            }
+            
+            fout.close();
+        }
+        
+        void saveTT(const std::string &, const int, const size_t nt=0,
+                    const int format=1) const;
+        
+        void saveTTgrad(const std::string &, const size_t nt=0,
+                        const bool vtkFormat=0) const {}
+        
+        const T1 getXmin() const { return xmin; }
+        const T1 getZmin() const { return zmin; }
+        const T1 getDx() const { return dx; }
+        const T1 getDz() const { return dz; }
+        const T2 getNcx() const { return ncx; }
+        const T2 getNcz() const { return ncz; }
+        
+        void dump_secondary(std::ofstream& os) const {
+            size_t nPrimary = (ncx+1) * (ncz+1);
+            for ( size_t n=nPrimary; n<nodes.size(); ++n ) {
+                os << nodes[n].getX() << ' ' << nodes[n].getZ() << '\n';
+            }
+        }
+
+    protected:
+        T1 dx;           // cell size in x
+        T1 dz;           // cell size in z
+        T1 xmin;         // x origin of the grid
+        T1 zmin;         // z origin of the grid
+        T1 xmax;         // x end of the grid
+        T1 zmax;         // z end of the grid
+        T2 ncx;          // number of cells in x
+        T2 ncz;          // number of cells in z
+        
+        mutable std::vector<NODE> nodes;
+        
+        CELL cells;   // column-wise (z axis) slowness vector of the cells
+               
+        void checkPts(const std::vector<S>&) const;
+        
+        bool inPolygon(const S& p, const S poly[], const size_t N) const;
+        
+        
+    };
+    
+    template<typename T1, typename T2, typename S, typename NODE, typename CELL>
+    Grid2Drc<T1,T2,S,NODE,CELL>::Grid2Drc(const T2 nx, const T2 nz,
+                                          const T1 ddx, const T1 ddz,
+                                          const T1 minx, const T1 minz,
+                                          const size_t nt) :
+    Grid2D<T1,T2,S>(nx*nz, nt),
+    dx(ddx), dz(ddz), xmin(minx), zmin(minz),
+    xmax(minx+nx*ddx), zmax(minz+nz*ddz),
+    ncx(nx), ncz(nz),
+    nodes(std::vector<NODE>( (ncx+1) * (ncz+1), NODE(nt) )),
+    cells(ncx*ncz)
+    { }
+    
+    
+    template<typename T1, typename T2, typename S, typename NODE, typename CELL>
+    void Grid2Drc<T1,T2,S,NODE,CELL>::saveTT(const std::string& fname,
+                                             const int all,
+                                             const size_t nt,
+                                             const int format) const {
+        
+        if ( format == 1 ) {
+            std::string filename = fname+".dat";
+            std::ofstream fout(filename.c_str());
+            fout.precision(12);
+            for ( T2 n=0; n<nodes.size(); ++n ) {
+                fout << nodes[n].getX() << '\t'
+                << nodes[n].getZ() << '\t'
+                << nodes[n].getTT(nt) << '\n';
+            }
+            fout.close();
+        } else  if ( format == 2 ) {
+#ifdef VTK
+            
+            std::string filename = fname+".vtr";
+            int nn[3] = {static_cast<int>(ncx+1), 1, static_cast<int>(ncz+1)};
+            
+            vtkSmartPointer<vtkDoubleArray> xCoords = vtkSmartPointer<vtkDoubleArray>::New();
+            for (size_t n=0; n<nn[0]; ++n)
+                xCoords->InsertNextValue( xmin + n*dx );
+            vtkSmartPointer<vtkDoubleArray> yCoords = vtkSmartPointer<vtkDoubleArray>::New();
+            yCoords->InsertNextValue( 0.0 );
+            vtkSmartPointer<vtkDoubleArray> zCoords = vtkSmartPointer<vtkDoubleArray>::New();
+            for (size_t n=0; n<nn[2]; ++n)
+                zCoords->InsertNextValue( zmin + n*dz );
+            
+            vtkSmartPointer<vtkRectilinearGrid> rgrid = vtkSmartPointer<vtkRectilinearGrid>::New();
+            rgrid->SetDimensions( nn );
+            rgrid->SetXCoordinates(xCoords);
+            rgrid->SetYCoordinates(yCoords);
+            rgrid->SetZCoordinates(zCoords);
+            
+            
+            vtkSmartPointer<vtkDoubleArray> newScalars =
+            vtkSmartPointer<vtkDoubleArray>::New();
+            
+            newScalars->SetName("Travel time");
+            newScalars->SetNumberOfComponents(1);
+            newScalars->SetNumberOfTuples( rgrid->GetNumberOfPoints() );
+            
+            
+            for ( size_t n=0; n<nodes.size(); ++n ) {
+                if ( nodes[n].isPrimary() ) {
+                    vtkIdType id = rgrid->FindPoint(nodes[n].getX(), 0.0, nodes[n].getZ());
+                    newScalars->SetTuple1(id, nodes[n].getTT(nt) );
+                }
+            }
+            rgrid->GetPointData()->SetScalars(newScalars);
+            
+            vtkSmartPointer<vtkXMLRectilinearGridWriter> writer =
+            vtkSmartPointer<vtkXMLRectilinearGridWriter>::New();
+            
+            writer->SetFileName( filename.c_str() );
+            writer->SetInputData( rgrid );
+            writer->SetDataModeToBinary();
+            writer->Update();
+#else
+            std::cerr << "VTK not included during compilation.\nNothing saved.\n";
+#endif
+        } else if ( format == 3 ) {
+            std::string filename = fname+".bin";
+            std::ofstream fout(filename.c_str(), std::ios::out | std::ios::binary | std::ios::trunc);
+            for ( T2 n=0; n<nodes.size(); ++n ) {
+                T1 tmp[] = { nodes[n].getX(), nodes[n].getZ(), nodes[n].getTT(nt) };
+                fout.write( (char*)tmp, 3*sizeof(T1) );
+            }
+            fout.close();
+        } else {
+            throw std::runtime_error("Unsupported format for saving traveltimes");
+        }
+    }
+    
+    
+    template<typename T1, typename T2, typename S, typename NODE, typename CELL>
+    void Grid2Drc<T1,T2,S,NODE,CELL>::checkPts(const std::vector<S>& pts) const {
+        for (size_t n=0; n<pts.size(); ++n) {
+            if ( pts[n].x < xmin || pts[n].x > xmax ||
+                pts[n].z < zmin || pts[n].z > zmax ) {
+                std::ostringstream msg;
+                msg << "Error: Point (" << pts[n].x << ", "<< pts[n] .z << ") outside grid.";
+                throw std::runtime_error(msg.str());
+            }
+        }
+    }
+    
+    
+    template<typename T1, typename T2, typename S, typename NODE, typename CELL>
+    bool Grid2Drc<T1,T2,S,NODE,CELL>::inPolygon(const S& p, const S poly[],
+                                                const size_t N) const {
+        bool c = false;
+        for (size_t i = 0, j = N-1; i < N; j = i++) {
+            if ((((poly[i].z <= p.z) && (p.z < poly[j].z)) ||
+                 ((poly[j].z <= p.z) && (p.z < poly[i].z))) &&
+                (p.x < (poly[j].x - poly[i].x) * (p.z - poly[i].z) / (poly[j].z - poly[i].z) + poly[i].x))
+                c = !c;
+        }
+        return c;
+    }
+    
+}
+
+
+
+#endif /* Grid2Drc_h */
