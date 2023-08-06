b604ae3307599589117275bc338c160d916a410d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:11 +0000

    Add package skill-sdk/0.9.0/skill_sdk-0.9.0-py3-none-any.whl

---
+++ packages/skill-sdk/skill_sdk-0.9.0-py3-none-any.whl/skill_sdk/__init__.py
@@ -0,0 +1,32 @@
+""" Magenta skill SDK for Python """
+
+import importlib
+import contextlib
+from typing import Any, Callable, ContextManager
+
+from .skill import Skill, initialize, intent_handler
+from .intents import Context, context
+from .sessions import Session
+from .responses import Card, Response, Reprompt, ask, tell, ask_freetext
+
+
+def lazy_load(module: str, attr: str) -> Callable[..., ContextManager]:
+    """ Lazy load a context manager from module, return `contextlib.suppress` (no-op) if loading failed
+
+    :param module:  Module name
+    :param attr:    Context manager name
+    :return:
+    """
+    def wrapper(*args) -> ContextManager:
+        """ Get a context manager """
+
+        try:
+            cls = getattr(importlib.import_module(module, __package__), attr)
+        except (AttributeError, ModuleNotFoundError):
+            cls = contextlib.suppress
+        return cls(*args)
+    return wrapper
+
+
+K8sChecks = lazy_load('.services.k8s', 'K8sChecks')
+RequiredForReadiness = lazy_load('.services.k8s', 'required_for_readiness')
