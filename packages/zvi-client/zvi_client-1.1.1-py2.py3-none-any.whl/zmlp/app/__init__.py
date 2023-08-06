da2779420b28d8fa526fe4a005b2ea46b552b7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:36 +0000

    Add package zvi-client/1.1.1/zvi_client-1.1.1-py2.py3-none-any.whl

---
+++ packages/zvi-client/zvi_client-1.1.1-py2.py3-none-any.whl/zmlp/app/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .asset_app import AssetApp
+from .datasource_app import DataSourceApp
+from .project_app import ProjectApp
+from .job_app import JobApp
+from .model_app import ModelApp
+from .pmod_app import PipelineModApp
+from .clip_app import ClipApp
