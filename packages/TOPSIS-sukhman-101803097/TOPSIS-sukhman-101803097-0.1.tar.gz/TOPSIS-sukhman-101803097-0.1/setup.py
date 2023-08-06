32ff290a9aa1181ccabe362f334d33d5194182c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package TOPSIS-sukhman-101803097/0.1/TOPSIS-sukhman-101803097-0.1.tar.gz

---
+++ packages/TOPSIS-sukhman-101803097/TOPSIS-sukhman-101803097-0.1.tar.gz/TOPSIS-sukhman-101803097-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'TOPSIS-sukhman-101803097',         
+  packages = ['TOPSIS-sukhman-101803097'],   
+  version = '0.1',    
+  license='MIT',        
+  description = 'Topsis implementation for ranking the parameters of a module',   
+  author = 'Sukhman',              
+  author_email = 'sukhmanpabla05@gmail.com',
+ 
+  
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
