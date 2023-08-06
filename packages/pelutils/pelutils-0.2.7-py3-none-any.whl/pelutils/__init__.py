979184f48a2241b7f9fe1357f74c4d0fc9e93e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package pelutils/0.2.7/pelutils-0.2.7-py3-none-any.whl

---
+++ packages/pelutils/pelutils-0.2.7-py3-none-any.whl/pelutils/__init__.py
@@ -0,0 +1,52 @@
+import random
+from datetime import datetime
+
+import numpy as np
+try:
+	import torch
+	_has_torch = True
+except ModuleNotFoundError:
+	_has_torch = False
+
+try:
+	import git
+	_has_git = True
+except ModuleNotFoundError:
+	_has_git = False
+
+def set_seeds(seed: int = 0) -> int:
+	np.random.seed(seed)
+	random.seed(seed)
+	if _has_torch:
+		torch.manual_seed(seed)
+		torch.cuda.manual_seed(seed)
+		torch.cuda.manual_seed_all(seed)
+		torch.backends.cudnn.deterministic = True
+		torch.backends.cudnn.benchmark = False
+	return seed
+
+def get_commit() -> str:
+	if _has_git:
+		repo = git.Repo(".")  # TODO: Search upwards in directories
+		return str(repo.head.commit)
+	return None
+
+def get_timestamp(for_file: bool = False, include_micros = False) -> str:
+	"""
+	Returns a time stamp
+	If for_file is true, it can be used to save files and: YYYY-MM-DD_HH-mm-SS
+	Else the timestamp will be YYYY-MM-DD HH:mm:SS:milliseconds
+	Set include_micros to include microseconds in time stamp (only if for_file is false)
+	Returns a time stamp for current time either in datetime format or, if for_file, in YYYY-MM-DD_HH-MM-SS
+	"""
+	d_string = str(datetime.now()) if include_micros else str(datetime.now())[:-3]
+	if for_file:
+		d_string = "-".join(d_string.split(".")[0].split(":")).replace(" ", "_")
+	return d_string
+
+# To allow imports directly from utils #
+# Currently to be placed lower because get_timestamp is needed by logger #
+from .logger import *
+from .parse import *
+from .ticktock import *
+from .datahandling import *
