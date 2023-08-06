d931ac13ea55960303f7492784641f766cd7953c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:25 +0000

    Add package TOPSIS-parteekpal-101803190/0.1/TOPSIS-parteekpal-101803190-0.1.tar.gz

---
+++ packages/TOPSIS-parteekpal-101803190/TOPSIS-parteekpal-101803190-0.1.tar.gz/TOPSIS-parteekpal-101803190-0.1/setup.py
@@ -0,0 +1,4 @@
+from sys import version
+from setuptools import _install_setup_requires, setup
+
+setup(name='TOPSIS-parteekpal-101803190',version='0.1',packages=['topsis'],install_requires=['pandas','numpy'])
