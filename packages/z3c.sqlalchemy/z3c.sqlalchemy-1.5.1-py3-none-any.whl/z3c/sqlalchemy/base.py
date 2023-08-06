dc04558fc7bcef1563dd8785b52c0a737370dea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:06 +0000

    Add package z3c.sqlalchemy/1.5.1/z3c.sqlalchemy-1.5.1-py3-none-any.whl

---
+++ packages/z3c.sqlalchemy/z3c.sqlalchemy-1.5.1-py3-none-any.whl/z3c/sqlalchemy/base.py
@@ -0,0 +1,145 @@
+##########################################################################
+# z3c.sqlalchemy - A SQLAlchemy wrapper for Python/Zope
+#
+# (C) Zope Corporation and Contributor
+# Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+# and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+##########################################################################
+
+from six import string_types
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import make_url
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from zope.component import getUtility
+from zope.interface import implementer
+from zope.interface.interfaces import ComponentLookupError
+from zope.sqlalchemy import register
+
+from z3c.sqlalchemy.interfaces import IModelProvider
+from z3c.sqlalchemy.interfaces import ISQLAlchemyWrapper
+from z3c.sqlalchemy.mapper import LazyMapperCollection
+from z3c.sqlalchemy.model import Model
+
+
+@implementer(ISQLAlchemyWrapper)
+class ZopeWrapper(object):
+
+    def __init__(self, dsn, model=None, transactional=True, twophase=False,
+                 engine_options={}, session_options={},
+                 extension_options={}, **kw):
+        """ 'dsn' - a RFC-1738-style connection string
+
+            'model' - optional instance of model.Model
+
+            'engine_options' - optional keyword arguments passed to
+            create_engine()
+
+            'session_options' - optional keyword arguments passed to
+            create_session() or sessionmaker()
+
+            'extension_options' - optional keyword argument passed to
+            ZopeTransactionExtension()
+
+            'transactional' - True|False, only used by SQLAlchemyDA,
+                              *don't touch it*
+        """
+
+        self.dsn = dsn
+        self.url = make_url(dsn)
+        self.host = self.url.host
+        self.port = self.url.port
+        self.username = self.url.username
+        self.password = self.url.password
+        self.dbname = self.url.database
+        self.twophase = twophase
+        self.drivername = self.url.drivername
+        self.transactional = transactional
+        self.engine_options = engine_options
+        if 'echo' in kw:
+            self.engine_options.update(echo=kw['echo'])
+        self.session_options = session_options
+        self.extension_options = extension_options
+        self._model = None
+        self._createEngine()
+
+        if model:
+
+            if isinstance(model, Model):
+                self._model = model
+
+            elif isinstance(model, string_types):
+
+                try:
+                    util = getUtility(IModelProvider, model)
+                except ComponentLookupError:
+                    msg = "No named utility '%s' providing IModelProvider"
+                    raise ComponentLookupError(msg % model)
+
+                self._model = util.getModel(self.metadata)
+
+            elif callable(model):
+                self._model = model(self.metadata)
+
+            else:
+                raise ValueError("The 'model' parameter passed to constructor "
+                                 "must either be the name of a named utility "
+                                 "implementing IModelProvider or an instance "
+                                 "of z3c.sqlalchemy.model.Model.")
+
+            if not isinstance(self._model, Model):
+                raise TypeError('_model is not an instance of model.Model')
+
+        # mappers must be initialized at last since we need to acces
+        # the 'model' from within the constructor of LazyMapperCollection
+        self._mappers = LazyMapperCollection(self)
+
+    @property
+    def metadata(self):
+        if not hasattr(self, '_v_metadata'):
+            self._v_metadata = MetaData(self._engine)
+        return self._v_metadata
+
+    @property
+    def session(self):
+        """ Return thread-local session """
+        return self._sessionmaker()
+
+    @property
+    def connection(self):
+        """ Return underlying connection """
+        session = self.session
+        # Return the ConnectionFairy
+        return session.connection().connection
+        # instead of the raw connection
+        # return session.connection().connection.connection
+
+    def registerMapper(self, mapper, name):
+        self._mappers._registerMapper(mapper, name)
+
+    def getMapper(self, tablename, schema='public'):
+        return self._mappers.getMapper(tablename, schema)
+
+    def getMappers(self, *names):
+        return tuple([self.getMapper(name) for name in names])
+
+    @property
+    def engine(self):
+        """ only for private purposes! """
+        return self._engine
+
+    @property
+    def model(self):
+        """ only for private purposes! """
+        return self._model
+
+    def _createEngine(self):
+        self._engine = create_engine(self.dsn, **self.engine_options)
+        self._sessionmaker = scoped_session(sessionmaker(bind=self._engine,
+                                            autocommit=not self.transactional,
+                                            twophase=self.twophase,
+                                            autoflush=True,
+                                            **self.session_options))
+        register(self._sessionmaker)
---
+++ packages/z3c.sqlalchemy/z3c.sqlalchemy-1.5.1-py3-none-any.whl/z3c/sqlalchemy/tests/test_SQLAlchemy.py
@@ -0,0 +1,258 @@
+##########################################################################
+# z3c.sqlalchemy - A SQLAlchemy wrapper for Python/Zope
+#
+# (C) Zope Corporation and Contributor
+# Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+# and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+"""
+Tests, tests, tests.........
+"""
+
+import os
+import tempfile
+import unittest
+
+import sqlalchemy
+import sqlalchemy.orm
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy import exc
+from sqlalchemy.ext.declarative import declarative_base
+
+from zope.interface.verify import verifyClass
+
+from z3c.sqlalchemy import Model
+from z3c.sqlalchemy import createSAWrapper
+from z3c.sqlalchemy import getSAWrapper
+from z3c.sqlalchemy import registerSAWrapper
+from z3c.sqlalchemy.interfaces import IModel
+from z3c.sqlalchemy.interfaces import ISQLAlchemyWrapper
+from z3c.sqlalchemy.mapper import MappedClassBase
+from z3c.sqlalchemy.postgres import ZopePostgresWrapper
+
+
+class WrapperTests(unittest.TestCase):
+
+    def setUp(self):
+
+        self.dsn = os.environ.get('TEST_DSN')
+        self.tempfile = None
+        if not self.dsn:
+            self.tempfile = tempfile.mktemp()
+            self.dsn = 'sqlite:///%s' % self.tempfile
+        self.db = wrapper = createSAWrapper(self.dsn)
+        metadata = MetaData(bind=wrapper.engine)
+
+        Table('users', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('firstname', String(255)),
+              Column('lastname', String(255)))
+
+        Table('skills', metadata,
+              Column('user_id', Integer, primary_key=True),
+              Column('name', String(255)))
+
+        metadata.create_all()
+
+    def tearDown(self):
+        if self.tempfile:
+            os.remove(self.tempfile)
+        else:
+            metadata = MetaData(bind=self.db.engine)
+            metadata.drop_all()
+
+    def testIFaceZopePostgres(self):
+        verifyClass(ISQLAlchemyWrapper, ZopePostgresWrapper)
+
+    def testIModel(self):
+        verifyClass(IModel, Model)
+
+    def testSimplePopulation(self):
+        db = createSAWrapper(self.dsn)
+        # obtain mapper for table 'users'
+
+        User = db.getMapper('users')
+        session = db.session
+
+        rows = session.query(User).all()
+        self.assertEqual(len(rows), 0)
+
+        session.add(User(id=1, firstname='udo', lastname='juergens'))
+        session.add(User(id=2, firstname='heino', lastname='n/a'))
+        session.flush()
+
+        rows = session.query(User).order_by(User.id).all()
+        self.assertEqual(len(rows), 2)
+        row1 = rows[0]
+        d = row1.asDict()
+        self.assertEqual(d, {'firstname': 'udo',
+                             'lastname': 'juergens',
+                             'id': 1})
+
+    def testMapperWithCustomModel(self):
+
+        class myUser(MappedClassBase):
+            pass
+
+        M = Model()
+        M.add('users', mapper_class=myUser)
+
+        db = createSAWrapper(self.dsn, model=M)
+        User = db.getMapper('users')
+        self.assertEqual(User, myUser)
+
+    def testCustomMapperRegister(self):
+        mytable = Table(
+            'mytable', self.db.metadata,
+            Column('id', Integer, primary_key=True),
+        )
+
+        class MyClass(object):
+            pass
+        mapper = sqlalchemy.orm.mapper(MyClass, mytable)
+        self.db.registerMapper(mapper, 'mymapper')
+
+#    def testCustomMapperClassWithWrongType(self):
+#
+#        class myUser(object):
+#            pass
+#
+#        M = Model()
+#        self.assertRaises(TypeError, M.add, 'users', mapper_class=myUser)
+
+    def testGetMappers(self):
+        db = createSAWrapper(self.dsn)
+        db.getMapper('users')
+        db.getMapper('skills')
+        db.getMappers('users', 'skills')
+
+    def testModelWeirdParameters(self):
+        M = Model()
+        self.assertRaises(ValueError,
+                          M.add,
+                          'users',
+                          relations=('foo', 'bar'),
+                          autodetect_relations=True)
+
+    def testModelWeirdRelationsParameters(self):
+        M = Model()
+        self.assertRaises(TypeError, M.add, 'users', relations=('foo'))
+
+    def testModelNonExistingTables(self):
+        M = Model()
+        M.add('non_existing_table')
+        db = createSAWrapper(self.dsn, model=M)
+        try:
+            db.getMapper('non_existing_table')
+        except exc.NoSuchTableError:
+            pass
+
+    def testWrapperRegistration(self):
+        wrapper = createSAWrapper(self.dsn)
+        registerSAWrapper(wrapper, 'test.wrapper1')
+        wrapper2 = getSAWrapper('test.wrapper1')
+        self.assertEqual(wrapper, wrapper2)
+
+    def testWrapperRegistrationFailing(self):
+        createSAWrapper(self.dsn)
+        self.assertRaises(ValueError, getSAWrapper, 'test.wrapperNonExistant')
+
+    def testWrapperDoubleRegistrationFailing(self):
+        wrapper = createSAWrapper(self.dsn)
+        registerSAWrapper(wrapper, 'test.wrapper2')
+        self.assertRaises(ValueError,
+                          registerSAWrapper,
+                          wrapper,
+                          'test.wrapper2')
+
+    def testWrapperDirectRegistration(self):
+        wrapper = createSAWrapper(self.dsn, name='test.wrapper3')
+        wrapper2 = getSAWrapper('test.wrapper3')
+        self.assertEqual(wrapper, wrapper2)
+
+    def testCheckConnection(self):
+        """ Check access to low-level connection """
+        db = createSAWrapper(self.dsn)
+        conn = db.connection
+        cursor = conn.cursor()
+        cursor.execute('select * from users')
+        rows = cursor.fetchall()
+        self.assertEqual(len(rows), 0)
+
+    def testConnectionPlusSession(self):
+        """ Check access to low-level connection """
+
+        User = self.db.getMapper('users')
+        session = self.db.session
+        session.add(User(id=1, firstname='udo', lastname='juergens'))
+        session.add(User(id=2, firstname='heino', lastname='n/a'))
+        session.flush()
+
+        conn = self.db.connection
+        cursor = conn.cursor()
+        cursor.execute('select * from users')
+        rows = cursor.fetchall()
+        self.assertEqual(len(rows), 2)
+
+    def testDeclarative(self):
+
+        db = createSAWrapper(self.dsn)
+        session = db.session
+        metadata = db.metadata
+        Base = declarative_base(metadata=metadata)
+
+        class Foo(Base):
+            __tablename__ = 'foo'
+
+            id = Column('id', Integer, primary_key=True)
+            name = Column('name', String(50))
+
+        Base.metadata.create_all(db._engine)
+
+        session.add(Foo(id=1, name='Andreas Jung'))
+        session.add(Foo(id=2, name='Peter Becker'))
+        session.flush()
+
+        rows = session.query(Foo).all()
+        self.assertEqual(len(rows), 2)
+
+    def testDeclarativeWithModel(self):
+        def getModel(metadata):
+
+            model = Model()
+            Base = declarative_base(metadata=metadata)
+
+            class Foo(Base):
+                __tablename__ = 'foo'
+
+                id = Column('id', Integer, primary_key=True)
+                name = Column('name', String(50))
+
+            model.add('foo', mapper_class=Foo)
+            Base.metadata.create_all()
+            return model
+
+        db = createSAWrapper(self.dsn, model=getModel)
+        session = db.session
+        Foo = db.getMapper('foo')
+
+        session.add(Foo(id=1, name='Andreas Jung'))
+        session.add(Foo(id=2, name='Peter Becker'))
+        session.flush()
+
+        rows = session.query(Foo).all()
+        self.assertEqual(len(rows), 2)
+
+
+def test_suite():
+    from unittest import TestSuite
+    from unittest import makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(WrapperTests))
+    return suite
