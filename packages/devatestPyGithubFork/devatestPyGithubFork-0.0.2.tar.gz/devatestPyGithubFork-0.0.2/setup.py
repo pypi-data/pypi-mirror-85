f2f28e0dd97edbd6a4426609cb3f275aad8eb89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:39 +0000

    Add package devatestPyGithubFork/0.0.2/devatestPyGithubFork-0.0.2.tar.gz

---
+++ packages/devatestPyGithubFork/devatestPyGithubFork-0.0.2.tar.gz/devatestPyGithubFork-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='devatestPyGithubFork',
+    version='0.0.2',
+    description='Code wraps PyGithub to simplify workflows dealing with forks',
+    author='hassenius',
+    url='https://github.com/devakodali/PyGithubFork',
+    packages=['githubfork']
+    # install_requires=[
+    #     'pyyaml',
+    #     'pygithub'
+    # ]
+)
