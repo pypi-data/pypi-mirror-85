45ccc89e309d86281aea9034aff3c210ea2b79d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:08 +0000

    Add package waybackpack/0.3.7/waybackpack-0.3.7.tar.gz

---
+++ packages/waybackpack/waybackpack-0.3.7.tar.gz/waybackpack-0.3.7/setup.py
@@ -0,0 +1,36 @@
+import sys, os
+from setuptools import setup, find_packages
+import subprocess
+
+version = "0.3.7"
+
+base_reqs = [
+    "requests"
+]
+
+setup(
+    name="waybackpack",
+    description="Command-line tool that lets you download the entire Wayback Machine archive for a given URL.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.4"
+    ],
+    keywords="wayback machine archive",
+    author="Jeremy Singer-Vine",
+    author_email="jsvine@gmail.com",
+    url="https://github.com/jsvine/waybackpack",
+    license="MIT",
+    version=version,
+    packages=find_packages(exclude=["test",]),
+    tests_require=[ "nose" ] + base_reqs,
+    install_requires=base_reqs,
+    entry_points={
+        "console_scripts": [ "waybackpack = waybackpack.cli:main" ]
+    }
+)
