3445333482f739d6638ad768b2b8809fc69bd538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:28 +0000

    Add package TOPSIS-Misha-101803590/1.0.6/TOPSIS-Misha-101803590-1.0.6.tar.gz

---
+++ packages/TOPSIS-Misha-101803590/TOPSIS-Misha-101803590-1.0.6.tar.gz/TOPSIS-Misha-101803590-1.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="TOPSIS-Misha-101803590",
+    version="1.0.6",
+    description="Topsis package to calculate the score and rank of a given data.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mishaaggarwal15",
+    author="Misha Aggarwal",
+    author_email="maggarwal1_be18@thapar.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_py.ques1:main",
+        ]
+    },
+)
