3e321e1358fa6100b34030edd14565dc763c15ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:13 +0000

    Add package pysdfgen/0.1.0/pysdfgen-0.1.0.tar.gz

---
+++ packages/pysdfgen/pysdfgen-0.1.0.tar.gz/pysdfgen-0.1.0/pysdfgen/__init__.py
@@ -0,0 +1,47 @@
+# flake8: noqa
+
+import pkg_resources
+import os.path as osp
+import subprocess
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+
+
+__version__ = pkg_resources.get_distribution('pysdfgen').version
+
+
+SDFGen_executable = osp.join(
+    osp.abspath(osp.dirname(__file__)), 'SDFGen')
+
+
+def obj2sdf(obj_filepath, dim=100, padding=5):
+    """Convert .obj to .sdf file.
+
+    Parameters
+    ----------
+    obj_filepath : str or pathlib.Path
+        filepath of .obj.
+    dim : int
+        number of sdf dimension.
+    padding : int
+        number of padding.
+    """
+    obj_filepath = str(obj_filepath)
+    parent = osp.dirname(obj_filepath)
+    basename = osp.basename(obj_filepath)
+    stem, ext = osp.splitext(basename)
+    if ext != '.obj':
+        raise ValueError("The input file name should end with '.obj'.")
+    sdf_filepath = osp.join(parent, stem + ".sdf")
+    p = subprocess.Popen(
+        [SDFGen_executable,
+         str(obj_filepath),
+         str(dim),
+         str(padding)],
+        stdout=DEVNULL)
+    p.wait()
+    return osp.join(parent, sdf_filepath)
