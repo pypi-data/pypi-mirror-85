8bf70c85a64b66eff009e94a3562d52d5943eac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:37 +0000

    Add package ilexconf/0.9.2/ilexconf-0.9.2.tar.gz

---
+++ packages/ilexconf/ilexconf-0.9.2.tar.gz/ilexconf-0.9.2/ilexconf/__init__.py
@@ -0,0 +1,23 @@
+from ilexconf.config import Config
+from ilexconf.adapters import (
+    from_json,
+    to_json,
+    from_ini,
+    to_ini,
+    from_env,
+    to_env,
+    from_yaml,
+    to_yaml,
+)
+
+__myall__ = [
+    Config,
+    from_json,
+    to_json,
+    from_ini,
+    to_ini,
+    from_env,
+    to_env,
+    from_yaml,
+    to_yaml,
+]
