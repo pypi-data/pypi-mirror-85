3b102ecec659f982944a0970bb1412fe9c55e861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:21 +0000

    Add package workalendar/13.0.0/workalendar-13.0.0.tar.gz

---
+++ packages/workalendar/workalendar-13.0.0.tar.gz/workalendar-13.0.0/workalendar/europe/belarus.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+from ..core import OrthodoxCalendar
+from ..registry_tools import iso_register
+
+
+@iso_register('BY')
+class Belarus(OrthodoxCalendar):
+    'Belarus'
+    "as of http://president.gov.by/en/holidays_en/"
+    # Civil holidays
+    include_labour_day = True
+
+    # Christian holidays
+    include_christmas = False
+
+    FIXED_HOLIDAYS = OrthodoxCalendar.FIXED_HOLIDAYS + (
+        (1, 2, "Day After New Year"),
+        (1, 7, "Christmas (Orthodox)"),
+        (3, 8, "International Women's Day"),
+        (5, 9, "Victory Day"),
+        (7, 3, "Republic Day"),
+        (11, 7, "October Revolution Day"),
+        (12, 25, "Christmas (Catholic)"),
+    )
+    # Radonitsa
+    # https://en.wikipedia.org/wiki/Radonitsa
+
+    def get_radonitsa(self, year):
+        # 9 Days after the Orthodox easter sunday
+        return self.get_easter_sunday(year) + timedelta(days=9)
+
+    def get_variable_days(self, year):
+        days = super().get_variable_days(year)
+        days.append((self.get_radonitsa(year), "Radonitsa"))
+        return days
---
+++ packages/workalendar/workalendar-13.0.0.tar.gz/workalendar-13.0.0/workalendar/europe/russia.py
@@ -0,0 +1,22 @@
+from ..core import OrthodoxCalendar
+from ..registry_tools import iso_register
+
+
+@iso_register('RU')
+class Russia(OrthodoxCalendar):
+    'Russia'
+
+    shift_new_years_day = True
+
+    # Civil holidays
+    include_labour_day = True
+
+    FIXED_HOLIDAYS = OrthodoxCalendar.FIXED_HOLIDAYS + (
+        (1, 2, "Day After New Year"),
+        (1, 7, "Christmas"),
+        (2, 23, "Defendence of the Fatherland"),
+        (3, 8, "International Women's Day"),
+        (5, 9, "Victory Day"),
+        (6, 12, "National Day"),
+        (11, 4, "Day of Unity"),
+    )
