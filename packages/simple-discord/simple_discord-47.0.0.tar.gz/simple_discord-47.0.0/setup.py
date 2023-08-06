9bdd8d142e86e99595f9a836a02977088d80af75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:07 +0000

    Add package simple-discord/47.0.0/simple_discord-47.0.0.tar.gz

---
+++ packages/simple-discord/simple_discord-47.0.0.tar.gz/simple_discord-47.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_discord",
+    version="47.0.0",
+    author="TricolorHen061",
+    author_email="thisisnotarealemail@pleasedonttrytoemailthis.com",
+    description="A simple library to build Discord Bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://simple-discord.000webhostapp.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
