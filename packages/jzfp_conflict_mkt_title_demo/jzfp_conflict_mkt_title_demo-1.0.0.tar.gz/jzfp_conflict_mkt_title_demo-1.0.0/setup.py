7c7d94ab50ac157e7a3f229b7b18198ea61ba4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:19 +0000

    Add package jzfp_conflict_mkt_title_demo/1.0.0/jzfp_conflict_mkt_title_demo-1.0.0.tar.gz

---
+++ packages/jzfp_conflict_mkt_title_demo/jzfp_conflict_mkt_title_demo-1.0.0.tar.gz/jzfp_conflict_mkt_title_demo-1.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import os
+import setuptools
+
+setuptools.setup(
+    name='jzfp_conflict_mkt_title_demo',
+    version='1.0.0',
+    keywords='jzfp_conflict',
+    description='A demo for python packaging.',
+    long_description=open( os.path.join( os.path.dirname(__file__), 'Readme.rst' )).read(),
+    author='liuyaqi',  # 替换为你的Pypi官网账户名
+    author_email='yaqi_617@163.com',  # 替换为你Pypi账户名绑定的邮箱
+
+    url='https://github.com/***/***',  # 这个地方为github项目地址，貌似非必须
+    packages=setuptools.find_packages(),
+    license='JD617'
+)
