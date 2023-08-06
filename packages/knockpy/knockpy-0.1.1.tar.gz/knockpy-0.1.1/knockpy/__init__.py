dc825409a96d142da2c6f8e86ca16885ffe8b08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:24 +0000

    Add package knockpy/0.1.1/knockpy-0.1.1.tar.gz

---
+++ packages/knockpy/knockpy-0.1.1.tar.gz/knockpy-0.1.1/knockpy/__init__.py
@@ -0,0 +1,26 @@
+name = "knockpy"
+all = [
+    "dgp",
+    "knockoff_stats",
+    "knockoff_filter",
+    "knockoffs",
+    "mac",
+    "metro",
+    "mrc",
+    "smatrix",
+    "tree_processing"
+    "utilities",
+]
+
+__version__ = "0.1.1"
+
+from . import dgp
+from . import knockoffs
+from . import knockoff_filter
+from . import knockoff_stats
+from . import mac
+from . import metro
+from . import mrc
+from . import smatrix
+from . import tree_processing
+from . import utilities
