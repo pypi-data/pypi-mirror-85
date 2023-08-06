a1a82e21daac964acab668a5aef87ce16f6804d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:37 +0000

    Add package topsis-kritika-101983031/1.0/topsis_kritika-101983031-1.0.tar.gz

---
+++ packages/topsis-kritika-101983031/topsis_kritika-101983031-1.0.tar.gz/topsis_kritika-101983031-1.0/topsis_kritika_101983031/__init__.py
@@ -0,0 +1,5 @@
+from TOPSIS-kritika-101983031 import sum_of_squares
+from TOPSIS-kritika-101983031 import normalized
+from TOPSIS-kritika-101983031 import best
+from TOPSIS-kritika-101983031 import worst
+from TOPSIS-kritika-101983031 import euclidean_distances
