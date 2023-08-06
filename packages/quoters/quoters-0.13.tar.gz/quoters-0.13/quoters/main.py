350fda96de7d2130e54da8a4d98f36fb2e175a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:32 +0000

    Add package quoters/0.13/quoters-0.13.tar.gz

---
+++ packages/quoters/quoters-0.13.tar.gz/quoters-0.13/quoters/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from quoters.extract_data import check_connection_and_generate_quote
+from quoters.enum import QuoteType
+
+class Quote:
+    @staticmethod
+    def print():
+        result = check_connection_and_generate_quote(QuoteType.QUOTE)
+        if result != False:
+            return result
+        
+    @staticmethod
+    def print_series_quotes():
+        result = check_connection_and_generate_quote(QuoteType.SERIES_QUOTE)
+        if result != False:
+            return result
