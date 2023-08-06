8d106068b104443bbe233a11df99fcd831d26d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:03 +0000

    Add package ColorText/1.0.1/ColorText-1.0.1.tar.gz

---
+++ packages/ColorText/ColorText-1.0.1.tar.gz/ColorText-1.0.1/README.md
@@ -0,0 +1,5 @@
+#ColorText
+A biblioteca color text é um programa de mudança de cor simples para o seu texto
+
+#Funções 
+mudaCor(texto,cor,estilo_texto) -- A função vai receber o texto junto com a cor e o estilo do texto(negrito,sublinhado ou normal)
---
+++ packages/ColorText/ColorText-1.0.1.tar.gz/ColorText-1.0.1/Setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = 'ColorText',
+    version = '1.0.1',
+    author = 'Jonatas Miguel',
+    author_email = 'jonatas.miguelss@gmail.com',
+    packages = ['ColorText'],
+    description = 'Programa simples de mudança de cor de texto',
+    url = 'https://github.com/JonatasMSS/ColorText',
+    projects_url = {'Codigo Fonte: https://github.com/JonatasMSS/ColorText.git'},
+    keywords = 'Mudar cor texto',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
+
+)
