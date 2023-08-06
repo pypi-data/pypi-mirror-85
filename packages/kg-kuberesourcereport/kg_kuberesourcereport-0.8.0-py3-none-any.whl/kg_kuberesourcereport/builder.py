c6e16baef794084c3db832978361dd0864236bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:09 +0000

    Add package kg-kuberesourcereport/0.8.0/kg_kuberesourcereport-0.8.0-py3-none-any.whl

---
+++ packages/kg-kuberesourcereport/kg_kuberesourcereport-0.8.0-py3-none-any.whl/kg_kuberesourcereport/builder.py
@@ -0,0 +1,342 @@
+from typing import Optional, Sequence
+
+from kubragen import KubraGen
+from kubragen.builder import Builder
+from kubragen.data import ValueData
+from kubragen.exception import InvalidParamError, InvalidNameError
+from kubragen.object import ObjectItem, Object
+from kubragen.types import TBuild, TBuildItem
+
+from .option import KubeResourceReportOptions
+
+
+class KubeResourceReportBuilder(Builder):
+    """
+    Kube Resource Report builder.
+
+    Based on `codeberg.org/hjacobs/kube-resource-report <https://codeberg.org/hjacobs/kube-resource-report>`_.
+
+    .. list-table::
+        :header-rows: 1
+
+        * - build
+          - description
+        * - BUILD_ACCESSCONTROL
+          - creates service account, roles, and roles bindings
+        * - BUILD_SERVICE
+          - creates Deployment and Service
+
+    .. list-table::
+        :header-rows: 1
+
+        * - build item
+          - description
+        * - BUILDITEM_SERVICE_ACCOUNT
+          - ServiceAccount
+        * - BUILDITEM_CLUSTER_ROLE
+          - ClusterRole
+        * - BUILDITEM_CLUSTER_ROLE_BINDING
+          - ClusterRoleBinding
+        * - BUILDITEM_DEPLOYMENT
+          - Deployment
+        * - BUILDITEM_SERVICE
+          - Service
+
+    .. list-table::
+        :header-rows: 1
+
+        * - object name
+          - description
+          - default value
+        * - service
+          - Service
+          - ```<basename>```
+        * - service-account
+          - ServiceAccount
+          - ```<basename>```
+        * - cluster-role
+          - ClusterRole
+          - ```<basename>```
+        * - cluster-role-binding
+          - ClusterRoleBinding
+          - ```<basename>```
+        * - deployment
+          - Deployment
+          - ```<basename>```
+        * - pod-label-all
+          - label *app* to be used by selection
+          - ```<basename>```
+    """
+    options: KubeResourceReportOptions
+    _namespace: str
+
+    SOURCE_NAME = 'kg_kuberesourcereport'
+
+    BUILD_ACCESSCONTROL = TBuild('accesscontrol')
+    BUILD_SERVICE = TBuild('service')
+
+    BUILDITEM_SERVICE_ACCOUNT = TBuildItem('service-account')
+    BUILDITEM_CLUSTER_ROLE = TBuildItem('cluster-role')
+    BUILDITEM_CLUSTER_ROLE_BINDING = TBuildItem('cluster-role-binding')
+    BUILDITEM_DEPLOYMENT = TBuildItem('deployment')
+    BUILDITEM_SERVICE = TBuildItem('service')
+
+    def __init__(self, kubragen: KubraGen, options: Optional[KubeResourceReportOptions] = None):
+        super().__init__(kubragen)
+        if options is None:
+            options = KubeResourceReportOptions()
+        self.options = options
+
+        self._namespace = self.option_get('namespace')
+
+        if self.option_get('config.authorization.serviceaccount_create') is not False:
+            serviceaccount_name = self.basename()
+        else:
+            serviceaccount_name = self.option_get('config.authorization.serviceaccount_use')
+            if serviceaccount_name == '':
+                serviceaccount_name = None
+
+        if self.option_get('config.authorization.roles_create') is not False:
+            role_name = self.basename()
+        else:
+            role_name = None
+
+        if self.option_get('config.authorization.roles_bind') is not False:
+            if serviceaccount_name is None:
+                raise InvalidParamError('To bind roles a service account is required')
+            if role_name is None:
+                raise InvalidParamError('To bind roles the role must be created')
+            rolebinding_name = self.basename()
+        else:
+            rolebinding_name = None
+
+        self.object_names_init({
+            'service': self.basename(),
+            'service-account': serviceaccount_name,
+            'cluster-role': role_name,
+            'cluster-role-binding': rolebinding_name,
+            'deployment': self.basename(),
+            'pod-label-app': self.basename(),
+        })
+
+    def option_get(self, name: str):
+        return self.kubragen.option_root_get(self.options, name)
+
+    def basename(self, suffix: str = ''):
+        return '{}{}'.format(self.option_get('basename'), suffix)
+
+    def namespace(self):
+        return self._namespace
+
+    def build_names(self) -> Sequence[TBuild]:
+        return [self.BUILD_ACCESSCONTROL, self.BUILD_SERVICE]
+
+    def build_names_required(self) -> Sequence[TBuild]:
+        ret = [self.BUILD_SERVICE]
+        if self.option_get('config.authorization.serviceaccount_create') is not False or \
+                self.option_get('config.authorization.roles_create') is not False:
+            ret.append(self.BUILD_ACCESSCONTROL)
+        return ret
+
+    def builditem_names(self) -> Sequence[TBuildItem]:
+        return [
+            self.BUILDITEM_CLUSTER_ROLE,
+            self.BUILDITEM_SERVICE_ACCOUNT,
+            self.BUILDITEM_CLUSTER_ROLE_BINDING,
+            self.BUILDITEM_DEPLOYMENT,
+            self.BUILDITEM_SERVICE,
+        ]
+
+    def internal_build(self, buildname: TBuild) -> Sequence[ObjectItem]:
+        if buildname == self.BUILD_ACCESSCONTROL:
+            return self.internal_build_accesscontrol()
+        elif buildname == self.BUILD_SERVICE:
+            return self.internal_build_service()
+        else:
+            raise InvalidNameError('Invalid build name: "{}"'.format(buildname))
+
+    def internal_build_accesscontrol(self) -> Sequence[ObjectItem]:
+        ret = []
+
+        if self.option_get('config.authorization.serviceaccount_create') is not False:
+            ret.extend([
+                Object({
+                    'apiVersion': 'v1',
+                    'kind': 'ServiceAccount',
+                    'metadata': {
+                        'name': self.object_name('service-account'),
+                        'namespace': self.namespace(),
+                    }
+                }, name=self.BUILDITEM_SERVICE_ACCOUNT, source=self.SOURCE_NAME, instance=self.basename())
+            ])
+
+        if self.option_get('config.authorization.roles_create') is not False:
+            ret.extend([
+                Object({
+                    'kind': 'ClusterRole',
+                    'apiVersion': 'rbac.authorization.k8s.io/v1',
+                    'metadata': {
+                        'name': self.object_name('cluster-role'),
+                    },
+                    'rules': [{
+                        'apiGroups': [''],
+                        'resources': ['nodes', 'pods', 'namespaces', 'services'],
+                        'verbs': ['get', 'list']
+                    },
+                    {
+                        'apiGroups': ['networking.k8s.io'],
+                        'resources': ['ingresses'],
+                        'verbs': ['list']
+                    },
+                    {
+                        'apiGroups': ['metrics.k8s.io'],
+                        'resources': ['nodes', 'pods'],
+                        'verbs': ['get', 'list']
+                    },
+                    {
+                        'apiGroups': [''],
+                        'resources': ['services/proxy'],
+                        'resourceNames': ['heapster'],
+                        'verbs': ['get']
+                    },
+                    {
+                        'apiGroups': ['autoscaling.k8s.io'],
+                        'resources': ['verticalpodautoscalers'],
+                        'verbs': ['get', 'list']
+                    },
+                    {
+                        'apiGroups': ['apps'],
+                        'resources': ['deployments',
+                                      'statefulsets',
+                                      'replicasets',
+                                      'daemonsets'],
+                        'verbs': ['get', 'list']
+                    }]
+                }, name=self.BUILDITEM_CLUSTER_ROLE, source=self.SOURCE_NAME, instance=self.basename())
+            ])
+
+        if self.option_get('config.authorization.roles_bind') is not False:
+            ret.extend([
+                Object({
+                    'apiVersion': 'rbac.authorization.k8s.io/v1',
+                    'kind': 'ClusterRoleBinding',
+                    'metadata': {
+                        'name': self.object_name('cluster-role-binding'),
+                    },
+                    'roleRef': {
+                        'apiGroup': 'rbac.authorization.k8s.io',
+                        'kind': 'ClusterRole',
+                        'name': self.object_name('cluster-role'),
+                    },
+                    'subjects': [{
+                        'kind': 'ServiceAccount',
+                        'name': self.object_name('service-account'),
+                        'namespace': self.namespace(),
+                    }]
+                }, name=self.BUILDITEM_CLUSTER_ROLE_BINDING, source=self.SOURCE_NAME, instance=self.basename())
+            ])
+
+        return ret
+
+    def internal_build_service(self) -> Sequence[ObjectItem]:
+        ret = [
+            Object({
+                'apiVersion': 'apps/v1',
+                'kind': 'Deployment',
+                'metadata': {
+                    'name': self.object_name('deployment'),
+                    'namespace': self.namespace(),
+                    'labels': {
+                        'app': self.object_name('pod-label-app'),
+                    },
+                },
+                'spec': {
+                    'replicas': 1,
+                    'selector': {
+                        'matchLabels': {
+                            'app': self.object_name('pod-label-app'),
+                        }
+                    },
+                    'template': {
+                        'metadata': {
+                            'labels': {
+                                'app': self.object_name('pod-label-app'),
+                            }
+                        },
+                        'spec': {
+                            'serviceAccountName': self.object_name('service-account'),
+                            'containers': [{
+                                'name': 'kube-resource-report',
+                                'image': self.option_get('container.kube-resource-report'),
+                                'args': ['--update-interval-minutes=1',
+                                         '--additional-cost-per-cluster=30.0',
+                                         '/output'],
+                                'volumeMounts': [{
+                                    'mountPath': '/output',
+                                    'name': 'report-data'
+                                }],
+                                'securityContext': {
+                                    'readOnlyRootFilesystem': True,
+                                    'runAsNonRoot': True,
+                                    'runAsUser': 1000
+                                },
+                                'resources': ValueData(value=self.option_get('kubernetes.resources.deployment'),
+                                                       disabled_if_none=True),
+                            },
+                            {
+                                'name': 'nginx',
+                                'image': self.option_get('container.nginx'),
+                                'volumeMounts': [{
+                                    'mountPath': '/usr/share/nginx/html',
+                                    'name': 'report-data',
+                                    'readOnly': True
+                                }],
+                                'ports': [{
+                                    'containerPort': 80
+                                }],
+                                'readinessProbe': {
+                                    'httpGet': {
+                                        'path': '/',
+                                        'port': 80
+                                    }
+                                },
+                                'resources': ValueData(value=self.option_get('kubernetes.resources.deployment-nginx'),
+                                                       disabled_if_none=True),
+
+                            }],
+                            'volumes': [{
+                                'name': 'report-data',
+                                'emptyDir': {
+                                    'sizeLimit': '500Mi'
+                                }
+                            }]
+                        }
+                    }
+                }
+            }, name=self.BUILDITEM_DEPLOYMENT, source=self.SOURCE_NAME, instance=self.basename()),
+            Object({
+                'apiVersion': 'v1',
+                'kind': 'Service',
+                'metadata': {
+                    'name': self.object_name('service'),
+                    'namespace': self.namespace(),
+                    'labels': {
+                        'app': self.object_name('pod-label-app'),
+                    },
+                },
+                'spec': {
+                    'selector': {
+                        'app': self.object_name('pod-label-app'),
+                    },
+                    'type': 'ClusterIP',
+                    'ports': [{
+                        'name': 'kube-resource-report-frontend',
+                        'port': self.option_get('config.service_port'),
+                        'protocol': 'TCP',
+                        'targetPort': 80
+                    }]
+                }
+            }, name=self.BUILDITEM_SERVICE, source=self.SOURCE_NAME, instance=self.basename()),
+        ]
+
+        return ret
