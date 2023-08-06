9db2bd883007d7ca2a675085e0920ba53bc562fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:52 +0000

    Add package indexed-gzip/1.3.3/indexed_gzip-1.3.3-cp27-cp27m-win_amd64.whl

---
+++ packages/indexed-gzip/indexed_gzip-1.3.3-cp27-cp27m-win_amd64.whl/indexed_gzip/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# __init__.py - The indexed_gzip namespace.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""The indexed_gzip namespace. """
+
+
+from .indexed_gzip import (_IndexedGzipFile,     # noqa
+                           IndexedGzipFile,
+                           NotCoveredError,
+                           NoHandleError,
+                           ZranError)
+
+
+SafeIndexedGzipFile = IndexedGzipFile
+"""Alias for ``IndexedGzipFile``, to preserve compatibility with older
+versions of ``nibabel``.
+"""
+
+
+__version__ = '1.3.3'
