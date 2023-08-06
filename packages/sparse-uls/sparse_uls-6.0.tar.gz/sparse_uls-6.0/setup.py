a134441ca6492bd543a4aacbb66d7d4183632655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:38 +0000

    Add package sparse-uls/6.0/sparse_uls-6.0.tar.gz

---
+++ packages/sparse-uls/sparse_uls-6.0.tar.gz/sparse_uls-6.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="sparse_uls",
+    version="6.0",
+    author="Nguyen Ngoc Khanh",
+    author_email="nguyenngockhanh.pbc@gmail.com",
+    description="optimize norm with underdetermined system equality constraint",
+    url="https://github.com/khanhhhh/sparse-uls",
+    packages=setuptools.find_packages(),
+)
