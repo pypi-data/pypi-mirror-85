bcbfd4036c31815f4b9fe7724920bfe27b073699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:27 +0000

    Add package Topsis-Yash-101803617/0.0.4/Topsis_Yash_101803617-0.0.4.tar.gz

---
+++ packages/Topsis-Yash-101803617/Topsis_Yash_101803617-0.0.4.tar.gz/Topsis_Yash_101803617-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="Topsis_Yash_101803617",
+    version="0.0.4",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Yash Shukla",
+    author_email="yash981815@gmail.comm",
+    url='https://pypi.org/project/Topsis-Yash-101803617/0.0.1/',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["Topsis_Yash_101803617"],
+    include_package_data=True,
+    install_requires=['scipy',
+                      'tabulate',
+                      'numpy',
+                      'pandas',
+                      ]
+)
