f20d5388587861cbc57d800359f325f745fedfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:04 +0000

    Add package kineticstoolkit/0.3.1/kineticstoolkit-0.3.1.tar.gz

---
+++ packages/kineticstoolkit/kineticstoolkit-0.3.1.tar.gz/kineticstoolkit-0.3.1/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("kineticstoolkit/VERSION", "r") as fh:
+    version = fh.read()
+
+
+setuptools.setup(
+    name='kineticstoolkit',
+    version=version,
+    description='An open-source python library to facilitate research in biomechanics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://felixchenier.uqam.ca/kineticstoolkit',
+    author='Félix Chénier',
+    author_email='chenier.felix@uqam.ca',
+    license='Apache',
+        license_files=['LICENSE', 'NOTICE.txt'],
+    packages=setuptools.find_packages(),
+    package_data={
+        'kineticstoolkit': ['VERSION'],
+    },
+    install_requires=['pandas',
+                      'pyqt5',
+                      'scipy',
+                      'matplotlib',
+                      'scikit-learn',
+                      ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.8',
+)
